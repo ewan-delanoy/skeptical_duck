bf4c74c414df68dc49508a3989c15190a1e7ec8c
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-19 16:57:45 +0100

    update config vars

--- Makefile_makers/coma_big_constant.ml
+++ Makefile_makers/coma_big_constant.ml
@@ -7,21 +7,21 @@
 
 let this_world=
                 Root_directory.of_string 
-                "/Users/ewandelanoy/Documents/OCaml/Ordinary";;
+                "/Users/ewandelanoy/Teuliou/OCaml/Ordinary";;
 
 let next_world=
                 Root_directory.of_string 
-                "/Users/ewandelanoy/Documents/OCaml/Idaho";;
+                "/Users/ewandelanoy/Teuliou/OCaml/Idaho";;
 
 let backup_dir_for_this_world=
                 Root_directory.of_string 
-                "/Users/ewandelanoy/Documents/OCaml/Githubbed_ocaml";;                
+                "/Users/ewandelanoy/Teuliou/OCaml/Githubbed_ocaml";;                
 
 let dummy_backup_dir=
                 Root_directory.of_string 
-                "/Users/ewandelanoy/Documents/OCaml/Dummy_directory";;  
+                "/Users/ewandelanoy/Teuliou/OCaml/Dummy_directory";;  
 
 let dummy_world=
                 Root_directory.of_string 
-                "/Users/ewandelanoy/Documents/OCaml/Cherokee";;
+                "/Users/ewandelanoy/Teuliou/OCaml/Cherokee";;
                            
