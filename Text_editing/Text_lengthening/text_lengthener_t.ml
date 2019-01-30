2cc7cc2c067e8cf576d37d309f9e39218a1b1d67
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-30 10:00:07 +0100

    created Text_lengthener_t.

---
+++ Text_editing/Text_lengthening/text_lengthener_t.ml
@@ -0,0 +1,20 @@
+(*
+
+#use "Text_editing/Text_lengthening/text_lengthener_t.ml";;
+
+*)
+
+type t={
+  (* fundamental variables *)  
+  decompressions : (string * (string * (string * string * string) list)) list;  
+  expansions : (string list list);
+  inert_words : string list;
+  left_core_abbreviations : (string*string) list;
+  prefix_abbreviations : (string*string) list;
+
+  (* derived variables *)
+  case_insensitive_decompressions : (string * (string * (string * string * string) list)) list;  
+  case_insensitive_left_core_abbreviations  : (string*string) list;
+  case_insensitive_inert_words : string list;
+  case_insensitive_prefix_abbreviations  : (string*string) list;
+};;
