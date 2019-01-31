477bd7f47653fcbb4785fbc57f98181dd8db0a5d
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-31 10:24:24 +0100

    created Ordering_for_text_lengthener.

---
+++ Text_editing/Text_lengthening/ordering_for_text_lengthener.ml
@@ -0,0 +1,34 @@
+(*
+
+#use "Text_editing/Text_lengthening/ordering_for_text_lengthener.ml";;
+
+*)
+
+module Private=struct
+
+let for_decompressions=
+   ((Total_ordering.product
+     Total_ordering.lex_for_strings
+     Total_ordering.standard): 
+     ((string * (string * (string * string * string) list))) 
+     Total_ordering.t);; 
+
+let for_expansions=Total_ordering.lex_for_string_lists;;
+let for_inert_words=Total_ordering.lex_for_strings;;
+let for_left_core_abbreviations=Total_ordering.product Total_ordering.lex_for_strings Total_ordering.lex_for_strings;;
+let for_prefix_abbreviations=Total_ordering.product Total_ordering.lex_for_strings Total_ordering.lex_for_strings;;
+
+end;;
+
+let order_decompressions = Ordered.diforchan_plaen Private.for_decompressions;;
+let order_expansions = Ordered.diforchan_plaen Private.for_expansions;;
+let order_inert_words = Ordered.diforchan_plaen Private.for_inert_words;;
+let order_left_core_abbreviations = Ordered.diforchan_plaen Private.for_left_core_abbreviations;;
+let order_prefix_abbreviations = Ordered.diforchan_plaen Private.for_left_core_abbreviations;;
+
+
+let insert_decompression = Ordered.insert_plaen Private.for_decompressions;;
+let insert_expansion = Ordered.insert_plaen Private.for_expansions;;
+let insert_inert_word = Ordered.insert_plaen Private.for_inert_words;;
+let insert_left_core_abbreviation = Ordered.insert_plaen Private.for_left_core_abbreviations;;
+let insert_prefix_abbreviation = Ordered.insert_plaen Private.for_left_core_abbreviations;;
