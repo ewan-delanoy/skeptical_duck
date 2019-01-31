1f5e6519cc0a98e3c3ed90f7c3660d4159590fc0
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-01-31 16:56:13 +0100

    replace three with false_three in French_capitalization

--- Text_editing/french_capitalization.ml
+++ Text_editing/french_capitalization.ml
@@ -28,15 +28,20 @@ let optional_for_two (s1,s2)=
    then None 
    else Some(Option.unpack os1,Option.unpack os2);;
 
-let optional_for_three (s1,s2,s3)=
+let optional_for_false_three (s1,s2,s3)=
    let os1=optional_for_one s1
-   and os2=optional_for_one s2 
    and os3=optional_for_one s3 in 
-   if List.mem None [os1;os2;os3]
+   if List.mem None [os1;os3]
    then None 
-   else Some(Option.unpack os1,Option.unpack os2,Option.unpack os3);;
+   else Some(Option.unpack os1,s2,Option.unpack os3);;
 
 let generalize_for_one l=l@(Option.filter_and_unpack optional_for_one l);;
 let generalize_for_two l=l@(Option.filter_and_unpack optional_for_two l);;
-let generalize_for_three l=l@(Option.filter_and_unpack optional_for_three l);;
+let generalize_for_false_three l=
+   Image.image(
+      fun (x,(y,li))->
+         let new_li=li@(Option.filter_and_unpack optional_for_false_three li) in 
+         (x,(y,new_li))
+   ) l;; 
+  
 
