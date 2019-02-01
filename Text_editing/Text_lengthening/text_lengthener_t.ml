858d3d9f8f3baa1abe3a8566683ff303e747726e
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-02-01 13:01:43 +0100

    change decompressions to adjustable_decompressions in Text_lengthener_t

--- Text_editing/Text_lengthening/text_lengthener_t.ml
+++ Text_editing/Text_lengthening/text_lengthener_t.ml
@@ -6,14 +6,14 @@
 
 type t={
   (* fundamental variables *)  
-  decompressions : (string * (string * (string * string * string) list)) list;  
+  adjustable_decompressions : (string * string * ((string * string * string) list)) list;  
   expansions : (string list list);
   inert_words : string list;
   left_core_abbreviations : (string*string) list;
   prefix_abbreviations : (string*string) list;
 
   (* derived variables *)
-  case_insensitive_decompressions : (string * (string * (string * string * string) list)) list;  
+  case_insensitive_adjustable_decompressions : (string * string * ((string * string * string) list)) list;  
   case_insensitive_left_core_abbreviations  : (string*string) list;
   case_insensitive_inert_words : string list;
   case_insensitive_prefix_abbreviations  : (string*string) list;
