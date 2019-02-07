d1c20141710b3885cc9ef22e9075e7003d4481ee
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-02-07 13:25:27 +0100

    created Txl_result_t.

---
+++ Text_editing/Text_lengthening/txl_result_t.ml
@@ -0,0 +1,13 @@
+(*
+
+#use "Text_editing/Text_lengthening/txl_result_t.ml";;
+
+*)
+
+
+type  t=
+   Builtin_inert of string
+   |Declared_inert of string
+   |Usual of ((string * string) option * (string * string) option *
+      (string list * string) option * string list * string);;
+
