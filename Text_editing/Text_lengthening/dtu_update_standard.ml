8b2ea55e1190b5d320bc0b47f00524241b77fe3a
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-02-11 16:32:44 +0100

    created Dtu_update_standard.

---
+++ Text_editing/Text_lengthening/dtu_update_standard.ml
@@ -0,0 +1,22 @@
+(*
+
+#use "Text_editing/Text_lengthening/dtu_update_standard.ml";;
+
+*)
+
+let location_for_persisting =
+  "Text_editing/Text_lengthening/dtu_standard.ml";;
+
+let persist_to_file ()=
+   let description = Dtu_print.print  (Dtu_standard.one) in 
+   let text = "\n\n\nlet one = \n "^description^"\n ;;\n\n\n" in 
+   let ap=Absolute_path.of_string location_for_persisting in   
+   Replace_inside.overwrite_between_markers_inside_file
+     (Overwriter.of_string text)
+     (
+       "(* Description of standard double tunnel starts here *)",
+       "(* Description of standard double tunnel ends here *)"
+     ) 
+     ap;;
+
+
