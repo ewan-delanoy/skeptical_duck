ad2d1318fbbdca14a8242ddccba370601fa617e8
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-02-11 15:59:14 +0100

    created Dtu_standard.

---
+++ Text_editing/Text_lengthening/dtu_standard.ml
@@ -0,0 +1,16 @@
+(*
+
+#use "Text_editing/Text_lengthening/dtu_standard.ml";;
+
+*)
+
+(* Description of standard abbreviation expander starts here *)
+
+
+let one = Dtu_construct.construct
+[
+
+];;
+
+
+(* Description of standard abbreviation expander ends here *)
