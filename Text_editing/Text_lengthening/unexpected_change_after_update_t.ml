883d5cf4029fae91fb78a5ea959d76b3b2973ae8
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-02-12 13:16:13 +0100

    created Unexpected_change_after_update_t.

---
+++ Text_editing/Text_lengthening/unexpected_change_after_update_t.ml
@@ -0,0 +1,10 @@
+(*
+
+#use "Text_editing/Text_lengthening/unexpected_change_after_update_t.ml";;
+
+*)
+
+(* The list is a list of (x,old_y,new_y) where new_y is distinct from old_y *)
+
+type t=Ucau of (string*string*string) list;;
+
