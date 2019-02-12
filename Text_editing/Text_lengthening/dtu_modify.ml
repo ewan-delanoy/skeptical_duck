cc99ebd7a78a1ccc5cb2448addd0b4161a017c33
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-02-12 12:57:05 +0100

    update computations in Dtu_modify

--- Text_editing/Text_lengthening/dtu_modify.ml
+++ Text_editing/Text_lengthening/dtu_modify.ml
@@ -6,44 +6,41 @@
 
 module Private = struct
 
-let recompute_at_index dtu txl accu k=
-      let inc=dtu.Double_tunnel_t.incoming 
-      and outc=dtu.Double_tunnel_t.outcoming in 
-      let old_x = Option.unpack (Array.get inc k)
-      and old_y = Option.unpack (Array.get outc k) in 
-      let new_y = Txl_apply.apply txl old_x in 
-      if new_y<>old_y
-      then  
-           accu:=(old_y,new_y)::(!accu);
-           Array.set outc k (Some new_y)
-    ;;
+
+
+let make inc outc=
+    {
+       Double_tunnel_t.size = List.length inc;
+       incoming = inc;
+       outcoming = outc;
+    };;
+
+let unmake dtu=
+   (dtu.Double_tunnel_t.incoming,
+    dtu.Double_tunnel_t.outcoming);;
 
 end;;
 
 let add_pair dtu (word1,word2)=
-   let c=(dtu.Double_tunnel_t.size)+1 in
-   (
-      Array.set dtu.incoming c (Some word1);
-      Array.set dtu.outcoming c (Some(word2));
-      dtu.Double_tunnel_t.size <- c;
-   );;
+   let (inc,outc)=Private.unmake dtu in
+   Private.make(word1::inc) (word2::outc);;
  
-let remove_last dtu =
-   let c=dtu.Double_tunnel_t.size in
-   (
-      Array.set dtu.incoming c None;
-      Array.set dtu.outcoming c None;
-      dtu.Double_tunnel_t.size <- (c-1);
-   );;
-  
+let remove_last dtu (word1,word2)=
+   let (inc,outc)=Private.unmake dtu in
+   Private.make(List.tl inc) (List.tl outc);;
 
 let recompute dtu txl =
-    let c=dtu.Double_tunnel_t.size in
+    let pairs=Dtu_construct.deconstruct dtu in 
     let accu=ref [] in
-    let _=(for k=1 to c do 
-        Private.recompute_at_index dtu txl accu k
-    done) in 
-    List.rev(!accu);;
+    let tempf=(
+        fun (x,old_y)->
+          let new_y=Txl_apply.apply txl x in 
+          if new_y=old_y
+          then (x,old_y)
+          else let _=(accu:=(x,old_y,new_y)::(!accu)) in 
+               (x,new_y)
+    ) in
+    let new_pairs = Image.image tempf pairs in 
+    (Dtu_construct.construct new_pairs,List.rev(!accu));;
 
 
-    
