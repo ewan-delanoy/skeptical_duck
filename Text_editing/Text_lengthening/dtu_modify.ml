b1c87c81730f47b26749b2276feebb5d8f706f69
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-02-12 09:50:25 +0100

    keep track of changed pairs in Dtu_modify.recompute

--- Text_editing/Text_lengthening/dtu_modify.ml
+++ Text_editing/Text_lengthening/dtu_modify.ml
@@ -6,10 +6,16 @@
 
 module Private = struct
 
-let recompute_at_index dtu txl k=
-      Array.set dtu.Double_tunnel_t.outcoming k
-      (Some(Txl_apply.apply txl 
-            (Option.unpack(Array.get dtu.Double_tunnel_t.incoming k)) ))
+let recompute_at_index dtu txl accu k=
+      let inc=dtu.Double_tunnel_t.incoming 
+      and outc=dtu.Double_tunnel_t.outcoming in 
+      let old_x = Option.unpack (Array.get inc k)
+      and old_y = Option.unpack (Array.get outc k) in 
+      let new_y = Txl_apply.apply txl old_x in 
+      if new_y<>old_y
+      then  
+           accu:=(old_y,new_y)::(!accu);
+           Array.set outc k (Some new_y)
     ;;
 
 end;;
@@ -33,6 +39,11 @@ let remove_last dtu =
 
 let recompute dtu txl =
     let c=dtu.Double_tunnel_t.size in
-    for k=1 to c do 
-        Private.recompute_at_index dtu txl k
-    done;;
+    let accu=ref [] in
+    let _=(for k=1 to c do 
+        Private.recompute_at_index dtu txl accu k
+    done) in 
+    List.rev(!accu);;
+
+
+    
