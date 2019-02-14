7c8a6ba5f8d7f10fa021ed7ae1514c7a66220c11
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-02-14 10:39:09 +0100

    add persistence method in Dtu_standard

--- Text_editing/Text_lengthening/dtu_standard.ml
+++ Text_editing/Text_lengthening/dtu_standard.ml
@@ -2,6 +2,9 @@
 #use "Text_editing/Text_lengthening/dtu_standard.ml";;
 *)
 
+let location_for_persisting =
+  "Text_editing/Text_lengthening/dtu_standard.ml";;
+
 module Private = struct
 
 (* Description of standard double tunnel starts here *)
@@ -25,3 +28,15 @@ let set_state txl=(Private.state_container:= txl);;
 end ;;
 
 let current_state ()=(!(Private.state_container));;
+
+let persist_to_file ()=
+   let description = Dtu_print.print  (!(Private.state_container)) in 
+   let text = "\n\n\nlet state_container = \n "^description^"\n ;;\n\n\n" in 
+   let ap=Absolute_path.of_string location_for_persisting in   
+   Replace_inside.overwrite_between_markers_inside_file
+     (Overwriter.of_string text)
+     (
+       "(* Description of standard double tunnel starts here *)",
+       "(* Description of standard double tunnel ends here *)"
+     ) 
+     ap;;
