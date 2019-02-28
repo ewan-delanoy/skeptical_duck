59e1caab10d39d89ac82ce20ab9459f13be17d2c
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-02-28 17:38:42 +0100

    create Command_on_abbreviation_expander_t.

---
+++ Text_editing/Text_lengthening/command_on_abbreviation_expander_t.ml
@@ -0,0 +1,22 @@
+(*
+
+#use "Text_editing/Text_lengthening/command_on_abbreviation_expander_t.ml";;
+
+*)
+
+type t=
+    Add_words of string list
+   |Insert_adjustment of string * string * (string * string * string)
+   |Insert_decompression of string * string
+   |Insert_expansion of string list
+   |Insert_inert_word of string
+   |Insert_left_core_abbreviation of string * string
+   |Insert_prefix_abbreviation of string * string
+   |Remove_adjustment of string * string * (string * string * string)
+   |Remove_decompression of string * string
+   |Remove_expansion of string list
+   |Remove_inert_word of string
+   |Remove_left_core_abbreviation of string * string
+   |Remove_prefix_abbreviation of string * string;;
+
+
