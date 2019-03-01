49bfc6512dbdcc9b7ffa2c657116fd3c6f55f0a7
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-03-01 17:44:49 +0100

    create Sabbrex_apply_command.

---
+++ Text_editing/Text_lengthening/sabbrex_apply_command.ml
@@ -0,0 +1,24 @@
+(*
+
+#use "Text_editing/Text_lengthening/sabbrex_apply_command.ml";;
+
+*)
+
+module Md = Sabbrex_modify ;;
+
+let apply cmd sabbrex = match cmd with 
+    Command_on_abbreviation_expander_t.Add_words (words)->(Md.add_words sabbrex words,Unexpected_change_after_update_t.Ucau [])
+   |Insert_adjustment(u,v,(ad1,ad2,ad3))->Md.i_adjustment sabbrex (u,v,(ad1,ad2,ad3))
+   |Insert_decompression(u,v)->Md.i_decompression sabbrex (u,v)
+   |Insert_expansion(expansion)->Md.i_expansion sabbrex expansion
+   |Insert_inert_word(word)->Md.i_inert_word sabbrex word
+   |Insert_left_core_abbreviation(u,v)->Md.i_left_core_abbreviation sabbrex (u,v)
+   |Insert_prefix_abbreviation(u,v)->Md.i_prefix_abbreviation sabbrex (u,v)
+   |Remove_adjustment(u,v,(ad1,ad2,ad3))->Md.r_adjustment sabbrex (u,v,(ad1,ad2,ad3))
+   |Remove_decompression(u,v)->Md.r_decompression sabbrex (u,v)
+   |Remove_expansion(expansion)->Md.r_expansion sabbrex expansion
+   |Remove_inert_word(word)->Md.r_inert_word sabbrex word
+   |Remove_left_core_abbreviation(u,v)->Md.r_left_core_abbreviation sabbrex (u,v)
+   |Remove_prefix_abbreviation(u,v)->Md.r_prefix_abbreviation sabbrex (u,v) ;;
+
+
