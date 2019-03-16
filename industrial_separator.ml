211fe0b1de948ee5ec3279c7554bdfb0849364c8
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-03-16 15:11:17 +0100

    add Double_tunnel_t.t- and Text_lengthener_t.t- keys in Industrial_separator

--- industrial_separator.ml
+++ industrial_separator.ml
@@ -1,6 +1,6 @@
 (*
 
-#use"jindustrial_separator.ml";;
+#use"industrial_separator.ml";;
 
 Separators used in encoding of data types by strings.
 The main key should not appear in any string inside the data types,
@@ -8,21 +8,35 @@ it is to be changed if necessary.
 
 *)
 
-let key="mpdykru"^"vueaoqhkt";;
+module Private = struct
+  let key="mpdykru"^"vueaoqhkt";;
+  let counter=ref(0);;
+  let new_key ()=
+     let v=(!counter)+1 in 
+     let _=(counter:=v) in 
+     key^(Strung.left_completed_string_of_int 3 v);;
+end;;     
 
-let alaskan_data=key^"001";;
-let alaskan_save_all1=key^"002";;
-let alaskan_save_all2=key^"003";;
-let half_dressed_module=key^"004";;
-let half_dressed_module_times_boolean=key^"005";;
-let ocaml_target1=key^"006";;
-let ocaml_target2=key^"007";;
-let mlx_ended_absolute_path=key^"008";;
-let modulesystem_data1=key^"009";;
-let modulesystem_data2=key^"010";;
-let slow_copy_task1=key^"011";;
-let slow_copy_task2=key^"012";;
-let small_array=key^"013";;
+let alaskan_data=Private.new_key();;
+let alaskan_save_all1=Private.new_key();;
+let alaskan_save_all2=Private.new_key();;
+let double_tunnel_inner=Private.new_key();;
+let double_tunnel_outer=Private.new_key();;
+let half_dressed_module=Private.new_key();;
+let half_dressed_module_times_boolean=Private.new_key();;;;
+let ocaml_target1=Private.new_key();;
+let ocaml_target2=Private.new_key();;;;
+let mlx_ended_absolute_path=Private.new_key();;
+let modulesystem_data1=Private.new_key();;
+let modulesystem_data2=Private.new_key();;
+let slow_copy_task1=Private.new_key();;
+let slow_copy_task2=Private.new_key();;
+let string_times_string=Private.new_key();;
+let small_array=Private.new_key();;
+let text_lengthener_decompression=Private.new_key();;
+let text_lengthener_field=Private.new_key();;
+let text_lengthener_level_one_list=Private.new_key();;
+let text_lengthener_level_two_list=Private.new_key();;
 
 
              
