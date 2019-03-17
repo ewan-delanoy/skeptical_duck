8651cef0ce9c50c3300addf978c60065c06594bf
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-03-17 11:01:13 +0100

    remove unused values in German_wrapper

--- Country/Germany/german_wrapper.ml
+++ Country/Germany/german_wrapper.ml
@@ -5,32 +5,14 @@
 
 *)
 
-let usual_root=Coma_big_constant.this_world;;
-let usual_dir_for_backup=Coma_big_constant.backup_dir_for_this_world;;
-
-
-
-let data ()= (Usual_coma_state.main_ref);;
-let directories ()=Coma_state.directories(Usual_coma_state.main_ref);;
-
-let declare_printer_equipped_type hm=
-  (
-    Coma_state.add_printer_equipped_type Usual_coma_state.main_ref hm; 
-    Usual_coma_state.save_all ()
-  );;
- 
  
 let end_debugging ()=
-  let sbuild=(Root_directory.connectable_to_subpath usual_root)
+  let sbuild=(Root_directory.connectable_to_subpath Coma_big_constant.this_world)
      ^"_build/" in
   let _=Unix_command.uc("rm -f "^sbuild^"*.d.cm*"^" "^
      sbuild^"*.ocaml_debuggable")  in
   ();;   
-   
-let forget_unregistered_file ap= 
-    let _=Coma_state.recompile Usual_coma_state.main_ref in
-    let _=German_forget_unregistered_file.forget ap in
-    Usual_coma_state.save_all();;       
+        
        
 let forget_file ap=
   let _=Coma_state.recompile Usual_coma_state.main_ref in 
@@ -47,33 +29,6 @@ let forget_module hm=
         let _=Usual_coma_state.save_all() in
         short_paths;;     
 
-let initialize ()=
-    Coma_state.initialize Usual_coma_state.main_ref ;; 
-
-let initialize_if_empty ()=
-      if (Coma_state.size(data())=0) 
-      then initialize();;    
-
-let printer_equipped_types ()=
-    Coma_state.preq_types Usual_coma_state.main_ref;;
-
-let recompile ()=
-   let data=Coma_state.recompile Usual_coma_state.main_ref in
-   let _=Usual_coma_state.save_all() in
-   data;;  
-
-
-let refresh ()=
-   let new_diff=Coma_state.refresh Usual_coma_state.main_ref in
-   let _=Usual_coma_state.save_all() in
-   new_diff;;
-
-let register_mlx_file mlx=
-    let _=Coma_state.recompile Usual_coma_state.main_ref in 
-    (
-     Coma_state.register_mlx_file Usual_coma_state.main_ref mlx;
-     Usual_coma_state.save_all();
-    );;  
  
 let relocate_module old_name new_subdir=
        let _=Coma_state.recompile Usual_coma_state.main_ref in 
@@ -99,27 +54,13 @@ let rename_module old_name new_name=
             Usual_coma_state.save_all();
     );;   
                    
-
-
-let reposition_module hm (l_before,l_after)=
-  let _=Coma_state.recompile Usual_coma_state.main_ref in
-  let _=Coma_state.reposition_module Usual_coma_state.main_ref hm (l_before,l_after) in
-  
-    Usual_coma_state.save_all();
- ;;   
   
 let start_debugging ()=
    let _=Coma_state.recompile Usual_coma_state.main_ref in 
    let _=Coma_state.start_debugging Usual_coma_state.main_ref in 
     Usual_coma_state.save_all();
  ;;    
-    
-    
-let undeclare_printer_equipped_type hm=
-    (
-      Coma_state.remove_printer_equipped_type Usual_coma_state.main_ref hm; 
-      Usual_coma_state.save_all ()
-    );;
+  
    
 
 let unregister_module hm=
@@ -148,6 +89,6 @@ let view_definition s=
   (print_string text;flush stdout);;   
 
    
-let whole=Usual_coma_state.whole;;
+
  
             
