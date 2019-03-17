f3173de4294475e5a7b4fa21545f735e4f362f1d
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-03-17 11:06:54 +0100

    add Needed_values.rsh

--- needed_values.ml
+++ needed_values.ml
@@ -18,6 +18,8 @@ let reco msg=Usual_coma_state.recompile (Some msg);;
 
 let regi short_path=Usual_coma_state.register_short_path short_path;;
 
+let rsh = Usual_coma_state.refresh_with_backup;;
+
 let ucc=Usual_coma_state.ucc;;   
 
 let ucs=Usual_coma_state.main_ref;;   
