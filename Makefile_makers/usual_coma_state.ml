39def04f583672e2096a955f19d25cc075cff8bc
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-03-17 09:31:59 +0100

    add Usual_coma_state.save_all

--- Makefile_makers/usual_coma_state.ml
+++ Makefile_makers/usual_coma_state.ml
@@ -5,28 +5,50 @@
 
 *)
 
+module Private = struct 
+
 let main_ref=Coma_state_field.empty_one
                 Coma_big_constant.this_world
                 Coma_big_constant.backup_dir_for_this_world;;
 
-let backup diff opt=Coma_state.backup main_ref diff opt;;
+let whole ()=Coma_state.uple_form main_ref;;                
 
-let from_outside ()= Coma_state.from_outside  main_ref Coma_big_constant.next_world;; 
+end;;
 
 
-let initialize ()=Coma_state.initialize main_ref ;; 
+
+let backup diff opt=Coma_state.backup Private.main_ref diff opt;;
+
+let from_outside ()= Coma_state.from_outside  Private.main_ref Coma_big_constant.next_world;; 
+
+
+let initialize ()=Coma_state.initialize Private.main_ref ;; 
 
 let initialize_if_empty ()=
-      if (Coma_state.size main_ref=0) 
+      if (Coma_state.size Private.main_ref=0) 
       then initialize();;                           
+
+let main_ref=Private.main_ref;;
 
 let polished_short_paths ()=
   Coma_state.all_polished_short_paths   
       main_ref Coma_big_constant.next_world;;
 
+let save_all ()=Coma_state.Save_all.write_all 
+  (Coma_big_constant.this_world, 
+    Coma_constant.name_for_makefile,
+    Coma_constant.name_for_targetfile,
+    Coma_constant.name_for_loadingsfile,
+    Coma_constant.name_for_printersfile
+  )
+  (
+	Private.whole()
+  );;
+
+
 let to_outside ()= Coma_state.to_outside  main_ref Coma_big_constant.next_world;;  
 
-let whole ()=Coma_state.uple_form main_ref;;
+let whole =Private.whole;;
   
 
 let ucc ()=
