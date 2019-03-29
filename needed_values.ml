b8cf33aea589730af748969fc4f88db822089506
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-03-29 12:45:30 +0100

    add Needed_values.ocs

--- needed_values.ml
+++ needed_values.ml
@@ -26,6 +26,8 @@ let image = Image.image ;;
 
 let initialize_toplevel=Usual_coma_state.initialize_if_empty;;
 
+let ocs=Usual_coma_state.main_ref;;
+
 let ofo=Ordered.forget_order;;
 
 let reco msg=Usual_coma_state.recompile (Some msg);; 
