15c8783f83d3bd11d6fce47f3c4e25e58471f432
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-04-03 11:09:27 +0200

    fix typo in Improve_readiris_ocr.force_french_spacing

--- Text_editing/improve_readiris_ocr.ml
+++ Text_editing/improve_readiris_ocr.ml
@@ -124,7 +124,7 @@ let force_french_spacing_in_string s=
    let temp4=force_two_sided_spacing_in_string_for_character temp3 '!' in 
    let temp5=force_right_spacing_in_string_for_character temp4 '.' in 
    let temp6=force_right_spacing_in_string_for_character temp5 ',' in 
-   temp4;;
+   temp6;;
        
 let  force_french_spacing_in_file argument_file=
     let old_text=Io.read_whole_file argument_file in 
