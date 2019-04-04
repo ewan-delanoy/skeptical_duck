bf11932b27cad075dfe2f6838cefc7673b3cefbc
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-04-04 12:38:54 +0200

    add Coherent_pdf.Bare

--- Text_editing/coherent_pdf.ml
+++ Text_editing/coherent_pdf.ml
@@ -11,67 +11,65 @@ odd/even parts to allow for recto-verso printing.
 
 let workspace_directory=ref("");;
 
-module Command = struct 
-  
-let cpdf = "/Applications/cpdf ";;
+module Helper = struct
 
-let bare_extract_page_range pdfname (i,j) output_name=
-  let si=string_of_int i and sj=string_of_int j in
-   cpdf^pdfname^".pdf "^si^"-"^sj^" -o "^output_name
+  let cpdf = "/Applications/cpdf ";;
+
+  let generic_extract_page_range pdfname (i,j) output_name=
+    let si=string_of_int i and sj=string_of_int j in
+    cpdf^pdfname^".pdf "^si^"-"^sj^" -o "^output_name
   ;;
 
-let internal_extract_page_range pdfname (i,j) output_name=
-  let old_dir=Sys.getcwd() in 
-  [
-     Unix_command.cd (!workspace_directory);
-     (bare_extract_page_range pdfname (i,j) output_name); 
-     Unix_command.cd old_dir;
-  ];;
+  let usual_name_in_extract_page_range pdfname (i,j)=
+    let si=string_of_int i and sj=string_of_int j in
+    pdfname^"_from_"^si^"_to_"^sj^".pdf" ;;
 
-let name_in_extract_page_range pdfname (i,j)=
-  let si=string_of_int i and sj=string_of_int j in
-  pdfname^"_from_"^si^"_to_"^sj^".pdf" ;;
+  let small_pieces (i,j) max_piece_size=
+         let num_of_pieces = Basic.frac_ceiling (j-i+1) max_piece_size in 
+         Ennig.doyle (
+            fun k->
+            let start=i+(k-1)*max_piece_size in
+            let ending=min j (start+max_piece_size-1) in 
+            (start,ending)
+      ) 1 num_of_pieces;;  
 
-
-let extract_page_range pdfname (i,j)=
-  let output_name = name_in_extract_page_range pdfname (i,j) in 
-  internal_extract_page_range pdfname (i,j) output_name;;
-
-let extract_even_pages pdfname=
-  let old_dir=Sys.getcwd() in 
-  [
-     Unix_command.cd (!workspace_directory);
-     cpdf^pdfname^".pdf even -o "^pdfname^"_even.pdf";
-     Unix_command.cd old_dir;
-  ];;
-
-   let extract_odd_pages pdfname=
-   let old_dir=Sys.getcwd() in 
-  [
-     Unix_command.cd (!workspace_directory);
-     cpdf^pdfname^".pdf odd -o "^pdfname^"_odd.pdf";
-     Unix_command.cd old_dir;
-  ];;
-
-   let explode (pdf_name_start,pdf_name_end) num_of_pages=
+  let wrap_list_inside_workspace  l=
     let old_dir=Sys.getcwd() in 
+    [Unix_command.cd (!workspace_directory)]@l@[Unix_command.cd old_dir];;
+  
+  let wrap_univar_inside_workspace f x1=wrap_list_inside_workspace(f x1);;
+  let wrap_bivar_inside_workspace f x1 x2=wrap_list_inside_workspace(f x1 x2);;
+  let wrap_trivar_inside_workspace f x1 x2 x3=wrap_list_inside_workspace(f x1 x2 x3);;
+  let wrap_quadrivar_inside_workspace f x1 x2 x3 x4=wrap_list_inside_workspace(f x1 x2 x3 x4);;
+
+end;;
+
+
+module Bare = struct 
+  
+  
+
+
+  let extract_page_range pdfname (i,j)=
+    let output_name = Helper.usual_name_in_extract_page_range pdfname (i,j) in 
+    [Helper.generic_extract_page_range pdfname (i,j) output_name];;
+
+  let extract_even_pages pdfname=
+    [Helper.cpdf^pdfname^".pdf even -o "^pdfname^"_even.pdf"];;
+
+  let extract_odd_pages pdfname=
+    [Helper.cpdf^pdfname^".pdf odd -o "^pdfname^"_odd.pdf"];;
+
+  let explode (pdf_name_start,pdf_name_end) num_of_pages=
     let full_name=pdf_name_start^pdf_name_end 
     and ending=pdf_name_end^".pdf" in 
-    let temp1=Ennig.doyle(
+    Ennig.doyle(
        fun i->
        let si=string_of_int i in 
-       cpdf^full_name^".pdf "^si^"-"^si^" -o "^pdf_name_start^si^ending;
-    ) 1 num_of_pages in 
-   [
-      Unix_command.cd (!workspace_directory)
-   ]@   
-     temp1
-   @[   
-     Unix_command.cd old_dir;
-   ];;
+       Helper.cpdf^full_name^".pdf "^si^"-"^si^" -o "^pdf_name_start^si^ending;
+    ) 1 num_of_pages;;
 
-   let implode (pdf_name_start,pdf_name_end)=
-      let old_dir=Sys.getcwd() in 
+  let implode (pdf_name_start,pdf_name_end)=
       let temp1=More_unix.quick_beheaded_complete_ls (!workspace_directory) 
       and ending=pdf_name_end^".pdf" in 
       let temp2=List.filter(
@@ -87,17 +85,11 @@ let extract_even_pages pdfname=
       ) temp2 in 
       let temp4=Ordered.forget_order (Tidel2.diforchan temp3) in 
       let all_pages=String.concat " " (Image.image snd temp4) in 
-      let main=cpdf^all_pages^" -o "^pdf_name_start^ending in 
-      [
-         Unix_command.cd (!workspace_directory);
-         main; 
-         Unix_command.cd old_dir;
-      ];;
+      [Helper.cpdf^all_pages^" -o "^pdf_name_start^ending];;
 
    
-    let prepare_recto_verso pdfname (i,j)=
-        let si=string_of_int i and sj=string_of_int j in
-        let excerpt_name = pdfname^"_from_"^si^"_to_"^sj  in 
+  let prepare_recto_verso pdfname (i,j)=
+        let excerpt_name = Helper.usual_name_in_extract_page_range pdfname (i,j)  in 
         let even_pages = excerpt_name^"_even.pdf"  
         and odd_pages = excerpt_name^"_odd.pdf" in 
         (extract_page_range pdfname (i,j))
@@ -111,70 +103,65 @@ let extract_even_pages pdfname=
           "open -a /Applications/Preview.app "^(!workspace_directory)^"/"^even_pages;
         ];;
 
-     let finish_recto_verso pdfname =
-      let old_dir=Sys.getcwd() in 
+  let finish_recto_verso pdfname =
       [
-         Unix_command.cd (!workspace_directory);  
          "rm "^pdfname^"_from_*.pdf";
-         Unix_command.cd old_dir;
       ];;
 
-    let append_on_the_right file1 file2 =
-       let old_dir=Sys.getcwd() in 
+  let append_on_the_right file1 file2 =
       [
-          Unix_command.cd (!workspace_directory);
-          cpdf^file1^".pdf "^file2^".pdf -o wghartnjklmiopfwhhokuuu.pdf";
+          Helper.cpdf^file1^".pdf "^file2^".pdf -o wghartnjklmiopfwhhokuuu.pdf";
           "mv wghartnjklmiopfwhhokuuu.pdf "^file1^".pdf" ;
-          Unix_command.cd old_dir;
-        ];;
+      ];;
 
-     let cut_in_two ~pdfname ~first_half_length ~total_length =
+  let cut_in_two ~pdfname ~first_half_length ~total_length =
         let half1=pdfname^"_half1.pdf"
         and half2=pdfname^"_half2.pdf" in 
-        (internal_extract_page_range pdfname (1,first_half_length) half1)
-        @
-        (internal_extract_page_range pdfname (first_half_length+1,total_length) half2);;
+        [
+        (Helper.generic_extract_page_range pdfname (1,first_half_length) half1);
+        (Helper.generic_extract_page_range pdfname (first_half_length+1,total_length) half2)
+        ];;
 
-     let merge parts whole=
-      let old_dir=Sys.getcwd() in 
+  let unlabeled_cut_in_two pdfname first_half_length total_length =
+     cut_in_two 
+       ~pdfname:pdfname 
+        ~first_half_length:first_half_length 
+         ~total_length:total_length;;
+
+  let merge parts whole=
       let parts=Image.image (fun name->name^".pdf") parts in 
       let joined_parts=String.concat " " parts in 
-      let main=cpdf^joined_parts^" -o "^whole^".pdf" in 
-      [
-         Unix_command.cd (!workspace_directory);
-         main; 
-         Unix_command.cd old_dir;
-      ];;
+      [Helper.cpdf^joined_parts^" -o "^whole^".pdf"];;
 
-      let insert_in_just_after ~inserted_one ~receiving_one ~page_number ~initial_total_length=
-        let old_dir=Sys.getcwd() in 
+  let insert_in_just_after ~inserted_one ~receiving_one ~page_number ~initial_total_length=
         (cut_in_two ~pdfname:receiving_one ~first_half_length:page_number ~total_length:initial_total_length)
         @
         (merge [receiving_one^"_half1";inserted_one;receiving_one^"_half2"] receiving_one )
         @
         [
-           Unix_command.cd (!workspace_directory);
            "rm "^receiving_one^"_half1.pdf";
            "rm "^receiving_one^"_half2.pdf"; 
-           Unix_command.cd old_dir;
         ];;
+  
+  let unlabeled_insert_in_just_after inserted_one receiving_one page_number initial_total_length=
+    insert_in_just_after
+      ~inserted_one:inserted_one 
+       ~receiving_one:receiving_one 
+        ~page_number:page_number 
+        ~initial_total_length:initial_total_length;;
 
-      let replace_first_page_number_in_by  ~receiving_one ~inserted_one  ~total_length=
-        let old_dir=Sys.getcwd()  in 
+  let replace_first_page_number_in_by  ~receiving_one ~inserted_one  ~total_length=
         (cut_in_two ~pdfname:receiving_one ~first_half_length:1 ~total_length:total_length)
         @
         (merge [inserted_one;receiving_one^"_half2"] receiving_one )
         @
         [
-           Unix_command.cd (!workspace_directory);
            "rm "^receiving_one^"_half1.pdf";
            "rm "^receiving_one^"_half2.pdf"; 
-           Unix_command.cd old_dir;
         ];;
 
-      let replace_nonfirst_page_number_in_by ~page_number ~receiving_one ~inserted_one  ~total_length=
-        let old_dir=Sys.getcwd() 
-        and temp=receiving_one^"_half2" in 
+  let replace_nonfirst_page_number_in_by ~page_number ~receiving_one ~inserted_one  ~total_length=
+        let temp=receiving_one^"_half2" in 
         (cut_in_two ~pdfname:receiving_one ~first_half_length:(page_number-1) ~total_length:total_length)
         @
         (cut_in_two ~pdfname:temp ~first_half_length:1 ~total_length:(total_length-page_number+1))
@@ -182,77 +169,81 @@ let extract_even_pages pdfname=
         (merge [receiving_one^"_half1";inserted_one;temp^"_half2"] receiving_one )
         @
         [
-           Unix_command.cd (!workspace_directory);
            "rm "^receiving_one^"_half1.pdf";
            "rm "^receiving_one^"_half2.pdf"; 
            "rm "^receiving_one^"_half2_half1.pdf"; 
            "rm "^receiving_one^"_half2_half2.pdf"; 
-           Unix_command.cd old_dir;
         ];;
             
-      let replace_page_number_in_by ~page_number ~receiving_one ~inserted_one  ~total_length=
-        if page_number=1
-        then replace_first_page_number_in_by  receiving_one inserted_one  total_length
-        else replace_nonfirst_page_number_in_by page_number receiving_one inserted_one  total_length;;
+  let replace_page_number_in_by ~page_number ~receiving_one ~inserted_one  ~total_length=
+    if page_number=1
+    then replace_first_page_number_in_by  receiving_one inserted_one  total_length
+    else replace_nonfirst_page_number_in_by page_number receiving_one inserted_one  total_length;;
 
+  let unlabeled_replace_page_number_in_by page_number receiving_one inserted_one  total_length=
+     replace_page_number_in_by
+    ~page_number:page_number 
+     ~receiving_one:receiving_one
+      ~inserted_one:inserted_one  
+       ~total_length:total_length;;
 
-      let small_pieces (i,j) max_piece_size=
-         let num_of_pieces = Basic.frac_ceiling (j-i+1) max_piece_size in 
-         Ennig.doyle (
-            fun k->
-            let start=i+(k-1)*max_piece_size in
-            let ending=min j (start+max_piece_size-1) in 
-            (start,ending)
-      ) 1 num_of_pieces;;
-     
-       let cut_into_small_pieces pdfname (i,j) max_piece_size=
-         let old_dir=Sys.getcwd() in 
-         let ranges = small_pieces (i,j) max_piece_size in 
+  let cut_into_small_pieces pdfname (i,j) max_piece_size=
+         let ranges = Helper.small_pieces (i,j) max_piece_size in 
          let base  = Image.image (
-            fun (k,pair)->(k,pair,name_in_extract_page_range pdfname pair)
+            fun (k,pair)->(k,pair,Helper.usual_name_in_extract_page_range pdfname pair)
          )(Ennig.index_everything ranges) in  
          let part1=Image.image (fun (idx,pair,name)->
-            bare_extract_page_range pdfname pair name
+            Helper.generic_extract_page_range pdfname pair name
          ) base
          and part2=Image.image(
             fun (idx,pair,name)->
             "mv "^name^" part"^(string_of_int idx)^".pdf"
-         ) base in 
-        
-        [
-         Unix_command.cd (!workspace_directory)
-        ]@ 
-           part1@part2 
-         @[
-          Unix_command.cd old_dir;
-         ];;
+         ) base in  
+         part1@part2 ;;
 
-       let rename old_pdfname new_pdfname=
-         let old_dir=Sys.getcwd() in 
-       [
-         Unix_command.cd (!workspace_directory);
-         "mv "^old_pdfname^".pdf "^new_pdfname^".pdf";
-         Unix_command.cd old_dir;
-       ];;
+  let rename old_pdfname new_pdfname=
+       ["mv "^old_pdfname^".pdf "^new_pdfname^".pdf"];;
       
-       let upside_down pdfname =  
-          let old_dir=Sys.getcwd() in 
+  let upside_down pdfname =  
        [
-          Unix_command.cd (!workspace_directory);
-          cpdf^" -rotate-contents 180 "^pdfname^".pdf -o wghartnjklmiopfwhhokuuu.pdf";
-          "mv wghartnjklmiopfwhhokuuu.pdf "^pdfname^".pdf" ;
-          Unix_command.cd old_dir;
+          Helper.cpdf^" -rotate-contents 180 "^pdfname^".pdf -o wghartnjklmiopfwhhokuuu.pdf";
+          "mv wghartnjklmiopfwhhokuuu.pdf "^pdfname^".pdf" ;  
         ];;
 
-      let lay_down pdfname =  
-          let old_dir=Sys.getcwd() in 
+  let lay_down pdfname =  
        [
-          Unix_command.cd (!workspace_directory);
-          cpdf^" -rotate-contents 90 "^pdfname^".pdf -o wghartnjklmiopfwhhokuuu.pdf";
+          Helper.cpdf^" -rotate-contents 90 "^pdfname^".pdf -o wghartnjklmiopfwhhokuuu.pdf";
           "mv wghartnjklmiopfwhhokuuu.pdf "^pdfname^".pdf" ;
-          Unix_command.cd old_dir;
         ];;
   
+
+end;;
+
+
+module Command = struct 
+  
+  let uni=Helper.wrap_univar_inside_workspace;;
+  let bi=Helper.wrap_bivar_inside_workspace;;
+  let tri=Helper.wrap_trivar_inside_workspace;;
+  let qdi=Helper.wrap_quadrivar_inside_workspace;;
+
+  let append_on_the_right =bi Bare.append_on_the_right;;
+  let cut_in_two =tri Bare.unlabeled_cut_in_two;;
+  let cut_into_small_pieces =tri Bare.cut_into_small_pieces;;
+  let explode =bi Bare.explode;;
+  let extract_page_range =bi Bare.extract_page_range;;
+  let extract_even_pages =uni Bare.extract_even_pages;;
+  let extract_odd_pages =uni Bare.extract_even_pages;;
+  let finish_recto_verso =uni Bare.finish_recto_verso;;
+  let implode =uni Bare.implode;;
+  let insert_in_just_after =qdi Bare.unlabeled_insert_in_just_after;;
+  let lay_down =uni Bare.lay_down;; 
+  let merge =bi Bare.merge;;
+  let prepare_recto_verso =bi Bare.prepare_recto_verso;;
+  let rename =bi Bare.rename;;
+  let replace_page_number_in_by=qdi Bare.unlabeled_replace_page_number_in_by;;
+  let upside_down =uni Bare.upside_down;; 
+     
 
 end;;
 
