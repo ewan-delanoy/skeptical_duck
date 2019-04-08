1161a35d66ae94856013acf3c312f436bc310e0e
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-04-08 15:39:28 +0200

    remove Detect_footnotes.Ios_exn (was for debugging)

--- Text_editing/detect_footnotes.ml
+++ Text_editing/detect_footnotes.ml
@@ -5,7 +5,6 @@
 
 *)
 
-exception Ios_exn of int*int*string;;
 
 let check_for_footnote_at_idx s start_idx =
   let n=String.length s in 
@@ -18,8 +17,7 @@ let check_for_footnote_at_idx s start_idx =
   let idx1=Option.unpack opt1 in 
   if (Strung.get s idx1)<>')' then None else   
   let pagenumber_description=Cull_string.interval s (start_idx+1) (idx1-1) in 
-  let pagenumber=(try int_of_string pagenumber_description with 
-  _->raise(Ios_exn(start_idx+1,idx1-1,pagenumber_description))) in 
+  let pagenumber=int_of_string pagenumber_description in 
   Some(pagenumber,(start_idx,idx1));;
 
 (*
