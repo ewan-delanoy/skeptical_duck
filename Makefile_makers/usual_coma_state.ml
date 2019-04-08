66736a5aeae6ed844d8d44c6c1e7676f7f919f26
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-04-08 12:35:32 +0200

    remove extra parameter in Usual_coma_state.forget

--- Makefile_makers/usual_coma_state.ml
+++ Makefile_makers/usual_coma_state.ml
@@ -29,9 +29,9 @@ let duplicate_module old_t1 old_t2=
 let find_half_dressed_module modname= 
   Coma_state.Almost_concrete.find_half_dressed_module (!(Private.main_ref)) modname;;
 
-let forget_with_backup x=Modify_persistent_coma_state.forget_with_backup Private.main_ref;;
+let forget_with_backup modname=Modify_persistent_coma_state.forget_with_backup Private.main_ref modname;;
 
-let forget_without_backup x=Modify_persistent_coma_state.forget_without_backup Private.main_ref;;      
+let forget_without_backup modname=Modify_persistent_coma_state.forget_without_backup Private.main_ref modname;;      
 
 
 
