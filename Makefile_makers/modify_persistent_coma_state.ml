b2475f8d24a61713f6378c4640b2953a1268b392
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-04-11 09:43:50 +0200

    add Modify_persistent_coma_state.rename_directory

--- Makefile_makers/modify_persistent_coma_state.ml
+++ Makefile_makers/modify_persistent_coma_state.ml
@@ -41,6 +41,10 @@ let relocate_module pcs old_name new_name=
    let new_cs = Coma_state.Almost_concrete.relocate_module (!pcs) old_name new_name in 
   pcs:=new_cs;;
 
+let rename_directory pcs (old_subdir,new_subdirname)=
+    let new_cs = Coma_state.Almost_concrete.rename_directory (!pcs) (old_subdir,new_subdirname) in 
+    pcs:=new_cs;;
+    
 let rename_module pcs old_hm_name new_subdir=
    let new_cs = Coma_state.Almost_concrete.rename_module (!pcs) old_hm_name new_subdir in 
   pcs:=new_cs;;  
