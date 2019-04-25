e2ff2a126949c17284c18e2e0c69e2a5a5ad85b3
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-04-25 15:31:37 +0200

    copy Alaskan_{printer_equipped_types,write_merlinfile}.instructions into Coma_state.Save_all

--- Compilation_management/coma_state.ml
+++ Compilation_management/coma_state.ml
@@ -1918,6 +1918,25 @@ module Save_all=struct
       and part5="\n\n\n" in
       part1^part2^part3^part4^part5;; 
           
+    
+    let instructions_for_merlinfile main_root dirs=
+      let s_root=Root_directory.connectable_to_subpath main_root in
+      let temp1=Image.image 
+        (fun sdir->"S "^s_root^(Subdirectory.connectable_to_subpath sdir) )
+      (Coma_constant.automatically_generated_subdir::dirs) in
+      let temp2=("B "^s_root^"_build/")::temp1 in
+      "\n\n\n"^(String.concat "\n" temp2)^"\n\n\n";; 
+
+    let instructions_for_printersfile printer_equipped_types=
+        let temp2=List.rev_map (
+          function (x,compiled_correctly)->
+          if compiled_correctly 
+          then "#install_printer "^(Half_dressed_module.capitalized_module_name x)^".print_out;"^";"
+          else ""
+        ) printer_equipped_types in
+        let temp3="\n\n\n"::(List.rev ("\n\n\n"::temp2)) in
+        let part2=String.concat "\n" temp3 in
+        part2;;  
 
     let save_loadingsfile (root,location_for_loadingsfile) (dirs,hms)=
        let path_for_loadingsfile=
@@ -1929,7 +1948,7 @@ module Save_all=struct
        Io.overwrite_with (Absolute_path.of_string lm) s;;
     
     let save_merlinfile (root,location_for_merlinfile) dirs=
-        let s=Alaskan_write_merlinfile.instructions root dirs 
+        let s=instructions_for_merlinfile root dirs 
         and lm=Root_directory.force_join root  location_for_merlinfile in
         Io.overwrite_with (Absolute_path.of_string lm) s;;
   
@@ -1937,7 +1956,7 @@ module Save_all=struct
        let init_dir=
         Subdirectory.connectable_to_subpath 
         (Coma_constant.automatically_generated_subdir) in
-       let s=Alaskan_printer_equipped_types.instructions printer_equipped_types
+       let s=instructions_for_printersfile printer_equipped_types
        and lm=Root_directory.force_join root  (init_dir^location_for_printersfile) in
        let beg_mark="(*Registered printers start here *)"
        and end_mark="(*Registered printers end here *)" in
