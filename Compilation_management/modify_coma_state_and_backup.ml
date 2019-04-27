9f58690f63bd325d5804d0d1b3a212a96f5bbdce
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-04-27 15:07:59 +0200

    add Modify_coma_state_and_backup.recompile

--- Compilation_management/modify_coma_state_and_backup.ml
+++ Compilation_management/modify_coma_state_and_backup.ml
@@ -7,9 +7,12 @@
 
 module Private = struct
 
-let backup cs diff opt= Backup_coma_state.backup
-  (Coma_state.root cs,Coma_state.backup_dir cs,Coma_state.github_after_backup cs) 
-    diff opt;;
+let backup cs diff opt= 
+  if not(Dircopy_diff.is_empty diff) 
+  then Backup_coma_state.backup
+       (Coma_state.root cs,Coma_state.backup_dir cs,Coma_state.github_after_backup cs) 
+       diff opt
+  else (print_string "No recent changes to commit ...";flush stdout);;
 
 end;;    
 
@@ -18,12 +21,13 @@ let forget cs x=
    let _=Private.backup cs2 diff None in 
    cs2;; 
 
+let recompile cs =
+   let (cs2,diff)=Coma_state.Almost_concrete.recompile_without_githubbing cs  in 
+   let _=Private.backup cs2 diff None in 
+   cs2;; 
 
 (*
-let forget cs x=
-   let cs2=Coma_state.Almost_concrete.forget cs x in 
-   let _=Save_coma_state.save cs2 in 
-   cs2;;
+
   
 let recompile cs opt_comment=
    let cs2=Coma_state.Almost_concrete.recompile cs opt_comment in 
