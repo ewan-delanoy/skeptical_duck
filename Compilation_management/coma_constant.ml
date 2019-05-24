518e1f599698095476d6a1025ed3494e88a7c7da
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-05-24 16:18:11 +0200

    add Coma_constant.persistent_compilation_data_subdir

--- Compilation_management/coma_constant.ml
+++ Compilation_management/coma_constant.ml
@@ -20,11 +20,13 @@ let temporary_subdir=
 let githubbed_archive=
   Subdirectory.of_string "Githubbed_archive";;
 
+let persistent_compilation_data_subdir = 
+   Subdirectory.of_string "Persistent_compilation_data";;
 
 let build_subdir=      Subdirectory.of_string "_build";;
 let debug_build_subdir=Subdirectory.of_string "_debug_build";;  
 let exec_build_subdir= Subdirectory.of_string "_exec_build";;  
-let parameters_subdir= Subdirectory.of_string "Makefile_makers";;
+let parameters_subdir= Subdirectory.of_string "Compilation_management";;
 
 
 let name_for_makefile="makefile";;
@@ -38,10 +40,12 @@ let name_for_parametersfile="coma_big_constant.ml";;
 
 let path_for_loadingsfile=
   (Subdirectory.connectable_to_subpath automatically_generated_subdir)^name_for_loadingsfile;;
-let path_for_printersfile=
-  (Subdirectory.connectable_to_subpath automatically_generated_subdir)^name_for_printersfile;;
 let path_for_parametersfile=
    (Subdirectory.connectable_to_subpath parameters_subdir)^name_for_parametersfile;;
+let path_for_printersfile=
+  (Subdirectory.connectable_to_subpath automatically_generated_subdir)^name_for_printersfile;;
+let path_for_targetfile=
+  (Subdirectory.connectable_to_subpath persistent_compilation_data_subdir)^name_for_targetfile;;     
 
 let up_to_date_but_not_registered_files=
     [
