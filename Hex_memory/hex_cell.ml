e880dc0a342cc1983388ae6e2da2023b74b42167
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-06-03 16:11:23 +0200

    register Hex_memory/hex_cell.ml

---
+++ Hex_memory/hex_cell.ml
@@ -0,0 +1,24 @@
+(* 
+
+#use"Hex_memory/hex_cell.ml";;
+
+First coordinate is column index, second is row index
+
+*)
+
+let cmp=((fun (Hex_cell_t.C(s1,i1)) (Hex_cell_t.C(s2,i2)) ->
+   (Total_ordering.product 
+     Total_ordering.lex_for_strings Total_ordering.standard)
+     (s1,i1) (s2,i2)) :> Hex_cell_t.t Total_ordering.t) ;;
+
+let of_string s =
+  Hex_cell_t.C(
+      String.sub s 0 1,
+      int_of_string(String.sub s 1 (String.length(s)-1))
+  );;
+
+let to_string (Hex_cell_t.C(s1,i1))=
+  s1^(string_of_int i1);;
+
+let print_out (fmt:Format.formatter) ap=
+   Format.fprintf fmt "@[%s@]" (to_string ap);;     
