de45d3474ea448c601f5dcc6a1281cf8dc5b584c
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-06-07 13:49:34 +0200

    register Hex_memory/hex_analysis_state_t.ml

---
+++ Hex_memory/hex_analysis_state_t.ml
@@ -0,0 +1,12 @@
+(* 
+
+#use"Hex_memory/hex_analysis_state_t.ml";;
+
+First coordinate is column index, second is row index
+
+*)
+
+type t= 
+     Not_yet_started
+    |Foreseen_so_far of Hex_partial_game_t.t 
+    |Awaiting_final_outcome of Hex_partial_game_t.t ;;
