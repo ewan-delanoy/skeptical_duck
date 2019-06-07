68acb220a7401c4a8d1b9146322b790409b66d12
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-06-07 17:33:20 +0200

    add Hex_partial_game.cut_by

--- Hex_memory/hex_partial_game.ml
+++ Hex_memory/hex_partial_game.ml
@@ -18,6 +18,10 @@ let starts_with (Hex_partial_game_t.PG(l1)) (Hex_partial_game_t.PG(l2))=
   let (common_left,right1,right2)=Listennou.factor (l1,l2) in
   right2=[];; 
 
+let cut_by (Hex_partial_game_t.PG(l1)) (Hex_partial_game_t.PG(l2))=
+  let (common_left,right1,right2)=Listennou.factor (l1,l2) in
+  if right2=[] then Some(Hex_partial_game_t.PG(right1)) else None;; 
+
 let empty_one = Hex_partial_game_t.PG  [];;
 let singleton cell = Hex_partial_game_t.PG [cell];;
 
