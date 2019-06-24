db2e766c26577ccc76760d9cae0892b3c0533f4a
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-06-24 14:50:12 +0200

    update subdirectory location in Hex_pgame_memorizer.remember_as_example

--- Hex_memory/Hex_initial_analysis/hex_pgame_memorizer.ml
+++ Hex_memory/Hex_initial_analysis/hex_pgame_memorizer.ml
@@ -30,7 +30,7 @@ let to_string mmrzr=
 
 let remember_as_example mmrzr =
   let assignment="\n\n\nlet z=Hex_pgame_memorizer.of_string\n\""^(to_string mmrzr)^"\";;\n\n\n" in 
-  let ap=Absolute_path.of_string "Hex_memory/hex_pgame_memorizer_example.ml" in 
+  let ap=Absolute_path.of_string "Hex_memory/Hex_initial_analysis/hex_pgame_memorizer_example.ml" in 
   Replace_inside.overwrite_between_markers_inside_file 
   (Overwriter.of_string assignment) ("(* Assignment starts here *)","(* Assignment ends here *)") ap ;;
 
