9d298e3404132940f2eb158d186a44498035b160
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-06-27 09:40:56 +0200

    add label in definition of Hex_explicit_game_t.t

--- Hex_memory/Hex_final_analysis/hex_explicit_game_t.ml
+++ Hex_memory/Hex_final_analysis/hex_explicit_game_t.ml
@@ -1,9 +1,9 @@
 (* 
 
-#use"Hex_memory/Hex_final_analysis/hex_full_game_t.ml";;
+#use"Hex_memory/Hex_final_analysis/hex_explicit_game_t.ml";;
 
 First coordinate is column index, second is row index
 
 *)
 
-type t= (Hex_cell_t.t*Hex_abstract_color_t.t) list;;
+type t= FG of (Hex_cell_t.t*Hex_abstract_color_t.t) list;;
