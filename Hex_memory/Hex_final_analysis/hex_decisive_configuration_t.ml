1c27e38c3d162169583031c88d31078c3ba2ee52
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-06-27 10:04:30 +0200

    fix some module names

--- Hex_memory/Hex_final_analysis/hex_decisive_configuration_t.ml
+++ Hex_memory/Hex_final_analysis/hex_decisive_configuration_t.ml
@@ -2,8 +2,8 @@
 
 #use"Hex_memory/Hex_final_analysis/hex_decisive_configuration_t.ml";;
 
-First coordinate is column index, second is row index
+
 
 *)
 
-type t= (Hex_cell_t.t*Hex_abstract_color_t.t) list;;
+type t= DC of (Hex_cell_t.t*Hex_abstract_color_t.t) list;;
--- Hex_memory/Hex_final_analysis/hex_explicit_game_t.ml
+++ Hex_memory/Hex_final_analysis/hex_explicit_game_t.ml
@@ -2,8 +2,6 @@
 
 #use"Hex_memory/Hex_final_analysis/hex_explicit_game_t.ml";;
 
-First coordinate is column index, second is row index
-
 *)
 
 type t= FG of (Hex_cell_t.t*Hex_abstract_color_t.t) list;;
