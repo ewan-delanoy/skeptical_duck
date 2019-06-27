188dbc96b9bb1f42c7580df88e1eef93de4deb25
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-06-27 08:36:02 +0200

    register Hex_memory/Hex_final_analysis/hex_decisive_configuration_t.ml

---
+++ Hex_memory/Hex_final_analysis/hex_decisive_configuration_t.ml
@@ -0,0 +1,9 @@
+(* 
+
+#use"Hex_memory/Hex_final_analysis/hex_decisive_configuration_t.ml";;
+
+First coordinate is column index, second is row index
+
+*)
+
+type t= (Hex_cell_t.t*Hex_abstract_color_t.t) list;;
