d7229e5621184832ba86d468634c418f469b8884
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-07-07 11:54:01 +0200

    register Hex_memory/Hex_final_analysis/hex_unchecked_game.ml

---
+++ Hex_memory/Hex_final_analysis/hex_unchecked_game.ml
@@ -0,0 +1,12 @@
+(* 
+
+#use"Hex_memory/Hex_final_analysis/hex_unchecked_game.ml";;
+
+*)
+
+let get (Hex_unchecked_game_t.FG l_moves) cell=
+   match Option.seek (fun (cell1,player)->cell1=cell) l_moves with 
+   None->None
+   |Some(_,player)->Some(player);;
+
+   
