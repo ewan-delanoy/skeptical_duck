82d863819720d04d9d2e4b22d9f6bc3e968703cd
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-07-09 16:57:59 +0200

    define Hex_common.cell_list_to_pretty_string

--- Hex_memory/hex_common.ml
+++ Hex_memory/hex_common.ml
@@ -12,3 +12,17 @@ let cell_list_of_string s =
 
 let cell_list_to_string l=
   String.concat joiner (Image.image Hex_cell.to_string l);;
+
+let cell_pair_to_string (cell1,cell2)=cell_list_to_string [cell1;cell2];;
+  
+
+let cell_list_to_pretty_string l=
+  let (pairs,optional_last)=Listennou.divide_by_two l in 
+  let part1= String.concat " | " (Image.image cell_pair_to_string pairs) in 
+  let part2=(match optional_last with 
+     None->""
+     |Some(cell)->Hex_cell.to_string cell
+  ) in 
+  if (part1="")||(part2="")
+  then part1^part2
+  else part1^joiner^part2;;
