662fad06c5ca48b00e0ab0478d54641fdccab79e
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-07-09 16:11:39 +0200

    register Hex_memory/hex_player.ml

---
+++ Hex_memory/hex_player.ml
@@ -0,0 +1,16 @@
+(* 
+
+#use"Hex_memory/hex_player.ml";;
+
+*)
+
+exception Incorrect_hex_player_of_string_arg of string;;
+
+let of_string s=
+   if s="1" then Hex_player_t.First_player else 
+   if s="2" then Hex_player_t.Second_player else 
+   raise(Incorrect_hex_player_of_string_arg(s));;
+
+let to_string=function  
+     Hex_player_t.First_player->"1" 
+    |Hex_player_t.Second_player->"2";;
