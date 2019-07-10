380cf1ba864e6275cc22abbbab8c789f07477fbd
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-07-10 15:55:08 +0200

    update definition in Hex_dc_double_list_t

--- Hex_memory/Hex_final_analysis/hex_dc_double_list_t.ml
+++ Hex_memory/Hex_final_analysis/hex_dc_double_list_t.ml
@@ -1,7 +1,10 @@
 (* 
 
-#use"Hex_memory/Hex_final_analysis/hex_dc_list_t.ml";;
+#use"Hex_memory/Hex_final_analysis/hex_dc_double_list_t.ml";;
 
 *)
 
-type t= L of Hex_decisive_configuration_t.t list ;;
+type t= DL of 
+  Hex_decisive_configuration_t.t list 
+  * 
+  Hex_decisive_configuration_t.t list ;;
