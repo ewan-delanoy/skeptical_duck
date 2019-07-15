b3f8651c14e1b0f60034a02ccf1a992327837b76
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-07-15 11:20:57 +0200

    rename Hex_memory/ as Hax_memory

---
+++ Hax_memory/hex_initializer_to_be_used_not_compiled.ml
@@ -0,0 +1,26 @@
+(* 
+
+#use"Hex_memory/hex_initializer_to_be_used_not_compiled.ml";;
+
+First coordinate is column index, second is row index
+
+*)
+
+
+(* Assignment for initial analysis starts here *)
+
+
+let z=Hex_ina_memorizer.of_string
+"
+First player : 
+
+ f6 - f8 - d9 - e7 - d7 - e6 - d6 
+ f6 - f8 - h7 - h8 - g8
+Second player : 
+
+ f6 - f8 - d9 - e7 - d7 - e6 - d6 - e4 
+ f6 - f8 - h7 - h8";;
+
+
+(* Assignment for initial analysis ends here *)
+
