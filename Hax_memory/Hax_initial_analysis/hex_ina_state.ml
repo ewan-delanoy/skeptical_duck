d03de8d148db46a761bca9a905948cfeeccd7615
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-07-16 09:27:10 +0200

    rename hex_ina_memorizer as hax_ina_memorizer

--- Hax_memory/Hax_initial_analysis/hex_ina_state.ml
+++ Hax_memory/Hax_initial_analysis/hex_ina_state.ml
@@ -12,7 +12,7 @@ let pgame_from_state sta=match sta with
    |Hax_ina_state_t.Awaiting_final_outcome(pgame)->pgame;;
 
 let analize_game mmrzr pgame =
-   let forecasts = Hex_ina_memorizer.cut_by mmrzr pgame in 
+   let forecasts = Hax_ina_memorizer.cut_by mmrzr pgame in 
    Hex_cigame_collection.classify_according_to_depth forecasts;;
 
 let consult mmrzr sta=analize_game mmrzr (pgame_from_state sta);;
@@ -22,11 +22,11 @@ let one_move_more mmrzr cell old_state= match old_state with
     |Hax_ina_state_t.Foreseen_so_far(pgame)->
       let new_pgame = Hax_checked_initial_game.one_move_more pgame cell in 
       let new_state=(
-      if Hex_ina_memorizer.is_foreseen_in new_pgame mmrzr
+      if Hax_ina_memorizer.is_foreseen_in new_pgame mmrzr
       then Hax_ina_state_t.Foreseen_so_far(new_pgame)
       else Hax_ina_state_t.Awaiting_final_outcome(new_pgame)
       ) in 
-      let forecasts = Hex_ina_memorizer.cut_by mmrzr new_pgame in 
+      let forecasts = Hax_ina_memorizer.cut_by mmrzr new_pgame in 
       let offers = Hex_cigame_collection.classify_according_to_depth forecasts in 
       (new_state,offers);;
 
