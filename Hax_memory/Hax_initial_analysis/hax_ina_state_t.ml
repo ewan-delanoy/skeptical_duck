e660ca6a4bd605efe4b75305c18242c2e60967d5
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-07-16 09:26:42 +0200

    rename hex_checked_initial_game_t as hax_checked_initial_game_t

--- Hax_memory/Hax_initial_analysis/hax_ina_state_t.ml
+++ Hax_memory/Hax_initial_analysis/hax_ina_state_t.ml
@@ -7,5 +7,5 @@ First coordinate is column index, second is row index
 *)
 
 type t= 
-     Foreseen_so_far of Hex_checked_initial_game_t.t 
-    |Awaiting_final_outcome of Hex_checked_initial_game_t.t ;;
+     Foreseen_so_far of Hax_checked_initial_game_t.t 
+    |Awaiting_final_outcome of Hax_checked_initial_game_t.t ;;
--- Hax_memory/Hax_initial_analysis/hex_checked_initial_game.ml
+++ Hax_memory/Hax_initial_analysis/hex_checked_initial_game.ml
@@ -7,41 +7,41 @@ First coordinate is column index, second is row index
 *)
 
 let cmp=((fun 
- (Hex_checked_initial_game_t.PG(l1)) (Hex_checked_initial_game_t.PG(l2)) ->
+ (Hax_checked_initial_game_t.PG(l1)) (Hax_checked_initial_game_t.PG(l2)) ->
    (Total_ordering.lex_compare 
        Hex_cell.cmp l1 l2) :> 
-       Hex_checked_initial_game_t.t Total_ordering.t) );;
+       Hax_checked_initial_game_t.t Total_ordering.t) );;
 
 let joiner = " - ";;
 
-let starts_with (Hex_checked_initial_game_t.PG(l1)) (Hex_checked_initial_game_t.PG(l2))=
+let starts_with (Hax_checked_initial_game_t.PG(l1)) (Hax_checked_initial_game_t.PG(l2))=
   let (common_left,right1,right2)=Listennou.factor (l1,l2) in
   right2=[];; 
 
-let cut_by (Hex_checked_initial_game_t.PG(l1)) (Hex_checked_initial_game_t.PG(l2))=
+let cut_by (Hax_checked_initial_game_t.PG(l1)) (Hax_checked_initial_game_t.PG(l2))=
   let (common_left,right1,right2)=Listennou.factor (l1,l2) in
-  if (right2=[])&&(right1<>[]) then Some(Hex_checked_initial_game_t.PG(right1)) else None;; 
+  if (right2=[])&&(right1<>[]) then Some(Hax_checked_initial_game_t.PG(right1)) else None;; 
 
-let empty_one = Hex_checked_initial_game_t.PG  [];;
-let singleton cell = Hex_checked_initial_game_t.PG [cell];;
+let empty_one = Hax_checked_initial_game_t.PG  [];;
+let singleton cell = Hax_checked_initial_game_t.PG [cell];;
 
-let one_move_more (Hex_checked_initial_game_t.PG(l)) cell = 
-  Hex_checked_initial_game_t.PG(l@[cell]);;
+let one_move_more (Hax_checked_initial_game_t.PG(l)) cell = 
+  Hax_checked_initial_game_t.PG(l@[cell]);;
 
-let last_one_to_play (Hex_checked_initial_game_t.PG(l)) =
+let last_one_to_play (Hax_checked_initial_game_t.PG(l)) =
    let d=(List.length l) mod 2 in 
    if d=0 then Hax_player_t.Second_player else Hax_player_t.First_player;;
 
-let depth (Hex_checked_initial_game_t.PG(l)) = List.length l ;; 
-let first_move (Hex_checked_initial_game_t.PG(l)) = List.hd l ;; 
+let depth (Hax_checked_initial_game_t.PG(l)) = List.length l ;; 
+let first_move (Hax_checked_initial_game_t.PG(l)) = List.hd l ;; 
 
-let of_string s =Hex_checked_initial_game_t.PG( 
+let of_string s =Hax_checked_initial_game_t.PG( 
    Hax_common.cell_list_of_string s
 );;
 
-let to_string (Hex_checked_initial_game_t.PG(l))=
+let to_string (Hax_checked_initial_game_t.PG(l))=
   Hax_common.cell_list_to_string  l;;
 
 
-let print_out (fmt:Format.formatter) (Hex_checked_initial_game_t.PG(l))=
+let print_out (fmt:Format.formatter) (Hax_checked_initial_game_t.PG(l))=
    Format.fprintf fmt "@[%s@]" (Hax_common.cell_list_to_pretty_string l);;     
