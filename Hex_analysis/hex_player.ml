a52352a9f263339db83498f119ecb02580ebe735
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-07-17 17:12:13 +0200

    register Hex_analysis/hex_player.ml

---
+++ Hex_analysis/hex_player.ml
@@ -0,0 +1,20 @@
+(* 
+
+#use"Hex_analysis/hex_player.ml";;
+
+*)
+
+let other_player = function 
+     Hex_player_t.First_player-> Hex_player_t.Second_player 
+    |Hex_player_t.Second_player->Hex_player_t.First_player;;
+
+exception Incorrect_hex_player_of_string_arg of string;;
+
+let of_string s=
+   if s="1" then Hex_player_t.First_player else 
+   if s="2" then Hex_player_t.Second_player else 
+   raise(Incorrect_hex_player_of_string_arg(s));;
+
+let to_string=function  
+     Hex_player_t.First_player->"1" 
+    |Hex_player_t.Second_player->"2";;
