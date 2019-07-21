d9907fa8837812128f0781932df75e57356a5204
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-07-21 15:24:33 +0200

    register Hex_analysis/hex_state.ml

---
+++ Hex_analysis/hex_state.ml
@@ -0,0 +1,44 @@
+(* 
+
+#use"Hex_analysis/hex_state.ml";;
+
+*)
+
+
+
+let initial_state = {
+   Hex_state_t.config_remains = (!(Hex_persistent.configs_ref)) ;
+   Hex_state_t.games_remains = (!(Hex_persistent.games_ref)) ;
+   Hex_state_t.moves_before = [] 
+};;
+
+let absorb_move sta cell=
+   let player = Hex_common.next_one_to_play (sta.Hex_state_t.moves_before) in 
+   {
+      Hex_state_t.config_remains = Hex_ec_double_list.absorb_move (player,cell) sta.Hex_state_t.config_remains ;
+      Hex_state_t.games_remains = Hex_fg_double_list.absorb_move cell sta.Hex_state_t.games_remains ;
+      Hex_state_t.moves_before = cell::(sta.Hex_state_t.moves_before) ;
+   };;
+
+let analize sta=
+  let player = Hex_common.next_one_to_play (sta.Hex_state_t.moves_before) in 
+  let dangers = Hex_ec_double_list.immediate_dangers player sta.Hex_state_t.config_remains in 
+  let condition = (
+     match dangers with 
+     []->None
+     |_->Some(Ordered.big_kengeij Hex_cell.cmp (Image.image fst dangers))
+  ) in 
+  let (unconditionned_winning_moves,unconditioned_used_moves)=
+      Hex_fg_double_list.suggested_moves player sta.Hex_state_t.games_remains in 
+  let winning_moves=Hex_common.apply_condition condition unconditionned_winning_moves 
+  and used_moves=Hex_common.apply_condition condition unconditioned_used_moves in 
+  {
+     Hex_analysis_result_t.mandatory_set = condition ;
+     winning_moves = winning_moves ;
+     already_used_moves = used_moves
+  } ;;
+
+
+
+
+  
