f64fd71f47b1d6431d8332c71de06923a0614e8e
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-07-21 14:47:40 +0200

    use sets rather than lists in Hex_end_configuration.immediate_dangers

--- Hex_analysis/hex_end_configuration.ml
+++ Hex_analysis/hex_end_configuration.ml
@@ -39,9 +39,8 @@ let immediate_dangers configs =
        fun config->
          let l=config.Hex_end_configuration_t.active_part in 
          if List.length(l)=1 
-         then let mandatory_set=
-               Ordered.insert_plaen Hex_cell.cmp 
-                (List.hd l) config.Hex_end_configuration_t.passive_part in 
+         then let passive_set = Ordered.safe_set Hex_cell.cmp config.Hex_end_configuration_t.passive_part in 
+              let mandatory_set=Ordered.insert Hex_cell.cmp (List.hd l) passive_set in 
                Some(mandatory_set,config.Hex_end_configuration_t.index)
          else None
    ) configs;;
