f0a18a82813b534ca2c7c79b7a55c6a8417fcfa0
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-07-21 15:39:52 +0200

    register Hex_analysis/hex_analysis.ml

---
+++ Hex_analysis/hex_analysis.ml
@@ -0,0 +1,31 @@
+(* 
+
+#use"Hex_analysis/hex_analysis.ml";;
+
+*)
+
+let initial_point = (
+   Hex_state.initial_state,
+   Hex_state.analize (Hex_state.initial_state)
+);;
+
+
+let walker=ref(initial_point);;
+
+let restart () =(walker := initial_point);;
+
+let absorb_move cell=
+   let (old_state,_)=(!walker) in 
+   let new_state = Hex_state.absorb_move old_state cell in 
+   let new_result = Hex_state.analize new_state in 
+   let _=() in 
+   new_result;;
+
+let usual_move ()=
+  let cell = Hex_analysis_result.usual_move (snd(!walker)) in 
+  absorb_move cell;;
+
+
+
+
+  
