0fe02fdff7faf78cd2f9fc45f2590af0c8cbbcb9
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-07-23 15:35:36 +0200

    fix typo in Hex_analysis.declare_winner

--- Hex_analysis/hex_analysis.ml
+++ Hex_analysis/hex_analysis.ml
@@ -34,7 +34,7 @@ let declare_winner player =
     Hex_finished_game_t.sequence_of_moves = List.rev((fst(!walker)).Hex_state_t.moves_before)
   } in 
   let _=Hex_persistent.add_finished_game new_fgame in 
-  let new_ec = Hex_finished_game.to_end_configuration fgame in 
+  let new_ec = Hex_finished_game.to_end_configuration new_fgame in 
   Hex_ascii_display.make_ready_for_editing new_ec;;
 
   
