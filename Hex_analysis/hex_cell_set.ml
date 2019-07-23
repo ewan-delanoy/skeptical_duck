a63a414eac9d7099d89cf659c982a63f31fd4c91
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-07-23 10:55:48 +0200

    fix Hex_cell_set.fold_intersect

--- Hex_analysis/hex_cell_set.ml
+++ Hex_analysis/hex_cell_set.ml
@@ -14,7 +14,9 @@ let  apply_condition opt_condition cell_set=match opt_condition with
      and (Hex_cell_set_t.S(l2))=condition_set in 
      Hex_cell_set_t.S(Ordered.kengeij_plaen Hex_cell.cmp l1 l2);;  
 
-let fold_intersect l=  Hex_cell_set_t.S (Ordered.forget_order(Ordered.big_kengeij Hex_cell.cmp l));;
+let fold_intersect l=  
+    let renamed_l=Image.image (fun (Hex_cell_set_t.S(e))->Ordered.S(e) ) l in 
+    Hex_cell_set_t.S (Ordered.forget_order(Ordered.big_kengeij Hex_cell.cmp renamed_l));;
 let insert elt (Hex_cell_set_t.S(l))= Hex_cell_set_t.S (Ordered.insert_plaen Hex_cell.cmp elt l);;
 let outsert elt (Hex_cell_set_t.S(l))= Hex_cell_set_t.S (Ordered.lemel_plaen Hex_cell.cmp l [elt]);;
 let length (Hex_cell_set_t.S(l))=List.length l;;
