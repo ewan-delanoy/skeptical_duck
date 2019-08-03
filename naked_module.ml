3dd626a04f112ac744ddab1ca8ee4497fd11d29c
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-08-03 16:43:44 +0200

    add uniformization to Naked_module

--- naked_module.ml
+++ naked_module.ml
@@ -13,6 +13,13 @@ let to_string (Naked_module_t.N s)=s;;
 let add_prefix_and_capitalize prefix (Naked_module_t.N name)=
   Naked_module_t.N(String.capitalize_ascii(prefix^name));;
 
+let to_concrete_object (Naked_module_t.N(s))=
+    Concrete_object_t.Variant("Naked_module_t.N",[Concrete_object_t.String(s)]);;
+
+let of_concrete_object ccrt_obj =
+   let (_,(arg1,_,_,_,_,_,_))=Concrete_object_field.unwrap_bounded_variant ccrt_obj in 
+   Naked_module_t.N(Concrete_object_field.unwrap_string arg1);;
+
 
 
            
