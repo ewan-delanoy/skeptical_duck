97723d15b875bf2728c958aa8f598ff143e2f238
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-08-06 16:23:01 +0200

    change viewpoint on Concrete_object_gatherer_t

--- Concrete_ocaml_objects/concrete_object_gatherer_t.ml
+++ Concrete_ocaml_objects/concrete_object_gatherer_t.ml
@@ -1,14 +1,20 @@
 (* 
 
-#use"Concrete_ocaml_objects/concrete_object_gatherer_t.ml";;
+#use"Concrete_ocaml_objects/partial_concrete_object_t.ml";;
+
+For convenience, the list is reversed when the partial object is closed and becomes full
+(new objects are therefore appended directly to the left on a partial object).
 
 
 *)
 
-type t=  
-    Uple 
-   |List 
-   |Array 
-   |Record 
-   |Variant ;;
+type base = Concrete_object_t.t
+
+type t= 
+   |Uple of base list
+   |List of base list
+   |Array of base list
+   |Record of ((string*base) list)
+   |RecordPlusRecordName of ((string*base) list)*string
+   |Variant of string*(base list);;
 
