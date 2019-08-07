a9a1471f16412febe9f92ff6bd3342268bf74a82
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-08-07 11:39:44 +0200

    register Concrete_ocaml_objects/double_partial_concrete_object_t.ml

---
+++ Concrete_ocaml_objects/double_partial_concrete_object_t.ml
@@ -0,0 +1,15 @@
+(* 
+
+#use"Concrete_ocaml_objects/double_partial_concrete_object_t.ml";;
+
+First bool says if a comma appears last, waiting for another item.
+Second bool says if a record arrow appears last, waiting for another item.
+
+The two last arguments are t * (t list) rather than just t list, to enforce
+a non-empty list. 
+
+*)
+
+
+type t= bool * bool * Partial_concrete_object_t.t * (Partial_concrete_object_t.t list) ;;
+
