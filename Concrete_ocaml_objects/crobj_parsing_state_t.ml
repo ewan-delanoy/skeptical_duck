b10475ddfb6c8499d03f1a750ed607683bbaca4e
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-08-07 16:58:54 +0200

    register Concrete_ocaml_objects/crobj_parsing_state_t.ml

---
+++ Concrete_ocaml_objects/crobj_parsing_state_t.ml
@@ -0,0 +1,14 @@
+(* 
+
+#use"Concrete_ocaml_objects/crobj_parsing_state_t.ml";;
+
+
+*)
+
+type t= 
+    {
+       parsed_one    : string;
+       current_index : int;
+       data : Double_partial_concrete_object_t.t;
+    };;
+
