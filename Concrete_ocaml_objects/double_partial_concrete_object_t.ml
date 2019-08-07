215efe03adb144d420ad97ee1ac4b4a150eba492
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-08-07 15:26:43 +0200

    remove one parameter in Double_partial_concrete_object_t.t definition

--- Concrete_ocaml_objects/double_partial_concrete_object_t.ml
+++ Concrete_ocaml_objects/double_partial_concrete_object_t.ml
@@ -2,8 +2,7 @@
 
 #use"Concrete_ocaml_objects/double_partial_concrete_object_t.ml";;
 
-First bool says if a comma appears last, waiting for another item.
-Second bool says if a record arrow appears last, waiting for another item.
+First argument says if a comma appears last, waiting for another item.
 
 The two last arguments are t * (t list) rather than just t list, to enforce
 a non-empty list. 
@@ -11,5 +10,5 @@ a non-empty list.
 *)
 
 
-type t= Double of bool * bool * Partial_concrete_object_t.t * (Partial_concrete_object_t.t list) ;;
+type t= Double of bool * Partial_concrete_object_t.t * (Partial_concrete_object_t.t list) ;;
 
