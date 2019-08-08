4a2a9da54ac4ee37e5f63e4d0e3d926084a07092
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-08-08 11:13:42 +0200

    remove unnecessary comment in Crobj_opening_t

--- Concrete_ocaml_objects/crobj_opening_t.ml
+++ Concrete_ocaml_objects/crobj_opening_t.ml
@@ -2,8 +2,6 @@
 
 #use"Concrete_ocaml_objects/crobj_opening_t.ml";;
 
-For convenience, the list is reversed when the partial object is closed and becomes full
-(new objects are therefore appended directly to the left on a partial object).
 
 
 *)
