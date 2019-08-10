f333a88c2236b86dbca661a17e4e0356226a6a62
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-08-10 17:52:15 +0200

    rename partial_concrete_object as partial_crobj

--- Concrete_ocaml_objects/double_partial_concrete_object.ml
+++ Concrete_ocaml_objects/double_partial_concrete_object.ml
@@ -13,29 +13,29 @@ module Private = struct
 
 let push_int i (Double_partial_concrete_object_t.Double(_,last_opened,opened_before))=
   (Double_partial_concrete_object_t.Double(false,
-    Partial_concrete_object.push_int i last_opened,opened_before));;
+    Partial_crobj.push_int i last_opened,opened_before));;
 
 let push_string s (Double_partial_concrete_object_t.Double(_,last_opened,opened_before))=
   (Double_partial_concrete_object_t.Double(false,
-    Partial_concrete_object.push_string s last_opened,opened_before));;    
+    Partial_crobj.push_string s last_opened,opened_before));;    
 
 let push_separator ctgr (Double_partial_concrete_object_t.Double(separator_present,last_opened,opened_before))=
   if separator_present
   then raise(Redundant_separator)
   else 
-        let ctgr2 = Partial_concrete_object.category last_opened in 
+        let ctgr2 = Partial_crobj.category last_opened in 
         if ctgr <> ctgr2
         then raise(Category_Mismatch(ctgr,last_opened))
         else  (Double_partial_concrete_object_t.Double(true,last_opened,opened_before));;
 
 let push_field_name record_name (Double_partial_concrete_object_t.Double(_,last_opened,opened_before))=
   (Double_partial_concrete_object_t.Double(false,
-    Partial_concrete_object.push_field_name record_name last_opened,opened_before));;    
+    Partial_crobj.push_field_name record_name last_opened,opened_before));;    
 
 let open_new opening 
    (Double_partial_concrete_object_t.Double(_,last_opened,opened_before))=
     Double_partial_concrete_object_t.Double(false,
-      Partial_concrete_object.of_opening opening,last_opened::opened_before);;
+      Partial_crobj.of_opening opening,last_opened::opened_before);;
 
 
 
@@ -44,17 +44,17 @@ let close ctgr
     if separator_present 
     then raise(Close_on_separator)
     else 
-    let newfound=Partial_concrete_object.check_category_and_close ctgr last_opened in 
+    let newfound=Partial_crobj.check_category_and_close ctgr last_opened in 
     match opened_before with 
     []->raise(End_reached(newfound))
     |next_opened_one::others ->
-      let new_frontier = Partial_concrete_object.push_one_more_item newfound next_opened_one in 
+      let new_frontier = Partial_crobj.push_one_more_item newfound next_opened_one in 
       Double_partial_concrete_object_t.Double(false,new_frontier,others);;
 
 end ;; 
 
 let initialize opening = 
-    Double_partial_concrete_object_t.Double(false,Partial_concrete_object.of_opening opening,[]);;
+    Double_partial_concrete_object_t.Double(false,Partial_crobj.of_opening opening,[]);;
 
 let increase = function 
    Crobj_basic_increase_t.Push_int(i)->Private.push_int i 
