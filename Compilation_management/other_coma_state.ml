e08391bbf3b47f991ee6c575c3d3dc8561222e16
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-08-14 19:15:33 +0200

    add Other_coma_state.rename_module

--- Compilation_management/other_coma_state.ml
+++ Compilation_management/other_coma_state.ml
@@ -58,6 +58,9 @@ let refresh ()=Modify_coma_state.Reference.refresh Private.main_ref;;
 
 let register_short_path x=Modify_coma_state.Reference.register_short_path Private.main_ref x;;
   
+let rename_module old_name new_name=
+   Modify_coma_state.Reference.rename_module Private.main_ref old_name new_name;;
+
 let rename_string_or_value old_name new_name=
    Modify_coma_state.Reference.rename_string_or_value
    (Private.main_ref) old_name new_name;;
