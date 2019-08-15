260be22db68629af6a850afeeb402d6a0129aaf1
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-08-15 10:47:10 +0200

    register Decomposed_filename/dfn_with_ending_removed_t.ml

---
+++ Decomposed_filename/dfn_with_ending_removed_t.ml
@@ -0,0 +1,14 @@
+(*
+
+#use"Decomposed_filename/dfn_with_ending_removed_t.ml";;
+
+*)
+
+
+          
+type t={
+   bundle_main_dir : string;
+   subdirectory    : string;
+   naked_module     : string;
+};;
+
