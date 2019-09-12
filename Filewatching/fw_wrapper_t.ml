e84071e46382f49d891ae0d7f3a928c21a980db1
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-09-12 17:06:12 +0200

    register Filewatching/fw_wrapper_t.ml

---
+++ Filewatching/fw_wrapper_t.ml
@@ -0,0 +1,13 @@
+(*
+
+#use"Filewatching/fw_wrapper_t.ml";;
+
+Acts on the physical Unix world around, within the limits
+defined in  the configuration parameter
+
+*)
+
+type t ={
+  configuration : Fw_configuration_t.t ;
+  watched_files : (Dfn_rootless_t.t * string * string) list;
+};;
