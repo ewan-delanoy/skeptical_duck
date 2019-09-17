65040cd660c3e289298cc645e7b90736b519c1f8
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-09-17 10:30:18 +0200

    change the definition of Fw_wrapper_t.t

--- Filewatching/fw_wrapper_t.ml
+++ Filewatching/fw_wrapper_t.ml
@@ -8,6 +8,7 @@ defined in  the configuration parameter
 *)
 
 type t ={
-  configuration : Fw_configuration_t.t ;
-  watched_files : (Dfn_rootless_t.t * string * string) list;
+  configuration         : Fw_configuration_t.t ;
+  watched_files         : (Dfn_rootless_t.t * string * string) list;
+  special_watched_files : (Dfn_rootless_t.t * string * string) list;
 };;
