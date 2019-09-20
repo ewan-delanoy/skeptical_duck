3f33db9a6e07aec0da406c5a260dda29fe032f3c
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-09-20 09:44:57 +0200

    add Fw_wrapper.register_special_rootless_path

--- Filewatching/fw_wrapper.ml
+++ Filewatching/fw_wrapper.ml
@@ -3,11 +3,11 @@
 #use"Filewatching/fw_wrapper.ml";;
 
 
-
 *)
 
+exception Register_rootless_path_exn of string;;
 
-
+let mtime file = string_of_float((Unix.stat file).Unix.st_mtime) ;;
 
 let remove_watched_files fw rootless_paths =
     let s_root = Dfa_root.connectable_to_subpath (Fw_wrapper_field.root fw) in 
@@ -36,7 +36,7 @@ let remove_special_watched_files fw rootless_paths=
    };;
 
 
-let remove_files_associated_to_module fw mod_name =
+let forget_module fw mod_name =
    let the_files = Option.filter_and_unpack (
       fun (path,_,_)-> 
         if (Dfn_rootless.to_module path)=mod_name 
@@ -45,7 +45,31 @@ let remove_files_associated_to_module fw mod_name =
    ) fw.Fw_wrapper_t.watched_files in 
    remove_watched_files fw the_files;;
 
+let register_rootless_path fw rootless_path= 
+   let s_root = Dfa_root.connectable_to_subpath (Fw_wrapper_field.root fw) in 
+   let s_full_path = s_root^(Dfn_rootless.to_line rootless_path)  in 
+   if not(Sys.file_exists s_full_path)
+   then raise(Register_rootless_path_exn(s_full_path))
+   else
+     let m_time =mtime s_full_path  
+     and content =Io.read_whole_file (Absolute_path.of_string s_full_path) in
+    {
+      fw with 
+      Fw_wrapper_t.watched_files =  
+        (fw.Fw_wrapper_t.watched_files)@[rootless_path,m_time,content]  
+    };;
 
-
-
+let register_special_rootless_path fw rootless_path= 
+   let s_root = Dfa_root.connectable_to_subpath (Fw_wrapper_field.root fw) in 
+   let s_full_path = s_root^(Dfn_rootless.to_line rootless_path)  in 
+   if not(Sys.file_exists s_full_path)
+   then raise(Register_rootless_path_exn(s_full_path))
+   else
+     let m_time =mtime s_full_path  
+     and content =Io.read_whole_file (Absolute_path.of_string s_full_path) in
+    {
+      fw with 
+      Fw_wrapper_t.special_watched_files =  
+        (fw.Fw_wrapper_t.special_watched_files)@[rootless_path,m_time,content]  
+    };;
 
