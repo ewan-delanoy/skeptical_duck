00da69289111779ec3cc3fd111dc025e44f9f59b
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-10-03 08:26:05 +0200

    reflect changes from Fw_configuration_t in Fw_configuration

--- Filewatching/fw_configuration.ml
+++ Filewatching/fw_configuration.ml
@@ -8,16 +8,16 @@ module Private = struct
 
 let salt = "Fw_"^"configuration_t.";;
 
-let root_label                      = salt ^ "root";;
-let ignored_subdirectories_label    = salt ^ "ignored_subdirectories";;
-let additional_files_label          = salt ^ "additional_files";;
+let root_label                   = salt ^ "root";;
+let ignored_subdirectories_label = salt ^ "ignored_subdirectories";;
+let ignored_files_label          = salt ^ "ignored_files";;
 
 let of_concrete_object ccrt_obj = 
    let g=Concrete_object_field.get_record ccrt_obj in
    {
       Fw_configuration_t.root = Dfa_root.of_concrete_object(g root_label);
       ignored_subdirectories = Concrete_object_field.to_list Dfa_subdirectory.of_concrete_object(g ignored_subdirectories_label);
-      additional_files = Concrete_object_field.to_list Dfn_rootless.of_concrete_object (g additional_files_label);
+      ignored_files = Concrete_object_field.to_string_list (g ignored_files_label);
    };; 
 
 let to_concrete_object config=
@@ -25,7 +25,7 @@ let to_concrete_object config=
    [
     root_label, Dfa_root.to_concrete_object config.Fw_configuration_t.root;
     ignored_subdirectories_label, Concrete_object_field.of_list Dfa_subdirectory.to_concrete_object config.Fw_configuration_t.ignored_subdirectories;
-    additional_files_label, Concrete_object_field.of_list Dfn_rootless.to_concrete_object  config.Fw_configuration_t.additional_files;
+    ignored_files_label, Concrete_object_field.of_string_list config.Fw_configuration_t.ignored_files;
    ]  in
    Concrete_object_t.Record items;;
 
