8c0f8c3c7e3dd3fc88d376c9419a0980f51d795d
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-10-10 14:46:20 +0200

    add index back again in Hex_end_configuration_t.t

---
+++ Hex_analysis/hex_initializer.ml
@@ -0,0 +1,44 @@
+(* 
+
+#use"Hex_analysis/hex_initializer.ml";;
+
+This file should be used not compiled. 
+
+
+
+*)
+
+(* Configs start here *)
+
+
+ Hex_persistent.configs_ref:=Hex_ec_double_list.of_string(
+"
+Beneficiary : 
+1
+Active part : 
+b6 - c4 - c6 - c9 - d4 - d6 - d7 - d8 - e3
+Passive part : 
+a11 - b5 - b10 - b11 - c5 - c10 - c11 - d9 - d10 - d11 - e1 - e2 - f1 - f2 - f3 - g1 - g2 - h1
+Index : 
+1
+<<>>
+");;
+
+
+(* Configs end here *)
+
+
+(* Games start here *)
+
+
+ Hex_persistent.games_ref:=Hex_fg_double_list.of_string(
+"
+Beneficiary : 
+1
+Active part : 
+f6 - f8 - c9 - e7 - d7 - e6 - d6 - e4 - e5 - f4 - f5 - g4 - d4 - d5 - b6 - d3 - c4 - c3 - e3 - c8
+<[<>]>
+");;
+
+
+(* Games end here *)
