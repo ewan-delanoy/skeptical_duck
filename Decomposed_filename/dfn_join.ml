2534d2bc46a86ae16ffaf99651b900a9c5221ae1
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-10-22 08:48:35 +0200

    add Dfn_join.root_to_middle

--- Decomposed_filename/dfn_join.ml
+++ Decomposed_filename/dfn_join.ml
@@ -5,7 +5,7 @@
 *)
 
 
-(* let root_to_short r (Dfn_short_t.J(s,m))=Dfn_endingless_t.J(r,s,m);; *)
+let root_to_middle r (Dfn_middle_t.J(s,m))=Dfn_endingless_t.J(r,s,m);; 
 let root_to_rootless r (Dfn_rootless_t.J(s,m,e))=Dfn_full_t.J(r,s,m,e);;
 let subdirectory_to s (Dfn_short_t.J(m,e))=(Dfn_rootless_t.J(s,m,e));;  
 let to_ending (Dfn_endingless_t.J(r,s,m)) e = Dfn_full_t.J(r,s,m,e);;
