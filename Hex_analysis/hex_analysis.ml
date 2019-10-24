8c30949157558fbe6f7fa0db940a99563822baac
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-10-24 15:46:37 +0200

    update Hex_analysis, using dimension

--- Hex_analysis/hex_analysis.ml
+++ Hex_analysis/hex_analysis.ml
@@ -4,6 +4,8 @@
 
 *)
 
+let current_dim = 11;;
+
 let initial_point ()= 
   let ista = Hex_state.initial_state () in 
    (
@@ -34,11 +36,12 @@ let usual_move ()=
 
 let declare_winner player =
   let new_fgame={
+    Hex_finished_game_t.dimension = current_dim;
     Hex_finished_game_t.winner = player ;
     Hex_finished_game_t.sequence_of_moves = List.rev((fst(!walker)).Hex_state_t.moves_before)
   } in 
   let _=Hex_persistent.add_finished_game new_fgame in 
-  let new_ec = Hex_finished_game.to_end_configuration new_fgame in 
-  Hex_ascii_display.make_ready_for_editing new_ec;;
+  let new_grid = Hex_ascii_grid.of_finished_game current_dim new_fgame in 
+  Hex_ascii_grid.make_ready_for_editing new_grid;;
 
   
