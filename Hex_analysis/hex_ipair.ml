6e1ee4df0ad614fa50d21c3644a0b72958f09106
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-10-24 17:33:23 +0200

    add Hex_ipair.support_for_upwards_pyramid

--- Hex_analysis/hex_ipair.ml
+++ Hex_analysis/hex_ipair.ml
@@ -18,7 +18,35 @@ let ipair_of_string s=
 let string_of_ipair (i,j)=
   (String.make 1 (char_of_int(j+96)))^(string_of_int i);;
 
+let core_support_for_upwards_pyramid=
+   [
+      ((1,1),'b');((1,2),'b');  
+      ((1,3),'c');((1,4),'c');
+      ((2,1),'d');((2,3),'d');
+      ((2,2),'e');((3,1),'e');
+      ((3,3),'f');((4,2),'f');
+   ]
+   @
+   [
+      ((1,7),'B');((1,8),'B');  
+      ((1,5),'C');((1,6),'C');
+      ((2,5),'D');((2,7),'D');
+      ((2,6),'E');((3,1),'6');
+      ((3,4),'F');((4,4),'F');
+   ]
+   @
+   [
+      ((3,2),'*');((3,5),'*');  
+   ];;
+
+let translator (dx,dy) l=
+   Image.image (fun ((a,b),c)->((a+dx,a+dy),c)) l;;
+
 end;;
 
 let of_cell cell= Private.ipair_of_string (Hex_cell.to_string cell);;
 let to_cell pair =Hex_cell.of_string(Private.string_of_ipair pair);;
+
+let support_for_upwards_pyramid (x,y)=
+   Private.translator (x-4,y-3) Private.core_support_for_upwards_pyramid;;
+
