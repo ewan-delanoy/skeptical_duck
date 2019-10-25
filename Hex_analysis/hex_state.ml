e2cd4eb0d010add69427e53b996119fdda146e5d
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-10-25 16:31:38 +0200

    use whoami field in Hex_state

--- Hex_analysis/hex_state.ml
+++ Hex_analysis/hex_state.ml
@@ -6,14 +6,16 @@
 
 let empty_state = 
 {
+   Hex_state_t.whoami = Hex_player_t.First_player (* random decision, should never be used *)
    Hex_state_t.config_remains = Hex_fles_double_list.empty_one ;
    Hex_state_t.games_remains = Hex_fg_double_list.empty_one ;
    Hex_state_t.moves_before = [] 
 };;
 
-let initial_state ()= 
+let initial_state my_name= 
    let _=Hex_persistent.retrieve_all_data () in 
 {
+   Hex_state_t.whoami = my_name;
    Hex_state_t.config_remains = ((Hex_end_strategy_factory.compute_all_end_configs Hex_persistent.wes_pair)) ;
    Hex_state_t.games_remains = (!(Hex_persistent.games_ref)) ;
    Hex_state_t.moves_before = [] 
@@ -22,6 +24,7 @@ let initial_state ()=
 let absorb_move sta cell=
    let player = Hex_common.next_one_to_play (sta.Hex_state_t.moves_before) in 
    {
+      sta with
       Hex_state_t.config_remains = Hex_fles_double_list.absorb_move (player,cell) sta.Hex_state_t.config_remains ;
       Hex_state_t.games_remains = Hex_fg_double_list.absorb_move cell sta.Hex_state_t.games_remains ;
       Hex_state_t.moves_before = cell::(sta.Hex_state_t.moves_before) ;
