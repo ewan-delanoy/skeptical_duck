5d0e91b1e38a69698c7eff480a543c4d8c72e591
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-10-26 17:08:28 +0200

    add openings_remains field in Hex_state_t.t

--- Hex_analysis/hex_state_t.ml
+++ Hex_analysis/hex_state_t.ml
@@ -8,6 +8,7 @@ type t= {
    whoami : Hex_player_t.t ; 
    config_remains : Hex_fles_double_list_t.t ;
    games_remains : Hex_fg_double_list_t.t;
+   openings_remains : Hex_strong_opening_t.t list;
    moves_before : Hex_cell_t.t list;
    strong_moves_before : (Hex_cell_t.t option) * (Hex_cell_t.t list);
 };;
