f9da1d63e50d1cf4483af451e00e8045c71728bf
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-10-26 15:42:14 +0200

    add optional first-weak move in Hex_state_t.strong_moves_before

--- Hex_analysis/hex_state_t.ml
+++ Hex_analysis/hex_state_t.ml
@@ -9,7 +9,6 @@ type t= {
    config_remains : Hex_fles_double_list_t.t ;
    games_remains : Hex_fg_double_list_t.t;
    moves_before : Hex_cell_t.t list;
-   still_strong : bool;
-   strong_moves_before : Hex_cell_t.t list;
+   strong_moves_before : (Hex_cell_t.t option) * (Hex_cell_t.t list);
 };;
 
