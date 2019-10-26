82dd27ac64adb6b89bcb88f07c9cff1affae912b
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-10-26 17:48:37 +0200

    Hex_player.of_int in Hex_state.initial_state

--- Hex_analysis/hex_state.ml
+++ Hex_analysis/hex_state.ml
@@ -17,7 +17,7 @@ let empty_state =
 let initial_state my_name= 
    let _=Hex_persistent.retrieve_all_data () in 
 {
-   Hex_state_t.whoami = my_name;
+   Hex_state_t.whoami = Hex_player.of_int my_name;
    Hex_state_t.config_remains = ((Hex_end_strategy_factory.compute_all_end_configs Hex_persistent.wes_pair)) ;
    Hex_state_t.games_remains = (!(Hex_persistent.games_ref)) ;
    Hex_state_t.openings_remains = (!(Hex_persistent.strong_openings_ref));
