fdbedcabba2aed2cc563102ed3740694e7f71aed
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-10-27 07:53:40 +0100

    remember opening in Hex_analysis.declare_winner

--- Hex_analysis/hex_analysis.ml
+++ Hex_analysis/hex_analysis.ml
@@ -61,7 +61,8 @@ let declare_winner player =
     Hex_finished_game_t.winner = player ;
     Hex_finished_game_t.sequence_of_moves = List.rev((fst(!walker)).Hex_state_t.moves_before)
   } in 
-  let _=Hex_persistent.add_finished_game new_fgame in 
+  let _=Hex_persistent.add_finished_game new_fgame in
+  let _=remember_opening_if_necessary player in  
   let new_grid = Hex_ascii_grid.of_finished_game new_fgame in 
   Hex_ascii_grid.print_on_sheet_for_editing new_grid;;
 
