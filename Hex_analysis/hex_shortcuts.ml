d894c0a7a45513914b3de293516f4acd5d83783f
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-10-27 17:44:56 +0100

    add Hex_shortcuts.u

--- Hex_analysis/hex_shortcuts.ml
+++ Hex_analysis/hex_shortcuts.ml
@@ -8,9 +8,10 @@ let abl = Hex_analysis.add_basic_linker ;;
 let am s = Hex_analysis.absorb_move (Hex_cell.of_string s);;
 let ams l = Hex_analysis.absorb_moves (Image.image Hex_cell.of_string l);;
 let dw k=Hex_analysis.declare_winner (Hex_player.of_int k);;
-let wk=Hex_analysis.walker;;
 let ps=Hex_ascii_grid.process_sheet;;
 let refs ()=(!Hex_persistent.games_ref,!Hex_persistent.strong_openings_ref,
 !Hex_persistent.winning_end_strategies_for_first_player_ref,!Hex_persistent.winning_end_strategies_for_second_player_ref);;
 let rep i = Hex_analysis.replay_and_declare_winner (Hex_player.of_int i);;   
 let res i = Hex_analysis.restart (Hex_player.of_int i);;   
+let u = Hex_analysis.move_as_usual;;
+let wk=Hex_analysis.walker;;
