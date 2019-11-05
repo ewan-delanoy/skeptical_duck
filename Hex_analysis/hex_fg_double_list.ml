55adfab376b297c6883dad683f04dbe154757f5d
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-05 14:50:05 +0100

    add Hex_fg_double_list.{of,to}_concrete_object

--- Hex_analysis/hex_fg_double_list.ml
+++ Hex_analysis/hex_fg_double_list.ml
@@ -4,7 +4,7 @@
 
 *)
 
-let empty_one = Hex_fg_double_list_t.DL([],[]);;
+
 
 let joiner_for_two="\n<[<>]>\n";;
 
@@ -21,6 +21,21 @@ let of_string s=
 
 let to_string (Hex_fg_double_list_t.DL(l1,l2))=
   (Hex_fg_list.to_string l1)^joiner_for_two^(Hex_fg_list.to_string l2);;
+
+let of_concrete_object crobj=
+   let (_,(arg1,arg2,_,_,_,_,_))=Concrete_object_field.unwrap_bounded_variant crobj in 
+   Hex_fg_double_list_t.DL(
+      Concrete_object_field.to_list Hex_finished_game.of_concrete_object arg1,
+      Concrete_object_field.to_list Hex_finished_game.of_concrete_object arg2
+   );;
+
+let to_concrete_object (Hex_fg_double_list_t.DL(l1,l2))=
+   Concrete_object_t.Variant("Hex_"^"fg_double_list_t.DL",
+     [Concrete_object_field.of_list Hex_finished_game.to_concrete_object l1;
+      Concrete_object_field.of_list Hex_finished_game.to_concrete_object l2
+     ]);;
+
+let empty_one = Hex_fg_double_list_t.DL([],[]);;
 
 let add_finished_game fgame (Hex_fg_double_list_t.DL(l1,l2))=
    match fgame.Hex_finished_game_t.winner with 
--- Hex_analysis/hex_fles_double_list.ml
+++ Hex_analysis/hex_fles_double_list.ml
@@ -45,35 +45,3 @@ let to_concrete_object (Hex_fles_double_list_t.DL(l1,l2))=
      ]);;
 
 
-(*
-let joiner_in_list ="\n&&&\n";;
-
-let single_list_of_string s= 
-    let temp1=Str.split (Str.regexp_string joiner_in_list) s in 
-    let temp2=Image.image Hex_flattened_end_strategy.of_string temp1 in 
-     Ordered.diforchan_plaen Hex_flattened_end_strategy.cmp temp2 ;;
-  
-let single_list_to_string l=String.concat joiner_in_list 
-    (Image.image Hex_flattened_end_strategy.to_string l);;
-
-let joiner_for_two="\n<<>>\n";;
-
-
-
-
-let of_string s=
-   let n=String.length s 
-   and i1=Substring.leftmost_index_of_in joiner_for_two s in 
-   let j1=i1+(String.length joiner_for_two)-1 in 
-   let part1=Cull_string.interval s 1 (i1-1) 
-   and part2=Cull_string.interval s (j1+1) n in 
-   Hex_fles_double_list_t.DL(
-     (single_list_of_string part1),
-     (single_list_of_string part2)
-   ) ;;        
-
-let to_string (Hex_fles_double_list_t.DL(l1,l2))=
-  (single_list_to_string l1)^joiner_for_two^(single_list_to_string l2);;
-
-*)  
-
