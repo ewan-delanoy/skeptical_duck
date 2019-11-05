01d700d6d91351581a74bb564e296540f2c6a99b
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-05 17:19:13 +0100

    say untamed opening rather than strong opening in Hex_shortcut

--- Hex_analysis/hex_shortcut.ml
+++ Hex_analysis/hex_shortcut.ml
@@ -13,7 +13,7 @@ let spec1 j = Hex_analysis.absorb_some_moves
 let dw k=Hex_analysis.declare_winner (Hex_player.of_int k);;
 let init = Hex_persistent.initialize_all_data_if_necessary ;;
 let ps=Hex_ascii_grid.process_sheet;;
-let refs ()=(!Hex_persistent.games_ref,!Hex_persistent.strong_openings_ref,
+let refs ()=(!Hex_persistent.games_ref,!Hex_persistent.untamed_openings_ref,
 !Hex_persistent.winning_end_strategies_for_first_player_ref,!Hex_persistent.winning_end_strategies_for_second_player_ref);;
 let rep = Hex_analysis.replay_and_declare_winner ;;   
 let res = Hex_analysis.restart ;;   
