0e66014bec3dcb644c80ec1aeda8669254431095
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-05 15:25:14 +0100

    add Hex_untamed_opening.recipient

--- Hex_analysis/hex_untamed_opening.ml
+++ Hex_analysis/hex_untamed_opening.ml
@@ -4,17 +4,31 @@
 
 *)
 
-
-let extends 
-   (Hex_untamed_opening_t.O l1) (Hex_untamed_opening_t.O l2)=
-   let (_,_,r2)=Listennou.factor (l1,l2) in r2=[];;
-
 let to_string (Hex_untamed_opening_t.O l)=
    "O("^(Hex_common.cell_list_to_string l)^")";;
 
 let of_string text =
      let text1=Cull_string.two_sided_cutting ("O(",")") text in 
      Hex_untamed_opening_t.O(Hex_common.cell_list_of_string text1);;
+
+
+let of_concrete_object crobj=
+   let (_,(arg1,_,_,_,_,_,_))=Concrete_object_field.unwrap_bounded_variant crobj in 
+   Hex_untamed_opening_t.O(
+      Concrete_object_field.to_list Hex_cell.of_concrete_object arg1
+   );;
+
+let to_concrete_object (Hex_untamed_opening_t.O(l))=
+   Concrete_object_t.Variant("Hex_"^"untamed_opening.O",
+     [Concrete_object_field.of_list Hex_cell.to_concrete_object l
+     ]);;
+
+let recipient (Hex_untamed_opening_t.O(l))=Hex_common.has_just_played l;;
+
+let extends 
+   (Hex_untamed_opening_t.O l1) (Hex_untamed_opening_t.O l2)=
+   let (_,_,r2)=Listennou.factor (l1,l2) in r2=[];;
+
 
 
 let simplify_by_move new_move (Hex_untamed_opening_t.O l) =
