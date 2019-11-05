ba3a8ba60aa1eed607c76fc554429d981a314e62
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-05 15:11:03 +0100

    rename hex_strong_opening as hex_untamed_opening

--- Hex_analysis/hex_so_list.ml
+++ Hex_analysis/hex_so_list.ml
@@ -9,21 +9,21 @@ let joiner_in_list =" | ";;
 
 let of_string s= 
     let temp1=Str.split (Str.regexp_string joiner_in_list) s in 
-    let temp2=Image.image Hex_strong_opening.of_string temp1 in 
-     Ordered.diforchan_plaen Hex_strong_opening.cmp temp2 ;;
+    let temp2=Image.image Hex_untamed_opening.of_string temp1 in 
+     Ordered.diforchan_plaen Hex_untamed_opening.cmp temp2 ;;
   
 let to_string l=String.concat joiner_in_list 
-    (Image.image Hex_strong_opening.to_string l);;
+    (Image.image Hex_untamed_opening.to_string l);;
 
 let insert_in new_so l=
-   if List.exists (fun so -> Hex_strong_opening.extends so new_so ) l
+   if List.exists (fun so -> Hex_untamed_opening.extends so new_so ) l
    then l 
    else 
      let cleaned_l=List.filter 
-       (fun so->not(Hex_strong_opening.extends new_so so)) l in 
-     Ordered.insert_plaen Hex_strong_opening.cmp new_so cleaned_l;;  
+       (fun so->not(Hex_untamed_opening.extends new_so so)) l in 
+     Ordered.insert_plaen Hex_untamed_opening.cmp new_so cleaned_l;;  
 
 let simplify_by_move move l=
-   Option.filter_and_unpack (Hex_strong_opening.simplify_by_move move) l;;     
+   Option.filter_and_unpack (Hex_untamed_opening.simplify_by_move move) l;;     
 
-let easy_advances l=Option.filter_and_unpack  Hex_strong_opening.easy_advance l;; 
+let easy_advances l=Option.filter_and_unpack  Hex_untamed_opening.easy_advance l;; 
