7ceb9cd0c4bca205ff038e571b9cadbeadb24776
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-05 15:48:07 +0100

    add Hex_uog_list.insert_in

--- Hex_analysis/hex_uog_list.ml
+++ Hex_analysis/hex_uog_list.ml
@@ -7,18 +7,6 @@
 
 module Private = struct 
 
-let remove_redundancies l=
- let rec tempf=(function
-   (treated,to_be_treated)->match to_be_treated with
-   []->treated
-   |uog::other_openings->
-     let cleaned_openings=List.filter (
-       fun uog2->not(Hex_untamed_opening.extends uog uog2)
-     ) other_openings in 
-     tempf(uog::treated,cleaned_openings)
- ) in
- tempf([],List.rev l);;
-
 let select_openings_with_next_player_as_recipient l =
    List.filter Hex_untamed_opening.has_odd_length l;;
 
@@ -29,9 +17,13 @@ let of_concrete_object crobj = Concrete_object_field.to_list Hex_untamed_opening
 let to_concrete_object l = Concrete_object_field.of_list Hex_untamed_opening.to_concrete_object l;;
 
 
-let add_opening fgame l=
-   let temp1= Ordered.insert_plaen Hex_untamed_opening.cmp fgame l  in 
-   Private.remove_redundancies temp1;;
+let insert_in new_uog l=
+   if List.exists (fun uog -> Hex_untamed_opening.extends uog new_uog ) l
+   then l 
+   else 
+     let cleaned_l=List.filter 
+       (fun uog->not(Hex_untamed_opening.extends new_uog uog)) l in 
+     Ordered.insert_plaen Hex_untamed_opening.cmp new_uog cleaned_l;;  
 
 
 let simplify_by_move move l=Option.filter_and_unpack (Hex_untamed_opening.simplify_by_move move) l;;     
