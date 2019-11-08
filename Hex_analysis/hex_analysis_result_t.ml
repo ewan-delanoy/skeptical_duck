9954ee2f10a9c345b05db73b7478da80c2a56479
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-08 06:42:13 +0100

    add next_to_play field in Hex_analysis_result_t

--- Hex_analysis/hex_analysis_result_t.ml
+++ Hex_analysis/hex_analysis_result_t.ml
@@ -6,11 +6,11 @@
 
 
 type t= {
+   next_to_play : Hex_player_t.t ; 
    mandatory_set : Hex_cell_set_t.t option;
    involved_end_strategies : int list;
    easy_advancer : (Hex_cell_t.t * (Hex_cell_t.t list)) option;
    strong_moves : Hex_cell_set_t.t ;
-   already_used_moves : Hex_cell_set_t.t;
    usual_move : Hex_cell_t.t ; 
    number_of_remaining_enemies : int ;
 };;
