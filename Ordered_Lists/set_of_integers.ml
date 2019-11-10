6eb3d71473fba7ff8d637ebec0dec8ccc1627232
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-10 12:05:29 +0100

    simplify Set_of_integers

--- Ordered_Lists/set_of_integers.ml
+++ Ordered_Lists/set_of_integers.ml
@@ -4,6 +4,23 @@
 
 *)
 
+let tr = ((fun x->Set_of_integers_t.S(x)),(fun (Set_of_integers_t.S(x))->x),Total_ordering.standard);;
+
+
+let forget_order x= Functor_for_sets.forget_order tr x;;
+let image f x= Functor_for_sets.image tr f x;;
+let mem a x= Functor_for_sets.mem tr a x;;
+let merge l= Functor_for_sets.merge tr l;;
+let safe_set l= Functor_for_sets.safe_set tr l;;
+let singleton a= Functor_for_sets.singleton tr a;;
+let sort l= Functor_for_sets.sort tr l;;
+let unsafe_set l= Functor_for_sets.unsafe_set tr l;;
+
+
+
+
+
+(*
 type set=int Ordered.old_set;;
 let cmp=(Total_ordering.standard:>(int Total_ordering.t));;let lt x y=x<y;;
 let unsafe_set=(Ordered.unsafe_set:>(int list-> set));;
@@ -53,3 +70,4 @@ let expand_boolean_algebra=
  ((fun x->Ordered.expand_boolean_algebra cmp x):>(set list->(set list)));; 
  
  
+*)
