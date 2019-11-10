64f2d0e6e8805c8a69ff2bf5c2ebb91015c7120f
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-10 07:22:12 +0100

    add more English terms in Ordered

--- ordered.ml
+++ ordered.ml
@@ -25,47 +25,47 @@ let forget_order (S x)=x;;
 let eq (S x) (S y)=(x=y);;
 let neq (S x) (S y)=(x<>y);;
 
-let is_nondecreasing (kenver:'a Total_ordering.t) l=
+let is_nondecreasing (cmpr:'a Total_ordering.t) l=
   if List.length(l)<2 then true else
   let rec tempf=(function
-  (a,da_ober)->match da_ober with
+  (a,to_be_treated)->match to_be_treated with
    []->true
-   |b::peurrest->if (kenver(a)(b)<>Total_ordering.Greater)
-                 then tempf(b,peurrest)
+   |b::others->if (cmpr(a)(b)<>Total_ordering.Greater)
+                 then tempf(b,others)
                  else false
   ) in
   tempf(List.hd l,List.tl l);;
   
-let is_increasing (kenver:'a Total_ordering.t) l=
+let is_increasing (cmpr:'a Total_ordering.t) l=
   if List.length(l)<2 then true else
   let rec tempf=(function
-  (a,da_ober)->match da_ober with
+  (a,to_be_treated)->match to_be_treated with
    []->true
-   |b::peurrest->if (kenver(a)(b)=Total_ordering.Lower)
-                 then tempf(b,peurrest)
+   |b::others->if (cmpr(a)(b)=Total_ordering.Lower)
+                 then tempf(b,others)
                  else false
   ) in
   tempf(List.hd l,List.tl l);;
 
-let rec elfenn (kenver:'a Total_ordering.t) x ol=
+let rec elfenn (cmpr:'a Total_ordering.t) x ol=
    let rec elfenn0=(function
     []->false
-    |a::peurrest->match kenver(x)(a) with
+    |a::others->match cmpr(x)(a) with
        Total_ordering.Lower->false
        |Total_ordering.Equal->true
-       |Total_ordering.Greater->elfenn0 peurrest
+       |Total_ordering.Greater->elfenn0 others
    )  in
    elfenn0 (forget_order ol);;
 		
             
-let teuzin (kenver:'a Total_ordering.t) ox oy=
+let teuzin (cmpr:'a Total_ordering.t) ox oy=
 let rec teuzin0=
 (function (u,v,accu)->
 if u=[] then (List.rev_append(accu)(v)) else
 if v=[] then (List.rev_append(accu)(u)) else
 let xu=List.hd(u) and yu=List.tl(u) 
 and xv=List.hd(v) and yv=List.tl(v) in
-match kenver(xu)(xv) with
+match cmpr(xu)(xv) with
    Total_ordering.Lower->teuzin0(yu,v,xu::accu)
    |Total_ordering.Equal->teuzin0(yu,yv,xu::accu)
    |Total_ordering.Greater->teuzin0(u,yv,xv::accu)
@@ -73,23 +73,23 @@ match kenver(xu)(xv) with
 ) in
 unsafe_set(teuzin0(forget_order ox,forget_order oy,[]));;
 
-let rec diforchan (kenver:'a Total_ordering.t) x=
+let rec diforchan (cmpr:'a Total_ordering.t) x=
   if List.length(x)<2
   then unsafe_set(x)
   else let temp1=Listennou.didrochan(x) in
-       let y1=diforchan(kenver)(fst temp1)
-       and y2=diforchan(kenver)(snd temp1) in
-       teuzin kenver y1 y2;;
+       let y1=diforchan(cmpr)(fst temp1)
+       and y2=diforchan(cmpr)(snd temp1) in
+       teuzin cmpr y1 y2;;
   
   
-  let lemel (kenver:'a Total_ordering.t) ox oy=
+  let lemel (cmpr:'a Total_ordering.t) ox oy=
 let rec lemel0=
 (function (u,v,accu)->
 if u=[] then (List.rev(accu)) else
 if v=[] then (List.rev_append(accu)(u)) else
 let xu=List.hd(u) and yu=List.tl(u) 
 and xv=List.hd(v) and yv=List.tl(v) in
-match kenver(xu)(xv) with
+match cmpr(xu)(xv) with
    Total_ordering.Lower->lemel0(yu,v,xu::accu)
    |Total_ordering.Equal->lemel0(yu,yv,accu)
    |Total_ordering.Greater->lemel0(u,yv,accu)
@@ -97,14 +97,14 @@ match kenver(xu)(xv) with
 ) in
 unsafe_set(lemel0(forget_order ox,forget_order oy,[]));;
 
-let kengeij (kenver:'a Total_ordering.t) ox oy=
+let kengeij (cmpr:'a Total_ordering.t) ox oy=
 let rec kengeij0=
 (function (u,v,accu)->
 if u=[] then (List.rev(accu)) else
 if v=[] then (List.rev(accu)) else
 let xu=List.hd(u) and yu=List.tl(u) 
 and xv=List.hd(v) and yv=List.tl(v) in
-match kenver(xu)(xv) with
+match cmpr(xu)(xv) with
    Total_ordering.Lower->kengeij0(yu,v,accu)
    |Total_ordering.Equal->kengeij0(yu,yv,xu::accu)
    |Total_ordering.Greater->kengeij0(u,yv,accu)
@@ -112,13 +112,13 @@ match kenver(xu)(xv) with
 ) in
 unsafe_set(kengeij0(forget_order ox,forget_order oy,[]));;
 
-let kengeij_goullo (kenver:'a Total_ordering.t) ox oy=
+let kengeij_goullo (cmpr:'a Total_ordering.t) ox oy=
 let rec kengeij_goullo0=
 (function (u,v)->
 if (u=[])||(v=[]) then true else
 let xu=List.hd(u) and yu=List.tl(u) 
 and xv=List.hd(v) and yv=List.tl(v) in
-match kenver(xu)(xv) with
+match cmpr(xu)(xv) with
    Total_ordering.Lower->kengeij_goullo0(yu,v)
    |Total_ordering.Equal->false
    |Total_ordering.Greater->kengeij_goullo0(u,yv)
@@ -126,124 +126,124 @@ match kenver(xu)(xv) with
 kengeij_goullo0(forget_order ox,forget_order oy);;
 
 
-let ental (kenver:'a Total_ordering.t) ox oy=
+let ental (cmpr:'a Total_ordering.t) ox oy=
 let rec ental0=
 (function (u,v)->
 if u=[] then true else
 if v=[] then false else
 let xu=List.hd(u) and yu=List.tl(u) 
 and xv=List.hd(v) and yv=List.tl(v) in
-match kenver(xu)(xv) with
+match cmpr(xu)(xv) with
    Total_ordering.Lower->false
    |Total_ordering.Equal->ental0(yu,yv)
    |Total_ordering.Greater->ental0(u,yv)
 ) in
 ental0(forget_order ox,forget_order oy);;
 
-let min=((fun kenver x->match x with
+let min=((fun cmpr x->match x with
   []->failwith("The empty set has no min")
   |a::b->
     let rec tempf=(fun 
-     (trecher,da_ober)->match da_ober with
+     (trecher,to_be_treated)->match to_be_treated with
       []->trecher
-      |c::peurrest->
-        if kenver(c)(trecher)=Total_ordering.Lower
-        then tempf(c,peurrest)
-        else tempf(trecher,peurrest)
+      |c::others->
+        if cmpr(c)(trecher)=Total_ordering.Lower
+        then tempf(c,others)
+        else tempf(trecher,others)
     ) in
     tempf(a,b)):> ('a Total_ordering.t -> 'a list->'a));;
 
-let max=((fun kenver x->match x with
+let max=((fun cmpr x->match x with
   []->failwith("The empty set has no max")
   |a::b->
     let rec tempf=(fun 
-     (trecher,da_ober)->match da_ober with
+     (trecher,to_be_treated)->match to_be_treated with
       []->trecher
-      |c::peurrest->
-        if kenver(c)(trecher)=Total_ordering.Greater
-        then tempf(c,peurrest)
-        else tempf(trecher,peurrest)
+      |c::others->
+        if cmpr(c)(trecher)=Total_ordering.Greater
+        then tempf(c,others)
+        else tempf(trecher,others)
     ) in
     tempf(a,b)):> ('a Total_ordering.t -> 'a list->'a));;
 
-let cooperation_for_two kenver x y=
-   (kengeij kenver x y,lemel kenver x y,lemel kenver y x);;
+let cooperation_for_two cmpr x y=
+   (kengeij cmpr x y,lemel cmpr x y,lemel cmpr y x);;
   
-let expand_boolean_algebra kenver l=
+let expand_boolean_algebra cmpr l=
   if List.length(l)<2 then l else
   let rec tempf=(fun 
-    (graet,y0,etre,da_ober)->
+    (graet,y0,etre,to_be_treated)->
       if etre=[] 
-      then if da_ober=[]
+      then if to_be_treated=[]
            then y0::graet
-           else let z0=List.hd(da_ober) and peurrest=List.tl(da_ober) in
-                tempf([],z0,y0::graet,peurrest)
+           else let z0=List.hd(to_be_treated) and others=List.tl(to_be_treated) in
+                tempf([],z0,y0::graet,others)
       else 
-      let x0=List.hd(etre) and peurrest_etre=List.tl(etre) in
-      let t1=kengeij kenver x0 y0 and t2=lemel kenver x0 y0 in
-      let y1=lemel kenver y0 t1 in
+      let x0=List.hd(etre) and others_etre=List.tl(etre) in
+      let t1=kengeij cmpr x0 y0 and t2=lemel cmpr x0 y0 in
+      let y1=lemel cmpr y0 t1 in
       let temp1=List.filter (fun ox->forget_order ox<>[]) [t1;t2] in
-      tempf(List.rev_append temp1 graet,y1,peurrest_etre,da_ober)
+      tempf(List.rev_append temp1 graet,y1,others_etre,to_be_treated)
   )  in
   let x1=List.hd(l) and r1=List.tl(l) in
   tempf([],x1,[],r1);;
 
 let length ox=List.length(forget_order ox);;
 let image f ox=Image.image(f)(forget_order ox);;
-let insert kenver x oy=teuzin kenver (unsafe_set [x])(oy);;
+let insert cmpr x oy=teuzin cmpr (unsafe_set [x])(oy);;
 let exists f ox=List.exists f (forget_order ox);;
-let safe_set kenver ox=if is_nondecreasing(kenver)(ox) 
+let safe_set cmpr ox=if is_nondecreasing(cmpr)(ox) 
                        then unsafe_set ox 
-                       else diforchan kenver ox;;
+                       else diforchan cmpr ox;;
 let rev_map f ox=List.rev_map(f)(forget_order ox);;
 let filter f ox=unsafe_set(List.filter(f)(forget_order ox));;
 let for_all f ox=List.for_all(f)(forget_order ox);;
 let singleton x=unsafe_set [x];;
 let empty_set=unsafe_set [];;
-let big_teuzin kenver l=
+let big_teuzin cmpr l=
    let rec tempf=(function
       (already_treated,to_be_treated)->match to_be_treated with 
       []->already_treated
-      |a::b->tempf(teuzin kenver a already_treated,b)
+      |a::b->tempf(teuzin cmpr a already_treated,b)
    ) in 
    tempf(empty_set,l);;
-let big_kengeij kenver=function
+let big_kengeij cmpr=function
    []->failwith("empty intersection undefined")
-  |a::b->List.fold_left(kengeij kenver)(a)(b);;
-let nelfenn kenver a ox=not(elfenn kenver a ox);;
-let nental kenver a ox=not(ental kenver a ox);;
+  |a::b->List.fold_left(kengeij cmpr)(a)(b);;
+let nelfenn cmpr a ox=not(elfenn cmpr a ox);;
+let nental cmpr a ox=not(ental cmpr a ox);;
 let eq ox oy=(forget_order ox)=(forget_order oy);;
   
-let diforchan_plaen kenver x=
-  forget_order(diforchan kenver  x);;
-let elfenn_plaen kenver e x=
-    elfenn kenver e (unsafe_set  x);;  
-let kengeij_plaen kenver x y=
-    forget_order(kengeij kenver  (unsafe_set x) (unsafe_set y) );;
-let lemel_plaen kenver x y=
-      forget_order(lemel kenver  (unsafe_set x) (unsafe_set y) );;
-let teuzin_kalz_plaen kenver l=
-        forget_order(big_teuzin kenver  (Image.image unsafe_set l) );;
-let insert_plaen kenver x l=
-        forget_order(insert kenver x (unsafe_set l));;     
-let diff_plaen (kenver: 'a Total_ordering.t) =
+let diforchan_plaen cmpr x=
+  forget_order(diforchan cmpr  x);;
+let elfenn_plaen cmpr e x=
+    elfenn cmpr e (unsafe_set  x);;  
+let kengeij_plaen cmpr x y=
+    forget_order(kengeij cmpr  (unsafe_set x) (unsafe_set y) );;
+let lemel_plaen cmpr x y=
+      forget_order(lemel cmpr  (unsafe_set x) (unsafe_set y) );;
+let teuzin_kalz_plaen cmpr l=
+        forget_order(big_teuzin cmpr  (Image.image unsafe_set l) );;
+let insert_plaen cmpr x l=
+        forget_order(insert cmpr x (unsafe_set l));;     
+let diff_plaen (cmpr: 'a Total_ordering.t) =
           let rec tempf=(fun
-            (graet_bc,graet_b,graet_c,da_ober1,da_ober2)->
-              match da_ober1 with
-              []->(graet_bc,graet_b,List.rev_append graet_c da_ober2)
-              |(a1,b1)::peurrest1->
+            (graet_bc,graet_b,graet_c,to_be_treated1,to_be_treated2)->
+              match to_be_treated1 with
+              []->(graet_bc,graet_b,List.rev_append graet_c to_be_treated2)
+              |(a1,b1)::others1->
               (
-                match da_ober2 with
-              []->(graet_bc,List.rev_append graet_b da_ober1,graet_c)     
-              |(a2,c2)::peurrest2->
+                match to_be_treated2 with
+              []->(graet_bc,List.rev_append graet_b to_be_treated1,graet_c)     
+              |(a2,c2)::others2->
                 (
-                  match kenver a1 a2 with
+                  match cmpr a1 a2 with
                   Total_ordering.Lower->
-                    tempf(graet_bc,(a1,b1)::graet_b,graet_c,peurrest1,da_ober2)
+                    tempf(graet_bc,(a1,b1)::graet_b,graet_c,others1,to_be_treated2)
                   |Total_ordering.Greater->
-                  tempf(graet_bc,graet_b,(a2,c2)::graet_c,da_ober1,peurrest2)
+                  tempf(graet_bc,graet_b,(a2,c2)::graet_c,to_be_treated1,others2)
                   |Total_ordering.Equal->
-                  tempf((a1,b1,c2)::graet_bc,graet_b,graet_c,peurrest1,peurrest2)  
+                  tempf((a1,b1,c2)::graet_bc,graet_b,graet_c,others1,others2)  
                 )
               )      
           ) in
