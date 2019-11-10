a0e149d7f224b3e6b6318cbe26be7f90ed7ce4e2
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-10 13:22:42 +0100

    rename Set_of_poly_pairs.diforchan as sort

--- Ordered_Lists/set_of_poly_pairs.ml
+++ Ordered_Lists/set_of_poly_pairs.ml
@@ -19,7 +19,7 @@ let kreskus x=Ordered.is_nondecreasing cmp x;;
 
 let elfenn=((fun a ox->Ordered.mem cmp a ox):>(('a*'b)->('a,'b) set->bool));;
 let teuzin=((fun ox oy->Ordered.merge cmp ox oy):>( ('a,'b) set->('a,'b) set->('a,'b) set));;
-let diforchan=((fun x->Ordered.sort cmp x):>(('a*'b) list->('a,'b) set));;
+let sort=((fun x->Ordered.sort cmp x):>(('a*'b) list->('a,'b) set));;
 let lemel=((fun ox oy->Ordered.setminus cmp ox oy):>(('a,'b) set->('a,'b) set->('a,'b) set));;
 let ental=((fun ox oy->Ordered.is_included_in cmp ox oy):>(('a,'b) set->('a,'b) set->bool));;
 let kengeij=((fun ox oy->Ordered.intersect cmp ox oy):>('a,'b) set->('a,'b) set->('a,'b) set);;
@@ -42,7 +42,7 @@ let nelfenn a ox=not(elfenn a ox);;
 let nental ox oy=not(ental ox oy);;
 
 let insert x oy=teuzin(singleton x) oy;;
-let safe_set x=if kreskus_strizh(x) then unsafe_set(x) else diforchan(x);;
+let safe_set x=if kreskus_strizh(x) then unsafe_set(x) else sort(x);;
 let outsert x oy=lemel(oy)(singleton x);;
 let delta_set ox oy=teuzin(lemel ox oy)(lemel oy ox);;
 let delta_distance ox oy=length(delta_set ox oy);;
