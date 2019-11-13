97531877abb2cc09307eb1bc32f31b92a2ab53b6
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-13 15:21:06 +0100

    add Hex_appendage.opt_of_string and powder

--- Hex_analysis/hex_appendage.ml
+++ Hex_analysis/hex_appendage.ml
@@ -2,6 +2,12 @@
 
 #use"Hex_analysis/hex_appendage.ml";;
 
-
 *)
 
+let opt_of_string text =
+   match Hex_eyed_claw.opt_of_string text with 
+    None -> None 
+   |Some(claw)->Some(Hex_appendage_t.Eyed_claw(claw));;
+
+let powder = function 
+   (Hex_appendage_t.Eyed_claw(claw)) -> Hex_eyed_claw.powder claw;;  
