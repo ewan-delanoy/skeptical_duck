0cf78f6c733fe1bf8619660801220d9fa8f9aee8
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-14 15:38:04 +0100

    add Hex_cardinal_direction.{of,to}_concrete_object

--- Hex_analysis/hex_cardinal_direction.ml
+++ Hex_analysis/hex_cardinal_direction.ml
@@ -12,7 +12,22 @@ let correspondences =
       'r', Hex_cardinal_direction_t.Right ;
       'u', Hex_cardinal_direction_t.Up ;
     ];;
+
+let salt = "Hex_"^"cardinal_direction_t.";;
+
+let crobj_correspondences = 
+    [
+       Hex_cardinal_direction_t.Down,  salt ^ "Down" ;
+       Hex_cardinal_direction_t.Left,  salt ^ "Left"  ;
+       Hex_cardinal_direction_t.Right, salt ^ "Right" ;
+       Hex_cardinal_direction_t.Up,    salt ^ "Up" ;
+    ];;
+
+
 end ;;
+
+let of_concrete_object = Concrete_object_field.unwrap_lonely_variant Private.crobj_correspondences;;
+
 
 let opt_of_char c =
    Option.find_and_stop (
@@ -23,3 +38,6 @@ let is_vertical = function
     Hex_cardinal_direction_t.Down 
    |Hex_cardinal_direction_t.Up -> true 
    | _ -> false ;;   
+
+let to_concrete_object = Concrete_object_field.wrap_lonely_variant Private.crobj_correspondences;;
+
