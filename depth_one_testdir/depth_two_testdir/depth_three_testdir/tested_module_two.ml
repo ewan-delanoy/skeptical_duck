03e11d0c5b7f6fc02a8ba76b15da9fb51a144880
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-14 09:14:03 +0100

    add some fixtures in test modules

--- depth_one_testdir/depth_two_testdir/depth_three_testdir/tested_module_two.ml
+++ depth_one_testdir/depth_two_testdir/depth_three_testdir/tested_module_two.ml
@@ -9,3 +9,7 @@ let f x y=
   let e=9+10 in
   let g=Tested_module_one.f 11 12 in
   e+g;;
+
+let an_imported_value = Tested_module_one.A_Submodule.a_value + 2;;  
+
+let snd_imported_value = Tested_module_one.a_deduced_value + 2;; 
