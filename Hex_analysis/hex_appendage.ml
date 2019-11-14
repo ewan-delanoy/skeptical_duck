f651265e6247032df721328f2eb4bf8f49d17b67
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-14 16:00:54 +0100

    add Hex_appendage.{of,to}_concrete_object

--- Hex_analysis/hex_appendage.ml
+++ Hex_analysis/hex_appendage.ml
@@ -4,6 +4,10 @@
 
 *)
 
+let of_concrete_object crobj=
+    let (_,(arg1,_,_,_,_,_,_))=Concrete_object_field.unwrap_bounded_variant crobj in 
+    Hex_appendage_t.Eyed_claw(Hex_eyed_claw.of_concrete_object arg1);;
+
 let opt_of_string text =
    match Hex_eyed_claw.opt_of_string text with 
     None -> None 
@@ -11,3 +15,8 @@ let opt_of_string text =
 
 let powder = function 
    (Hex_appendage_t.Eyed_claw(claw)) -> Hex_eyed_claw.powder claw;;  
+
+let to_concrete_object (Hex_appendage_t.Eyed_claw(claw))=
+   Concrete_object_t.Variant("Hex_"^"appendage_t.Eyed_claw",[
+      Hex_eyed_claw.to_concrete_object claw
+   ]);;   
