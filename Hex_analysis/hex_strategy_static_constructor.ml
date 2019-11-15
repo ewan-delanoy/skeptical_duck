776cc758f3d993853bb17508ff625f6650be4ee1
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-15 14:50:33 +0100

    use Hex_octopus in Hex_strategy_static_constructor

--- Hex_analysis/hex_strategy_static_constructor.ml
+++ Hex_analysis/hex_strategy_static_constructor.ml
@@ -5,7 +5,7 @@
 *)
 
 let summarize_in_string = function
-    Hex_strategy_static_constructor_t.Basic_Linker(_,_)->"Basic linker"
+    Hex_strategy_static_constructor_t.Basic_Linker(_,_,_)->"Basic linker"
    |Gluing -> "Gluing" 
    |Disjunction (_)->"Disjunction";;
    
@@ -18,11 +18,12 @@ let hook_for_disjunction  = salt ^ "Disjunction";;
 exception Of_concrete_object_exn of string;;
 
 let of_concrete_object crobj =
-    let (hook,(arg1,arg2,_,_,_,_,_))=Concrete_object_field.unwrap_bounded_variant crobj in 
+    let (hook,(arg1,arg2,arg3,_,_,_,_))=Concrete_object_field.unwrap_bounded_variant crobj in 
     if hook = hook_for_basic_linker 
     then Hex_strategy_static_constructor_t.Basic_Linker(
-           Hex_cell_set.of_concrete_object arg1,
-           Hex_cell_pair_set.of_concrete_object arg2) 
+           Hex_octopus.of_concrete_object arg1,
+           Hex_cell_set.of_concrete_object arg2,
+           Hex_cell_pair_set.of_concrete_object arg3) 
     else 
     if hook = hook_for_gluing
     then Hex_strategy_static_constructor_t.Gluing
@@ -34,9 +35,11 @@ let of_concrete_object crobj =
     else raise(Of_concrete_object_exn(hook));;
 
 let to_concrete_object = function
-    Hex_strategy_static_constructor_t.Basic_Linker(cells,pairs)->
+    Hex_strategy_static_constructor_t.Basic_Linker(octop,cells,pairs)->
         Concrete_object_t.Variant(hook_for_basic_linker,
-           [Hex_cell_set.to_concrete_object cells;Hex_cell_pair_set.to_concrete_object pairs])
+           [Hex_octopus.to_concrete_object octop;
+            Hex_cell_set.to_concrete_object cells;
+            Hex_cell_pair_set.to_concrete_object pairs])
    |Gluing -> Concrete_object_t.Variant(hook_for_gluing,[])
    |Disjunction (cells)->
             Concrete_object_t.Variant(hook_for_disjunction,
