4cd77d6e7014a97d5b57b66025933bcbd304459c
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-15 13:57:03 +0100

    add Hex_appendage.actives_and_passives

--- Hex_analysis/hex_appendage.ml
+++ Hex_analysis/hex_appendage.ml
@@ -4,6 +4,20 @@
 
 *)
 
+module Private = struct 
+
+let powder = function 
+   (Hex_appendage_t.Eyed_claw(claw)) -> Hex_eyed_claw.powder claw;;  
+
+end ;; 
+
+let actives_and_passives l=
+   let actives=Hex_cell_set.safe_set(Image.image (fun (adg,p)->Hex_ipair.to_cell p) l) in 
+   let temp1=List.flatten(Image.image (fun (adg,p)->Private.powder adg p) l) in 
+   let passives=Hex_cell_set.safe_set(Image.image Hex_ipair.to_cell temp1) in
+   (actives,passives);;
+
+
 let of_concrete_object crobj=
     let (_,(arg1,_,_,_,_,_,_))=Concrete_object_field.unwrap_bounded_variant crobj in 
     Hex_appendage_t.Eyed_claw(Hex_eyed_claw.of_concrete_object arg1);;
@@ -13,8 +27,7 @@ let opt_of_string text =
     None -> None 
    |Some(claw)->Some(Hex_appendage_t.Eyed_claw(claw));;
 
-let powder = function 
-   (Hex_appendage_t.Eyed_claw(claw)) -> Hex_eyed_claw.powder claw;;  
+let powder = Private.powder;;  
 
 let to_concrete_object (Hex_appendage_t.Eyed_claw(claw))=
    Concrete_object_t.Variant("Hex_"^"appendage_t.Eyed_claw",[
