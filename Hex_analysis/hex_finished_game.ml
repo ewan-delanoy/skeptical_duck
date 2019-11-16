4baf59230c495a65f2d35e1e3c5e48cbd8c2adc0
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-16 13:35:52 +0100

    add Hex_finished_game.seek_relevant_strategies

--- Hex_analysis/hex_finished_game.ml
+++ Hex_analysis/hex_finished_game.ml
@@ -12,6 +12,48 @@ let empty_one = {
    sequence_of_moves = [];
 };;
 
+let rec helper1_for_relevant_strategy_seeking (moves_before,player,moves_after,fles)=
+   if Hex_cell_set.length(fles.Hex_flattened_end_strategy_t.active_part)=1
+   then  (* Here the next player is forced to move in a certain set,
+         so the strategy is relevant indeed *)
+         Some(List.rev(moves_before)) 
+   else (
+          match moves_after with 
+          []->None 
+          |move::following_moves ->
+             (match Hex_flattened_end_strategy.use_move_to_simplify_one (player,move) fles with
+               None -> None 
+               |Some(new_fles) ->
+               helper1_for_relevant_strategy_seeking 
+                 (move::moves_before,Hex_player.other_player player,following_moves,new_fles) 
+             ) 
+        );;
+
+let rec helper2_for_relevant_strategy_seeking 
+   (seq_of_moves,already_found_relevants,flesses) = 
+    match flesses with 
+     []->(seq_of_moves,List.rev already_found_relevants)
+    |fles::others ->
+       (match helper1_for_relevant_strategy_seeking 
+        ([],Hex_player_t.First_player,seq_of_moves,fles) with 
+        None -> helper2_for_relevant_strategy_seeking 
+               (seq_of_moves,already_found_relevants,others)
+       |Some(shorter_seq)->
+             if shorter_seq = seq_of_moves 
+             then  helper2_for_relevant_strategy_seeking 
+                   (seq_of_moves,fles::already_found_relevants,others)
+             else  helper2_for_relevant_strategy_seeking 
+                   (shorter_seq,[fles],others)        
+       );;
+
+let seek_relevant_strategies fgame flesses = 
+    let (seq,relvs)= helper2_for_relevant_strategy_seeking 
+   (fgame.Hex_finished_game_t.sequence_of_moves,[],flesses)  in 
+   ({fgame with Hex_finished_game_t.sequence_of_moves=seq},relvs);;
+
+
+
+
 let compute_largest_unconclusive_beginning fgame fles =
    if Hex_cell_set.length(fles.Hex_flattened_end_strategy_t.active_part)<2
    then raise(End_strategy_is_too_fast(fles))
