59b1186795bbdbf0f5af212755af7e478331435e
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-16 14:36:22 +0100

    remove displaced functions in Hex_analysis

--- Hex_analysis/hex_analysis.ml
+++ Hex_analysis/hex_analysis.ml
@@ -95,21 +95,3 @@ let replay_and_declare_winner ()=
 let move_as_usual () =
    let cell = (snd(!walker)).Hex_analysis_result_t.usual_move in 
    absorb_move cell;; 
-
-let best_fits fles =
-  let (Hex_fg_double_list_t.DL(l1,l2))=(!(Hex_persistent.games_ref)) in 
-  let fgames = (match fles.Hex_flattened_end_strategy_t.beneficiary with 
-      Hex_player_t.First_player -> l1
-     |Hex_player_t.Second_player -> l2
-  ) in 
-  Hex_finished_game.best_fits fles fgames;;
-
-let relevancies ()=
-   Hex_fg_double_list.relevancies 
-    (!(Hex_persistent.games_ref)) 
-   (Hex_end_strategy_factory.compute_all_end_configs Hex_persistent.wes_pair);;
-
-let check_relevancies ()=
-   Hex_fg_double_list.check_relevancies 
-    (!(Hex_persistent.games_ref)) 
-   (Hex_end_strategy_factory.compute_all_end_configs Hex_persistent.wes_pair);;
