413a54c295ffbf9a74d9bef10b8d1bbabbac7f45
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-16 05:19:11 +0100

    add Hex_uog_list.compute_maximal_strong_openings

--- Hex_analysis/hex_uog_list.ml
+++ Hex_analysis/hex_uog_list.ml
@@ -10,18 +10,32 @@ module Private = struct
 let select_openings_with_next_player_as_recipient l =
    List.filter Hex_untamed_opening.has_odd_length l;;
 
-(*
-let compute_maximal_strong_openings (treated,to_be_treated)=
+let absorb_move move (moves_before,next_to_move,dfg)=
+  (move::moves_before,
+    Hex_player.other_player next_to_move, 
+     Hex_fg_double_list.simplify_by_move move dfg);;
+
+let rec compute_maximal_strong_openings (treated,to_be_treated)=
    match to_be_treated with 
-   []->List.rev treated 
-   |(moves_before,next_to_move,dfg)
-*)
+   []->Ordered.sort Hex_untamed_opening.cmp (List.rev treated) 
+   |triple :: others-> 
+      let (moves_before,next_to_move,dfg) = triple in 
+      let (Hex_cell_set_t.S new_pushes) = Hex_fg_double_list.first_moves next_to_move dfg in
+      if new_pushes = []
+      then let new_untamed = Hex_untamed_opening_t.O(List.rev(moves_before)) in 
+           compute_maximal_strong_openings 
+             (new_untamed::treated,others)    
+      else compute_maximal_strong_openings 
+             (treated, 
+               (Image.image (fun move -> absorb_move move triple) new_pushes) @ others);;
 
 end ;; 
 
+let compute_maximal_strong_openings dfg=
+  Private.compute_maximal_strong_openings 
+   ([],[[],Hex_player_t.First_player,dfg]);;
 
 let of_concrete_object crobj = Concrete_object_field.to_list Hex_untamed_opening.of_concrete_object crobj;;
-let to_concrete_object l = Concrete_object_field.of_list Hex_untamed_opening.to_concrete_object l;;
 
 
 let insert_in new_uog l=
@@ -33,12 +47,13 @@ let insert_in new_uog l=
      Ordered.insert Hex_untamed_opening.cmp new_uog cleaned_l;;  
 
 
-let simplify_by_move move l=Option.filter_and_unpack 
-(Hex_untamed_opening.simplify_by_move move) l;;     
-
 let seek_interesting_move l=
    let temp1=Private.select_openings_with_next_player_as_recipient l in 
    if temp1=[] then None else 
    let temp2=List.hd temp1 in 
    Some(Hex_untamed_opening.first_move temp2,List.tl(Hex_untamed_opening.unveil temp2));;
 
+let simplify_by_move move l=Option.filter_and_unpack 
+(Hex_untamed_opening.simplify_by_move move) l;;     
+
+let to_concrete_object l = Concrete_object_field.of_list Hex_untamed_opening.to_concrete_object l;;
