32757956b56c8d10601db5887ffdab6a19ba3237
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-17 13:48:36 +0100

    simplify Hex_persistent

--- Hex_analysis/hex_persistent.ml
+++ Hex_analysis/hex_persistent.ml
@@ -38,7 +38,7 @@ let snd_player_strat_at_idx k=Hex_end_strategy_factory.get_elt_at_idx
 let path_for_fp_strats = Absolute_path.of_string "Hex_analysis/hex_end_strategies_for_first_player.txt";;
 let path_for_sp_strats = Absolute_path.of_string "Hex_analysis/hex_end_strategies_for_second_player.txt";;
 let path_for_fgames =   Absolute_path.of_string "Hex_analysis/hex_finished_games.txt";;
-let path_for_openings = Absolute_path.of_string "Hex_analysis/hex_boomerangs.txt";;
+
 
 
 let persist_strategies ()=
@@ -51,8 +51,7 @@ let persist_strategies ()=
 let persist_games ()=
      Io.overwrite_with path_for_fgames (Crobj_parsing.unparse(Hex_fg_double_list.to_concrete_object (!games_ref)));;   
 
-let persist_openings ()=
-    Io.overwrite_with path_for_openings (Crobj_parsing.unparse(Hex_uog_list.to_concrete_object(!untamed_openings_ref)));;
+
 
 let reset_all_to_empty ()=
    let _=(
@@ -77,20 +76,23 @@ let initialize_all_data_if_necessary ()=
   if (!data_has_been_initialized_already)
   then ()
   else 
+    let new_games = (Hex_fg_double_list.of_concrete_object(Crobj_parsing.parse(Io.read_whole_file path_for_fgames))) in 
   (
     Hex_end_strategy_factory.fill_with_string (fst(wes_pair)) (Io.read_whole_file path_for_fp_strats);
     Hex_end_strategy_factory.fill_with_string (snd(wes_pair)) (Io.read_whole_file path_for_sp_strats);
-    games_ref:=(Hex_fg_double_list.of_concrete_object(Crobj_parsing.parse(Io.read_whole_file path_for_fgames)));
-    untamed_openings_ref:=(Hex_uog_list.of_concrete_object(Crobj_parsing.parse(Io.read_whole_file path_for_openings)));
+    games_ref:=new_games;
+    untamed_openings_ref:=(Hex_uog_list.extract_untamed_openings new_games);
     dimension_ref:=compute_dim_the_first_time();
     data_has_been_initialized_already:=true;
   );;
 
 
 let add_end_strategy_without_persisting (player,static_constructor,comment,indices) =
-   let ec = Hex_end_strategy_factory.create_new_strategy true wes_pair player static_constructor comment indices in 
+   let fles = Hex_end_strategy_factory.create_new_strategy true wes_pair player static_constructor comment indices in 
+   let new_games = Hex_fg_double_list.take_end_config_into_account fles (!games_ref) in 
    (
-    games_ref:=Hex_fg_double_list.take_end_config_into_account ec (!games_ref)
+    games_ref:=new_games;
+    untamed_openings_ref:=(Hex_uog_list.extract_untamed_openings new_games);
    );;
 
 
@@ -102,25 +104,23 @@ let add_finished_game_without_persisting fgame =
     let checked_fgame=Hex_fles_double_list.iterated_largest_unconclusive_beginning 
              fgame (Hex_end_strategy_factory.compute_all_end_configs wes_pair) in 
     let _=(last_game_added:=checked_fgame) in 
+    let new_games = Hex_fg_double_list.add_finished_game checked_fgame (!games_ref) in 
    (
-    games_ref:=Hex_fg_double_list.add_finished_game checked_fgame (!games_ref)
+    games_ref:=Hex_fg_double_list.add_finished_game checked_fgame (!games_ref);
+    untamed_openings_ref:=(Hex_uog_list.extract_untamed_openings new_games);
    );;
 
 let remove_last_finished_game_without_persisting () =
     let fgame=(!last_game_added) in 
+    let new_games = Hex_fg_double_list.remove_finished_game fgame (!games_ref) in 
    (
-    games_ref:=Hex_fg_double_list.remove_finished_game fgame (!games_ref)
+    games_ref:=Hex_fg_double_list.remove_finished_game fgame (!games_ref);
+    untamed_openings_ref:=(Hex_uog_list.extract_untamed_openings new_games);
    );;
 
-
-let add_untamed_opening_without_persisting opng=
-   let old_ones=(!untamed_openings_ref) in 
-   let new_ones = Hex_uog_list.insert_in opng old_ones in 
-   untamed_openings_ref := new_ones;;
-
-let add_end_strategy ec =
+let add_end_strategy fles =
    (
-     add_end_strategy_without_persisting ec;
+     add_end_strategy_without_persisting fles;
      persist_strategies();
      persist_games()
    );;
@@ -138,17 +138,10 @@ let remove_last_finished_game fgame =
    );;
 
 
-let add_untamed_opening opng=
-   (
-     add_untamed_opening_without_persisting opng;
-     persist_openings()
-   );;
-
 end ;;
 
 let add_end_strategy = Private.add_end_strategy;;
 let add_finished_game = Private.add_finished_game;;
-let add_untamed_opening = Private.add_untamed_opening;;
 let cancel_last_game = Private.remove_last_finished_game;;
 let dimension ()=(!(Private.dimension_ref));;
 let reset_all_to_empty = Private.reset_all_to_empty;;
