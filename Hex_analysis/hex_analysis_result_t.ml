7a1d8db10b698f9611d87ac3f4f33f749456cdf3
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-17 14:05:02 +0100

    change implementation of Hex_analysis_result_t

--- Hex_analysis/hex_analysis_result_t.ml
+++ Hex_analysis/hex_analysis_result_t.ml
@@ -8,11 +8,13 @@
 type t= {
    next_to_play : Hex_player_t.t ; 
    mandatory_set : Hex_cell_set_t.t option;
-   involved_end_strategies : int list;
-   easy_advancer : (Hex_cell_t.t * (Hex_cell_t.t list)) option;
-   strong_moves : Hex_cell_set_t.t ;
+   dangerous_enemy_strategies : int list;
+   strong_move : (Hex_cell_t.t * (Hex_cell_t.t list)) option;
+   familiar_moves : Hex_cell_set_t.t ;
    usual_move : Hex_cell_t.t ; 
    number_of_remaining_enemies : int ;
+   declared_partipant : Hex_player_t.t option;
+   info_needed : bool;
 };;
 
   
