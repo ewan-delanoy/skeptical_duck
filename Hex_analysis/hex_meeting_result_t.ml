6ab39b4d4d9415a7c6110348ce5d7350d13bf932
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-17 07:33:24 +0100

    add Incomplete field to Hex_meeting_result_t

--- Hex_analysis/hex_meeting_result_t.ml
+++ Hex_analysis/hex_meeting_result_t.ml
@@ -6,8 +6,11 @@ In the Relevance field, the first argument is the sequence of moves
 that reached the position , and the second argument is the single
 remaining unplayed active cell.
 
+In the Incomplete field, the argument is the set of remaining unplayed active cells.
+
 *)
 
 type t= 
    Separation of Hex_player_t.t * Hex_cell_t.t 
-  |Relevance of (Hex_cell_t.t list) * (Hex_cell_t.t) ;;
+  |Relevance of (Hex_cell_t.t list) * (Hex_cell_t.t) 
+  |Incomplete of Hex_cell_set_t.t;;
