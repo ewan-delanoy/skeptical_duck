046c216df8d7b23e5e85e2eb517c74768e0effb0
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-22 07:29:36 +0100

    change definition of Dfa_ending_t.t

--- Filewatching/fw_configuration_t.ml
+++ Filewatching/fw_configuration_t.ml
@@ -6,6 +6,7 @@
 
 type t ={
   root : Dfa_root_t.t ;
+  allowed_endings : Dfa_ending_t.t list;
   ignored_subdirectories : Dfa_subdirectory_t.t list;
-  ignored_files : string list;
+  special_files : Dfn_rootless_t.t list;
 };;
