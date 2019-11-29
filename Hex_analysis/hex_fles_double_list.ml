278c499c04859cb89c7aaea3b01436957e920e55
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-11-29 18:09:55 +0100

    improve Hex_fles_double_list.simplify_by_move

--- Hex_analysis/hex_fles_double_list.ml
+++ Hex_analysis/hex_fles_double_list.ml
@@ -6,9 +6,30 @@
 
 let empty_one = Hex_fles_double_list_t.DL([],[]);;
 
-exception Nail_in_coffin of Hex_player_t.t * Hex_cell_t.t * int ;; 
+let immediate_dangers player (Hex_fles_double_list_t.DL(l1,l2))=
+  match player with 
+   Hex_player_t.First_player -> 
+      Hex_flattened_end_strategy.immediate_opportunities l2
+  |Hex_player_t.Second_player -> 
+      Hex_flattened_end_strategy.immediate_opportunities l1;; 
+    
 
-let simplify_by_move (player,cell) (Hex_fles_double_list_t.DL(l1,l2))=
+let test_for_non_compliance (player,cell) dl=
+   match immediate_dangers player dl with 
+   []->None
+   |dangers -> let mandatory_set = Hex_cell_set.fold_intersect (Image.image fst dangers) in 
+               if not(Hex_cell_set.mem cell mandatory_set) 
+               then Some(Image.image snd dangers)
+               else None;;
+
+
+exception Suicide of Hex_player_t.t * Hex_cell_t.t * (int list) ;; 
+
+let simplify_by_move (player,cell) dl=
+  match test_for_non_compliance (player,cell) dl with 
+  Some(killers)->raise(Suicide(player,cell,killers))
+  |None->
+  let  (Hex_fles_double_list_t.DL(l1,l2))= dl in 
   let (new_l1,new_l2)=
   (match player with 
    Hex_player_t.First_player -> 
@@ -18,27 +39,8 @@ let simplify_by_move (player,cell) (Hex_fles_double_list_t.DL(l1,l2))=
       (Hex_flattened_end_strategy.use_enemy_move_to_simplify_several cell l1,
        Hex_flattened_end_strategy.use_ally_move_to_simplify_several cell l2)
      ) in  
-  let new_ally_l=(
-     if player = Hex_player_t.First_player
-     then new_l1
-     else new_l2
-  )  in 
-  match Option.seek Hex_flattened_end_strategy.is_fulfilled new_ally_l with 
-   Some(fles)-> raise(Nail_in_coffin(player,cell,fles.Hex_flattened_end_strategy_t.index))
-  |None -> Hex_fles_double_list_t.DL(new_l1,new_l2)      ;; 
+  Hex_fles_double_list_t.DL(new_l1,new_l2)      ;; 
     
-let immediate_dangers player (Hex_fles_double_list_t.DL(l1,l2))=
-  match player with 
-   Hex_player_t.First_player -> 
-      Hex_flattened_end_strategy.immediate_opportunities l2
-  |Hex_player_t.Second_player -> 
-      Hex_flattened_end_strategy.immediate_opportunities l1;; 
-    
-let mandatory_set player dl = (
-     match immediate_dangers player dl with 
-     []->None
-     |dangers->Some(Hex_cell_set.fold_intersect (Image.image fst dangers))
-  ) ;;
 
 let iterated_largest_unconclusive_beginning fgame (Hex_fles_double_list_t.DL(l1,l2))=
     Hex_finished_game.largest_unconclusive_beginning fgame (l1@l2);;
