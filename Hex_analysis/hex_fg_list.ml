b794dfb8b94105491756adb075f5a93d66fd797d
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-12-01 10:17:14 +0100

    change definition of Hex_meeting_result_t.t (5th try, updating Hex_fg_list)

--- Hex_analysis/hex_fg_list.ml
+++ Hex_analysis/hex_fg_list.ml
@@ -31,7 +31,7 @@ let remove_finished_game fgame l=
 
 let take_new_end_strategy_into_account end_config l=
     let temp1=Image.image 
-    (fun fgame->Hex_finished_game.largest_unconclusive_beginning fgame [end_config]) l in 
+    (fun fgame->Hex_finished_game.largest_nonsurrendering_beginning fgame [end_config]) l in 
     remove_redundancies temp1;;
 
 let simplify_by_move move l=Option.filter_and_unpack (Hex_finished_game.simplify_by_move move) l;;     
@@ -43,29 +43,7 @@ let lookup_dimension =function
     []->None
    |fgame::_->Some(fgame.Hex_finished_game_t.dimension);;
 
-let relevancies fgames flesses=
-  let for_games=Image.image 
-    (fun fgame->(fgame,Hex_finished_game.seek_relevant_strategies fgame flesses) ) fgames in 
-  let tempf=(fun fles -> 
-      (fles,Option.filter_and_unpack (
-        fun (_,(fgame,flesses2))->
-         if List.mem fles flesses2 
-         then Some(fgame)
-         else None
-     ) for_games) 
-   ) in 
-   let for_strats = Image.image tempf flesses in 
-   (for_games,for_strats);;
 
-let check_relevancies  fgames flesses=
-  let (for_games,for_strats)=relevancies fgames flesses in 
-  let bad_games1 = Option.filter_and_unpack(fun (fgame1,(fgame2,_))->
-     if fgame1<>fgame2 then Some(fgame1,fgame2) else None) for_games 
-  and bad_games2 = Option.filter_and_unpack(fun (_,(fgame,l))->
-    if l=[] then Some(fgame) else None) for_games
-  and bad_strats= Option.filter_and_unpack(fun (fles,l)->
-    if l=[] then Some(fles) else None) for_strats in 
-  (bad_games1,bad_games2,bad_strats);;
 
 
 
