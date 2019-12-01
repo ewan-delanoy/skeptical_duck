2a3f1a29d11f2fea92e855a5a13a1c9836309ee3
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-12-01 08:55:08 +0100

    change definition of Hex_meeting_result_t.t

--- Hex_analysis/hex_meeting_result_t.ml
+++ Hex_analysis/hex_meeting_result_t.ml
@@ -2,15 +2,19 @@
 
 #use"Hex_analysis/hex_meeting_result_t.ml";;
 
-In the Relevance field, the first argument is the sequence of moves
+In the Attack_but_no_surrender variant, the first argument is the sequence of moves
 that reached the position , and the second argument is the single
 remaining unplayed active cell.
 
-In the Incomplete field, the argument is the set of remaining unplayed active cells.
+In the Surrender variant, the first two argument are as in the Attack_but_no_surrender variant
+and the third is the "surrendering" move.
+
+In the Stalemate variant, the argument is the set of remaining unplayed active cells.
 
 *)
 
 type t= 
-   Separation of Hex_player_t.t * Hex_cell_t.t 
-  |Relevance of (Hex_cell_t.t list) * (Hex_cell_t.t) 
-  |Incomplete of Hex_cell_set_t.t;;
+   Victory of Hex_player_t.t * Hex_cell_t.t 
+  |Attack_but_no_surrender of (Hex_cell_t.t list) * (Hex_cell_t.t) 
+  |Surrender of (Hex_cell_t.t list) * (Hex_cell_t.t) * (Hex_cell_t.t)  
+  |Stalemate of Hex_cell_set_t.t;;
