5a6defd6c5fe6ebee7ea20b2cf0a7783a2f79377
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-12-04 09:19:44 +0100

    change implementation of Fw_final_cleaner.clean

--- Filewatching/fw_final_cleaner.ml
+++ Filewatching/fw_final_cleaner.ml
@@ -2,17 +2,18 @@
 
 #use"Filewatching/fw_final_cleaner.ml";;
 
-
-
 *)
 
-let test cleaner untested_l candidate=
+let clean cleaner l_to_be_cleaned=
     let pairs = cleaner.Fw_final_cleaner_t.redundant_dependencies in 
-    let (Dfn_rootless_t.J(s,m,e),_,_) = candidate in 
-    match Option.seek (fun (has_priority,does_not_have_priority)->has_priority=e) pairs with 
-     None -> untested_l 
-    |Some(_,does_not_have_priority)-> 
-        let excluded_one = Dfn_rootless_t.J(s,m,does_not_have_priority) in 
-        List.filter (
-          fun (rootless,_,_) ->rootless <> excluded_one
-        ) untested_l;; 
+    let l_to_be_excluded = Option.filter_and_unpack (fun 
+       (Dfn_rootless_t.J(s,m,e))->
+        match Option.seek (fun (has_priority,_)->has_priority=e) pairs with 
+         None -> None 
+        |Some(_,does_not_have_priority)-> 
+           Some(Dfn_rootless_t.J(s,m,does_not_have_priority))
+    ) l_to_be_cleaned in
+    let to_be_excluded = Set_of_polys.sort(l_to_be_excluded) in 
+    List.filter (fun x->Set_of_polys.nmem x to_be_excluded) l_to_be_cleaned ;; 
+
+    
