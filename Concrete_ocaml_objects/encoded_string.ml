92c5553a5b1450f07a1aa7f7f8d6fbb918899c91
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-12-23 09:30:57 +0100

    register Concrete_ocaml_objects/encoded_string.ml

---
+++ Concrete_ocaml_objects/encoded_string.ml
@@ -0,0 +1,25 @@
+(* 
+
+#use"Concrete_ocaml_objects/encoded_string.ml";;
+
+
+*)
+
+exception Forbidden_substring;;
+exception Forbidden_recombination;;
+
+let salt = 
+  String.concat "" ["a"; "Y"; "2"; "u"; "k"; "k"; "w"; "D"; "z"; "y"; "K"; "d"];;
+let replacement_salt = 
+  String.concat "" ["b"; "Z"; "3"; "v"; "l"; "m"; "x"; "E"; "A"; "z"; "L"; "e"];;
+
+let encode s=
+   if Substring.is_a_substring_of replacement_salt s 
+   then raise(Forbidden_substring)
+   else let encoded_s=Replace_inside.replace_inside_string (salt,replacement_salt) s in 
+        if  Substring.is_a_substring_of salt encoded_s 
+        then raise(Forbidden_substring)
+        else Encoded_string_t.E(encoded_s);;
+
+let decode (Encoded_string_t.E(encoded_s))=
+   Replace_inside.replace_inside_string (replacement_salt,salt) encoded_s;;
