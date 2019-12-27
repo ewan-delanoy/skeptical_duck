09e50d644084bac9117039e12729069a4070d0e0
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-12-27 10:33:42 +0100

    register Hex_analysis/hex_old_strategy_static_constructor_t.ml

---
+++ Hex_analysis/hex_old_strategy_static_constructor_t.ml
@@ -0,0 +1,12 @@
+(* 
+
+#use"Hex_analysis/hex_old_strategy_static_constructor_t.ml";;
+
+*)
+
+type t= 
+    Basic_Linker of  Hex_octopus_t.t *  Hex_cell_set_t.t * Hex_cell_pair_set_t.t
+   |Gluing 
+   |Disjunction of Hex_cell_t.t  list;;
+   
+   
