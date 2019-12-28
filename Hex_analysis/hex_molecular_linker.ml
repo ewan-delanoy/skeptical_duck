b20e6d8e2000fd26ddd2e11985a8c44ede542e83
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-12-28 14:24:32 +0100

    add Hex_molecular_linker.setminus,is_included_in

--- Hex_analysis/hex_molecular_linker.ml
+++ Hex_analysis/hex_molecular_linker.ml
@@ -11,6 +11,8 @@ module Private = struct
 
 let tr = ((fun x->Hex_molecular_linker_t.M(x)),
           (fun (Hex_molecular_linker_t.M(x))->x),Hex_atomic_linker.cmp);;
+
+
 end;;
 
 
@@ -18,11 +20,6 @@ let active_part (Hex_molecular_linker_t.M l)=
     Hex_cell_set.fold_merge (Image.image Hex_atomic_linker.active_part l);;    
    
 
-let of_concrete_object crobj=
-   let (_,(arg1,_,_,_,_,_,_))=Concrete_object_field.unwrap_bounded_variant crobj in 
-   Hex_molecular_linker_t.M(
-      Concrete_object_field.to_list Hex_atomic_linker.of_concrete_object arg1
-   );;
 
 let constructor unordered_l= 
   let l= Ordered.sort Hex_atomic_linker.cmp unordered_l in 
@@ -34,8 +31,19 @@ let constructor unordered_l=
   None -> Hex_molecular_linker_t.M(l)
   |Some((atl1,supp1),(atl2,supp2))->raise(Nondisjoint_atoms(atl1,atl2));;
 
+let is_included_in = Functor_for_sets.is_included_in Private.tr ;;
+
+let of_concrete_object crobj=
+   let (_,(arg1,_,_,_,_,_,_))=Concrete_object_field.unwrap_bounded_variant crobj in 
+   Hex_molecular_linker_t.M(
+      Concrete_object_field.to_list Hex_atomic_linker.of_concrete_object arg1
+   );;
+
+
 let passive_part (Hex_molecular_linker_t.M(l))= 
    Hex_cell_set.fold_merge (Image.image Hex_atomic_linker.passive_part l);;
+
+let setminus = Functor_for_sets.setminus Private.tr ;;
 
 let to_concrete_object (Hex_molecular_linker_t.M(l))=
    Concrete_object_t.Variant("Hex_"^"molecular_linker_t.M",
