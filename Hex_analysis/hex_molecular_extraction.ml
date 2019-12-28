e30b2d45360ce5a94fb9a94ee018ec750482a970
Ewan Delanoy <ewan.delanoy@zoho.com>
2019-12-28 18:16:06 +0100

    register Hex_analysis/hex_molecular_extraction.ml

---
+++ Hex_analysis/hex_molecular_extraction.ml
@@ -0,0 +1,28 @@
+(* 
+
+#use"Hex_analysis/hex_molecular_extraction.ml";;
+
+*)
+
+type t= E of Hex_molecular_linker_t.t * Hex_cell_set_t.t ;;
+  
+let extract_from fles mlclr =
+    let remaining_ones =
+      Hex_cell_set.setminus 
+        (fles.Hex_flattened_end_strategy_t.passive_part) 
+          (Hex_molecular_linker.passive_part mlclr) in 
+    Hex_molecular_extraction_t.E(mlclr,remaining_ones);;
+
+let of_concrete_object crobj= 
+   let (arg1,arg2,_,_,_,_,_)=Concrete_object_field.unwrap_bounded_uple crobj in 
+   Hex_molecular_extraction_t.E
+   (
+    Hex_molecular_linker.of_concrete_object arg1,
+    Hex_cell_set.of_concrete_object(arg2)
+   );;
+
+let to_concrete_object (Hex_molecular_extraction_t.E(mlclr,remaining_ones)) =
+   Concrete_object_t.Uple [
+      Hex_molecular_linker.to_concrete_object mlclr;
+      Hex_cell_set.to_concrete_object(remaining_ones)
+   ]  ;;
---
+++ Hex_analysis/hex_molecular_extraction_t.ml
@@ -0,0 +1,8 @@
+(* 
+
+#use"Hex_analysis/hex_molecular_extraction_t.ml";;
+
+*)
+
+type t= E of Hex_molecular_linker_t.t * Hex_cell_set_t.t ;;
+  
