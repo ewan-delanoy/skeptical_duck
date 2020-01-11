ec352e808e08355be87128b4b7ad0972427e0dfa
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-01-11 06:46:15 +0100

    use Hex_flattened_end_strategy_field in Hex_molecular_extraction.extract_from_strategy

--- Hex_analysis/hex_molecular_extraction.ml
+++ Hex_analysis/hex_molecular_extraction.ml
@@ -34,7 +34,7 @@ let apply_to_immediate_dangers l=
       Private.extract_from_set common_part mandatory_set ;; 
 
 let extract_from_strategy  mlclr fles =
-    Private.extract_from_set mlclr (fles.Hex_flattened_end_strategy_t.passive_part);;
+    Private.extract_from_set mlclr (Hex_flattened_end_strategy_field.passive_part fles);;
 
 let of_concrete_object crobj= 
    let (arg1,arg2,_,_,_,_,_)=Concrete_object_field.unwrap_bounded_uple crobj in 
