1a723876cde46d8802ad0984212d52e550ac5480
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-01-12 10:11:11 +0100

    add Hex_extended_molecular.{use_ally_move_to_simplify_one,withstands_enemy_move}

--- Hex_analysis/hex_extended_molecular.ml
+++ Hex_analysis/hex_extended_molecular.ml
@@ -11,15 +11,20 @@ let passive_part extmol =
        (Hex_molecular_linker.support extmol.Hex_extended_molecular_t.molecular_part)
          extmol.Hex_extended_molecular_t.nonmolecular_passive_part;;
 
-(*
+
 let use_ally_move_to_simplify_one cell extmol =
    {
-      Hex_extended_molecular_t.molecular_part = Hex_molecular_linker.of_concrete_object (g molecular_part_label);
-      nonmolecular_passive_part = Hex_cell_set.of_concrete_object (g nonmolecular_passive_part_label);
-      active_part = Hex_cell_set.of_concrete_object (g active_part_label);
+      Hex_extended_molecular_t.molecular_part = Hex_molecular_linker.use_ally_move_to_simplify_one cell (extmol.Hex_extended_molecular_t.molecular_part);
+      nonmolecular_passive_part = Hex_cell_set.outsert cell (extmol.Hex_extended_molecular_t.nonmolecular_passive_part);
+      active_part = Hex_cell_set.outsert cell (extmol.Hex_extended_molecular_t.active_part);
    };;
-*)   
-
+   
+let withstands_enemy_move cell extmol = 
+    (Hex_molecular_linker.withstands_enemy_move cell extmol.Hex_extended_molecular_t.molecular_part)
+    &&
+    (not(Hex_cell_set.mem cell extmol.Hex_extended_molecular_t.nonmolecular_passive_part))
+    &&
+    (not(Hex_cell_set.mem cell extmol.Hex_extended_molecular_t.active_part));;    
 
 let salt = "Hex_"^"extended_molecular_t.";;
 
@@ -50,3 +55,5 @@ let active_part extmol = extmol.Hex_extended_molecular_t.active_part;;
 let of_concrete_object = Private.of_concrete_object;;
 let passive_part = Private.passive_part ;; 
 let to_concrete_object = Private.to_concrete_object;;
+let use_ally_move_to_simplify_one = Private.use_ally_move_to_simplify_one;;
+let withstands_enemy_move = Private.withstands_enemy_move;;
