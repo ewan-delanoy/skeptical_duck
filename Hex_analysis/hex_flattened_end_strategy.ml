5ad5dddecba80d70ae64e02145eb942e0785f04f
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-01-13 12:51:31 +0100

    invisible change in Hex_flattened_end_strategy

--- Hex_analysis/hex_flattened_end_strategy.ml
+++ Hex_analysis/hex_flattened_end_strategy.ml
@@ -45,6 +45,24 @@ let immediate_opportunities flesses =
          else None
    ) flesses;;
 
+let neuop flesses =
+   let temp1 = Option.filter_and_unpack (
+       fun fles->
+         let l=Hex_flattened_end_strategy_field.active_part fles in 
+         if Hex_cell_set.length(l)=1 
+         then let passive_set = Hex_flattened_end_strategy_field.passive_part fles in 
+              let mandatory_set=Hex_cell_set.insert (Hex_cell_set.min l) passive_set in 
+               Some(fles,mandatory_set)
+         else None
+   ) flesses in 
+   let interesting_indices = Image.image (fun (fles,_)->Hex_flattened_end_strategy_field.index fles) temp1 in 
+   let temp2 = Image.image (fun (fles,_)->fles.Hex_flattened_end_strategy_t.data) temp1 in
+   let main = Hex_extended_molecular.disjunction temp2 in  
+   let condition =(if temp1=[] then None else 
+     Some(Hex_cell_set.fold_intersect (Image.image snd temp1))
+   ) in 
+   (interesting_indices,main,condition);;
+
 let support fles =
    Hex_cell_set.fold_merge
    [Hex_flattened_end_strategy_field.active_part fles;
