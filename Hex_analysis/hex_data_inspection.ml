e1a5736ead0fb929ab3385a89dbc902faebfdbc0
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-01-19 15:28:22 +0100

    fix error in Hex_data_inspection.check_companions

--- Hex_analysis/hex_data_inspection.ml
+++ Hex_analysis/hex_data_inspection.ml
@@ -4,15 +4,16 @@
 
 *)
 
-
+(*
 let seek_companions ()=
    let _=Hex_persistent.initialize_all_data_if_necessary () in 
    Hex_fg_double_list.seek_companions 
     (!(Hex_persistent.games_ref)) 
-   (Hex_end_strategy_factory.compute_all_end_configs Hex_persistent.wes_pair);;
+   (Hex_end_strategy_factory.compute_isolated_end_configs Hex_persistent.wes_pair);;
+*)
 
 let check_companions ()=
    let _=Hex_persistent.initialize_all_data_if_necessary () in 
    Hex_fg_double_list.check_companions
     (!(Hex_persistent.games_ref)) 
-   (Hex_end_strategy_factory.compute_all_end_configs Hex_persistent.wes_pair);;
+   (Hex_end_strategy_factory.compute_isolated_end_configs Hex_persistent.wes_pair);;
