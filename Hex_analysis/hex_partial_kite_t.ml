0987c938c8040043798c88995b1a1c49c1499a66
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-01-21 16:06:10 +0100

    add direction in definition of Hex_partial_kite_t.t

--- Hex_analysis/hex_partial_kite_t.ml
+++ Hex_analysis/hex_partial_kite_t.ml
@@ -2,8 +2,9 @@
 
 #use"Hex_analysis/hex_partial_kite_t.ml";;
 
-The second argument is the set of cells involved in the kite 
+The second argument is the set of cells involved in the kite.
+The third argument says on which side of the board the kite started.
 
 *)
 
-type t= P of Hex_kite_element_t.t list * Hex_cell_set_t.t ;;
+type t= P of Hex_kite_element_t.t list * Hex_cell_set_t.t * Hex_cardinal_direction_t.t ;;
