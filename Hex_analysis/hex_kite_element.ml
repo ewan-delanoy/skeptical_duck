8022d9a3f7511db248f730ffe4571ca8286223aa
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-01-21 15:00:10 +0100

    add Hex_kite_element.support

--- Hex_analysis/hex_kite_element.ml
+++ Hex_analysis/hex_kite_element.ml
@@ -60,3 +60,6 @@ neighbors Hex_dimension.eleven (bridge(Hex_cell.of_string "e8",Hex_cell.of_strin
 neighbors Hex_dimension.eleven (bridge(Hex_cell.of_string "f7",Hex_cell.of_string "f8"));; 
 *)
 
+let support = function 
+    Hex_kite_element_t.Active_cell(cell)->Hex_cell_set_t.S[cell]
+                    |Bridge(cell1,cell2)->Hex_cell_set.safe_set [cell1;cell2];;
--- Hex_analysis/hex_partial_kite_t.ml
+++ Hex_analysis/hex_partial_kite_t.ml
@@ -2,6 +2,8 @@
 
 #use"Hex_analysis/hex_partial_kite_t.ml";;
 
+The second argument is the set of cells involved in the kite 
+
 *)
 
-type t= P of Hex_kite_element_t.t list;;
+type t= P of Hex_kite_element_t.t list * Hex_cell_set_t.t ;;
