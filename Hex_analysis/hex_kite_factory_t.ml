0aec75946d75269ad0b57f079e582ae68ead2eb6
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-01-22 14:16:10 +0100

    register Hex_analysis/hex_kite_factory_t.ml

---
+++ Hex_analysis/hex_kite_factory_t.ml
@@ -0,0 +1,13 @@
+(* 
+
+#use"Hex_analysis/hex_kite_factory_t.ml";;
+
+*)
+
+type t= {
+      dimension     : Hex_dimension_t.t ;
+      winner        : Hex_player_t.t ;
+      starting_side : Hex_cardinal_direction_t.t ;
+      finished      : Hex_molecular_linker_t.t list ;
+      unfinished    : Hex_partial_kite_t.t list;
+};;
