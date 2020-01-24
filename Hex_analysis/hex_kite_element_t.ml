2c10c6451a6e86abdb86a800faa7a59a5654133f
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-01-24 14:28:05 +0100

    (temporarily) add increment_t and new_whole_t types in Hex_kite_element_t as a convenience

--- Hex_analysis/hex_kite_element_t.ml
+++ Hex_analysis/hex_kite_element_t.ml
@@ -8,3 +8,12 @@ type t=
    Active_cell of Hex_cell_t.t 
   |Bridge of Hex_cell_t.t * Hex_cell_t.t (* the pair is in Hex_cell.cmp order *)
   ;;
+
+type increment_t =
+    Eyed_claw of Hex_cardinal_direction_t.t * Hex_cardinal_direction_t.t 
+   |Noneyed_claw of Hex_double_hump_qualifier_t.t *  Hex_cardinal_direction_t.t
+   |Pyramid of  Hex_cardinal_direction_t.t ;;
+
+type new_whole_t =
+    Old of t 
+   |New of increment_t;;   
