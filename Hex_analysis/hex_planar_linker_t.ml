0800a38863cc7b8a4dfeea415dde2ed1d2116b10
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-01-27 11:38:03 +0100

    register Hex_analysis/hex_planar_linker_t.ml

---
+++ Hex_analysis/hex_planar_linker_t.ml
@@ -0,0 +1,14 @@
+(* 
+
+#use"Hex_analysis/hex_planer_linker_t.ml";;
+
+Static constructors for Hex_planar_linker_data_t.t objects 
+
+*)
+
+type t= 
+    Eyed_claw of Hex_cardinal_direction_t.t * Hex_cardinal_direction_t.t 
+   |Noneyed_claw of Hex_double_hump_qualifier_t.t *  Hex_cardinal_direction_t.t 
+   |Pyramid of  Hex_cardinal_direction_t.t  ;;
+
+
