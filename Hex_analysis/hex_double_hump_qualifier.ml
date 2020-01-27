dd2008e8597df3563c0c3c98998e4234fceb1359
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-01-27 16:26:09 +0100

    add Hex_double_hump_qualifier.print_out

--- Hex_analysis/hex_double_hump_qualifier.ml
+++ Hex_analysis/hex_double_hump_qualifier.ml
@@ -4,7 +4,21 @@
 
 *)
 
+module Private = struct 
+
+let to_readable_string = function 
+     Hex_double_hump_qualifier_t.Big_followed_by_small -> "bs"
+    |Small_followed_by_big -> "sb" ;;    
+
+end ;; 
+
+let print_out (fmt:Format.formatter) dh=
+   Format.fprintf fmt "@[%s@]" (Private.to_readable_string dh);;     
+
+
 let to_int = function 
     (* the correspondance is arbitrary, it just needs to be bijective. *)
-     Hex_double_hump_qualifier_t.Small_followed_by_big -> 1
-    |Big_followed_by_small -> 2 ;;
+     Hex_double_hump_qualifier_t.Big_followed_by_small -> 1
+    |Small_followed_by_big -> 2 ;;
+
+let to_readable_string = Private.to_readable_string;;  
