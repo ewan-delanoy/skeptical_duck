413bdc53a83aa93731feb2dd4e1dee67e7fcd05f
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-01-27 16:05:49 +0100

    remove temporary Hex_kite_element_t.increment_t type

--- Hex_analysis/hex_kite_element_t.ml
+++ Hex_analysis/hex_kite_element_t.ml
@@ -7,7 +7,5 @@
 type t= 
    Active_cell of Hex_cell_t.t 
   |Bridge of Hex_cell_t.t * Hex_cell_t.t (* the pair is in Hex_cell.cmp order *)
-  ;;
-
-type increment_t = Planar of Hex_planar_linker_t.t * Hex_cell_t.t;;
+  |Planar of Hex_planar_linker_t.t * Hex_cell_t.t;;
 
