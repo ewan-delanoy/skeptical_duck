fb7a96f7393ea4629261ea2c3f6ac9e199a8b357
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-01-28 06:03:00 +0100

    add small_pyramid stuff in Hex_planar_linker

--- Hex_analysis/hex_planar_linker.ml
+++ Hex_analysis/hex_planar_linker.ml
@@ -13,26 +13,30 @@ let to_readable_string = function
          (Hex_cardinal_direction.for_eye_description d1)^"e"^(Hex_cardinal_direction.for_ground_description d2)
    |Noneyed_claw(dh,d) -> let s = (Hex_double_hump_qualifier.to_readable_string dh) in 
                           (String.sub s 0 1)^(Hex_cardinal_direction.for_ground_description d)^(String.sub s 1 1)
-   |Pyramid(d) ->  (Hex_cardinal_direction.for_ground_description d)^"py" ;;
+   |Pyramid(d) ->  (Hex_cardinal_direction.for_ground_description d)^"py" 
+   |Small_pyramid(d) ->  (Hex_cardinal_direction.for_ground_description d)^"sy" ;;
 
 end ;;
 
 let check dim pllk cell = match pllk with 
     Hex_planar_linker_t.Eyed_claw(d1,d2) -> Hex_planar_linker_data.check_eyed_claw dim d1 d2 cell  
    |Noneyed_claw(double_hump,d) -> Hex_planar_linker_data.check_noneyed_claw dim double_hump d cell
-   |Pyramid(d) -> Hex_planar_linker_data.check_pyramid dim d cell ;;
+   |Pyramid(d) -> Hex_planar_linker_data.check_pyramid dim d cell 
+   |Small_pyramid(d) -> Hex_planar_linker_data.check_small_pyramid dim d cell ;;
 
 let ground pllk = 
     match pllk with 
     Hex_planar_linker_t.Eyed_claw(d1,d2) -> d2 
    |Noneyed_claw(double_hump,d) -> d
-   |Pyramid(d) -> d ;;
+   |Pyramid(d) -> d 
+   |Small_pyramid(d) -> d;;
 
 
 let level = function 
     Hex_planar_linker_t.Eyed_claw(d1,d2) -> 4*((Hex_cardinal_direction.to_int d1)-1)+(Hex_cardinal_direction.to_int d2)  
    |Noneyed_claw(dh,d) ->16+4*((Hex_double_hump_qualifier.to_int dh)-1)+(Hex_cardinal_direction.to_int d)
-   |Pyramid(d) -> 24 + (Hex_cardinal_direction.to_int d) ;;
+   |Pyramid(d) -> 24 + (Hex_cardinal_direction.to_int d) 
+   |Small_pyramid(d) -> 28 + (Hex_cardinal_direction.to_int d) ;;
 
 let print_out (fmt:Format.formatter) pllk=
    Format.fprintf fmt "@[%s@]" (Private.to_readable_string pllk);;     
@@ -41,7 +45,8 @@ let print_out (fmt:Format.formatter) pllk=
 let support pllk cell = match pllk with 
     Hex_planar_linker_t.Eyed_claw(d1,d2) -> Hex_planar_linker_data.support_for_eyed_claw d1 d2 cell  
    |Noneyed_claw(double_hump,d) -> Hex_planar_linker_data.support_for_noneyed_claw double_hump d cell
-   |Pyramid(d) -> Hex_planar_linker_data.support_for_pyramid d cell ;;
+   |Pyramid(d) -> Hex_planar_linker_data.support_for_pyramid d cell 
+   |Small_pyramid(d) -> Hex_planar_linker_data.support_for_small_pyramid d cell ;;
 
 let to_molecular_linker pllk cell = 
    let ipair = Hex_cell.to_int_pair cell in 
@@ -50,7 +55,8 @@ let to_molecular_linker pllk cell =
     Hex_planar_linker_t.Eyed_claw(d1,d2) -> 
       (Some(Hex_molecular_linker_t.M[Hex_atomic_linker_t.Eyed_claw(d1,d2,cell)]),[])   
    |Noneyed_claw(dh,d) -> (None, Hex_planar_linker_data.bridges_in_noneyed_claw dh d ipair )
-   |Pyramid(d) -> (None, Hex_planar_linker_data.bridges_in_pyramid d ipair ) ) in 
+   |Pyramid(d) -> (None, Hex_planar_linker_data.bridges_in_pyramid d ipair ) 
+   |Small_pyramid(d) -> (None, Hex_planar_linker_data.bridges_in_small_pyramid d ipair ) ) in 
    if first_trial<>None 
    then Option.unpack first_trial
    else 
@@ -72,7 +78,10 @@ let unfold_all_around_cell dim cell=
    and part3=Image.image (
       fun d  -> Hex_planar_linker_t.Pyramid d
    ) (Hex_planar_linker_data.unfold_pyramids_around_cell dim cell) in 
-   part1@part2@part3;;
+   and part4=Image.image (
+      fun d  -> Hex_planar_linker_t.Small_pyramid d
+   ) (Hex_planar_linker_data.unfold_small_pyramids_around_cell dim cell) in 
+   part1@part2@part3@part4;;
 
 let unfold_all_around_side dim side=
    let part1=Image.image (
@@ -84,5 +93,8 @@ let unfold_all_around_side dim side=
    and part3=Image.image (
       fun (d,cell)  -> (Hex_planar_linker_t.Pyramid d,cell)
    ) (Hex_planar_linker_data.unfold_pyramids_around_side dim side) in 
-   part1@part2@part3;;
+   and part4=Image.image (
+      fun (d,cell)  -> (Hex_planar_linker_t.Small_pyramid d,cell)
+   ) (Hex_planar_linker_data.unfold_small_pyramids_around_side dim side) in 
+   part1@part2@part3@part4;;
 
