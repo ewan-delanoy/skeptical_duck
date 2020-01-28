273635780b8ffb3bb6f1ad9e8f91273e68657f03
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-01-28 05:58:28 +0100

    add Hex_cardinal_direction_t.Small_pyramid

--- Hex_analysis/hex_planar_linker_t.ml
+++ Hex_analysis/hex_planar_linker_t.ml
@@ -9,6 +9,7 @@ Static constructors for Hex_planar_linker_data_t.t objects
 type t= 
     Eyed_claw of Hex_cardinal_direction_t.t * Hex_cardinal_direction_t.t 
    |Noneyed_claw of Hex_double_hump_qualifier_t.t *  Hex_cardinal_direction_t.t 
-   |Pyramid of  Hex_cardinal_direction_t.t  ;;
+   |Pyramid of  Hex_cardinal_direction_t.t
+   |Small_pyramid of  Hex_cardinal_direction_t.t  ;;
 
 
