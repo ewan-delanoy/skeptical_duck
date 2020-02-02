fb8767f955a9c6fd70fac94facd6a9b2cec24f0d
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-02-02 10:02:17 +0100

    add Hex_connector.translates

--- Hex_analysis/hex_connector.ml
+++ Hex_analysis/hex_connector.ml
@@ -4,6 +4,22 @@
 
 *)
 
+module Private = struct 
+
+let translate cnnctr (dx,dy)=
+   let trl = (fun z->
+      Set_of_poly_pairs.safe_set( Set_of_poly_pairs.image (fun (x,y)->(x+dx,y+dy)) z) 
+   )  in 
+   let (Hex_island_t.I(opt1,elts1)) = cnnctr.Hex_connector_t.entry 
+   and (Hex_island_t.I(opt2,elts2)) = cnnctr.Hex_connector_t.exit in 
+   {
+    Hex_connector_t.entry =Hex_island_t.I(opt1,trl elts1);
+    junction = trl (cnnctr.Hex_connector_t.junction) ;
+    exit = Hex_island_t.I(opt2,trl elts2);
+};;
+
+end ;;
+
 
 let check_entry island cnnctr =
     Hex_island.is_included_in cnnctr.Hex_connector_t.entry island;;
@@ -35,10 +51,21 @@ let reverse cnnctr = {
                     exit  = (cnnctr.Hex_connector_t.entry);
 };;
 
-(*
-let translates (Hex_dimension_t.D dim) cnnctr = 
+
+let translates formal_dim cnnctr = 
+   let (Hex_dimension_t.D dim) = formal_dim in 
    let (Hex_island_t.I(opt1,elts1)) = cnnctr.Hex_connector_t.entry 
    and (Hex_island_t.I(opt2,elts2)) = cnnctr.Hex_connector_t.exit 
    and elts3 = cnnctr.Hex_connector_t.junction in 
    let opt = (if opt1<>None then opt1 else opt2) in 
-*)
+   let base = Hex_cardinal_direction.authorized_translations formal_dim opt in 
+   let elts = Set_of_poly_pairs.fold_merge [elts1;elts2;elts3] in 
+   let abscissas = Set_of_poly_pairs.image fst elts 
+   and ordinates = Set_of_poly_pairs.image snd elts in 
+   let xmin = Min.list abscissas and xmax = Max.list abscissas 
+   and ymin = Min.list ordinates and ymax = Max.list ordinates in    
+   let cleaned_base =  List.filter ( 
+       fun (dx,dy) ->  (1-xmin <= dx) && (dx <= dim-xmax) 
+                    && (1-ymin <= dy) && (dy <= dim-ymax) 
+    )  base in 
+   Image.image (Private.translate cnnctr) cleaned_base ;; 
