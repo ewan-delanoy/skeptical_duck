53e1de828c75ace6794277f8d274e87f6638ebcb
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-02-02 17:16:11 +0100

    add Hex_connector.Private.upwards_small_pyramid

--- Hex_analysis/hex_connector.ml
+++ Hex_analysis/hex_connector.ml
@@ -114,13 +114,7 @@ let right_eyed_upwards_claw = {
     exit = Hex_island_t.I(Some(Hex_cardinal_direction_t.Up),Set_of_poly_pairs.empty_set);
 } ;;   
 
-
-let rightwards_small_pyramid = {
-    Hex_connector_t.entry = Hex_island_t.I(None,Set_of_poly_pairs_t.S [(4,1)]);
-    junction =  [(5, 3); (4, 3); (2, 3); (1, 3); (5, 2); (2, 2); (4, 2); (5, 1); 
-     (3, 2); (3, 1)];
-    exit = Hex_island_t.I(Some(Hex_cardinal_direction_t.Right),Set_of_poly_pairs.empty_set);
-} ;;   
+ 
 
 let sb_rightwards_claw = {
     Hex_connector_t.entry = Hex_island_t.I(None,Set_of_poly_pairs_t.S [(3,1)]);
@@ -138,39 +132,14 @@ let upwards_pyramid =
    exit =
     Hex_island_t.I (Some Hex_cardinal_direction_t.Up, Set_of_poly_pairs_t.S [])};;
 
-(*
-let oppose = Hex_connector.oppose Hex_dimension.eleven and reflect = Hex_connector.reflect ;;
+let upwards_small_pyramid = 
+{Hex_connector_t.entry =
+    Hex_island_t.I (None, Set_of_poly_pairs_t.S [(3, 2)]);
+   junction =
+    [(1, 1) ; (1, 2) ;   (1, 4); (1, 5); 
+     (2, 1) ; (2, 4) ;   (2, 2); (3, 1); (2, 3); (3, 3)];
+   exit =
+    Hex_island_t.I (Some Hex_cardinal_direction_t.Up, Set_of_poly_pairs_t.S [])};;
 
-let low_eyed_leftwards_claw = oppose high_eyed_rightwards_claw;;
-let left_eyed_downwards_claw = reflect high_eyed_rightwards_claw;;
-let right_eyed_upwards_claw = oppose left_eyed_downwards_claw;;
-
-let high_eyed_leftwards_claw = oppose low_eyed_rightwards_claw;;
-let left_eyed_upwards_claw = reflect high_eyed_leftwards_claw;;
-let right_eyed_downwards_claw = oppose left_eyed_upwards_claw;;
-
-let sb_leftwards_claw = oppose bs_rightwards_claw;;
-let bs_upwards_claw = reflect sb_leftwards_claw;;
-let sb_downwards_claw = oppose bs_upwards_claw;;
-
-let bs_leftwards_claw = oppose sb_rightwards_claw;;
-let sb_upwards_claw = reflect bs_leftwards_claw;;
-let bs_downwards_claw = oppose sb_upwards_claw;;
-
-let leftwards_pyramid = oppose rightwards_pyramid;;
-let upwards_pyramid = reflect leftwards_pyramid;;
-let downwards_pyramid = oppose upwards_pyramid;;
-
-let leftwards_small_pyramid = oppose rightwards_small_pyramid;;
-let upwards_small_pyramid = reflect leftwards_small_pyramid;;
-let downwards_small_pyramid = oppose upwards_small_pyramid;;
-
-let southwest_bridge = oppose northeast_bridge ;;
-let south_bridge = reflect  northeast_bridge ;;
-let north_bridge = oppose south_bridge ;;
-
-let southeast_bridge = oppose northwest_bridge ;;
-
- *)
 
 end ;;
