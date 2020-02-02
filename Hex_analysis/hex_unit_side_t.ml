a173850a2741b166ca0cbfc291a14172ff662e49
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-02-02 10:19:16 +0100

    register Hex_analysis/hex_unit_side_t.ml

---
+++ Hex_analysis/hex_unit_side_t.ml
@@ -0,0 +1,15 @@
+(* 
+
+#use"Hex_analysis/hex_unit_side_t.ml";;
+
+Enumrates the sides of a hexagon.
+
+*)
+
+type t= 
+    North 
+   |North_east
+   |North_west 
+   |South
+   |South_east
+   |South_west ;;
