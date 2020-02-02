5486c750716888d7a5be42f84348bb9a5b1b935c
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-02-02 11:04:04 +0100

    change definition of Hex_connector_t.t

--- Hex_analysis/hex_connector_t.ml
+++ Hex_analysis/hex_connector_t.ml
@@ -6,6 +6,6 @@
 
 type t= {
      entry    : Hex_island_t.t ;
-     junction : (int,int) Set_of_poly_pairs_t.t ;
+     junction : (int * int) list ; (* not ordered because we want to be able to associate pairs *)
      exit     : Hex_island_t.t ;
 };;
