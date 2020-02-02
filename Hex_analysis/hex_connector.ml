024631f6b20da89f6ed4be0863ce2f75102543b7
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-02-02 16:42:26 +0100

    add Hex_connector.Private.left_eyed_upwards_claw

--- Hex_analysis/hex_connector.ml
+++ Hex_analysis/hex_connector.ml
@@ -76,29 +76,19 @@ let bs_upwards_claw = {
     exit = Hex_island_t.I(Some(Hex_cardinal_direction_t.Up),Set_of_poly_pairs.empty_set);
 } ;;   
 
-let high_eyed_rightwards_claw = {
-    Hex_connector_t.entry = Hex_island_t.I(None,Set_of_poly_pairs_t.S [(5,1)]);
-    junction = [                        (1, 4); 
-                                (2, 3); (2, 4); 
-                        (3, 2); (3, 3); (3, 4); 
-                        (4, 2); (4, 3); (4, 4);
-                        (5, 2); (5, 3); (5, 4); 
-                (6, 1); (6, 2); (6, 3); (6, 4); 
-                        (7, 2); (7, 3); (7, 4)];
-    exit = Hex_island_t.I(Some(Hex_cardinal_direction_t.Right),Set_of_poly_pairs.empty_set);
-} ;;   
 
-let low_eyed_rightwards_claw = {
-    Hex_connector_t.entry = Hex_island_t.I(None,Set_of_poly_pairs_t.S [(6,1)]);
-    junction = [                        (1, 4); 
-                                (2, 3); (2, 4); 
-                        (3, 2); (3, 3); (3, 4); 
-                        (4, 2); (4, 3); (4, 4);
-                (5, 1); (5, 2); (5, 3); (5, 4); 
-                        (6, 2); (6, 3); (6, 4); 
-                        (7, 2); (7, 3); (7, 4)];
-    exit = Hex_island_t.I(Some(Hex_cardinal_direction_t.Right),Set_of_poly_pairs.empty_set);
-} ;;   
+
+let left_eyed_upwards_claw = {Hex_connector_t.entry =
+    Hex_island_t.I (None, Set_of_poly_pairs_t.S [(4, 2)]);
+   junction =
+    [(1, 1); (1, 2); (1, 3); (1, 4); (1, 5); (1, 6); (1, 7); 
+     (2, 1); (2, 2); (2, 3); (2, 4); (2, 5); (2, 6); 
+     (3, 1); (3, 2); (3, 3); (3, 4); (3, 5); 
+                     (4, 3)];
+   exit =
+    Hex_island_t.I (Some Hex_cardinal_direction_t.Up, Set_of_poly_pairs_t.S [])};;
+
+
 
 
 let northeast_bridge = {
