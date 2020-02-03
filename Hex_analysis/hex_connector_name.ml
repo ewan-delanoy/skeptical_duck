7f86d56981ef612cdffd3c9548127c2d436e1022
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-02-03 17:40:12 +0100

    add Hex_unit_side.to_readable_string

--- Hex_analysis/hex_connector_name.ml
+++ Hex_analysis/hex_connector_name.ml
@@ -11,3 +11,11 @@ let is_inner = function
    |Pyramid(_,_) 
    |Small_pyramid(_,_) -> false ;;
 
+let to_readable_string = function 
+    Hex_connector_name_t.Bridge(us)-> Hex_unit_side.to_readable_string us
+   |Eyed_claw(_,d1,d2) -> 
+         (Hex_cardinal_direction.for_eye_description d1)^"e"^(Hex_cardinal_direction.for_ground_description d2)
+   |Noneyed_claw(_,dh,d) -> let s = (Hex_double_hump_qualifier.to_readable_string dh) in 
+                          (String.sub s 0 1)^(Hex_cardinal_direction.for_ground_description d)^(String.sub s 1 1)
+   |Pyramid(_,d) ->  (Hex_cardinal_direction.for_ground_description d)^"py" 
+   |Small_pyramid(_,d) ->  (Hex_cardinal_direction.for_ground_description d)^"sy" ;;
--- Hex_analysis/hex_unit_side.ml
+++ Hex_analysis/hex_unit_side.ml
@@ -21,3 +21,11 @@ let reflect = function
    |Hex_unit_side_t.South      ->  Hex_unit_side_t.North_east
    |Hex_unit_side_t.South_east ->  Hex_unit_side_t.South_east
    |Hex_unit_side_t.South_west ->  Hex_unit_side_t.North ;;
+
+let to_readable_string = function 
+    Hex_unit_side_t.North      ->  "n"
+   |Hex_unit_side_t.North_east ->  "ne"
+   |Hex_unit_side_t.North_west ->  "nw"
+   |Hex_unit_side_t.South      ->  "s"
+   |Hex_unit_side_t.South_east ->  "se"
+   |Hex_unit_side_t.South_west ->  "sw" ;;
