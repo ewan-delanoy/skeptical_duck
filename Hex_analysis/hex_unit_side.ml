7970f2645b7ade081bb83114c3abfc26bf83729c
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-02-05 06:50:07 +0100

    add Hex_unit_side.all

--- Hex_analysis/hex_unit_side.ml
+++ Hex_analysis/hex_unit_side.ml
@@ -5,6 +5,16 @@
 
 *)
 
+let all = 
+   [ 
+    Hex_unit_side_t.North      ;
+    Hex_unit_side_t.North_east ;
+    Hex_unit_side_t.North_west ;
+    Hex_unit_side_t.South      ;
+    Hex_unit_side_t.South_east ;
+    Hex_unit_side_t.South_west ;
+   ];;
+
 let oppose = function 
     Hex_unit_side_t.North      ->  Hex_unit_side_t.South
    |Hex_unit_side_t.North_east ->  Hex_unit_side_t.South_west
