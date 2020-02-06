6009eebd594d329ea4352c7e29a67f9ac0bcfd23
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-02-06 16:46:34 +0100

    simplify implementation of Hex_connector_name_t.t (step two)

--- Hex_analysis/hex_connector_name.ml
+++ Hex_analysis/hex_connector_name.ml
@@ -9,17 +9,9 @@ let base_for_starters =
 *)   
 
 let is_inner = function
-    Hex_connector_name_t.Bridge(_)-> true
-   |Eyed_claw(_,_,_) 
-   |Noneyed_claw(_,_,_) 
-   |Pyramid(_,_) 
-   |Small_pyramid(_,_) -> false ;;
+    Hex_connector_name_t.Inner(_)-> true
+   |Border(_,_) -> false ;;
 
 let to_readable_string = function 
-    Hex_connector_name_t.Bridge(us)-> Hex_unit_side.to_readable_string us
-   |Eyed_claw(_,d1,d2) -> 
-         (Hex_cardinal_direction.for_eye_description d1)^"e"^(Hex_cardinal_direction.for_ground_description d2)
-   |Noneyed_claw(_,dh,d) -> let s = (Hex_double_hump_qualifier.to_readable_string dh) in 
-                          (String.sub s 0 1)^(Hex_cardinal_direction.for_ground_description d)^(String.sub s 1 1)
-   |Pyramid(_,d) ->  (Hex_cardinal_direction.for_ground_description d)^"py" 
-   |Small_pyramid(_,d) ->  (Hex_cardinal_direction.for_ground_description d)^"sy" ;;
+    Hex_connector_name_t.Inner(inner)-> Hex_inner_connector_name.to_readable_string inner
+   |Border(_,border) -> Hex_border_connector_name.to_readable_string border;;
