3094fe16a42a090efa9c86deedd41eb032782362
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-02-08 14:39:05 +0100

    add Hex_inner_connector_name.all

--- Hex_analysis/hex_connector_name.ml
+++ Hex_analysis/hex_connector_name.ml
@@ -4,9 +4,20 @@
 
 *)
 
-(*
-let base_for_starters =
-*)   
+module Private = struct 
+
+let siders bw side =
+    Image.image (
+        fun border -> 
+           Hex_connector_name_t.Border(bw,border)
+    ) (Hex_border_connector_name.for_side side);;
+
+end ;;
+
+let enders_for_side = Private.siders Hex_borderwise_t.To_border;;
+
+let starters_for_side = Private.siders Hex_borderwise_t.From_border;;
+   
 
 let is_inner = function
     Hex_connector_name_t.Inner(_)-> true
--- Hex_analysis/hex_inner_connector_name.ml
+++ Hex_analysis/hex_inner_connector_name.ml
@@ -4,5 +4,10 @@
 
 *)
 
+let all =
+    Image.image (
+      fun us -> Hex_inner_connector_name_t.Bridge(us)
+    ) Hex_unit_side.all ;;
+
 let to_readable_string = function 
     Hex_inner_connector_name_t.Bridge(us)-> Hex_unit_side.to_readable_string us ;;
