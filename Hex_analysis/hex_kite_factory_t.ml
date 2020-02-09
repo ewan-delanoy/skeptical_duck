f367c208e774657e4cdd4beadd97f61475fe4c76
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-02-09 15:05:20 +0100

    rename Hex_partial_kite_t.t as old_t (step two)

--- Hex_analysis/hex_kite_factory_t.ml
+++ Hex_analysis/hex_kite_factory_t.ml
@@ -9,6 +9,6 @@ type t= {
       winner        : Hex_player_t.t ;
       initial_data  : Hex_end_of_battle_t.t ;
       finished      : Hex_molecular_linker_t.t list ;
-      unfinished    : Hex_partial_kite_t.t list;
+      unfinished    : Hex_partial_kite_t.old_t list;
 };;
 
