56bdb6e8d7a1d7127ddf49d69c4ec4f2b3c402c4
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-02-09 11:04:36 +0100

    rename Hex_kite_element_t.t as old_t

--- Hex_analysis/hex_kite_element_t.ml
+++ Hex_analysis/hex_kite_element_t.ml
@@ -4,7 +4,7 @@
 
 *)
 
-type t= 
+type old_t= 
    Active_cell of Hex_cell_t.t 
   |Bridge of Hex_cell_t.t * Hex_cell_t.t (* the pair is in Hex_cell.cmp order *)
   |Planar of Hex_planar_linker_t.t * Hex_cell_t.t;;
