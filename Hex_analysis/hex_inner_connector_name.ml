b8c711ba5ff63c497cd8b1648ada7d262841c9b2
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-02-09 08:36:03 +0100

    add Hex_inner_connector_name.to_nondefault_molecular_linker

--- Hex_analysis/hex_inner_connector_name.ml
+++ Hex_analysis/hex_inner_connector_name.ml
@@ -9,5 +9,8 @@ let all =
       fun us -> Hex_inner_connector_name_t.Bridge(us)
     ) Hex_unit_side.all ;;
 
+let to_nondefault_molecular_linker = function 
+    Hex_inner_connector_name_t.Bridge(us)-> None ;;
+
 let to_readable_string = function 
     Hex_inner_connector_name_t.Bridge(us)-> Hex_unit_side.to_readable_string us ;;
