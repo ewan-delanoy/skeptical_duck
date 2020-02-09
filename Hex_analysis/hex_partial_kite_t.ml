e0dee7166ad800e5593d0fcffcc9d08a000b6ad3
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-02-09 17:06:50 +0100

    add Hex_partial_kite_t.t

--- Hex_analysis/hex_partial_kite_t.ml
+++ Hex_analysis/hex_partial_kite_t.ml
@@ -9,3 +9,11 @@ The third argument says on which side of the board the kite started.
 *)
 
 type old_t= P of Hex_kite_element_t.old_t list * Hex_cell_set_t.t * Hex_cardinal_direction_t.t ;;
+
+
+type t={
+   stops_so_far : Hex_kite_element_t.t list;
+   original_side : Hex_cardinal_direction_t.t;
+   unvisited_islands : Hex_island_t.t list;
+   unvisited_seas : Hex_named_connector_t.t list;
+};;
