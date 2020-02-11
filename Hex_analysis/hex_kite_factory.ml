b292fdc60be5742753cf4ca0c961f2637a8ca5d7
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-02-11 18:09:52 +0100

    add failures record in Hex_kite_factory_t.t (step three)

--- Hex_analysis/hex_kite_factory.ml
+++ Hex_analysis/hex_kite_factory.ml
@@ -11,24 +11,28 @@ let starters eob =
     {
       Hex_kite_factory_t.dimension  = eob.Hex_end_of_battle_t.dimension ;
       winner         = eob.Hex_end_of_battle_t.winner ;
-      finished       = [] ;
       initial_data   = eob ;
+      finished       = [] ;
       failures       = [] ;
-      unfinished     = Hex_partial_kite.Elderly.starters eob;
+      unfinished     = Hex_partial_kite.starters eob;
     };;    
 
 let pusher (factory,_) = 
    let raw_result=Image.image (
-         Hex_partial_kite.Elderly.extensions factory.Hex_kite_factory_t.initial_data
-   ) factory.Hex_kite_factory_t.unfinished in 
-   let new_moleculars = List.flatten (Image.image fst raw_result)
-   and new_partial_kites = List.flatten (Image.image snd raw_result) in 
+         fun pk->
+         (pk,Hex_partial_kite.extensions pk) 
+   ) factory.Hex_kite_factory_t.unfinished in  
+   let (failures1,nonfailures1) = List.partition (fun (_,p)->p=([],[]) ) raw_result in 
+   let new_failures = List.rev_append (Image.image fst failures1) factory.Hex_kite_factory_t.failures in 
+   let new_moleculars = List.flatten (Image.image (fun (_,p)->fst p) nonfailures1)
+   and new_partial_kites = List.flatten (Image.image (fun (_,p)->snd p) nonfailures1) in 
    let ordered_new_moleculars = Ordered.sort Total_ordering.standard new_moleculars in 
    let new_finished_ones = Ordered.merge Total_ordering.standard 
           ordered_new_moleculars (factory.Hex_kite_factory_t.finished) in      
    ({
       factory with 
       Hex_kite_factory_t.finished = new_finished_ones ;
+      failures = new_failures ;
       unfinished     = new_partial_kites ;
     },new_partial_kites=[]);;
 
