e295c42def22c04de550fa121204bd4c0b9142ce
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-02-13 09:23:52 +0100

    add Hex_eyed_claw.check_parameters

--- Hex_analysis/hex_eyed_claw.ml
+++ Hex_analysis/hex_eyed_claw.ml
@@ -5,7 +5,9 @@
 *)
 
 exception Bad_specification of Hex_cardinal_direction_t.t * Hex_cardinal_direction_t.t ;;
-
+exception Incorrect_abscissa of Hex_cardinal_direction_t.t * Hex_cardinal_direction_t.t ;;
+exception Incorrect_ordinate of Hex_cardinal_direction_t.t * Hex_cardinal_direction_t.t ;;
+exception Mismatch of Hex_cardinal_direction_t.t * Hex_cardinal_direction_t.t ;;
 
 module Private = struct 
 
@@ -112,10 +114,100 @@ let advanced_constructor d1 d2 (i,j)=
    let ((i0,j0),default_ipairs) =  default_constructor d1 d2 in 
    Image.image (fun (x,y)-> (i-i0+x,j-j0+y)) default_ipairs;;
 
+module Check = struct 
+
+let left_eyed_downwards (Hex_dimension_t.D dim) cell=
+    let x=Hex_cell.abscissa cell 
+    and y=Hex_cell.ordinate cell in 
+    if x<>dim-3         then raise(Incorrect_abscissa(left,down)) else 
+    if (y<5)||(y>dim-2) then raise(Incorrect_ordinate(left,down)) else
+    ();;
+
+let right_eyed_downwards (Hex_dimension_t.D dim) cell=
+    let x=Hex_cell.abscissa cell 
+    and y=Hex_cell.ordinate cell in 
+    if x<>dim-3         then raise(Incorrect_abscissa(right,down)) else 
+    if (y<6)||(y>dim-1) then raise(Incorrect_ordinate(right,down)) else
+    ();;    
+
+
+let left_eyed_upwards (Hex_dimension_t.D dim) cell=
+    let x=Hex_cell.abscissa cell 
+    and y=Hex_cell.ordinate cell in 
+    if x<>4         then raise(Incorrect_abscissa(left,up)) else 
+    if (y<2)||(y>dim-5) then raise(Incorrect_ordinate(left,up)) else
+    ();;
+
+let right_eyed_upwards (Hex_dimension_t.D dim) cell=
+    let x=Hex_cell.abscissa cell 
+    and y=Hex_cell.ordinate cell in 
+    if x<>4         then raise(Incorrect_abscissa(right,up)) else 
+    if (y<3)||(y>dim-4) then raise(Incorrect_ordinate(right,up)) else
+    ();;    
+
+let high_eyed_leftwards (Hex_dimension_t.D dim) cell=
+    let x=Hex_cell.abscissa cell 
+    and y=Hex_cell.ordinate cell in 
+    if  y<>4            then raise(Incorrect_ordinate(high,left)) else
+    if (x<2)||(x>dim-5) then raise(Incorrect_abscissa(high,left)) else 
+    ();;
+
+let low_eyed_leftwards (Hex_dimension_t.D dim) cell=
+    let x=Hex_cell.abscissa cell 
+    and y=Hex_cell.ordinate cell in 
+    if  y<>4            then raise(Incorrect_ordinate(low,left)) else
+    if (x<3)||(x>dim-4) then raise(Incorrect_abscissa(low,left)) else 
+    ();;    
+
+
+let high_eyed_rightwards (Hex_dimension_t.D dim) cell=
+    let x=Hex_cell.abscissa cell 
+    and y=Hex_cell.ordinate cell in 
+    if  y<>dim-3        then raise(Incorrect_ordinate(high,left)) else
+    if (x<5)||(x>dim-2) then raise(Incorrect_abscissa(high,left)) else 
+    ();;
+
+let low_eyed_rightwards (Hex_dimension_t.D dim) cell=
+    let x=Hex_cell.abscissa cell 
+    and y=Hex_cell.ordinate cell in 
+    if  y<>dim-3        then raise(Incorrect_ordinate(low,left)) else
+    if (x<6)||(x>dim-1) then raise(Incorrect_abscissa(low,left)) else 
+    ();;    
+
+
+let left_eyed dim direction cell = match direction with 
+     Hex_cardinal_direction_t.Down -> left_eyed_downwards dim cell 
+    |Hex_cardinal_direction_t.Up   -> left_eyed_upwards dim cell    
+    |                           _  -> raise(Mismatch(left,direction));;
+
+let right_eyed dim direction cell = match direction with 
+     Hex_cardinal_direction_t.Down -> right_eyed_downwards dim cell 
+    |Hex_cardinal_direction_t.Up   -> right_eyed_upwards dim cell    
+    |                           _  -> raise(Mismatch(right,direction));;
+
+let high_eyed dim direction cell = match direction with 
+     Hex_cardinal_direction_t.Left  -> high_eyed_leftwards dim cell 
+    |Hex_cardinal_direction_t.Right -> high_eyed_rightwards dim cell    
+    |                           _   -> raise(Mismatch(high,direction));;
+
+let low_eyed dim direction cell = match direction with 
+     Hex_cardinal_direction_t.Left  -> low_eyed_leftwards dim cell 
+    |Hex_cardinal_direction_t.Right -> low_eyed_rightwards dim cell    
+    |                           _   -> raise(Mismatch(low,direction));;
+
+let eyed dim =function
+   Hex_cardinal_direction_t.Down  -> low_eyed dim 
+  |Hex_cardinal_direction_t.Left  -> left_eyed dim
+  |Hex_cardinal_direction_t.Right -> right_eyed dim
+  |Hex_cardinal_direction_t.Up    -> high_eyed dim ;;  
+
+end ;;
+
 
 end ;;
 
 let advanced_constructor = Private.advanced_constructor;;
+let check_parameters = Private.Check.eyed;;
 let compute_apex_coordinates = Private.compute_apex_coordinates;;
 let default_constructor = Private.default_constructor;;
 
