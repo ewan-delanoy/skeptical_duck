6bd65a73cd395fa13bcd766eedce7da895cda88e
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-02-14 09:54:31 +0100

    add apex as argument in Hex_connector_name.to_nondefault_molecular_linker (step three)

--- Hex_analysis/hex_border_connector_name.ml
+++ Hex_analysis/hex_border_connector_name.ml
@@ -5,6 +5,8 @@
 *)
 
 
+exception Bad_apex_for_eyed_claw of Hex_cardinal_direction_t.t * Hex_cardinal_direction_t.t ;;
+
 let for_side side =
     let ortho = Hex_cardinal_direction.orthogonal_directions side in 
     (Image.image (fun specifier->
@@ -24,6 +26,7 @@ let for_side side =
 
 let to_nondefault_molecular_linker nm apex junction = match nm with 
     Hex_border_connector_name_t.Eyed_claw(d1,d2) -> 
+         if apex=None then raise (Bad_apex_for_eyed_claw(d1,d2)) else
          Some(Hex_molecular_linker.constructor [
               Hex_atomic_linker_t.Eyed_claw(d1,d2,Hex_cell.of_int_pair (Option.unpack apex))])
    |Noneyed_claw(_,_) -> None
