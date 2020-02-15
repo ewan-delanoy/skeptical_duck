2f2ce2ceb6d58ee13aba350208a4d1b4ac797014
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-02-15 06:54:53 +0100

    update comments in Hex_meeting_result_t

--- Hex_analysis/hex_meeting_result_t.ml
+++ Hex_analysis/hex_meeting_result_t.ml
@@ -2,11 +2,11 @@
 
 #use"Hex_analysis/hex_meeting_result_t.ml";;
 
-In the Attack_but_no_surrender variant, the first argument is the sequence of moves
+In the Player_is_attacked_but_fights_back variant, the first argument is the sequence of moves
 that reached the position , and the second argument is the single
 remaining unplayed active cell.
 
-In the Surrender variant, the first two argument are as in the Attack_but_no_surrender variant
+In the Player_surrenders variant, the first two argument are as in the Player_is_attacked_but_fights_back variant
 and the third is the "surrendering" move.
 
 In the Stalemate variant, the argument is the set of remaining unplayed active cells.
