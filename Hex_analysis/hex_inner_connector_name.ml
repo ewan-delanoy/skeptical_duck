19a6e889258d17f9f345e373d0e8910161593517
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-02-22 06:29:29 +0100

    add Hex_inner_connector_name_t.Haddock1 (step three : update in Hex_inner_connector_name)

--- Hex_analysis/hex_inner_connector_name.ml
+++ Hex_analysis/hex_inner_connector_name.ml
@@ -5,12 +5,15 @@
 *)
 
 let all =
-    Image.image (
+    (Image.image (
       fun us -> Hex_inner_connector_name_t.Bridge(us)
-    ) Hex_unit_side.all ;;
+    ) Hex_unit_side.all)
+    @[Haddock1] ;;
 
 let to_nondefault_molecular_linker = function 
-    Hex_inner_connector_name_t.Bridge(us)-> None ;;
+    Hex_inner_connector_name_t.Bridge(_)
+    |Haddock1 -> None ;;
 
 let to_readable_string = function 
-    Hex_inner_connector_name_t.Bridge(us)-> Hex_unit_side.to_readable_string us ;;
+     Hex_inner_connector_name_t.Bridge(us)-> Hex_unit_side.to_readable_string us 
+    |Haddock1 -> "hk1";;
