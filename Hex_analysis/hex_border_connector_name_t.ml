61487e83f4f8112728ac034e2e2e241511b0f3d5
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-03-04 16:20:25 +0100

    add Border_bridge variant in Hex_border_connector_name_t.t

--- Hex_analysis/hex_border_connector_name_t.ml
+++ Hex_analysis/hex_border_connector_name_t.ml
@@ -8,4 +8,5 @@ type t=
      Eyed_claw of     Hex_cardinal_direction_t.t * Hex_cardinal_direction_t.t 
     |Noneyed_claw of  Hex_double_hump_qualifier_t.t *  Hex_cardinal_direction_t.t 
     |Pyramid of       Hex_cardinal_direction_t.t
-    |Small_pyramid of Hex_cardinal_direction_t.t  ;;
+    |Small_pyramid of Hex_cardinal_direction_t.t  
+    |Border_bridge of Hex_cardinal_direction_t.t;;
