23e0086412551dd9bb927bd625185e764a333ce0
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-03-08 08:28:19 +0100

    sort alphabetically the values in Option module

--- option.ml
+++ option.ml
@@ -2,21 +2,19 @@
 
 exception Unpackable of string;;
 
+module Private = struct 
+
 let unpack_with_error_message s=function
 None->raise(Unpackable(s))
 |Some(x)->x;;
 
-let unpack x =unpack_with_error_message "void is not unpackable" x;;
+end ;;
 
 
-let propagate f=function
-None->None
-|Some(x)->Some(f(x));;
-
-let rec seek f =function
-[]->None
-|a::b->if f(a) then Some(a) else seek(f)(b);;
-
+let add_element_on_the_right l x=match x with
+  None->l
+  |Some(a)->l@[a];;
+ 
 let rec filter_and_unpack f l=
  let rec filter0=(function
   (graet,da_ober)->match da_ober with
@@ -27,18 +25,7 @@ let rec filter_and_unpack f l=
  ) in
  filter0([],l);;
 
-let rec partition_and_unpack f l=
- let rec filter0=(function
-  (graet1,graet2,da_ober)->match da_ober with
-   []->(List.rev(graet1),List.rev(graet2))
-   |x::peurrest->match f(x) with
-		None->filter0(graet1,x::graet2,peurrest)
-		|Some(y)->filter0(y::graet1,graet2,peurrest)
- ) in
- filter0([],[],l);;
-
-
-let rec find_and_stop f l=
+let  find_and_stop f l=
  let rec find_and_stop0=(function
   da_ober->match da_ober with
    []->None
@@ -48,48 +35,18 @@ let rec find_and_stop f l=
  ) in
  find_and_stop0(l);;
 
+let propagate f=function
+None->None
+|Some(x)->Some(f(x));;
 
-let rec filter_and_develop f l=
- let rec filter_and_develop0=(function
-  (graet,da_ober)->match da_ober with
-   []->List.rev(graet)
-   |a::peurrest->match a with
-		None->filter_and_develop0(graet,peurrest)
-		|Some(x)->filter_and_develop0(f(x)::graet,peurrest)
- ) in
- filter_and_develop0([],l);;
- 
- let add_element_on_the_right l x=match x with
-  None->l
-  |Some(a)->l@[a];;
- 
- let add_if_nonempty x y=if x=[] then y else x::y;;
- 
- let original_filter_and_separate opt_f l=
-   let rec tempf1=(function
-    (graet1,graet2,da_ober)->
-      if da_ober=[] 
-      then List.rev(add_if_nonempty (List.rev (graet2)) graet1) else
-      let x0=List.hd(da_ober) and peurrest=List.tl(da_ober) in
-      match opt_f x0 with
-      None->tempf1(add_if_nonempty (List.rev (graet2)) graet1,[],peurrest)
-      |Some(y0)->tempf1(graet1,y0::graet2,peurrest)
-    ) in
-    tempf1([],[],l);;
- 
-  
- let catch_test f x=if f x then Some x else None;;  
- let catch_exception f x=try(Some(f x)) with _->None;;
- 
- let filter_and_except f l=original_filter_and_separate 
-   (catch_exception f) l;;
-   
- let filter_and_test f l=original_filter_and_separate 
-   (catch_test f) l;;
-  
-let add_perhaps opt l=match opt with
-None->l
-|Some(a)->a::l;;      
-      
+let rec seek f =function
+[]->None
+|a::b->if f(a) then Some(a) else seek(f)(b);;
 
-             
+let unpack x =Private.unpack_with_error_message "void is not unpackable" x;;
+
+
+
+
+
+ 
