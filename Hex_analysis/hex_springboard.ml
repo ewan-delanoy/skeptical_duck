74005627fbcda92bf6a010cae8b4847ed7b9f57c
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-03-12 16:00:59 +0100

    register Hex_analysis/hex_springboard.ml

---
+++ Hex_analysis/hex_springboard.ml
@@ -0,0 +1,44 @@
+(* 
+
+#use"Hex_analysis/hex_springboard.ml";;
+
+
+*)
+
+module Private = struct 
+
+let we_for_homogeneous_list l =
+   Hex_cell_set.fold_merge (Image.image Hex_kite_springless_element.wet_earth l);;
+
+let we_for_list = function 
+  [] -> Hex_cell_set.empty_set 
+  |joiner ::others ->
+    let nc = Hex_kite_springless_element.claim_named_connector joiner in 
+    Hex_cell_set.merge 
+      (Hex_named_connector.wet_earth_with_entry_unchecked nc) 
+       (we_for_homogeneous_list others) ;; 
+
+let we_for_pair  (cell,path)=    
+     Hex_cell_set.insert cell (we_for_list path) ;; 
+
+let we_for_springboard (Hex_springboard_t.Sp(cell,path,solution,cell2,nc2)) =
+    we_for_pair (cell,path) ;;
+
+end ;;
+
+let opt_constr (cell,path,solution,cell2,nc2) = 
+   let w1 = Private.we_for_pair (cell,path)
+   and w2 = Hex_cell_set.insert cell2 (Hex_named_connector.wet_earth nc2) in 
+   if Hex_cell_set.does_not_intersect w1 w2 
+   then Some(Hex_springboard_t.Sp(cell,path,solution,cell2,nc2)) 
+   else None ;;
+
+let check_island springboard island = 
+   Hex_cell_set.does_not_intersect 
+      (Private.we_for_springboard springboard)
+         (Hex_island.inner_earth island);;
+
+let check_sea springboard nc = 
+   Hex_cell_set.does_not_intersect 
+      (Private.we_for_springboard springboard)
+         (Hex_named_connector.wet_earth nc);;         
