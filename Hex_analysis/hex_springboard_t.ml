fd156018d9da3032c0adbc077a821cbc8b560559
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-03-12 15:10:43 +0100

    register Hex_analysis/hex_springboard_t.ml

---
+++ Hex_analysis/hex_springboard_t.ml
@@ -0,0 +1,16 @@
+(* 
+
+#use"Hex_analysis/hex_springboard_t.ml";;
+
+
+The first four argument represent a winning casing with solution explicited 
+in the two last arguments of those four.
+The last two argument represent a (yet untried) casing. 
+
+*)
+
+type t = Sp of   
+   Hex_cell_t.t * Hex_named_connector_t.t *
+       Hex_kite_springless_element_t.t list * Hex_molecular_linker_t.t *
+   Hex_cell_t.t * Hex_named_connector_t.t
+  ;; 
