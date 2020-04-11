2e42e7d555bb3b2ce7f7203d53e078e3b8f1646e
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-04-11 06:31:38 +0200

    fix error in Hex_partial_kite.Private.border_casings

--- Hex_analysis/hex_partial_kite.ml
+++ Hex_analysis/hex_partial_kite.ml
@@ -137,7 +137,8 @@ let border_casings eob pk =
      ) in 
     let last_island = Hex_kite_element.claim_island last_stop 
     and goal_side = Hex_cardinal_direction.oppose (Hex_springless_analysis.original_side pk) in  
-    let temp1 = Hex_island.short_connections_to_border dim  last_island goal_side in
+    let goal_island = Hex_island.get_side goal_side pk.Hex_partial_kite_t.unvisited_islands in
+    let temp1 = Hex_island.short_connections_to_other_islands dim  last_island [goal_island] in
     List.filter (
       fun cell-> Hex_end_of_battle.assess eob cell = Hex_eob_result_t.Unoccupied
     )  temp1 ;;    
