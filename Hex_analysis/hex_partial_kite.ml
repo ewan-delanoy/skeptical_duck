8f5e844c4ff14b9d53444172add9f6cfb50fb377
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-04-16 13:45:42 +0200

    change implementation of Hex_springless_analysis.Private.active_part (step three)

--- Hex_analysis/hex_partial_kite.ml
+++ Hex_analysis/hex_partial_kite.ml
@@ -168,9 +168,9 @@ let explore_minimal_casings eob pk =
         let pk1 = add_cell_by_casing eob.Hex_end_of_battle_t.dimension cell pk in 
         let (ext1,ext2) = Hex_springless_analysis.extensions dim eob pk1 in 
         let part1 = Image.image (fun (elt,new_pk)->
-          (cell,(true,Hex_kite_springless_element.claim_named_connector elt))) ext1 
+          (cell,(true,Hex_kite_springless_element.claim_sea elt))) ext1 
         and part2 = Image.image (fun (elt,new_pk)->
-          (cell,(false,Hex_kite_springless_element.claim_named_connector elt))) ext2 in   
+          (cell,(false,Hex_kite_springless_element.claim_sea elt))) ext2 in   
         part1@part2
       ) minimal_casings
    ) in 
