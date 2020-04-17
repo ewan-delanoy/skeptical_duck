9f80d2597ed168cf957d5e6514370e11495445fe
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-04-17 16:01:31 +0200

    fix error in Tools_for_debugging.extract_from_iteration

--- tools_for_debugging.ml
+++ tools_for_debugging.ml
@@ -7,10 +7,10 @@
 let detect_exception f x = try (fun _->false)(f x) with _->true ;;
 
 let extract_from_iteration f v0=
-   let tempf = (fun (j,v)->
+   let rec tempf = (fun (j,v)->
      if detect_exception f v 
      then (j,v)
-     else (j+1,f v)
+     else tempf(j+1,f v)
    ) in 
    tempf (0,v0);;
 
