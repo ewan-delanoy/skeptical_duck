71439bde546cf58c517129ce26682b3f48c50a9d
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-04-17 15:57:37 +0200

    add Tools_for_debugging.extract_from_iteration

--- tools_for_debugging.ml
+++ tools_for_debugging.ml
@@ -4,7 +4,17 @@
 
 *)
 
+let detect_exception f x = try (fun _->false)(f x) with _->true ;;
+
+let extract_from_iteration f v0=
+   let tempf = (fun (j,v)->
+     if detect_exception f v 
+     then (j,v)
+     else (j+1,f v)
+   ) in 
+   tempf (0,v0);;
+
 let extract_from_list f l =
    let temp1 = Ennig.index_everything l in 
-   let tempf= (fun (j,elt)->try (fun _->false)(f elt) with _->true) in 
+   let tempf= (fun (j,elt)->detect_exception f elt) in 
    Listennou.force_find tempf temp1 ;; 
