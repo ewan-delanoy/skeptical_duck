bd2d29218b7758494891f8173d1a0ff70eb2ff92
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-04-20 16:31:33 +0200

    fix error in Hex_impose_active_call.impose_cell_by_casing

--- Hex_analysis/hex_impose_active_cell.ml
+++ Hex_analysis/hex_impose_active_cell.ml
@@ -13,12 +13,12 @@ let enhance dim pk cell =
     let death = Hex_cardinal_direction.oppose birth in 
     Hex_cardinal_direction.enhance dim death cell ;; 
 
-let impose_cell_by_casing_in_contact_case dim new_cell pk (old_islands,old_abc) (old_last_island,previous_stops)=  
+let impose_cell_by_casing_in_contact_case dim new_cell pk (old_islands,old_abc) (old_last_island,old_last_stop,previous_stops)=  
     let sided_cell =  enhance dim pk new_cell 
     and old_free_cells = pk.Hex_partial_kite_t.remaining_free_cells in 
     let (remade_last_island,new_islands) = Hex_island.add_sided_cell_by_casing dim sided_cell (old_last_island::old_islands) 
     and new_abc = Hex_cell_set.insert (snd sided_cell) old_abc in 
-    let remade_last_stop = Hex_kite_element_t.Earth(remade_last_island) in 
+    let remade_last_stop = Hex_kite_element.change_island_component old_last_stop remade_last_island in 
    {
       pk with
       Hex_partial_kite_t.stops_so_far = (remade_last_stop::previous_stops);
@@ -47,7 +47,7 @@ let impose_cell_by_casing dim new_cell pk=
     let (last_stop,previous_stops) = Listennou.ht rl in 
     let last_island = Hex_kite_element.extract_island last_stop in 
     if Hex_island.test_for_neighbor dim last_island new_cell 
-    then impose_cell_by_casing_in_contact_case    dim new_cell pk (old_islands,old_abc) (last_island,previous_stops)
+    then impose_cell_by_casing_in_contact_case    dim new_cell pk (old_islands,old_abc) (last_island,last_stop,previous_stops)
     else impose_cell_by_casing_in_no_contact_case dim new_cell pk (old_islands,old_abc) ;;
 
 
