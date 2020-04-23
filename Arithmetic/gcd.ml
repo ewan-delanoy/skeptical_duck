8be46850ffff69bf789ffa3a7b86c0b6a1c56d72
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-04-23 09:17:49 +0200

    make cosmetic changes on Gcd

--- Arithmetic/gcd.ml
+++ Arithmetic/gcd.ml
@@ -1,22 +1,32 @@
+(*
+
+#use"Arithmetic/gcd.ml";;
+
+*)
+
+module Private = struct 
+
 let arouez x=if x<0 then -1 else if x=0 then 0 else 1;;
 
-let rec complete_gcd0 
+let rec helper_for_complete_gcd 
 (aa,xa,ya,bb,xb,yb)=
 (* we suppose 0<aa<bb and aa=xa*a+ya*b, bb=xb*a+yb*b *)
 let q=(bb/aa) and r=(bb mod aa) in
 if r=0 
 then ( ((1-q)*xa)+xb,((1-q)*ya)+yb,aa)
-else complete_gcd0(r,xb-(q*xa),yb-(q*ya),aa,xa,ya);;
+else helper_for_complete_gcd(r,xb-(q*xa),yb-(q*ya),aa,xa,ya);;
+
+end ;; 
 
 let complete_gcd a b=
-if a=0 then if b=0 then (1,1,0) else (0,arouez(b),abs(b)) else
-if b=0 then (arouez(a),0,abs(a)) else
+if a=0 then if b=0 then (1,1,0) else (0,Private.arouez(b),abs(b)) else
+if b=0 then (Private.arouez(a),0,abs(a)) else
 (*now a and b are nonzero*)
 let aa=abs(a) and bb=abs(b) in
-if aa=bb then (arouez(a),0,abs(a)) else
+if aa=bb then (Private.arouez(a),0,abs(a)) else
 if aa<bb
-then complete_gcd0(aa,arouez(a),0,bb,0,arouez(b))
-else complete_gcd0(bb,0,arouez(b),aa,arouez(a),0);;
+then Private.helper_for_complete_gcd(aa,Private.arouez(a),0,bb,0,Private.arouez(b))
+else Private.helper_for_complete_gcd(bb,0,Private.arouez(b),aa,Private.arouez(a),0);;
 
 let find_bezout_relation a b=(function (e1,e2,e3)->(e1,e2))(complete_gcd(a)(b));;
 
