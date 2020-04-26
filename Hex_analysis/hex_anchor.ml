d54382b3913a6ef486626a59d786df6685334d14
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-04-26 08:29:18 +0200

    add Hex_anchor.any_side

--- Hex_analysis/hex_anchor.ml
+++ Hex_analysis/hex_anchor.ml
@@ -6,6 +6,11 @@
 
 *)
 
+let any_side = function 
+     Hex_anchor_t.No_anchor -> None
+    |Single_anchor (d) -> Some d 
+    |Double_anchor (d1,d2) -> Some d1 ;;
+
 let to_readable_string = function 
      Hex_anchor_t.No_anchor -> ""
     |Single_anchor (d) -> "<"^(Hex_cardinal_direction.for_ground_description d)^">"
