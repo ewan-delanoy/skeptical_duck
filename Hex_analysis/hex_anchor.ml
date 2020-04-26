813e30c01be252ab8c569b52aa7912cd25da5144
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-04-26 08:50:12 +0200

    add Hex_anchor.reflect

--- Hex_analysis/hex_anchor.ml
+++ Hex_analysis/hex_anchor.ml
@@ -6,6 +6,15 @@
 
 *)
 
+module Private = struct 
+
+let normalized_double_anchor = function 
+    Hex_cardinal_direction_t.Down 
+   |Hex_cardinal_direction_t.Up -> Hex_anchor_t.Double_anchor (Hex_cardinal_direction_t.Down,Hex_cardinal_direction_t.Up)
+   | _ -> Hex_anchor_t.Double_anchor (Hex_cardinal_direction_t.Left,Hex_cardinal_direction_t.Right) ;;  
+
+end ;;
+
 let any_side = function 
      Hex_anchor_t.No_anchor -> None
     |Single_anchor (d) -> Some d 
@@ -13,8 +22,13 @@ let any_side = function
 
 let oppose = function 
      Hex_anchor_t.No_anchor -> Hex_anchor_t.No_anchor
-    |Single_anchor (d) -> Hex_anchor_t.Single_anchor (d)
+    |Single_anchor (d) -> Hex_anchor_t.Single_anchor (Hex_cardinal_direction.oppose d)
     |Double_anchor (d1,d2) -> Hex_anchor_t.Double_anchor (d1,d2) ;;
+
+let reflect = function 
+     Hex_anchor_t.No_anchor -> Hex_anchor_t.No_anchor
+    |Single_anchor (d) -> Hex_anchor_t.Single_anchor (Hex_cardinal_direction.reflect d)
+    |Double_anchor (d1,d2) -> Private.normalized_double_anchor (Hex_cardinal_direction.reflect d1) ;;
 
 let to_readable_string = function 
      Hex_anchor_t.No_anchor -> ""
