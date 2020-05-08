3a9344fe681cd5bfedcfe6357c97892b09e42c6b
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-05-08 14:31:12 +0200

    register Hex_analysis/hex_first_alternative_in_springboard_t.ml

---
+++ Hex_analysis/hex_first_alternative_in_springboard_t.ml
@@ -0,0 +1,10 @@
+(* 
+
+#use"Hex_analysis/hex_first_alternative_in_springboard_t.ml";;
+
+
+*)
+
+type t = Fa of   
+   Hex_cell_t.t * Hex_kite_springless_element_t.t list * Hex_molecular_linker_t.t * Hex_cell_set_t.t 
+  ;; 
