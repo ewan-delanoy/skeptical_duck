045e34b831822b0db3f7dd6d789c132006fcc93c
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-05-14 16:09:19 +0200

    simplify returned triple in Hex_typical_sacrifice.compatible_sacrifices (step two : update Hex_sacrifice)

--- Hex_analysis/hex_sacrifice.ml
+++ Hex_analysis/hex_sacrifice.ml
@@ -4,50 +4,21 @@
 
 *)
 
-module Private = struct 
 
-let all_sacrifices dim =
-   List.flatten (
-     Image.image (fun side ->
-       Image.image (fun (cell1,cell2,cell3)->
-         Hex_sacrifice_t.Scr(side,cell1,cell2,cell3)
-       ) (Hex_cardinal_direction.triangles_for_side dim side)
-     )
-     Hex_cardinal_direction.all
-   ) ;;
-
-end ;; 
 
 let compatible_sacrifices eob = 
-   let base = Private.all_sacrifices (eob.Hex_end_of_battle_t.dimension) 
-   and (side1,side2) = Hex_cardinal_direction.sides_for_player (eob.Hex_end_of_battle_t.winner) in 
-   let sides = [side1;side2] in 
-   let evl = Hex_end_of_battle.assess eob in 
-   List.filter (
-      function (Hex_sacrifice_t.Scr(side,cell1,cell2,cell3)) -> 
-        (evl cell1 = Hex_eob_result_t.Ally_territory) &&
-        (evl cell2 = Hex_eob_result_t.Unoccupied) &&
-        (evl cell3 = Hex_eob_result_t.Unoccupied) && 
-        (List.mem side sides)
-   ) base ;;
+   let temp1 = Hex_typical_sacrifice.compatible_sacrifices eob in 
+   Image.image ( fun
+     (tscr,side,pairs) -> Hex_sacrifice_t.Scr (tscr,side,pairs)
+   )   temp1 ;; 
+
+
 
 
 let data_for_sacrificial_starter end_of_battle 
-  (Hex_sacrifice_t.Scr(sacrifice_side,cell1,cell2,cell3)) = 
-  let dim = end_of_battle.Hex_end_of_battle_t.dimension in 
-  let artificial_eob = {
-      end_of_battle with 
-      Hex_end_of_battle_t.enemy_territory = 
-        (Hex_cell_set.merge (Hex_cell_set.safe_set [cell2;cell3]) 
-          end_of_battle.Hex_end_of_battle_t.enemy_territory)
-  } in 
-  let natural_islands = Hex_island.decompose artificial_eob in 
-  let artificial_islands = Hex_island.add_and_forget_the_adding dim (sacrifice_side,cell2) natural_islands in 
-  (artificial_eob,artificial_islands);; 
+  (Hex_sacrifice_t.Scr (_,_,pairs)) = 
+  Hex_typical_sacrifice.data_for_sacrificial_starter end_of_battle pairs;; 
 
-let reconstruct_sacrificial_solutions scr mlclr=
-    match scr with 
-    (Hex_sacrifice_t.Scr(side,cell1,cell2,cell3)) -> 
-      let pair = Hex_atomic_linker.pair (cell2,cell3) in 
-      Hex_molecular_linker.insert pair mlclr ;;
+let reconstruct_sacrificial_solutions (Hex_sacrifice_t.Scr (_,_,pairs)) mlclr=
+    Hex_typical_sacrifice.reconstruct_sacrificial_solutions pairs mlclr;;
 
