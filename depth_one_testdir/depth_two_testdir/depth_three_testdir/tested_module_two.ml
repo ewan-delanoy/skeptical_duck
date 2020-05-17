65f52ca1db5f78af1180c2a3d8161b47a562ea85
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-05-17 15:37:22 +0200

    add Tested_module_two.print_out

--- depth_one_testdir/depth_two_testdir/depth_three_testdir/tested_module_two.ml
+++ depth_one_testdir/depth_two_testdir/depth_three_testdir/tested_module_two.ml
@@ -13,3 +13,9 @@ let f x y=
 let an_imported_value = Tested_module_one.A_Submodule.o_value + 2;;  
 
 let snd_imported_value = Tested_module_one.o_deduced_value + 2;; 
+
+type t = T of int ;;
+
+let print_out (fmt:Format.formatter) (T x)=
+   Format.fprintf fmt "@[%s@]" (string_of_int x);;     
+
