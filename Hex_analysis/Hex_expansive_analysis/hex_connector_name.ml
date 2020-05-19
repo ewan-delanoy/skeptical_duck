5364bd1e3df19bdee5c87aaee271c0a7a56d6119
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-05-19 08:32:53 +0200

    rename hex_inner_connector_name as hex_expsv_inner_connector_name

--- Hex_analysis/Hex_expansive_analysis/hex_connector_name.ml
+++ Hex_analysis/Hex_expansive_analysis/hex_connector_name.ml
@@ -19,7 +19,7 @@ let enders_for_side side =
 
 let middlers = Image.image (
   fun inner -> Hex_expsv_connector_name_t.Inner(inner)
-) Hex_inner_connector_name.all;;
+) Hex_expsv_inner_connector_name.all;;
 
 let starters_for_side = Private.siders Hex_expsv_borderwise_t.From_border;;
    
@@ -29,9 +29,9 @@ let is_inner = function
    |Border(_,_) -> false ;;
 
 let to_nondefault_molecular_linker cname apex junction= match cname with 
-    Hex_expsv_connector_name_t.Inner(inner)-> Hex_inner_connector_name.to_nondefault_molecular_linker inner
+    Hex_expsv_connector_name_t.Inner(inner)-> Hex_expsv_inner_connector_name.to_nondefault_molecular_linker inner
    |Border(_,border) -> Hex_expsv_border_connector_name.to_nondefault_molecular_linker border apex junction;;
 
 let to_readable_string = function 
-    Hex_expsv_connector_name_t.Inner(inner)-> Hex_inner_connector_name.to_readable_string inner
+    Hex_expsv_connector_name_t.Inner(inner)-> Hex_expsv_inner_connector_name.to_readable_string inner
    |Border(_,border) -> Hex_expsv_border_connector_name.to_readable_string border;;
