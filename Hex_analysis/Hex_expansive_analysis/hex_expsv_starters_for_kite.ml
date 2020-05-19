1011c767e9826047f5edbe1be686b51391574424
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-05-19 09:42:33 +0200

    rename hex_expsv_nimed_connector as hex_expsv_named_connector

--- Hex_analysis/Hex_expansive_analysis/hex_expsv_starters_for_kite.ml
+++ Hex_analysis/Hex_expansive_analysis/hex_expsv_starters_for_kite.ml
@@ -15,22 +15,22 @@ let local_cmp = Total_ordering.product
 let constructor end_of_battle islands side =
    let dim = end_of_battle.Hex_end_of_battle_t.dimension in 
    let clean = List.filter (
-      Hex_expsv_nimed_connector.check_compatiblity end_of_battle ) in 
-   let pre_middle_base = clean (Hex_expsv_nimed_connector.middlers dim)
-   and pre_end_base = clean (Hex_expsv_nimed_connector.enders_for_side dim side) in                             
+      Hex_expsv_named_connector.check_compatiblity end_of_battle ) in 
+   let pre_middle_base = clean (Hex_expsv_named_connector.middlers dim)
+   and pre_end_base = clean (Hex_expsv_named_connector.enders_for_side dim side) in                             
    let first_island = Hex_island.get_side side islands  in   
    let unexpected_starters = List.filter (
-       Hex_expsv_nimed_connector.check_entry first_island
+       Hex_expsv_named_connector.check_entry first_island
    ) pre_middle_base in 
    let start_base = clean (
-      (Hex_expsv_nimed_connector.starters_for_side dim side)
+      (Hex_expsv_named_connector.starters_for_side dim side)
       @
       unexpected_starters
       @
-      (Hex_expsv_nimed_connector.islanders dim first_island islands)
+      (Hex_expsv_named_connector.islanders dim first_island islands)
    ) in 
    let full_base = Ordered.sort local_cmp (Image.image (
-     fun nc -> (Hex_expsv_nimed_connector.missing_earth end_of_battle nc,nc)
+     fun nc -> (Hex_expsv_named_connector.missing_earth end_of_battle nc,nc)
    )  (start_base@pre_middle_base@pre_end_base) ) in            
    let free_ones = Hex_end_of_battle.remaining_free_cells end_of_battle in    
    Hex_expsv_partial_kite_field.constructor first_island islands (full_base) free_ones ;;
