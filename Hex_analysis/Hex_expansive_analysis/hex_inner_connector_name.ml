fef5e6cb079abec994a15dbd06d6637123e7bd09
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-05-19 08:32:25 +0200

    rename hex_typical_inner_connector_name as hex_expsv_typical_inner_connector_name

--- Hex_analysis/Hex_expansive_analysis/hex_inner_connector_name.ml
+++ Hex_analysis/Hex_expansive_analysis/hex_inner_connector_name.ml
@@ -6,7 +6,7 @@
 
 let all =
     let base = Cartesian.tproduct 
-      Hex_typical_inner_connector_name.all  Hex_cardinal_direction.all [true;false] in 
+      Hex_expsv_typical_inner_connector_name.all  Hex_cardinal_direction.all [true;false] in 
     (Image.image (
       fun us -> Hex_expsv_inner_connector_name_t.Bridge(us)
     ) Hex_unit_side.all)
@@ -27,5 +27,5 @@ let to_readable_string = function
      Hex_expsv_inner_connector_name_t.Bridge(us)-> Hex_unit_side.to_readable_string us 
     |Broken_bridge(_,cell1,cell2,_) -> 
         (Hex_cell.to_string cell1)^"\126"^(Hex_cell.to_string cell2)
-    |Typical(tic,_,_) -> Hex_typical_inner_connector_name.to_readable_string tic   ;;
+    |Typical(tic,_,_) -> Hex_expsv_typical_inner_connector_name.to_readable_string tic   ;;
     
