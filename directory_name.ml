e265f36a4b8069057dd5be19078f21adae358c90
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-06-07 08:37:50 +0200

    add Directory_name.find_file_with_directory_list

--- directory_name.ml
+++ directory_name.ml
@@ -9,6 +9,19 @@ Directories name, with the trailing slash removed.
 
 
 exception Non_directory of string;;
+exception File_not_found of string * (Directory_name_t.t list);;
+
+let find_file_with_directory_list fname l=
+  match Option.find_and_stop (
+     fun (Directory_name_t.D s_dir) ->
+      let full_path = s_dir^"/"^fname in 
+      if Sys.file_exists full_path 
+      then Some(Absolute_path.of_string full_path)
+      else None
+  ) l with 
+  None -> raise(File_not_found(fname,l))
+  |Some(ap) -> ap;;
+
 
 let of_string s=
   let temp1=Tools_for_absolute_path.of_string s in
@@ -17,3 +30,4 @@ let of_string s=
   else raise(Non_directory(s));;
 
 let connectable_to_subpath (Directory_name_t.D s)=s^"/";;
+
