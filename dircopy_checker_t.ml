d367de1f89c490b199dbc299326479dec66f5b1f
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-06-12 09:23:34 +0200

    remove comment in Dircoy_checker_t

--- dircopy_checker_t.ml
+++ dircopy_checker_t.ml
@@ -15,34 +15,3 @@ type t ={
 
 };;
   
-(*  
-let name_of_clone_directory="/Users/ewandelanoy/Downloads/Clone";;  
-let github_clone_command=
-"git clone https://github.com/ewan-delanoy/skeptical_duck "^
-name_of_clone_directory;;  
-
-exception Failure_in_clone_directory_creation;;
-exception Failure_during_github_cloning;;
-
-let check root_dir=
-  let i=(
-    if Sys.file_exists(name_of_clone_directory)
-    then Unix_command.uc("rm -rf "^name_of_clone_directory) 
-    else 0
-  ) in
-  if i<>0
-  then raise(Failure_in_clone_directory_creation)
-  else 
-  let _=Unix_command.uc("mkdir -p "^name_of_clone_directory) in
-  let remotedir=Dfa_root.of_line name_of_clone_directory in
-  let j=Unix_command.uc github_clone_command in
-  if j<>0
-  then raise(Failure_during_github_cloning)
-  else 
-  let diff=Prepare_dircopy_update.compute_restricted_diff
-     root_dir remotedir (Coma_constant.git_ignored_subdirectories,["README";"makefile";"debugged.ml"]) in
-  let rc1=List.filter is_admissible (Dircopy_diff.recently_deleted diff)
-  and rc2=List.filter is_admissible (Dircopy_diff.recently_changed diff)
-  and rc3=List.filter is_admissible (Dircopy_diff.recently_created diff) in
-  (rc1,rc2,rc3);;
-*)           
