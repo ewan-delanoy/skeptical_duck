20edc08ddc7c46805baea30fb832636c5cdb05c3
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-06-24 14:53:08 +0200

    remove Fw_configuration_t.final_cleaner field

--- Filewatching/fw_configuration.ml
+++ Filewatching/fw_configuration.ml
@@ -6,13 +6,6 @@
 
 module Private = struct 
 
-type t ={
-  root : Dfa_root_t.t ;
-  allowed_endings : Dfa_ending_t.t list;
-  git_ignored_subdirectories : Dfa_subdirectory_t.t list;
-  special_git_saved_files : Dfn_rootless_t.t list;
-  final_cleaner : Fw_final_cleaner_t.t ;
-};;
 
 let salt = "Fw_"^"configuration_t.";;
 
@@ -20,7 +13,6 @@ let root_label                       = salt ^ "root";;
 let allowed_endings_label            = salt ^ "allowed_endings";;
 let git_ignored_subdirectories_label = salt ^ "git_ignored_subdirectories";;
 let special_git_saved_files_label    = salt ^ "special_git_saved_files";;
-let final_cleaner_label              = salt ^ "final_cleaner";;
 let ignored_files_label              = salt ^ "ignored_files";;
 
 let of_concrete_object ccrt_obj = 
@@ -30,7 +22,6 @@ let of_concrete_object ccrt_obj =
       allowed_endings = Concrete_object_field.to_list Dfa_ending.of_concrete_object (g allowed_endings_label);
       git_ignored_subdirectories = Concrete_object_field.to_list Dfa_subdirectory.of_concrete_object(g git_ignored_subdirectories_label);
       special_git_saved_files = Concrete_object_field.to_list Dfn_rootless.of_concrete_object (g special_git_saved_files_label);
-      final_cleaner = Fw_final_cleaner.of_concrete_object (g final_cleaner_label); 
       ignored_files = Concrete_object_field.to_list Dfn_rootless.of_concrete_object (g ignored_files_label);
    };; 
 
@@ -41,7 +32,6 @@ let to_concrete_object config=
     allowed_endings_label, Concrete_object_field.of_list Dfa_ending.to_concrete_object config.Fw_configuration_t.allowed_endings;
     git_ignored_subdirectories_label, Concrete_object_field.of_list Dfa_subdirectory.to_concrete_object config.Fw_configuration_t.git_ignored_subdirectories;
     special_git_saved_files_label, Concrete_object_field.of_list Dfn_rootless.to_concrete_object config.Fw_configuration_t.special_git_saved_files;
-    final_cleaner_label, Fw_final_cleaner.to_concrete_object config.Fw_configuration_t.final_cleaner;
     ignored_files_label, Concrete_object_field.of_list Dfn_rootless.to_concrete_object config.Fw_configuration_t.ignored_files;
    ]  in
    Concrete_object_t.Record items;;
@@ -52,13 +42,12 @@ let root config = config.Fw_configuration_t.root;;
 let of_concrete_object = Private.of_concrete_object;;
 let to_concrete_object = Private.to_concrete_object;;
 
-let constructor (root_dir,edgs,ign_subdirs,spc_files,deps,tmns,ign_files)= 
+let constructor (root_dir,edgs,ign_subdirs,spc_files,ign_files)= 
     {
       Fw_configuration_t.root = root_dir;
       allowed_endings = edgs;
       git_ignored_subdirectories = ign_subdirs;
       special_git_saved_files = spc_files;
-      final_cleaner = Fw_final_cleaner.constructor(deps,tmns,ign_files);
       ignored_files = ign_files;
     };; 
 
@@ -68,7 +57,6 @@ let default root_dir =
       allowed_endings = Dfa_ending.all_ocaml_endings;
       git_ignored_subdirectories = Coma_constant.git_ignored_subdirectories;
       special_git_saved_files = Coma_constant.special_git_saved_files;
-      final_cleaner = Fw_final_cleaner.default;
       ignored_files = Coma_constant.git_ignored_files;
     };; 
 
--- Filewatching/fw_configuration_t.ml
+++ Filewatching/fw_configuration_t.ml
@@ -9,6 +9,5 @@ type t ={
   allowed_endings : Dfa_ending_t.t list;
   git_ignored_subdirectories : Dfa_subdirectory_t.t list;
   special_git_saved_files : Dfn_rootless_t.t list;
-  final_cleaner : Fw_final_cleaner_t.t ;
   ignored_files : Dfn_rootless_t.t list;
 };;
