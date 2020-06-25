ea3fbd5d55e90df3faee3834fc4ccad135763c3e
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-06-25 15:37:41 +0200

    use More_unix.complete_ls_with_ignored_subdirs in Fw_initialize.first_init

--- Filewatching/fw_initialize.ml
+++ Filewatching/fw_initialize.ml
@@ -7,7 +7,7 @@
 let first_init config =
    let the_root = config.Fw_configuration_t.root in 
    let the_dir =  Directory_name.of_string (Dfa_root.without_trailing_slash the_root) in 
-   let list1 = More_unix.complete_ls_with_nondirectories_only the_dir in 
+   let (list1,_) = More_unix.complete_ls_with_ignored_subdirs the_dir config.Fw_configuration_t.git_ignored_subdirectories in 
    let list2 = Option.filter_and_unpack(
      fun ap-> try Some(Dfn_common.decompose_absolute_path_using_root ap the_root) with 
               _->None 
@@ -19,12 +19,6 @@ let first_init config =
    let nonspecials_to_be_watched1 = List.filter (
       fun rootless ->  (List.mem (Dfn_rootless.to_ending rootless)
          config.Fw_configuration_t.allowed_endings )
-         &&
-         (
-            not(List.exists ( Dfa_subdirectory.begins_with (Dfn_rootless.to_subdirectory rootless))
-             config.Fw_configuration_t.git_ignored_subdirectories
-            )
-         )
    ) nonspecials in 
    let nonspecials_to_be_watched = 
    List.filter (fun x->
