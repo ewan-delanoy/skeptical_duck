c6f81ca0bd9d3cd8032b41ab20328bb1d9266983
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-06-26 16:38:54 +0200

    use Dircopy_diff_t in Dircopy_diff

--- dircopy_diff.ml
+++ dircopy_diff.ml
@@ -4,29 +4,24 @@
 
 *)
 
-type t={
-   recently_deleted : string list;
-   recently_changed : string list;
-   recently_created : string list;
-};;
 
-let recently_deleted x=x.recently_deleted;;
-let recently_created x=x.recently_created;;
-let recently_changed x=x.recently_changed;;
+let recently_deleted x=x.Dircopy_diff_t.recently_deleted;;
+let recently_created x=x.Dircopy_diff_t.recently_created;;
+let recently_changed x=x.Dircopy_diff_t.recently_changed;;
 
 let constructor a b c={
-   recently_deleted =Recently_deleted.to_string_list a;
-   recently_changed =Recently_changed.to_string_list b;
-   recently_created =Recently_created.to_string_list c;
+   Dircopy_diff_t.recently_deleted =Recently_deleted.to_string_list a;
+   Dircopy_diff_t.recently_changed =Recently_changed.to_string_list b;
+   Dircopy_diff_t.recently_created =Recently_created.to_string_list c;
 };;
 
 let display x=
    let tempf=(fun msg l->
    "\n"::msg::(Image.image(fun w->"\t\t"^w) l)
    ) in
-   let temp1=tempf "Deleted : " (x.recently_deleted)
-   and temp2=tempf "Created : " (x.recently_created)
-   and temp3=tempf "Changed : " (x.recently_changed) in
+   let temp1=tempf "Deleted : " (x.Dircopy_diff_t.recently_deleted)
+   and temp2=tempf "Created : " (x.Dircopy_diff_t.recently_created)
+   and temp3=tempf "Changed : " (x.Dircopy_diff_t.recently_changed) in
    let temp4=String.concat "\n" (temp1@temp2@temp3) in
    (print_string temp4;
     flush stdout);;
@@ -41,7 +36,7 @@ let summarize_short_path_list l=
     Ordered.sort Total_ordering.silex_for_strings temp1;;
 
 
-let salt = "Dircopy_"^"diff.";;
+let salt = "Dircopy_"^"diff_t.";;
 
 let recently_deleted_label = salt ^ "recently_deleted";;
 let recently_changed_label = salt ^ "recently_changed";;
@@ -50,7 +45,7 @@ let recently_created_label = salt ^ "recently_created";;
 let of_concrete_object ccrt_obj = 
    let g=Concrete_object_field.get_record ccrt_obj in
    {
-      recently_deleted = Concrete_object_field.to_string_list (g recently_deleted_label);
+      Dircopy_diff_t.recently_deleted = Concrete_object_field.to_string_list (g recently_deleted_label);
       recently_changed = Concrete_object_field.to_string_list (g recently_changed_label);
       recently_created = Concrete_object_field.to_string_list (g recently_created_label);
    };; 
@@ -58,9 +53,9 @@ let of_concrete_object ccrt_obj =
 let to_concrete_object dirdiff=
    let items= 
    [
-    recently_deleted_label, Concrete_object_field.of_string_list dirdiff.recently_deleted;
-    recently_changed_label, Concrete_object_field.of_string_list dirdiff.recently_changed;
-    recently_created_label, Concrete_object_field.of_string_list dirdiff.recently_created;
+    recently_deleted_label, Concrete_object_field.of_string_list dirdiff.Dircopy_diff_t.recently_deleted;
+    recently_changed_label, Concrete_object_field.of_string_list dirdiff.Dircopy_diff_t.recently_changed;
+    recently_created_label, Concrete_object_field.of_string_list dirdiff.Dircopy_diff_t.recently_created;
    ]  in
    Concrete_object_t.Record items;;
 
@@ -75,16 +70,16 @@ let explain x=
    let temp1=Option.filter_and_unpack tempf
    (* we use infinitives for github format *)
    [
-     "Delete",Private.summarize_short_path_list(x.recently_deleted);
-     "Create",Private.summarize_short_path_list(x.recently_created);
-     "Modify",Private.summarize_short_path_list(x.recently_changed);
+     "Delete",Private.summarize_short_path_list(x.Dircopy_diff_t.recently_deleted);
+     "Create",Private.summarize_short_path_list(x.Dircopy_diff_t.recently_created);
+     "Modify",Private.summarize_short_path_list(x.Dircopy_diff_t.recently_changed);
    ] in
    if temp1=[] then "" else
    let temp2=(String.uncapitalize_ascii (List.hd temp1))::(List.tl temp1) in
    String.concat " " temp2;; 
    
 let is_empty x=
-  (x.recently_deleted,x.recently_created,x.recently_changed)=
+  (x.Dircopy_diff_t.recently_deleted,x.Dircopy_diff_t.recently_created,x.Dircopy_diff_t.recently_changed)=
    ([],[],[]);;   
    
 let of_concrete_object = Private.of_concrete_object ;;
