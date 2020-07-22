ed112f6065c793619c0458a3dc2315434070eb16
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-07-22 15:28:15 +0200

    add Compact_replacer.reverse_execute and a few other reverse functions

--- Text_editing/compact_replacer.ml
+++ Text_editing/compact_replacer.ml
@@ -22,14 +22,28 @@ let replace_inside_string (Compact_replacer_t.CR(l)) old_text =
 
 let replace_inside_file (Compact_replacer_t.CR(l)) fn =
    Replace_inside.replace_several_inside_file l fn ;;
-   
+
+let reverse_replace_inside_string (Compact_replacer_t.CR(old_l)) old_text =
+   let l = Image.image (fun (x,y)->(y,x)) old_l in 
+   Replace_inside.replace_several_inside_string l old_text ;;
+
+let reverse_replace_inside_file (Compact_replacer_t.CR(old_l)) fn =
+   let l = Image.image (fun (x,y)->(y,x)) old_l in 
+   Replace_inside.replace_several_inside_file l fn ;;
+
 let execute s=
    let temp1 = Str.split (Str.regexp "[ \t]+") s in 
    let temp2 = Image.image Absolute_path.of_string  temp1 in 
    let replacements = Io.read_whole_file (List.nth temp2 0) 
    and recipient = (List.nth temp2 1) in 
-   replace_inside_file 
-     (parse replacements) recipient ;;
+   replace_inside_file (parse replacements) recipient ;;
+
+let reverse_execute s=
+   let temp1 = Str.split (Str.regexp "[ \t]+") s in 
+   let temp2 = Image.image Absolute_path.of_string  temp1 in 
+   let replacements = Io.read_whole_file (List.nth temp2 0) 
+   and recipient = (List.nth temp2 1) in 
+   reverse_replace_inside_file (parse replacements) recipient ;;   
 
 (*
 
