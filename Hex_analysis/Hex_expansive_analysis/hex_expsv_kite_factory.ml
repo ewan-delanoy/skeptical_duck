15e68f9c6c84a88874597308b910668e82e80497
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-07-31 11:19:11 +0200

    rename hex_expsv_starters_for_kite as hex_starters_for_kite

--- Hex_analysis/Hex_expansive_analysis/hex_expsv_kite_factory.ml
+++ Hex_analysis/Hex_expansive_analysis/hex_expsv_kite_factory.ml
@@ -13,7 +13,7 @@ let nonsacrificial_starters eob =
       winner         = eob.Hex_end_of_battle_t.winner ;
       finished       = [] ;
       failures       = [] ;
-      unfinished     = Hex_expsv_starters_for_kite.nonsacrificial_starters eob;
+      unfinished     = Hex_starters_for_kite.nonsacrificial_starters eob;
     };;    
    
 let sacrificial_starter eob pk= 
@@ -59,7 +59,7 @@ let extract_solutions l = Ordered.sort Total_ordering.standard
 let nonsacrificial_full_solutions eob = full_solutions_from_factory (nonsacrificial_starters eob);;
 
 let sacrificial_full_solutions eob = 
-   let temp1 = Hex_expsv_starters_for_kite.sacrificial_starters eob in 
+   let temp1 = Hex_starters_for_kite.sacrificial_starters eob in 
    List.flatten( Image.image (fun (seed,pk)->
       let ttemp3 = full_solutions_from_factory (sacrificial_starter eob pk) in 
       Image.image (fun sol->(seed,sol)) ttemp3
