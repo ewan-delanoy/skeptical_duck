aeb2429113d48a4811c8ab1b053f9656cf2fb154
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-07-31 11:15:49 +0200

    rename hex_expsv_kite_springless_element_t as hex_kite_springless_element_t

--- Hex_analysis/Hex_expansive_analysis/hex_expsv_kite_springless_element.ml
+++ Hex_analysis/Hex_expansive_analysis/hex_expsv_kite_springless_element.ml
@@ -5,42 +5,42 @@
 *)
 
 
-exception Claim_island_exn of Hex_expsv_kite_springless_element_t.t ;;
-exception Claim_named_connector_exn of Hex_expsv_kite_springless_element_t.t ;;
+exception Claim_island_exn of Hex_kite_springless_element_t.t ;;
+exception Claim_named_connector_exn of Hex_kite_springless_element_t.t ;;
 
 module Private = struct 
 
 let to_readable_string = function
-  Hex_expsv_kite_springless_element_t.Earth(island)-> Hex_island.to_readable_string island
+  Hex_kite_springless_element_t.Earth(island)-> Hex_island.to_readable_string island
    |Sea(nc)-> Hex_expsv_named_connector.to_readable_string nc;;
 
 end ;;
 
 let claim_island x = match x with 
-    Hex_expsv_kite_springless_element_t.Sea(nc)-> raise(Claim_island_exn(x))
+    Hex_kite_springless_element_t.Sea(nc)-> raise(Claim_island_exn(x))
    |Earth(island)-> island ;;
 
 let claim_sea x = match x with 
-    Hex_expsv_kite_springless_element_t.Earth(island)-> raise(Claim_named_connector_exn(x))
+    Hex_kite_springless_element_t.Earth(island)-> raise(Claim_named_connector_exn(x))
    |Sea(nc)-> nc ;;
 
 let is_an_island x = match x with 
-    Hex_expsv_kite_springless_element_t.Earth(_)-> true
+    Hex_kite_springless_element_t.Earth(_)-> true
    |Sea(_)-> false ;;
 
 let opt_island x = match x with 
-    Hex_expsv_kite_springless_element_t.Earth(island)-> Some island
+    Hex_kite_springless_element_t.Earth(island)-> Some island
    |Sea(_)-> None ;;
 
 let print_out (fmt:Format.formatter) elt=
    Format.fprintf fmt "@[%s@]" (Private.to_readable_string elt);;     
 
 let to_molecular_linker = function
-  Hex_expsv_kite_springless_element_t.Earth(island)-> None
+  Hex_kite_springless_element_t.Earth(island)-> None
    |Sea(nc)-> Some(Hex_expsv_named_connector.to_molecular_linker nc);;
 
 let to_readable_string = Private.to_readable_string ;;
 
 let wet_earth = function
-  Hex_expsv_kite_springless_element_t.Earth(island)-> Hex_island.inner_earth island
+  Hex_kite_springless_element_t.Earth(island)-> Hex_island.inner_earth island
    |Sea(nc)-> Hex_expsv_named_connector.wet_earth nc;;
