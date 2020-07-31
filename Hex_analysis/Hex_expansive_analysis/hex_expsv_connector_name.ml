b519f692907cb3a60a6ca6d23728520a7881458d
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-07-31 11:15:31 +0200

    rename hex_expsv_connector_name_t as hex_connector_name_t

--- Hex_analysis/Hex_expansive_analysis/hex_expsv_connector_name.ml
+++ Hex_analysis/Hex_expansive_analysis/hex_expsv_connector_name.ml
@@ -9,7 +9,7 @@ module Private = struct
 let siders bw side =
     Image.image (
         fun border -> 
-           Hex_expsv_connector_name_t.Border(bw,border)
+           Hex_connector_name_t.Border(bw,border)
     ) (Hex_expsv_border_connector_name.for_side side);;
 
 end ;;
@@ -18,20 +18,20 @@ let enders_for_side side =
    Private.siders Hex_borderwise_t.To_border (Hex_cardinal_direction.oppose side);;
 
 let middlers = Image.image (
-  fun inner -> Hex_expsv_connector_name_t.Inner(inner)
+  fun inner -> Hex_connector_name_t.Inner(inner)
 ) Hex_expsv_inner_connector_name.all;;
 
 let starters_for_side = Private.siders Hex_borderwise_t.From_border;;
    
 
 let is_inner = function
-    Hex_expsv_connector_name_t.Inner(_)-> true
+    Hex_connector_name_t.Inner(_)-> true
    |Border(_,_) -> false ;;
 
 let to_nondefault_molecular_linker cname apex junction= match cname with 
-    Hex_expsv_connector_name_t.Inner(inner)-> Hex_expsv_inner_connector_name.to_nondefault_molecular_linker inner
+    Hex_connector_name_t.Inner(inner)-> Hex_expsv_inner_connector_name.to_nondefault_molecular_linker inner
    |Border(_,border) -> Hex_expsv_border_connector_name.to_nondefault_molecular_linker border apex junction;;
 
 let to_readable_string = function 
-    Hex_expsv_connector_name_t.Inner(inner)-> Hex_expsv_inner_connector_name.to_readable_string inner
+    Hex_connector_name_t.Inner(inner)-> Hex_expsv_inner_connector_name.to_readable_string inner
    |Border(_,border) -> Hex_expsv_border_connector_name.to_readable_string border;;
--- Hex_analysis/Hex_expansive_analysis/hex_expsv_named_connector_t.ml
+++ Hex_analysis/Hex_expansive_analysis/hex_expsv_named_connector_t.ml
@@ -5,7 +5,7 @@
 *)
 
 type t= {
-     name     : Hex_expsv_connector_name_t.t ;
+     name     : Hex_connector_name_t.t ;
      entry    : Hex_island_t.t ;
      junction : (int * int) list ; (* not ordered because we want to be able to associate pairs *)
      exit     : Hex_island_t.t ;
