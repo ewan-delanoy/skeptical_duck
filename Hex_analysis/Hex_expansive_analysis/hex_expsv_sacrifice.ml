a5641e0e078708f31724ba2e01f6a840276171d7
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-07-31 11:18:23 +0200

    rename hex_expsv_sacrifice_t as hex_sacrifice_t

--- Hex_analysis/Hex_expansive_analysis/hex_expsv_sacrifice.ml
+++ Hex_analysis/Hex_expansive_analysis/hex_expsv_sacrifice.ml
@@ -9,16 +9,16 @@
 let compatible_sacrifices eob = 
    let temp1 = Hex_expsv_typical_sacrifice.compatible_sacrifices eob in 
    Image.image ( fun
-     (tscr,side,pairs) -> Hex_expsv_sacrifice_t.Scr (tscr,side,pairs)
+     (tscr,side,pairs) -> Hex_sacrifice_t.Scr (tscr,side,pairs)
    )   temp1 ;; 
 
 
 
 
 let data_for_sacrificial_starter end_of_battle 
-  (Hex_expsv_sacrifice_t.Scr (_,_,pairs)) = 
+  (Hex_sacrifice_t.Scr (_,_,pairs)) = 
   Hex_expsv_typical_sacrifice.data_for_sacrificial_starter end_of_battle pairs;; 
 
-let reconstruct_sacrificial_solutions (Hex_expsv_sacrifice_t.Scr (_,_,pairs)) mlclr=
+let reconstruct_sacrificial_solutions (Hex_sacrifice_t.Scr (_,_,pairs)) mlclr=
     Hex_expsv_typical_sacrifice.reconstruct_sacrificial_solutions pairs mlclr;;
 
