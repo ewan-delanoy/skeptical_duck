58f12d3ec646b745b0563549bc3e4c668411147a
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-07-31 11:17:32 +0200

    rename hex_expsv_first_alternative_in_springboard as hex_first_alternative_in_springboard

--- Hex_analysis/Hex_expansive_analysis/hex_expsv_springboard.ml
+++ Hex_analysis/Hex_expansive_analysis/hex_expsv_springboard.ml
@@ -7,12 +7,12 @@
 module Private = struct 
 
 let wet_earth (Hex_springboard_t.Sp(fa,cell2,new_island)) =
-    Hex_expsv_first_alternative_in_springboard.wet_earth fa ;;
+    Hex_first_alternative_in_springboard.wet_earth fa ;;
 
 end ;;
 
 let active_part (Hex_springboard_t.Sp(fa,cell2,new_island)) =
-   Hex_expsv_first_alternative_in_springboard.active_part_when_joined_to_another_cell
+   Hex_first_alternative_in_springboard.active_part_when_joined_to_another_cell
      fa cell2  ;;
 
 let change_island_component 
@@ -25,7 +25,7 @@ let new_island (Hex_springboard_t.Sp(fa,cell2,new_island)) =
     new_island ;;
 
 let opt_constructor (fa,cell2,new_island) = 
-   let w1 = Hex_expsv_first_alternative_in_springboard.wet_earth fa
+   let w1 = Hex_first_alternative_in_springboard.wet_earth fa
    and w2 =  Hex_cell_set.safe_set [cell2] in 
    if Hex_cell_set.does_not_intersect w1 w2 
    then Some(Hex_springboard_t.Sp(fa,cell2,new_island)) 
@@ -33,12 +33,12 @@ let opt_constructor (fa,cell2,new_island) =
 
 let to_molecular_linker (Hex_springboard_t.Sp(fa,cell2,new_island)) =
    (* strictly speaking, this is only a partial molecular linker *)
-   Hex_expsv_first_alternative_in_springboard.molecular_linker_when_joined_to_another_cell
+   Hex_first_alternative_in_springboard.molecular_linker_when_joined_to_another_cell
      fa cell2  ;;
 
 
 let to_readable_string (Hex_springboard_t.Sp(fa,cell2,new_island))=
-  "\194\171 "^(Hex_expsv_first_alternative_in_springboard.to_readable_string fa)^" \199\128 "^
+  "\194\171 "^(Hex_first_alternative_in_springboard.to_readable_string fa)^" \199\128 "^
               (Hex_cell.to_string cell2)^" -> "^(Hex_island.to_readable_string new_island)^
   " \194\187" ;;
 
