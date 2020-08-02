d9d7f27ffd096dcb842e93e9c2340c626a35a542
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-08-02 15:12:56 +0200

    remove Springboard variant in Hex_kite_element_t.t (step two : update Hex_kite_element)

--- Hex_analysis/hex_kite_element.ml
+++ Hex_analysis/hex_kite_element.ml
@@ -12,21 +12,18 @@ module Private = struct
 
 let to_readable_string = function
   Hex_kite_element_t.Earth(island)-> Hex_island.to_readable_string island
-   |Sea(nc)-> Hex_named_connector.to_readable_string nc
-   |Springboard(springboard)-> Hex_springboard.to_readable_string springboard;;
+   |Sea(nc)-> Hex_named_connector.to_readable_string nc ;;
 
 let opt_island_component elt = match elt with 
     Hex_kite_element_t.Earth(island)-> Some island
-   |Sea(nc)-> None
-   |Springboard(springboard)-> Some (Hex_springboard.new_island springboard) ;;
+   |Sea(nc)-> None ;;
   
 
 end ;;
 
 let compress_to_springless elt= match elt with 
     Hex_kite_element_t.Earth(island)-> Hex_kite_springless_element_t.Earth(island)
-   |Sea(nc)-> Hex_kite_springless_element_t.Sea(nc)
-   |Springboard(springboard)->Hex_kite_springless_element_t.Earth(Hex_springboard.new_island springboard);;
+   |Sea(nc)-> Hex_kite_springless_element_t.Sea(nc);;
 
 
 let extract_island elt= match Private.opt_island_component elt with 
@@ -42,5 +39,4 @@ let print_out (fmt:Format.formatter) elt=
 
 let to_molecular_linker = function
   Hex_kite_element_t.Earth(island)-> None
-   |Sea(nc)-> Some(Hex_named_connector.to_molecular_linker nc)
-   |Springboard(sb) -> Some(Hex_springboard.to_molecular_linker sb)  ;;
+   |Sea(nc)-> Some(Hex_named_connector.to_molecular_linker nc) ;;
