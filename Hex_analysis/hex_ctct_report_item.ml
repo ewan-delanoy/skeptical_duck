aa648114b7c857a56d261be11fb30f63e6cb9b40
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-08-06 10:41:41 +0200

    add Hex_ctct_report.adjusted_common_neighbors

--- Hex_analysis/hex_ctct_report_item.ml
+++ Hex_analysis/hex_ctct_report_item.ml
@@ -4,10 +4,22 @@
 
 *)
 
-let common_neighbors item1 item2 =
-   Hex_cell_set.intersect 
+let adjusted_common_neighbors formal_dim item1 item2 =
+   let temp1 = Hex_cell_set.intersect 
      (item1.Hex_ctct_report_item_t.passive_neighbors) 
-     (item2.Hex_ctct_report_item_t.passive_neighbors);;
+     (item2.Hex_ctct_report_item_t.passive_neighbors) in 
+   if Hex_cell_set.length(temp1)<=2 then temp1 else  
+   let opt1 = item1.Hex_ctct_report_item_t.opt_side 
+   and opt2 = item2.Hex_ctct_report_item_t.opt_side in 
+   let opt = (if opt1=None then opt2 else opt1) in 
+   match opt with 
+   None -> temp1 
+   |Some(side) ->
+      let corners = Hex_cardinal_direction.Border.corners formal_dim side in 
+      match Option.seek (fun corner -> Hex_cell_set.is_included_in corner temp1) corners with 
+      Some(corner)->corner 
+      |None -> temp1;;
+
 
 let to_island item =
   let anchor =  (match item.Hex_ctct_report_item_t.opt_side with 
