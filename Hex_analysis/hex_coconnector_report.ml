2909f318827ae751c4519d4ea7fbf1fceaa361fc
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-08-10 10:43:39 +0200

    add memoizing to Hex_coconnector_report

--- Hex_analysis/hex_coconnector_report.ml
+++ Hex_analysis/hex_coconnector_report.ml
@@ -2,17 +2,12 @@
 
 #use"Hex_analysis/hex_coconnector_report.ml";;
 
-*)
+*) 
 
 
-
-
-let first_draft fgame = 
-   let formal_dim = fgame.Hex_finished_game_t.dimension  
-   and eob = Hex_end_of_battle.of_finished_game fgame in 
-   let report = Hex_ctct_report.about_end_of_battle eob 
-   and base = Hex_base_of_connectors.from_end_of_battle eob in 
-   let (Hex_ctct_report_t.R(l)) = report in 
+let draft_from_previous_items eob base report = 
+   let formal_dim = eob.Hex_end_of_battle_t.dimension  
+   and (Hex_ctct_report_t.R(l)) = report in 
    let indexed_l = Ennig.index_everything l in 
    let temp1 = Uple.list_of_pairs indexed_l in 
    (Image.image (
@@ -27,6 +22,33 @@ let first_draft fgame =
          connectors))
    ) temp1);;
 
+let ref_for_games = ref [];;
+let ref_for_eobs = ref [];;
+let ref_for_ctct_reports = ref [];;
+let ref_for_bases = ref [];;
+let ref_for_drafts = ref [];;
+
+let reset_all () = (
+    ref_for_games :=[];
+    ref_for_eobs :=[];
+    ref_for_ctct_reports :=[];
+    ref_for_bases :=[];
+    ref_for_drafts :=[];
+);;
+
+let analize_game fgame = 
+   let eob = Hex_end_of_battle.of_finished_game fgame in 
+   let report = Hex_ctct_report.about_end_of_battle eob 
+   and base = Hex_base_of_connectors.from_end_of_battle eob in 
+   let draft = draft_from_previous_items eob base report in 
+   (
+    ref_for_games :=fgame::(!ref_for_games);
+    ref_for_eobs :=eob::(!ref_for_eobs);
+    ref_for_ctct_reports := report::(!ref_for_ctct_reports);
+    ref_for_bases := base :: (!ref_for_bases);
+    ref_for_drafts := draft :: (!ref_for_drafts);
+   );;
+
 let verify_item_in_draft item = 
     let ((i,j),(neighbors,connectors)) = item in 
     let n1 = Hex_cell_set.length neighbors 
@@ -36,16 +58,16 @@ let verify_item_in_draft item =
     if n1 > 2 then Some("More than two neighbors",item) else 
     None ;;
 
-let verify_indexed_game (game_idx,fgame) = 
-   let draft = first_draft fgame in 
+let verify_indexed_draft (draft_idx,draft) = 
+   
    let problems = Option.filter_and_unpack verify_item_in_draft draft in 
    if problems = []
    then None 
-   else Some (Image.image (fun (msg,item)->(game_idx,msg,item)) problems) ;;
+   else Some (Image.image (fun (msg,item)->(draft_idx,msg,item)) problems) ;;
 
-let verify_games fgames=
-   let indexed_games = Ennig.index_everything fgames in 
-   List.flatten(Option.filter_and_unpack verify_indexed_game indexed_games);;
+let verify_all_drafts ()=
+   let indexed_drafts = Ennig.index_everything (!ref_for_drafts) in 
+   List.flatten(Option.filter_and_unpack verify_indexed_draft indexed_drafts);;
 
 let expand triple = 
    let (pair,(b,ncs)) = triple in 
@@ -58,15 +80,14 @@ let check_disjointness_on_draft l=
    let temp2 = Uple.list_of_pairs temp1 in 
    List.filter (fun ((triple1,z1),(triple2,z2))->Hex_cell_set.intersects z1 z2) temp2;; 
 
-let check_disjointness_on_indexed_game (game_idx,fgame) = 
-   let draft = first_draft fgame in 
+let check_disjointness_on_indexed_draft (draft_idx,draft) = 
    let problems = check_disjointness_on_draft draft in 
    if problems = []
    then None 
-   else Some (Image.image (fun (a,b)->(game_idx,a,b)) problems) ;;
+   else Some (Image.image (fun (a,b)->(draft_idx,a,b)) problems) ;;
 
-let check_disjointness_on_games fgames=
-   let indexed_games = Ennig.index_everything fgames in 
-   List.flatten(Option.filter_and_unpack check_disjointness_on_indexed_game indexed_games);;   
+let check_disjointness_on_all_drafts ()=
+   let indexed_drafts = Ennig.index_everything (!ref_for_drafts) in 
+   List.flatten(Option.filter_and_unpack check_disjointness_on_indexed_draft indexed_drafts);;
 
 
