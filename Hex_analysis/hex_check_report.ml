49309dc808e36490b1b240e723d7904d33fbfff7
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-08-18 10:10:37 +0200

    rename hex_coconnector_report_t as hex_ccnn_report_t

--- Hex_analysis/hex_check_report.ml
+++ Hex_analysis/hex_check_report.ml
@@ -25,7 +25,7 @@ let analize_game fgame =
    let eob = Hex_end_of_battle.of_finished_game fgame in 
    let report = Hex_ctct_report.about_end_of_battle eob 
    and base = Hex_base_of_connectors.from_end_of_battle eob in 
-   let (Hex_coconnector_report_t.R draft) = Hex_coconnector_report.draft_from_previous_items eob base report in 
+   let (Hex_ccnn_report_t.R draft) = Hex_coconnector_report.draft_from_previous_items eob base report in 
    (
     ref_for_games :=fgame::(!ref_for_games);
     ref_for_eobs :=eob::(!ref_for_eobs);
--- Hex_analysis/hex_coconnector_report.ml
+++ Hex_analysis/hex_coconnector_report.ml
@@ -12,7 +12,7 @@ let first_draft_from_previous_items eob base ctct_report =
    and (Hex_ctct_report_t.R(l)) = ctct_report in 
    let indexed_l = Ennig.index_everything l in 
    let temp1 = Uple.list_of_pairs indexed_l in 
-   Hex_coconnector_report_t.R(Image.image (
+   Hex_ccnn_report_t.R(Image.image (
      fun ((i,item1),(j,item2))->
         let common = Hex_ctct_report_item.adjusted_common_neighbors formal_dim item1 item2 
         and connectors1 = Hex_base_of_connectors.select_coconnectors base item1 item2 in 
@@ -26,7 +26,7 @@ let first_draft_from_previous_items eob base ctct_report =
 
 let deduce_removabilities_from_pattern
    (Hex_ctct_report_t.R(l_report)) 
-    (Hex_coconnector_report_t.R draft) (Hex_pattern_t.Pat l_pat) = 
+    (Hex_ccnn_report_t.R draft) (Hex_pattern_t.Pat l_pat) = 
    let points = Option.filter_and_unpack (
      fun (p,is_active) -> 
       if is_active 
@@ -64,12 +64,12 @@ let deduce_removabilities
 let second_draft_from_previous_items eob base ctct_report = 
    let draft1 = first_draft_from_previous_items eob base ctct_report in 
    let removabilities = deduce_removabilities eob ctct_report draft1 in 
-   let (Hex_coconnector_report_t.R l_draft1) = draft1 in 
+   let (Hex_ccnn_report_t.R l_draft1) = draft1 in 
    let l_draft2 = List.filter (
       fun (key,answer) ->
          (not (List.mem key removabilities)) && (answer<>(Hex_cell_set.empty_set,[])) 
    ) l_draft1 in 
-   Hex_coconnector_report_t.R l_draft2;;
+   Hex_ccnn_report_t.R l_draft2;;
 
 end ;; 
 
