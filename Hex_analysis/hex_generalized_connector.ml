6a20631a4b96245de03d4bccde5f5536be12bda7
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-08-20 17:09:40 +0200

    add Hex_generalized_connector.is_not_empty

--- Hex_analysis/hex_generalized_connector.ml
+++ Hex_analysis/hex_generalized_connector.ml
@@ -6,3 +6,6 @@
 
 let constructor common connectors = 
    Hex_generalized_connector_t.G(common,connectors);;
+
+let is_not_empty (Hex_generalized_connector_t.G(common,connectors)) =
+      ((common,connectors) <> (Hex_cell_set.empty_set,[])) ;;
