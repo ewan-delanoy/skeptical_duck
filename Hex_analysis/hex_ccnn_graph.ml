b75d003196da540d01d34ae7633e63f0a5ef1050
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-08-20 17:24:49 +0200

    simplify Hex_ccnn_graph.initializer_for_connected_region_computation (step two)

--- Hex_analysis/hex_ccnn_graph.ml
+++ Hex_analysis/hex_ccnn_graph.ml
@@ -12,7 +12,8 @@ let neighbors (Hex_ccnn_report_t.R l) i =
    ) l ;;
   
 let initializer_for_connected_region_computation ccnn origin =
-   let germ = [origin,neighbors ccnn origin] in 
+
+   let germ = [origin,Image.image (fun (k,data)->(k,[data])) (neighbors ccnn origin)] in 
   (Set_of_integers.singleton origin,
    germ,
    germ
