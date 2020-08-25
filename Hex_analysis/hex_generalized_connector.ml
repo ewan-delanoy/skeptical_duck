cdc22a2d652ebd324a735cafcfc6de9765fa096a
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-08-25 10:20:46 +0200

    change definition of Hex_generalized_connector_t.t (step three)

--- Hex_analysis/hex_generalized_connector.ml
+++ Hex_analysis/hex_generalized_connector.ml
@@ -4,9 +4,10 @@
 
 *)
 
+(*
 let constructor common connectors = 
    Hex_generalized_connector_t.G(common,connectors);;
-
+*)
 
 let opt_constructor_in_half_checked_case (common,connectors) =
     if (Hex_cell_set.length common)>1
