d6d003422420549f691d76dbabaf7406ec3a6666
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-08-25 10:19:35 +0200

    change definition of Hex_generalized_connector_t.t (step two)

--- Hex_analysis/hex_generalized_connector.ml
+++ Hex_analysis/hex_generalized_connector.ml
@@ -9,6 +9,11 @@ let constructor common connectors =
 
 
 let opt_constructor_in_half_checked_case (common,connectors) =
-    if   ((Hex_cell_set.length common)>1) || (connectors <> []) 
-    then Some(Hex_generalized_connector_t.G(common,connectors))
+    if (Hex_cell_set.length common)>1
+    then let l_common = Hex_cell_set.forget_order common in 
+         let nth = (fun j->List.nth l_common (j-1)) in 
+         Some(Hex_generalized_connector_t.Bridge(nth 1,nth 2))
+    else      
+    if   (connectors <> []) 
+    then Some(Hex_generalized_connector_t.Named(List.hd connectors))
     else None;;
