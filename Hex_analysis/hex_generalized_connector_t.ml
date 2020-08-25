8afc787134d2ebc677b43f41ef735ff57808dee7
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-08-25 10:12:48 +0200

    change definition of Hex_generalized_connector_t.t

--- Hex_analysis/hex_generalized_connector_t.ml
+++ Hex_analysis/hex_generalized_connector_t.ml
@@ -2,4 +2,6 @@
 #use"Hex_analysis/hex_generalized_connector_t.ml";;
 *)
 
-type t= G of  (Hex_cell_set_t.t * Hex_named_connector_t.t list) ;;
+type t= 
+   Bridge of Hex_cell_t.t * Hex_cell_t.t 
+  |Named of  Hex_named_connector_t.t ;;
