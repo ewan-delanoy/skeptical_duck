387f9035b1e47d77c51a6ac4c635708f3db61251
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-08-31 15:49:48 +0200

    register Hex_analysis/hex_polychrome_t.ml

---
+++ Hex_analysis/hex_polychrome_t.ml
@@ -0,0 +1,17 @@
+(* 
+
+#use"Hex_analysis/hex_polychrome_t.ml";;
+
+A Hex_polychrome_t.t object is a generalization of the idea of a decomposition into connected
+components. Initially, the two decompositions coindice. Then, components can be merged into bigger
+components using Hex_generalized_connector_t.t objects.
+
+*)
+
+type t= {
+    classes    : (Hex_polychrome_label_t.t * Hex_cell_set_t.t) list ;
+    labels     : (Hex_cell_t.t * Hex_polychrome_label_t.t) list ;
+    free_cells : Hex_cell_set_t.t ;
+    history    : (Hex_polychrome_label_t.t * Hex_generalized_connector_t.t * Hex_polychrome_label_t.t) list;
+};;
+
