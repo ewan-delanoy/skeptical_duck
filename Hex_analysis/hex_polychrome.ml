4456be65443c0c7494712653e3775f9809cf91a6
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-09-02 14:26:12 +0200

    add Hex_polychrome.add_new_mergeing

--- Hex_analysis/hex_polychrome.ml
+++ Hex_analysis/hex_polychrome.ml
@@ -2,19 +2,41 @@
 
 #use"Hex_analysis/hex_polychrome.ml";;
 
-
 *)
 
-(*
-let add_mergeing pochro new_action =
+
+let add_new_mergeing pochro new_action =
     let (husband,gc,wife) = new_action in 
+    let absorbed_ones = Hex_generalized_connector.support gc in 
+    let new_index = (Max.list(Image.image (fun (_,Hex_polychrome_label_t.L(i))->i) 
+        pochro.Hex_polychrome_t.labels))+1 in 
+    let new_lbl = Hex_polychrome_label_t.L new_index in     
+    let new_labels = Option.filter_and_unpack (
+       fun (cell,lbl) -> 
+         if Hex_cell_set.mem cell absorbed_ones 
+         then None
+         else 
+         if List.mem lbl [husband;wife]
+         then Some(cell,new_lbl)
+         else Some(cell,lbl)
+    ) pochro.Hex_polychrome_t.labels in 
+    let old_classes = pochro.Hex_polychrome_t.classes in 
+    let new_merged_class = Hex_cell_set.fold_merge 
+      [
+          List.assoc husband old_classes ; absorbed_ones ;
+          List.assoc wife old_classes
+      ]
+    in 
+    let new_classes = (List.filter (fun (lbl,_)->
+         not(List.mem lbl [husband;wife])
+    ) old_classes) @ [new_lbl,new_merged_class] in 
      {
-      Hex_polychrome_t.classes    = the_classes ;
-      labels     = the_labels ;
-      free_cells = all_free_cells ;
+      Hex_polychrome_t.classes    = new_classes ;
+      labels     = new_labels ;
+      free_cells = Hex_cell_set.setminus (pochro.Hex_polychrome_t.free_cells) absorbed_ones ;
       history    = new_action :: (pochro.Hex_polychrome_t.history);
     };;
-*)   
+ 
 
 let of_ctct_report (Hex_ctct_report_t.R(l))=
     let all_free_cells = Hex_cell_set.fold_merge 
