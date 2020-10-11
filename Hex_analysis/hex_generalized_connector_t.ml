0af1793c84fd2508556d296b188411da3de35c5f
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-10-11 09:46:54 +0200

    change implementation of Hex_generalized_connector_t.t

--- Hex_analysis/hex_generalized_connector_t.ml
+++ Hex_analysis/hex_generalized_connector_t.ml
@@ -2,19 +2,9 @@
 #use"Hex_analysis/hex_generalized_connector_t.ml";;
 *)
 
-type half_bridge_t = {
-    start_side : Hex_cell_t.t ;
-    bridger :  Hex_cell_t.t ;
-    end_side : Hex_cell_t.t 
-};;
 
-type bridge_t = {
-    left_half : half_bridge_t ;
-    right_half : half_bridge_t ;
-};;
 
 type t= 
-   Bridge of bridge_t
+   Bridge of Hex_simple_bridge_t.t
   |Named of  Hex_named_connector_t.t ;;
 
-  
