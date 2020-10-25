17489ec558ea86072c4ce502b573b91e758cc4d2
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-10-25 13:57:19 +0100

    add Hex_compute_immediately_usable_connectors.Private.choose_unique_connector

--- Hex_analysis/hex_compute_immediately_usable_connectors.ml
+++ Hex_analysis/hex_compute_immediately_usable_connectors.ml
@@ -4,6 +4,17 @@
 
 *) 
 
+module Private =struct 
+
+let choose_unique_connector common connectors =
+      if Hex_cell_set.length (common) >= 2 
+      then let temp1 = Hex_cell_set.forget_order common in 
+           let tempf = (fun k->List.nth temp1 (k-1)) in 
+           Hex_generalized_connector_t.Bridge(tempf 1,tempf 2)   
+      else Hex_generalized_connector_t.Named(List.hd connectors)     
+
+end ;;   
+
 let from_previous_items eob base ctct_report = 
    let formal_dim = eob.Hex_end_of_battle_t.dimension  
    and l = ctct_report.Hex_ctct_report_t.items in 
@@ -17,7 +28,7 @@ let from_previous_items eob base ctct_report =
             fun nc->(Hex_named_connector.inner_sea nc)<> common 
         ) connectors1 in  
        ((i,j),
-        (common,connectors))
+        Private.choose_unique_connector common connectors)
    ) temp1 ;;
 
 
