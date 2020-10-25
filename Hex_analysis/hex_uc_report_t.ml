a81e2aab541cc8d23698b6acc320fd48268f33b6
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-10-25 14:20:13 +0100

    register Hex_analysis/hex_uc_report_t.ml

---
+++ Hex_analysis/hex_uc_report_t.ml
@@ -0,0 +1,13 @@
+(* 
+
+#use"Hex_analysis/hex_uc_report_t.ml";;
+
+*)
+
+type t= {
+      dimension : Hex_dimension_t.t;
+         winner : Hex_player_t.t;
+enemy_territory : Hex_cell_set_t.t;
+         items  : Hex_ctct_report_item_t.t list;
+     connectors : ((int * int) * Hex_unified_connector_t.t) list  
+} ;;
