f1903aa5b0df19454f2f2711b2b3938a707fd49e
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-10-25 09:06:16 +0100

    deal with Hex_typical_border_connector_name_t.Armchair in Hex_typical_border_connector_name

--- Hex_analysis/hex_typical_border_connector_name.ml
+++ Hex_analysis/hex_typical_border_connector_name.ml
@@ -10,6 +10,13 @@ module Private = struct
 
 let prepare_for_journey = 
   [
+   Hex_typical_border_connector_name_t.Armchair , 
+       (Some"ac",None,(5,3),
+             [(1, 1); (1, 2);  (1, 3); (1, 4);  (1, 5); (1, 6);
+              (2, 1); (2, 3);  (2, 2); (3, 1);  (2, 5); (3, 2);
+              (3, 3); (4, 2); 
+              (4, 3); (5, 2); 
+             ],[(3,4);(4,4)]) ;
      Hex_typical_border_connector_name_t.Border_bridge , 
        (Some"bb",None,(2,1),[(1, 1); (1, 2)],[]) ;
      Hex_typical_border_connector_name_t.Byssus , 
