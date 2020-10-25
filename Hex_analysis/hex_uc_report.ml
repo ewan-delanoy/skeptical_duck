5bfd07b82bf18de60cacfa8d3fecbbd9abe14f47
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-10-25 14:55:35 +0100

    fix error in Hex_uc_report.Private.compute_connectors

--- Hex_analysis/hex_uc_report.ml
+++ Hex_analysis/hex_uc_report.ml
@@ -21,8 +21,10 @@ let choose_unique_connector common connectors =
    if Hex_cell_set.length (common) >= 2 
    then let temp1 = Hex_cell_set.forget_order common in 
         let tempf = (fun k->List.nth temp1 (k-1)) in 
-        Hex_unified_connector_t.Bridge(tempf 1,tempf 2)   
-   else Hex_unified_connector_t.Named(List.hd connectors)     
+        Some(Hex_unified_connector_t.Bridge(tempf 1,tempf 2))   
+   else match connectors with 
+        []->None 
+        |connector :: _ ->Some(Hex_unified_connector_t.Named(connector));;     
 
  
 
@@ -31,14 +33,16 @@ let compute_connectors eob base ctct_report =
    and l = ctct_report.Hex_ctct_report_t.items in 
    let indexed_l = Ennig.index_everything l in 
    let temp1 = Uple.list_of_pairs indexed_l in 
-   Image.image (
+   Option.filter_and_unpack (
       fun ((i,item1),(j,item2))->
       let common = Hex_ctct_report_item.adjusted_common_neighbors formal_dim item1 item2 
       and connectors1 = Hex_base_of_connectors.select_coconnectors base item1 item2 in
       let connectors = List.filter (
          fun nc->(Hex_named_connector.inner_sea nc)<> common 
      ) connectors1 in  
-    ((i,j),choose_unique_connector common connectors)
+     match choose_unique_connector common connectors with 
+     None -> None 
+     | Some(nc)->Some((i,j),nc)
    ) temp1 ;;
 
 end ;;   
