089566867c32a255bc7b98fd950ee2f88b98452c
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-11-01 10:27:39 +0100

    register Hex_analysis/hex_mp_report_t.ml

---
+++ Hex_analysis/hex_mp_report_t.ml
@@ -0,0 +1,18 @@
+(* 
+
+#use"Hex_analysis/hex_mp_report_t.ml";;
+
+mp is for "maximal paths"
+
+*)
+
+type t= {
+      dimension   : Hex_dimension_t.t;
+         winner   : Hex_player_t.t;
+enemy_territory   : Hex_cell_set_t.t;
+         items    : Hex_ctct_report_item_t.t list;
+         base     : Hex_base_of_connectors_t.t ;
+     connectors   : ((int * int) * Hex_unified_connector_t.t) list ;
+     paths_from_1 : (int * (Hex_unified_connector_t.t * int) list) list ;
+     paths_from_2 : (int * (Hex_unified_connector_t.t * int) list) list ;  
+} ;;
