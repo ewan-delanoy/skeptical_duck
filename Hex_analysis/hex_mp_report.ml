5fe565baec919f01fdce3d31454dee36f46bc0b0
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-11-03 14:48:40 +0100

    add Hex_unified_connector.irregularities

--- Hex_analysis/hex_mp_report.ml
+++ Hex_analysis/hex_mp_report.ml
@@ -20,6 +20,22 @@ let one_step_constructor uc_report  =
 
 } ;;
 
+let irregularities l= 
+   let temp1 = List.flatten (Image.image snd l) in 
+   let temp2 = Image.image fst temp1 in 
+   let temp3 = Ordered.sort Total_ordering.standard temp2 in 
+   let temp4 = Uple.list_of_pairs temp3 in 
+   Option.filter_and_unpack (
+     fun (uc1,uc2) ->
+        let z = Hex_cell_set.intersect 
+          (Hex_unified_connector.support uc1) 
+          (Hex_unified_connector.support uc2)  in 
+        if Hex_cell_set.length(z)>0 
+        then Some(uc1,uc2,z)
+        else None   
+   ) temp4 ;;
+
+
 end ;; 
 
 
@@ -28,4 +44,12 @@ let cumulative_constructor fg =
    let uc_report = Hex_uc_report.one_step_constructor ctct_report in 
    Private.one_step_constructor uc_report  ;;  
 
+let irregularities mp_report =
+    (
+      Private.irregularities mp_report.Hex_mp_report_t.paths_from_1,
+      Private.irregularities mp_report.Hex_mp_report_t.paths_from_2
+    );;
+
 let one_step_constructor = Private.one_step_constructor ;;
+
+
