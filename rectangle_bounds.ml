fa259268041e87fb2148fe695b625ac28d6243e1
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-11-10 17:34:34 +0100

    add Rectangle_bounds.{combine,test}

--- rectangle_bounds.ml
+++ rectangle_bounds.ml
@@ -4,4 +4,17 @@
 
 *)
 
+let combine 
+  (Rectangle_bounds_t.B(xmin1,xmax1,ymin1,ymax1))
+    (Rectangle_bounds_t.B(xmin2,xmax2,ymin2,ymax2)) = 
+    (Rectangle_bounds_t.B(
+        max xmin1 xmin2,
+        min xmax1 xmax2,
+        max ymin1 ymin2,
+        min ymax1 ymax2)) ;;
+
+let test         
+(Rectangle_bounds_t.B(xmin,xmax,ymin,ymax)) (x,y)=
+  (xmin<=x) && (x<=xmax) && (ymin <=y) && (y<= ymax) ;;
+
 
