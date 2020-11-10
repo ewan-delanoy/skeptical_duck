cf1f9382912df8af51e48511ea705a283a4715f1
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-11-10 14:37:18 +0100

    add Hex_connector_name.opt_side

--- Hex_analysis/hex_connector_name.ml
+++ Hex_analysis/hex_connector_name.ml
@@ -17,18 +17,21 @@ end ;;
 let enders_for_side side = 
    Private.siders  (Hex_cardinal_direction.oppose side);;
 
+let is_inner = function
+   Hex_connector_name_t.Inner(_)-> true
+  |Border(_) -> false ;;
+
 let middlers = Image.image (
   fun inner -> Hex_connector_name_t.Inner(inner)
 ) Hex_inner_connector_name.all;;
 
+let opt_side = function
+    Hex_connector_name_t.Inner(inner)-> None
+   |Border(border) -> Some(Hex_border_connector_name.side border);;
+
 
 let starters_for_side = Private.siders ;;
    
-
-let is_inner = function
-    Hex_connector_name_t.Inner(_)-> true
-   |Border(_) -> false ;;
-
 let to_nondefault_molecular_linker cname apex junction= match cname with 
     Hex_connector_name_t.Inner(inner)-> Hex_inner_connector_name.to_nondefault_molecular_linker inner
    |Border(border) -> Hex_border_connector_name.to_nondefault_molecular_linker border apex junction;;
@@ -37,4 +40,3 @@ let to_readable_string = function
     Hex_connector_name_t.Inner(inner)-> Hex_inner_connector_name.to_readable_string inner
    |Border(border) -> Hex_border_connector_name.to_readable_string border;;
 
-   
