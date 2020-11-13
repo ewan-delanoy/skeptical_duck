0f66e7a0521c8859386989805fa99d007c4e6bc3
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-11-13 14:26:40 +0100

    add a few explanations in Hex_body_movement_t

--- Hex_analysis/hex_body_movement_t.ml
+++ Hex_analysis/hex_body_movement_t.ml
@@ -2,7 +2,7 @@
 
 #use"Hex_analysis/hex_body_movement_t.ml";;
 
-Data describing the border component of a Hex_island_t.t object
+Transformation to send a Hex_connector_t.t object to another Hex_connector_t.t object
 
 *)
 
@@ -11,4 +11,4 @@ type t= {
      reflect : bool ;
      oppose : bool ;
      translation : int * int 
-}
+} ;; 
