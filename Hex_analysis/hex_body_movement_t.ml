b30eb0de35bb922abbf29fcddc70faa6f55809d6
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-11-16 15:20:11 +0100

    add a dimension field to Hex_body_movement_t.t

--- Hex_analysis/hex_body_movement_t.ml
+++ Hex_analysis/hex_body_movement_t.ml
@@ -8,6 +8,7 @@ Transformation to send a Hex_connector_t.t object to another Hex_connector_t.t o
 
 
 type t= {
+     dimension : Hex_dimension_t.t ;
      reflect : bool ;
      oppose : bool ;
      translation : int * int 
