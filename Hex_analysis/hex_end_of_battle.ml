ce167fdbc9d7b944f0327490513a772b64861b0c
Ewan Delanoy <ewan.delanoy@zoho.com>
2020-11-27 14:41:36 +0100

    add free_territory field in Hex_end_of_battle_t (step two : update Hex_end_of_battle)

--- Hex_analysis/hex_end_of_battle.ml
+++ Hex_analysis/hex_end_of_battle.ml
@@ -18,14 +18,21 @@ let of_finished_game fgame =
          Hex_player_t.First_player -> (black_moves,white_moves)
                                 |_ -> (white_moves,black_moves)
      ) in 
+     let formal_dim = fgame.Hex_finished_game_t.dimension in 
+     let ally_terr  = Hex_cell_set.safe_set winner_moves 
+     and enemy_terr = Hex_cell_set.safe_set loser_moves in 
+     let occupied_terr = Hex_cell_set.merge ally_terr enemy_terr in 
+     let all_cells = Hex_common.all_cells formal_dim in 
      {
          Hex_end_of_battle_t.dimension       
-                         = fgame.Hex_finished_game_t.dimension ;
+                         = formal_dim ;
          winner          = w ; 
-         ally_territory  = Hex_cell_set.safe_set winner_moves ; 
-         enemy_territory = Hex_cell_set.safe_set loser_moves ;
+         ally_territory  = ally_terr ; 
+         enemy_territory = enemy_terr ;
+         free_territory = Hex_cell_set.setminus all_cells occupied_terr;
      } ;;
 
+(*     
 let of_activated_molecular (dim,the_winner) (active_part,mlclr) = 
     let passive_part = Hex_molecular_linker.support mlclr in 
     let needed_cells = Hex_cell_set.merge active_part passive_part in 
@@ -38,12 +45,4 @@ let of_activated_molecular (dim,the_winner) (active_part,mlclr) =
          ally_territory  = active_part ; 
          enemy_territory = unneeded_cells ;
      } ;;
-
-let remaining_free_cells end_of_battle = 
-    let dim = end_of_battle.Hex_end_of_battle_t.dimension in 
-    let all_cells = Hex_common.all_cells dim in 
-    let already_used_cells =
-        Hex_cell_set.merge 
-           end_of_battle.Hex_end_of_battle_t.ally_territory 
-            end_of_battle.Hex_end_of_battle_t.enemy_territory in 
-    Hex_cell_set.setminus all_cells already_used_cells ;;          
+*)
