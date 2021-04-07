8445d92eaa6b1febf1d890b7519f72280fda249d
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-04-07 09:19:46 +0200

    distinguish between foreign and national triples in Copy_and_staticize_calls_for_resources.script_to_wget_remote_data

--- Text_editing/copy_and_staticize_calls_for_resources.ml
+++ Text_editing/copy_and_staticize_calls_for_resources.ml
@@ -92,8 +92,11 @@ let enumerate_calls_for_several_starters starters text =
      let fn = Cull_string.cobeginning j1 url in 
      "curl -L \""^url^"\" > "^static_subdir_name^"/"^fn ;;  
      
+ let command_for_national 
+     website  (a,b,c) = 
+      "curl -L \""^website^"/"^c^"\" > "^a^"/"^b ;;
 
- let command_for_nonproxy  
+ let command_for_foreign  
     (website,static_subdir_name,endings_for_special_files)  (k,(a,b,c)) = 
      let ending = compute_ending endings_for_special_files b in 
      let sk = string_of_int k in 
@@ -103,11 +106,16 @@ let enumerate_calls_for_several_starters starters text =
       let (temp1,temp2) = List.partition (fun 
         (a,b,c) -> List.mem b list_of_proxies
       ) triples in 
-      let temp3 = Image.image (command_for_proxy static_subdir_name) temp1 in 
-      let temp4 = Image.image (command_for_nonproxy 
-        (website,static_subdir_name,endings_for_special_files))(Ennig.index_everything temp2) in 
+      let (temp3,temp4) = List.partition (fun 
+        (a,b,c) -> List.exists (fun (x,_)->x=b) endings_for_special_files
+      ) temp2 in 
+      let temp5 = Image.image (command_for_proxy static_subdir_name) temp1  
+      and temp6 = Image.image (command_for_foreign 
+        (website,static_subdir_name,endings_for_special_files))(Ennig.index_everything temp3) 
+      and temp7 = Image.image (command_for_national website) temp1 in 
       let create_static_subdir="mkdir -p "^building_site^"/"^static_subdir_name in      
-      String.concat "\n\n" (create_static_subdir :: (temp3@temp4)) ;;   
+      String.concat "\n\n" 
+         (create_static_subdir :: (temp5@["\n\n"]@temp6@["\n\n"]@temp7)) ;;   
 
     
  
