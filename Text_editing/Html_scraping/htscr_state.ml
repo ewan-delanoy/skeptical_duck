09d525a26eacaa35ffa63b4e1aacfe306d943114
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-04-10 14:56:08 +0200

    add Htscr_state.read_text

--- Text_editing/Html_scraping/htscr_state.ml
+++ Text_editing/Html_scraping/htscr_state.ml
@@ -39,11 +39,52 @@ let check_for_already_registered_request st req=
     let stored_ones = st.Htscr_state_t.stored_proxies @ st.Htscr_state_t.stored_static_homemades @ st.Htscr_state_t.stored_dynamic_homemades in 
     Option.seek (fun item->item.Htscr_item_t.original_request = req) stored_ones ;;
 
+let treat_one_request config st request =
+    match check_for_already_registered_request st request with 
+    Some(_) -> None
+    |None ->
+    (  
+    let old_proxy_count = st.Htscr_state_t.proxy_count 
+    and old_dynamic_count = st.Htscr_state_t.dynamic_count in  
+    match Htscr_item.compute_candidate config  ~old_proxy_count ~old_dynamic_count ~request with 
+     None -> None 
+    |Some(candidate) ->
+      let new_st = (match candidate.Htscr_item_t.category with 
+      Htscr_item_category_t.Proxy -> 
+        {st with 
+          Htscr_state_t.stored_proxies = (candidate :: st.Htscr_state_t.stored_proxies);
+          Htscr_state_t.proxy_count = 1+(st.Htscr_state_t.proxy_count);
+          }
+      |Static_homemade -> 
+        {st with 
+        Htscr_state_t.stored_static_homemades = (candidate :: st.Htscr_state_t.stored_static_homemades);
+        }
+      |Dynamic_homemade -> 
+        {st with 
+          Htscr_state_t.stored_dynamic_homemades = (candidate :: st.Htscr_state_t.stored_dynamic_homemades);
+          Htscr_state_t.dynamic_count = 1+(st.Htscr_state_t.dynamic_count);
+          }
+      ) in 
+      Some(candidate,new_st)  
+    );;     
 
-    
+let rec treat_several_requests config (accu,st,requests) = match requests with 
+  [] -> (accu,st)
+  |req :: other_requests -> 
+     (
+         match treat_one_request config st req with 
+         None -> treat_several_requests config (accu,st,other_requests)
+         |Some(item,new_st) ->  treat_several_requests config (item::accu,new_st,other_requests)
+     ) ;;
+
+let read_text config st ~text =
+    let requests = Htscr_extract_requests_from_text.main config ~text in 
+    treat_several_requests config ([],st,requests) ;;     
+
 end ;;
 
 
 let of_concrete_object = Private.of_concrete_object ;;
+let read_text = Private.read_text ;;
 let to_concrete_object = Private.to_concrete_object ;;
 
