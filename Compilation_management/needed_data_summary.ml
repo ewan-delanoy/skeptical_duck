9048232fcb04a0a57a85456e82306eabb271de4d
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-04-22 14:21:59 +0200

    modify Needed_data_summary.expand

--- Compilation_management/needed_data_summary.ml
+++ Compilation_management/needed_data_summary.ml
@@ -5,7 +5,7 @@
 
 *)
 
-(*
+
 module Private = struct 
 
 let modules_coming_from_modules_or_subdirs cs needed_modules needed_subdirs =
@@ -24,34 +24,37 @@ let modules_coming_from_modules_or_subdirs cs needed_modules needed_subdirs =
         else None)
     all_elesses ;;
     
-let rootlesses_to_be_copied cs summary =
+let expand cs summary =
         let fw = cs.Coma_state_t.frontier_with_unix_world in 
         let all_needed_modules =
         (match summary with 
         Needed_data_summary_t.Everything -> Coma_state.ordered_list_of_modules cs
        |Selection(needed_modules,needed_subdirs)-> 
         modules_coming_from_modules_or_subdirs cs needed_modules needed_subdirs
-             ) in   
-             let compilables= selector (fw.Fw_wrapper_t.compilable_files)
-             and noncompilables= selector (fw.Fw_wrapper_t.noncompilable_files) in 
-             let all_needed_modules= (Image.image Dfn_rootless.to_module compilables) 
-                              @ needed_modules in 
-             let (modules_in_good_order,all_nonspecials)=rootlesses_coming_from_modules cs all_needed_modules in 
-             (modules_in_good_order,all_nonspecials,noncompilables);;
+             ) in 
+        let original_noncompilables = fw.Fw_wrapper_t.noncompilable_files in      
+        let noncompilables =
+            (match summary with 
+             Needed_data_summary_t.Everything -> Image.image fst original_noncompilables
+            |Selection(needed_modules,needed_subdirs)-> 
+              Option.filter_and_unpack (
+                fun (rless,_)->
+                    if List.mem (Dfn_rootless.to_subdirectory rless) needed_subdirs 
+                    then Some rless
+                    else None    
+            ) original_noncompilables) in        
+        let compilables= Option.filter_and_unpack (
+            fun (rless,_)->
+                if List.mem (Dfn_rootless.to_module rless) all_needed_modules 
+                then Some rless
+                else None    
+        ) (fw.Fw_wrapper_t.compilable_files) in
+        (all_needed_modules,compilables,noncompilables);;
 
 
 end ;;
 
-let expand cs summary=
-  let modules = (
-    match summary with 
-    Needed_data_summary_t.Everything -> cs.Coma_state_t.modules 
-    |Selection(needed_modules,needed_subdirs) ->
-        Private.rootlesses_coming_from_modules_or_subdirs 
-            cs needed_modules needed_subdirs   
-  ) in 
-  let collected_acolytes=List.flatten  (Image.image (Coma_state.acolytes_at_module cs) modules) in 
-  (modules,Image.image Dfn_full.to_rootless collected_acolytes);;
+let expand = Private.expand ;;
 
 let is_everything = function Needed_data_summary_t.Everything -> true | _ -> false ;;
-*)
+
