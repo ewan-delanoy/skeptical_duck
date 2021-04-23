32a505f3d2a2928f0780664e91eb2c95ad8d03ba
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-04-23 14:20:14 +0200

    add Create_world_copy.frozen_copy

--- Compilation_management/create_world_copy.ml
+++ Compilation_management/create_world_copy.ml
@@ -54,12 +54,7 @@ module Private = struct
   
   let default_backup_dir=Coma_big_constant.Next_World.backup_dir;;
 
-
-  end ;;   
-  
-  
-  
-  let cwc cs ~destination ?(destbackupdir=Private.default_backup_dir) ?(destgab=false) summary=
+  let frozen_copy cs ~destination ?(destbackupdir=default_backup_dir) ?(destgab=false) summary =
       let url=Coma_big_constant.github_url in 
       let conv_files = (
         if Needed_data_summary.is_everything summary
@@ -74,12 +69,16 @@ module Private = struct
       let (modules_in_good_order,compilables,noncompilables) = 
           Needed_data_summary.expand cs summary in 
       let _=Image.image Unix_command.uc 
-       (Private.commands_for_copying cs (compilables@noncompilables) destination) in
+       (commands_for_copying cs (compilables@noncompilables) destination) in
       let faraway_config = Fw_configuration.constructor (destination,destbackupdir,destgab,url,[]) in 
       let faraway_fw1 = Fw_initialize.compute_and_store_modification_times faraway_config (compilables,noncompilables) in  
-      let faraway_fw = Fw_wrapper.overwrite_compilable_file_if_it_exists faraway_fw1 
+      let faraway_fw =Fw_wrapper.overwrite_compilable_file_if_it_exists faraway_fw1 
                      Coma_constant.rootless_path_for_parametersfile 
-                       (Private.text_for_big_constants_file_in_other_world destination destbackupdir destgab) in 
+                       (text_for_big_constants_file_in_other_world destination destbackupdir destgab) in 
+      (modules_in_good_order,faraway_fw);; 
+
+  let fully_developed_copy cs ~destination ?(destbackupdir=default_backup_dir) ?(destgab=false) summary=
+      let (modules_in_good_order,faraway_fw) = frozen_copy cs ~destination ~destbackupdir ~destgab summary in 
       let restricted_cs=(if Needed_data_summary.is_everything summary
           then cs 
           else Coma_state_field.restrict cs modules_in_good_order
@@ -89,7 +88,17 @@ module Private = struct
       let faraway_cs = Coma_state.update_just_one_module faraway_cs1  Coma_constant.rootless_path_for_parametersfile in   
       let faraway_cs2 = Modify_coma_state.Internal.recompile (faraway_cs,[],[]) in 
       let _=Save_coma_state.save faraway_cs2 in   
-      faraway_cs2;;    
+      faraway_cs2;;                      
+
+
+  end ;;   
+  
+  let frozen_copy cs ~destination ?(destbackupdir=Private.default_backup_dir) ?(destgab=false) summary=
+     let _ = Private.frozen_copy cs ~destination ~destbackupdir ~destgab summary in 
+     () ;;
+  
+  let cwc = Private.fully_developed_copy ;; 
+
   
   
   
