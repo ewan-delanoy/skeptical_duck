0376ff378b48aeb47e31e8ae3ab62c6490ee4b0f
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-04-23 07:18:41 +0200

    do more rigorous analysis in Needed_data_summary.expand

--- Compilation_management/needed_data_summary.ml
+++ Compilation_management/needed_data_summary.ml
@@ -8,7 +8,7 @@
 
 module Private = struct 
 
-let compute_all_needed_modules cs needed_modules needed_subdirs =
+let compute_all_needed_elesses cs needed_modules needed_subdirs =
     let all_elesses = Coma_state.all_endinglesses cs in 
     let step1_modules = Option.filter_and_unpack 
     (fun eless->
@@ -23,33 +23,35 @@ let compute_all_needed_modules cs needed_modules needed_subdirs =
     let list_of_modules_with_nonstandard_ordering = 
           Ordered.sort Total_ordering.standard (modules_above@step2_modules) in 
     let all_elesses = Coma_state.all_endinglesses cs in 
-    Option.filter_and_unpack 
-        (fun eless->
-          if List.mem (Dfn_endingless.to_module eless) list_of_modules_with_nonstandard_ordering
-        then Some(Dfn_endingless.to_module eless)
-        else None)
+    List.filter 
+        (fun eless-> List.mem (Dfn_endingless.to_module eless) list_of_modules_with_nonstandard_ordering)
     all_elesses ;;
     
 let expand cs summary =
         let fw = cs.Coma_state_t.frontier_with_unix_world in 
-        let all_needed_modules =
+        let all_needed_elesses =
         (match summary with 
-        Needed_data_summary_t.Everything -> Coma_state.ordered_list_of_modules cs
+        Needed_data_summary_t.Everything -> Coma_state.all_endinglesses cs
        |Selection(needed_modules,needed_subdirs)-> 
-        compute_all_needed_modules cs needed_modules needed_subdirs
+        compute_all_needed_elesses cs needed_modules needed_subdirs
              ) in 
-        (*     
         let all_needed_subdirs = 
-                Ordered.sort Total_ordering.standard (Image.image Dfa_ all_needed_module) in  
-        *)            
-        let original_noncompilables = fw.Fw_wrapper_t.noncompilable_files in      
+                Ordered.sort Total_ordering.standard 
+                  (Image.image Dfn_endingless.to_subdirectory all_needed_elesses) 
+        and all_needed_modules = 
+         Image.image Dfn_endingless.to_module all_needed_elesses in      
+        let original_noncompilables = fw.Fw_wrapper_t.noncompilable_files in
+        (*
+           we do not know a priori if the noncompilables in other subdirectories
+           are needed, so we include them all by default 
+        *)      
         let noncompilables =
             (match summary with 
              Needed_data_summary_t.Everything -> Image.image fst original_noncompilables
             |Selection(needed_modules,needed_subdirs)-> 
               Option.filter_and_unpack (
                 fun (rless,_)->
-                    if List.mem (Dfn_rootless.to_subdirectory rless) needed_subdirs 
+                    if List.mem (Dfn_rootless.to_subdirectory rless) all_needed_subdirs 
                     then Some rless
                     else None    
             ) original_noncompilables) in        
