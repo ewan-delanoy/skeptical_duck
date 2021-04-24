c2b2e9353da3c46058dc451595cd3ee921e81ef5
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-04-24 10:31:59 +0200

    add Coma_big_constant.home (and use it)

--- Compilation_management/coma_big_constant.ml
+++ Compilation_management/coma_big_constant.ml
@@ -1,30 +1,31 @@
 
 (* 
-#use"Makefile_makers/coma_big_constant.ml";;
+#use"Compilation_management/coma_big_constant.ml";;
 *)
 
 let github_url = "https://github.com/ewan-delanoy/skeptical_duck";;
+let home = Sys.getenv "HOME" ;;
 
 module This_World=struct
 
-let root=Dfa_root.of_line "/Users/ewandelanoy/Teuliou/OCaml/Ordinary";;
-let backup_dir=Dfa_root.of_line "/Users/ewandelanoy/Teuliou/OCaml/Githubbed_ocaml";;
+let root=Dfa_root.of_line (home^"/Teuliou/OCaml/Ordinary");;
+let backup_dir=Dfa_root.of_line (home^"/Teuliou/OCaml/Githubbed_ocaml");;
 let githubbing=true;;
 let triple = (root,backup_dir,githubbing);;
 
 end;;
 module Next_World=struct
 
-let root=Dfa_root.of_line "/Users/ewandelanoy/Teuliou/OCaml/Idaho";;
-let backup_dir=Dfa_root.of_line "/Users/ewandelanoy/Teuliou/OCaml/Idaho_backup";;
+let root=Dfa_root.of_line (home^"/Teuliou/OCaml/Idaho");;
+let backup_dir=Dfa_root.of_line (home^"/Teuliou/OCaml/Idaho_backup") ;;
 let githubbing=false;;
 let triple = (root,backup_dir,githubbing);;
 
 end;;
 module Third_World=struct
 
-let root=Dfa_root.of_line "/Users/ewandelanoy/Teuliou/OCaml/Cherokee";;
-let backup_dir=Dfa_root.of_line "/Users/ewandelanoy/Teuliou/OCaml/Cherokee_backup";;
+let root=Dfa_root.of_line (home^"/Teuliou/OCaml/Cherokee") ;;
+let backup_dir=Dfa_root.of_line (home^"/Teuliou/OCaml/Cherokee_backup") ;;
 let githubbing=false;;
 let triple = (root,backup_dir,githubbing);;
 
