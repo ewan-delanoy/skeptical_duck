5b76c48195a98a5052e3ca48eeda630c6fe7f3cc
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-04-30 15:25:12 +0200

    remove unused values in Coma_constant.Private

--- Compilation_management/coma_constant.ml
+++ Compilation_management/coma_constant.ml
@@ -27,12 +27,9 @@ let exec_build_subdir=  Dfa_subdirectory.extend build_subdir "_exec_build";;
 let parameters_subdir= Dfa_subdirectory.of_line "Compilation_management";;
 
 
-let bare_name_for_makefile="makefile";;
-let bare_name_for_merlinfile=".merlin";;
 
 
 let short_path_for_loadingsfile= Dfn_short.of_line"my_loadings.ml";;
-let short_path_for_ocamldebug_printersfile= Dfn_short.of_line"cmos_for_ocamldebug.txt";;
 let short_path_for_painful_debugging_file=Dfn_short.of_line"painful_debugging.ml";;
 let short_path_for_parametersfile= Dfn_short.of_line "coma_big_constant.ml";;
 let short_path_for_printersfile= Dfn_short.of_line "my_printers.ml";;
@@ -51,15 +48,6 @@ let rootless_path_for_targetfile=
 
 let rootless_path_for_ocamlinit = Dfn_rootless.of_line ".ocamlinit";;
 
-
-
-let rootless_paths_needed_for_compiler_copy=
-    [
-      rootless_path_for_loadingsfile;
-      rootless_path_for_printersfile;
-      rootless_path_for_targetfile;
-      rootless_path_for_ocamlinit
-    ];;
 
 let git_ignored_subdirectories =
   [
