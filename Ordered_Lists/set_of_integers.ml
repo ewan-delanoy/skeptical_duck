e93c74a75bcc9a3d276be7f5de39f0470060f9f9
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-01 15:27:20 +0200

    add Set_of_integers.size_of_intersection

--- Ordered_Lists/set_of_integers.ml
+++ Ordered_Lists/set_of_integers.ml
@@ -19,6 +19,7 @@ let outsert x oy= Functor_for_sets.outsert tr x oy;;
 let safe_set l= Functor_for_sets.safe_set tr l;;
 let setminus x y= Functor_for_sets.setminus tr x y;;
 let singleton a= Functor_for_sets.singleton tr a;;
+let size_of_intersection x y= Functor_for_sets.size_of_intersection tr x y;;
 let sort l= Functor_for_sets.sort tr l;;
 let unsafe_set l= Functor_for_sets.unsafe_set tr l;;
 
