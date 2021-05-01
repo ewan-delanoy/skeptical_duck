84f7ad0a6125d542e423db2e025c9df932d7675f
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-01 11:28:25 +0200

    add Set_of_integers.is_included_in

--- Ordered_Lists/set_of_integers.ml
+++ Ordered_Lists/set_of_integers.ml
@@ -9,6 +9,7 @@ let tr = ((fun x->Set_of_integers_t.S(x)),(fun (Set_of_integers_t.S(x))->x),Tota
 let fold_merge x = Functor_for_sets.fold_merge tr x;;
 let forget_order x= Functor_for_sets.forget_order tr x;;
 let image f x= Functor_for_sets.image tr f x;;
+let is_included_in x y= Functor_for_sets.is_included_in tr x y;;
 let mem a x= Functor_for_sets.mem tr a x;;
 let merge l= Functor_for_sets.merge tr l;;
 let safe_set l= Functor_for_sets.safe_set tr l;;
