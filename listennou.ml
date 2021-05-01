8b287472b36f6de891f1dba11513566171817c68
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-01 16:18:17 +0200

    add Listennou decompose_into_connected_components

--- listennou.ml
+++ listennou.ml
@@ -397,5 +397,34 @@ let partition_according_to_fst pairs=
    ) in 
    tempf ([],pairs) ;;
   
+exception Compute_largest_connected_interval_on_the_left_exn ;;
+
+let compute_largest_connected_interval_on_the_left initial_l =
+  let rec tempf = (fun  (a,b,l)->
+   match l with 
+   [] -> ((a,b),[])
+   |head :: tail -> if head = b+1 
+                    then tempf(a,b+1,tail)
+                    else ((a,b),l)  
+  ) in  
+  match initial_l with 
+  [] -> raise Compute_largest_connected_interval_on_the_left_exn
+  | head2 :: tail2 -> tempf (head2,head2,tail2) ;;
+
+let decompose_into_connected_components l=
+  let rec tempf = (fun 
+     (treated,to_be_treated)->
+     if to_be_treated = [] then List.rev treated else 
+     let (interval,others) =  compute_largest_connected_interval_on_the_left to_be_treated in 
+     tempf (interval::treated,others)
+  ) in 
+  tempf ([],l);;
+
+(*
+
+decompose_into_connected_components [3; 4; 5; 6; 7; 10; 11; 12; 13; 14; 15; 31; 32; 33; 34; 35; 36; 37; 38; 39; 40;
+ 41; 42; 43; 44; 45; 46; 47; 48];;
+
+*)
 
 
