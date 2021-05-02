768bf5a933b39568ed05bfbc7fc680e272711dac
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-02 14:11:49 +0200

    register Van_der_waerden/vdw_memoize.ml

---
+++ Van_der_waerden/vdw_memoize.ml
@@ -0,0 +1,150 @@
+(*
+
+#use"Van_der_waerden/vdw_memoize.ml";;
+
+*)
+
+
+let hashtbl_for_vdw_computation = ((Hashtbl.create 100): (int * Set_of_integers_t.t, int * Set_of_integers_t.t) Hashtbl.t) ;;
+
+let ii x y =  (Ennig.ennig x y);;
+let iii l =   (List.flatten(Image.image (fun (x,y)->Ennig.ennig x y) l));;
+
+let translate d soi = Set_of_integers.safe_set (Set_of_integers.image (fun x->x+d) soi);;
+
+let test_for_disjointness ll=
+   let temp1 = Uple.list_of_pairs ll in 
+   List.for_all (fun (x,y)->(Set_of_integers.size_of_intersection x y) = 0) temp1 ;; 
+
+let solution_in_disjoint_case obstructions soi = 
+   let temp1 = Image.image Set_of_integers.max obstructions in
+   Set_of_integers.setminus soi (Set_of_integers.safe_set temp1) ;;
+
+let set_start_to_one soi =
+     let d = (Set_of_integers.min soi)-1 in 
+     (d,translate (-d) soi);;
+
+let level1 soi = 
+     let l = Set_of_integers.forget_order soi in 
+     let intervals = Listennou.decompose_into_connected_components l in 
+     let temp1 = List.flatten(Image.image (
+       fun (a,b)-> List.filter (fun x->List.mem ((x-a) mod 3)[0;1] ) (Ennig.ennig a b)
+     ) intervals) in 
+     (List.length temp1,Set_of_integers.safe_set temp1) ;;
+    
+let partial_level3 n = List.filter (fun x->List.mem(x mod 8)[1;2;4;5]) (Ennig.ennig 1 n);;     
+
+let check_for_precomputed_value (width,soi) =
+   let (d,relocated_soi) = set_start_to_one soi in 
+   match Hashtbl.find_opt hashtbl_for_vdw_computation (width,relocated_soi) with 
+   (Some(optimal_size,sol)) -> Some(optimal_size,translate d sol)
+   |None ->
+      let obstructions = Vdw_common.look_for_arithmetic_progressions_in_with_width_up_to width soi in 
+      if test_for_disjointness obstructions
+      then let sol = solution_in_disjoint_case obstructions soi in 
+            Some(Set_of_integers.length sol,sol)
+      else      
+      if width=1 
+      then Some(level1 soi) 
+      else None;;
+ 
+exception Too_small_for_extension of int;;
+exception Unforeseen_value_in_extend of int * Set_of_integers_t.t ;;
+exception Extension_not_allowed of int * Set_of_integers_t.t * Set_of_integers_t.t * Set_of_integers_t.t ;;
+
+let extend width ?opt_sol l_soi =
+ let soi= Set_of_integers.safe_set l_soi in  
+  if width<2 then raise(Too_small_for_extension width) else 
+  match  check_for_precomputed_value (width-1,soi) with 
+  None -> raise (Unforeseen_value_in_extend(width-1,soi))
+  |Some (optimal_size,old_sol) ->
+     let sol=(match opt_sol with None ->old_sol |Some(forced_sol) -> forced_sol) in 
+     let temp1= Vdw_common.look_for_arithmetic_progressions_in_with_width_up_to width sol in 
+     if temp1<>[]
+     then raise(Extension_not_allowed(width,soi,sol,List.hd temp1))
+     else Hashtbl.add hashtbl_for_vdw_computation (width,soi) (optimal_size,sol) ;;
+
+
+exception Unforeseen_value_in_decomposition of int * Set_of_integers_t.t * Set_of_integers_t.t ;;
+exception Incorrect_input_in_decomposition of int * Set_of_integers_t.t * Set_of_integers_t.t ;;
+exception Size_mismatch of int * int * int ;;
+     
+let get_half width whole half =match check_for_precomputed_value (width,half) with 
+     None -> raise(Unforeseen_value_in_decomposition(width,whole,half))
+     |Some(optimal_size,_) -> optimal_size ;;
+
+let register_decomposition width l_first_half l_other_half l_sol=
+   let first_half = Set_of_integers.safe_set l_first_half 
+   and other_half = Set_of_integers.safe_set l_other_half 
+   and sol = Set_of_integers.safe_set l_sol in
+   let temp1= Vdw_common.look_for_arithmetic_progressions_in_with_width_up_to width sol in 
+   if temp1<>[]
+   then raise(Incorrect_input_in_decomposition(width,sol,List.hd temp1))
+   else 
+   let whole = Set_of_integers.merge first_half other_half in 
+   let n1 = get_half width whole first_half
+   and n2 = get_half width whole other_half 
+   and n=Set_of_integers.length sol in
+   if n<>n1+n2
+   then  raise(Size_mismatch(n,n1,n2))
+   else Hashtbl.add hashtbl_for_vdw_computation (width,whole) (n,sol) ;;
+ 
+
+exception  Unforeseen_value_in_fork of  int * Set_of_integers_t.t * Set_of_integers_t.t ;;
+exception  Incorrect_triple of int * (int * int * int) ;;
+exception  Foreign_triple of  Set_of_integers_t.t * (int * int * int) ;;
+
+let get_third width whole third =match check_for_precomputed_value (width,third) with 
+   None -> raise(Unforeseen_value_in_decomposition(width,whole,third))
+   |Some(optimal_size,sol) -> (optimal_size,sol) ;;   
+
+let register_fork width (a,b,c) l_soi=
+   let soi = Set_of_integers.safe_set l_soi in
+   if (a>=b)||(b>=c)||(b-a<>c-b)||(b-a>width)
+   then raise(Incorrect_triple(width,(a,b,c)))
+   else 
+   let triple = Set_of_integers.safe_set [a;b;c]  in 
+   if not(Set_of_integers.is_included_in triple soi) 
+   then raise(Foreign_triple(soi,(a,b,c)))
+   else 
+   let temp1 = Image.image (fun v->
+      get_third width soi (Set_of_integers.outsert v soi)) [a;b;c] in 
+   let (optimal_size,pairs) = Max.maximize_it_with_care fst temp1 in
+   let sols = Image.image snd pairs in 
+   let best_sol = Total_ordering.min Vdw_common.silex_order sols in 
+   Hashtbl.add hashtbl_for_vdw_computation (width,soi) (optimal_size,best_sol) ;;
+
+extend 2 (ii 1 3) ;;
+extend 2 (iii [1,3;5,7]);;
+extend 3 (ii 1 3) ;;
+extend 3 (iii [1,3;5,7]);;
+register_decomposition 3 (ii 1 3) [4]      (partial_level3 4);;
+register_decomposition 3 (ii 1 3) (ii 4 5) (partial_level3 5);;
+register_decomposition 3 (ii 1 3) (ii 4 6) (partial_level3 6);;
+register_fork 3 (1,4,7) (ii 1 7) ;;
+
+(*
+let comp width l_soi = check_for_precomputed_value(width,Set_of_integers.safe_set l_soi) ;;
+
+comp  3 (ii 1 3) ;;
+let ff n = comp 3 (ii 1 n);;
+let ns w l= Vdw_common.naive_solver 
+    (Vdw_list_of_constraints_t.Defined_by_max_width w) 
+      (Set_of_integers.safe_set l) ;;
+
+let w1 = Vdw_list_of_constraints_t.Defined_by_max_width 3 ;;
+let x1 = Set_of_integers.safe_set (ii 1 8) ;;
+
+let res1 = Vdw_common.naive_solver w1 x1 ;;
+let res2 = Vdw_common.naively_compute_decomposers w1 x1 ;;
+let res3 = Vdw_common.first_cut w1 x1;;
+     
+let x1 = Set_of_integers.safe_set (iii [1,4;6,8]) ;;
+
+let res1 = Vdw_common.naive_solver w1 x1 ;;
+let res2 = Vdw_common.naively_compute_decomposers w1 x1 ;;
+let res3 = Vdw_common.first_cut w1 x1;;
+
+
+let g1 = iii [1,3;5,7];;
+*)
