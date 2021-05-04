9111dff30df131eb3224399d162fc46618c630df
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-04 06:28:43 +0200

    insert one more boolean in Scct_uple_level_t

--- Ocaml_analysis/Standardized_concrete_ocaml_types/scct_uple_level_t.ml
+++ Ocaml_analysis/Standardized_concrete_ocaml_types/scct_uple_level_t.ml
@@ -6,8 +6,10 @@ An element in the uple is represented by a string * bool * Scct_atomic_type_t.t 
 
 The string is used for variable naming, the bool indicates whether we have a list or not.
 
+Similary, the leftmost bool in the definition indicates wether we have 
+an uple or a list of uples.
 
 *)
 
-type t= U of (string * bool * Scct_atomic_type_t.t ) list ;;
+type t= U of bool * ((string * bool * Scct_atomic_type_t.t ) list) ;;
 
