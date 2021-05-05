40439397dbc98f6936f102a76a8cbb19d9bc392a
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-05 07:26:08 +0200

    add Scct_common.arguments_in_input

--- Ocaml_analysis/Standardized_concrete_ocaml_types/scct_common.ml
+++ Ocaml_analysis/Standardized_concrete_ocaml_types/scct_common.ml
@@ -5,7 +5,19 @@
 
 *)
 
+exception Too_many_arguments of int ;;
+
 let wrap_in_parentheses_if_needed typename =
     if String.contains typename ' '
     then "( "^typename^" )"
     else  typename ;;    
+
+let max_nbr_of_arguments = 7 ;;    
+
+let arguments_in_input argname l=
+    let n = List.length(l) in 
+    if n> max_nbr_of_arguments 
+    then raise(Too_many_arguments(n))
+    else let temp1 = Ennig.doyle (fun k->
+          if k<=n then argname^(string_of_int k) else "_") 1 max_nbr_of_arguments in 
+         "(" ^ (String.concat "," temp1) ^ ")" ;;    
