64023dc19e8be28974f8558d316149c8b261a883
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-08 18:47:28 +0200

    register Ocaml_analysis/Standardized_concrete_ocaml_types/scct_main_t.ml

---
+++ Ocaml_analysis/Standardized_concrete_ocaml_types/scct_main_t.ml
@@ -0,0 +1,14 @@
+(* 
+
+#use"Ocaml_analysis/Standardized_concrete_ocaml_types/scct_main_t.ml";;
+
+
+
+*)
+
+type t= {
+   modulename : string ;
+   kind : Scct_either_variant_or_record_t.t ;
+   data : Scct_possibly_listy_uple_t.t list ;
+} ;;
+
