d84f1409a4a51ef3d9fe4cfd154b8c5ddfa7aca2
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-11 16:10:04 +0200

    register Ocaml_analysis/Concrete_ocaml_objects/concrete_ocaml_type_t.ml

---
+++ Ocaml_analysis/Concrete_ocaml_objects/concrete_ocaml_type_t.ml
@@ -0,0 +1,20 @@
+(* 
+
+#use"Ocaml_analysis/Concrete_ocaml_objects/concrete_ocaml_type_t.ml";;
+
+You might be suprised that in Uple or Variant we have
+a (string*t) list instead of just a t list. 
+That's because we associate a descriptive name to each type
+appearing in a definition.
+
+*)
+
+type t= 
+    Int 
+   |String 
+   |Uple of (string * t) list
+   |List of string * t 
+   |Array of string * t 
+   |Record of (string * t) list
+   |Variant of string * ((string * t) list);;
+
