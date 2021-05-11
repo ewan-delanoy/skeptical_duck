305e0ed169d465d5597c3dd559200a1dd625f552
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-11 09:19:34 +0200

    delete Hall_algorithm.

--- hall_algorithm.mli
+++
@@ -1,8 +0,0 @@
-type ('a, 'b) t 
-val constructor :
-  ('a -> 'a -> bool) -> ('a * 'b Set_of_polys_t.t) list -> ('a, 'b) t
-val add_chooser : ('a -> 'a -> bool) -> 'a * 'b -> ('a, 'b) t -> ('a, 'b) t
-val solve_explicitly :
-  ('a -> 'a -> bool) ->
-  ('a, 'b) t -> (('a * 'b Set_of_polys_t.t) * 'b) list * ('a * 'b) list
-val solve : ('a -> 'a -> bool) -> ('a, 'b) t -> ('a * 'b) list
