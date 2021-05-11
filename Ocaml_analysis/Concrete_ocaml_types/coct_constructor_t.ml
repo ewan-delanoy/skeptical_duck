c2544bf8050d671c8fd2468ef8b37fe0961a3b6e
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-11 18:31:34 +0200

    register Ocaml_analysis/Concrete_ocaml_types/coct_constructor_t.ml

---
+++ Ocaml_analysis/Concrete_ocaml_types/coct_constructor_t.ml
@@ -0,0 +1,17 @@
+(* 
+
+#use"Ocaml_analysis/Concrete_ocaml_types/coct_constructor_t.ml";;
+
+
+*)
+
+type t= 
+    Int 
+   |String 
+   |Uple 
+   |List 
+   |Array 
+   |Record 
+   |DisjunctionOfVariants 
+   |Modularized ;;
+
