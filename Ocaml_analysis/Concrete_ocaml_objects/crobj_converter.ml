92e22c9782f1272191ef4ea5ff7b7f1d1d785b9f
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-18 15:15:41 +0200

    add Crobj_converter.{int,string}_{of,to}_concrete_object

--- Ocaml_analysis/Concrete_ocaml_objects/crobj_converter.ml
+++ Ocaml_analysis/Concrete_ocaml_objects/crobj_converter.ml
@@ -8,8 +8,7 @@
 
 exception Unwrap_int_exn of Concrete_object_t.t;;
 exception Unwrap_string_exn of Concrete_object_t.t;;
-   
-module Temp = struct
+
    
 module Pair = struct 
 
@@ -90,4 +89,17 @@ module To = struct
 
 end ;;
 
-end ;;
+
+
+
+let int_of_concrete_object ccrt_obj =
+    match ccrt_obj with 
+     Concrete_object_t.Int(i)->i 
+    |_->raise(Unwrap_int_exn(ccrt_obj)) ;;
+let int_to_concrete_object i = Concrete_object_t.Int i ;;       
+
+let string_of_concrete_object ccrt_obj =
+       match ccrt_obj with 
+       Concrete_object_t.String(encoded_s)->Encoded_string.decode encoded_s 
+       |_->raise(Unwrap_string_exn(ccrt_obj)) ;;
+let string_to_concrete_object s = Concrete_object_t.String(Encoded_string.encode s) ;;       
