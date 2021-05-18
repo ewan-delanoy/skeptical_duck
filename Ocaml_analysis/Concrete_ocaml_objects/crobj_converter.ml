f804043095838a40cdea050892eee01384aa7899
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-18 15:33:59 +0200

    add Crobj_converter.bool_{of,to}_concrete_object

--- Ocaml_analysis/Concrete_ocaml_objects/crobj_converter.ml
+++ Ocaml_analysis/Concrete_ocaml_objects/crobj_converter.ml
@@ -9,7 +9,7 @@
 exception Unwrap_int_exn of Concrete_object_t.t;;
 exception Unwrap_string_exn of Concrete_object_t.t;;
 
-(*   
+   
 module Pair = struct 
 
    let bool =((fun bowl->
@@ -88,9 +88,13 @@ module To = struct
    let string_triple = snd(Pair.string_triple);; 
 
 end ;;
-*)
 
 
+let bool_of_concrete_object = Concrete_object_field.unwrap_lonely_variant [true,"True";false,"False"];;
+let bool_to_concrete_object bowl = 
+    if bowl 
+    then Concrete_object_t.Variant("True",[]) 
+    else Concrete_object_t.Variant("False",[]);;  
 
 let int_of_concrete_object ccrt_obj =
     match ccrt_obj with 
