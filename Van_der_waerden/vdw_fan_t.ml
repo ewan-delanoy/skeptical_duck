a620c8808a721e874fa1c17a1176e5d818bf7f03
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-26 16:38:34 +0200

    register Van_der_Waerden/vdw_fan_t.ml

---
+++ Van_der_waerden/vdw_fan_t.ml
@@ -0,0 +1,12 @@
+(*
+
+#use"Van_der_Waerden/vdw_fan_t.ml";;
+
+A fan is a finite union of translates of parts from
+an initial partition (i.e. an object of type )
+
+*)
+
+type t = F of (int * (int list)) list;;
+
+  
