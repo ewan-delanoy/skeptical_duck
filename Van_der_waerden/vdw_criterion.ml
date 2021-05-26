e8ae5757193998050fb3d684ec18ba7e5fc80734
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-26 07:04:15 +0200

    register Van_der_Waerden/vdw_criterion.ml

---
+++ Van_der_waerden/vdw_criterion.ml
@@ -0,0 +1,22 @@
+(*
+
+#use"Van_der_Waerden/vdw_criterion.ml";;
+
+*)
+
+module Private = struct
+
+let test criterion l = match criterion with 
+   (Vdw_criterion_t.Cardinality_lower_than_or_equal_to k) ->
+        (List.length l)=k
+  |(Compatible_with l2) ->
+        let z= Set_of_integers.safe_set 
+         (l@l2) in 
+       Vdw_common.test_for_admissibility
+      (Vdw_list_of_constraints_t.Defined_by_max_width Vdw_current_bound.bound)
+      z ;;
+
+end ;;     
+  
+let partition criterion ll =
+     List.partition (Private.test criterion) ll;;
