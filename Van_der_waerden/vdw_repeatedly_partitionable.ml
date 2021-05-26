cea16441f30e80940360ff84b4947ea9d26617ff
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-26 15:46:41 +0200

    register Van_der_Waerden/vdw_repeatedly_partitionable.ml

---
+++ Van_der_waerden/vdw_repeatedly_partitionable.ml
@@ -0,0 +1,50 @@
+(*
+
+#use"Van_der_Waerden/vdw_repeatedly_partitionable.ml";;
+
+*)
+
+module Private = struct 
+
+let enhance rp (part_idx,criterion) =
+    let old_parts = rp.Vdw_repeatedly_partitionable_t.parts 
+    and old_main = rp.Vdw_repeatedly_partitionable_t.main 
+    and old_history = rp.Vdw_repeatedly_partitionable_t.history in
+    let part = List.assoc part_idx old_parts in 
+    let (part1,part2) = Vdw_criterion.partition criterion part in
+    if (part1=[]) || (part2=[])
+    then (rp,None) 
+    else 
+    let n = List.length old_parts in 
+    let summary = (part_idx,criterion,n+1,n+2) in 
+    ({
+      Vdw_repeatedly_partitionable_t.parts = old_parts @ [(n+1,part1);(n+2,part2)];
+       main = List.flatten (Image.image (fun idx->
+              if idx=part_idx then [n+1;n+2] else [idx]
+              ) old_main) ;
+       history = (part_idx,criterion,n+1,n+2) :: old_history; 
+     },Some(summary)) ;;
+      
+let rec iterator_for_multiple_enhancer (already_treated,walker,to_be_treated) =
+    match to_be_treated with 
+    [] ->  (walker,List.rev already_treated)
+    | item :: others -> 
+        let (new_walker,opt_enhancement) = enhance  walker item in 
+        let already_treated2 = (match  opt_enhancement with 
+        None ->  already_treated
+        |Some(enhancement) -> enhancement::already_treated
+        ) in 
+        iterator_for_multiple_enhancer (already_treated2,new_walker,others) ;;
+end ;;  
+
+let partition rp enhancements =
+   Private.iterator_for_multiple_enhancer ([],rp,enhancements) ;;
+
+let start ll =
+   {
+    Vdw_repeatedly_partitionable_t.parts = [1,ll];
+     main = [1];
+     history = []; 
+   } ;;
+
+  
