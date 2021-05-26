11704c131c7cd44cf5f24b9cb1ff75520d2b01fe
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-26 07:16:08 +0200

    register Van_der_Waerden/vdw_repeatedly_partitionable_t.ml

---
+++ Van_der_waerden/vdw_repeatedly_partitionable_t.ml
@@ -0,0 +1,14 @@
+(*
+
+#use"Van_der_Waerden/vdw_repeatedly_partitionable_t.ml";;
+
+*)
+
+type t =
+   {
+     parts : (int * (int list list)) list;
+     main : int list;
+     history : ( int * Vdw_criterion_t.t * int * int) list; 
+   } ;;
+
+  
