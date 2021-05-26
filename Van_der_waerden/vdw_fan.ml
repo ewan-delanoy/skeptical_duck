316fbb87c448ea20797445375213eb3f28bae05c
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-26 17:06:31 +0200

    register Van_der_Waerden/vdw_fan.ml

---
+++ Van_der_waerden/vdw_fan.ml
@@ -0,0 +1,42 @@
+(*
+
+#use"Van_der_Waerden/vdw_fan.ml";;
+
+*)
+
+module Private = struct 
+
+let apply_change 
+  (Vdw_fan_t.F components) (old_idx,_,idx1,idx2) =
+  let new_components = List.flatten (Image.image 
+    (fun (idx,translation)->
+       if idx = old_idx 
+       then [(idx1,translation);(idx2,translation)]
+      else  [(idx,translation)]
+      )
+  components) in 
+  Vdw_fan_t.F new_components;;
+
+
+
+end ;;  
+
+let apply_changes fan changes = 
+  List.fold_left  Private.apply_change  fan changes ;; 
+
+let partition (rp,fan) criterion=
+   let (Vdw_fan_t.F components) = fan in 
+   let temp1 = Image.image (fun (idx,l)->(idx,criterion)) components in 
+   let (new_rp,changes) = 
+     Vdw_repeatedly_partitionable.partition rp temp1 in 
+   let new_fan = apply_changes fan changes in 
+   (new_rp,new_fan,changes);;  
+    
+let translate 
+   (Vdw_fan_t.F components) translation =
+   let new_components = Image.image 
+     (fun (idx,translation2)->
+        (idx,Ordered.merge Total_ordering.for_integers translation translation2)
+       )
+   components in 
+   Vdw_fan_t.F new_components;;
--- Van_der_waerden/vdw_fan_t.ml
+++ Van_der_waerden/vdw_fan_t.ml
@@ -3,8 +3,11 @@
 #use"Van_der_Waerden/vdw_fan_t.ml";;
 
 A fan is a finite union of translates of parts from
-an initial partition (i.e. an object of type )
+an initial partition (i.e. an object of type 
+Vdw_repeatedly_partitionable_t.t)
+
 
 *)
 
 type t = F of (int * (int list)) list;;
+
