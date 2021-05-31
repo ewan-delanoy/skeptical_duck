0982bb21153455953d41440e86fa41d0bfa77a77
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-31 10:47:16 +0200

    register Van_der_Waerden/Second_try/vdw_atomizer_t.ml

---
+++ Van_der_waerden/Second_try/vdw_atomizer_t.ml
@@ -0,0 +1,12 @@
+(*
+
+#use"Van_der_Waerden/Second_try/vdw_atomizer_t.ml";;
+
+*)
+
+type t = {
+   atoms     : (Vdw_atom_t.t * (int list list)) list;
+   molecules : (Vdw_molecule_t.t * ((Vdw_atom_t.t * Vdw_translation_t.t) list)) list;
+   history   : ( (int * Vdw_translation_t.t) * (int *int) ) list ;
+   gains     : ( (int * Vdw_translation_t.t) * (int *int) ) list ;
+} ;;
