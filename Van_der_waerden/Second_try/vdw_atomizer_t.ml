073cf610703dee67ddfdfa6d29a3cd0a5dfc1b04
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-31 15:51:25 +0200

    simpler version of Vdw_translation.partition (for the time being)

--- Van_der_waerden/Second_try/vdw_atomizer_t.ml
+++ Van_der_waerden/Second_try/vdw_atomizer_t.ml
@@ -7,6 +7,7 @@
 type t = {
    atoms     : (Vdw_atom_t.t * (int list list)) list;
    molecules : (Vdw_molecule_t.t * ((Vdw_atom_t.t * Vdw_translation_t.t) list)) list;
-   history   : ( (int * Vdw_translation_t.t) * ((int * Vdw_translation_t.t) * (int * Vdw_translation_t.t)) ) list ;
-   gains     : ( (int * Vdw_translation_t.t) * ((int * Vdw_translation_t.t) * (int * Vdw_translation_t.t)) ) list ;
+   gains_for_atoms     : (  (Vdw_atom_t.t * Vdw_translation_t.t) * bool) list;
+   gains_for_molecules : (  (Vdw_molecule_t.t * Vdw_translation_t.t) * bool) list;
+   history   : ( (Vdw_molecule_t.t * Vdw_translation_t.t) * ((int * Vdw_translation_t.t) * (int * Vdw_translation_t.t)) ) list ;
 } ;;
