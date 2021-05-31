5b1ce3a5b0e169f964c3f8f967caeee53d7d4b32
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-31 14:26:02 +0200

    enhance implementation of Vdw_atomizer_t.t (second try)

--- Van_der_waerden/Second_try/vdw_atomizer_t.ml
+++ Van_der_waerden/Second_try/vdw_atomizer_t.ml
@@ -7,6 +7,6 @@
 type t = {
    atoms     : (Vdw_atom_t.t * (int list list)) list;
    molecules : (Vdw_molecule_t.t * ((Vdw_atom_t.t * Vdw_translation_t.t) list)) list;
-   history   : ( (int * Vdw_translation_t.t) * (int *int) ) list ;
-   gains     : ( (int * Vdw_translation_t.t) * (int *int) ) list ;
+   history   : ( (int * Vdw_translation_t.t) * ((int * Vdw_translation_t.t) * (int * Vdw_translation_t.t)) ) list ;
+   gains     : ( (int * Vdw_translation_t.t) * ((int * Vdw_translation_t.t) * (int * Vdw_translation_t.t)) ) list ;
 } ;;
--- Van_der_waerden/Second_try/vdw_translation.ml
+++ Van_der_waerden/Second_try/vdw_translation.ml
@@ -21,4 +21,10 @@ let partition (Vdw_translation_t.T translation) (ll,common)=
     (Vdw_comm_on.extract_core temp1,
      Vdw_comm_on.extract_core temp2);;
 
-       
+let setminus 
+     (Vdw_translation_t.T translation1)
+       (Vdw_translation_t.T translation2) =
+       let oint = Total_ordering.for_integers in 
+       Vdw_translation_t.T(
+         Ordered.setminus oint translation1 translation2
+       );;       
