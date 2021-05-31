2adad6b887475700534cdf7064fc97b96820c79a
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-05-31 13:32:58 +0200

    add Vdw_translation.setminus

--- Van_der_waerden/Second_try/vdw_translation.ml
+++ Van_der_waerden/Second_try/vdw_translation.ml
@@ -4,6 +4,14 @@
 
 *)
 
+let merge 
+  (Vdw_translation_t.T translation1)
+    (Vdw_translation_t.T translation2) =
+    let oint = Total_ordering.for_integers in 
+    Vdw_translation_t.T(
+      Ordered.merge oint translation1 translation2
+    );;
+
 let partition (Vdw_translation_t.T translation) (ll,common)=
     let oint = Total_ordering.for_integers in 
     let translation2 = Ordered.setminus oint translation common in 
@@ -13,4 +21,4 @@ let partition (Vdw_translation_t.T translation) (ll,common)=
     (Vdw_comm_on.extract_core temp1,
      Vdw_comm_on.extract_core temp2);;
 
-     
+       
