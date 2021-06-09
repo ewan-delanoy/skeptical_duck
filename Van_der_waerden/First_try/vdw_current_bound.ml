d0c4af4e7d6a4e83d82dcf71bcaf0e45f7e1bca0
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-06-09 18:26:23 +0200

    test commit

--- Van_der_waerden/First_try/vdw_current_bound.ml
+++
@@ -1,7 +0,0 @@
-(*
-
-#use"Van_der_Waerden/First_try/vdw_current_bound.ml";;
-
-*)
-
-let bound = 4 ;;
--- Van_der_waerden/First_try/vdw_translated_criterion_t.ml
+++
@@ -1,9 +0,0 @@
-(*
-
-#use"Van_der_Waerden/First_try/vdw_translated_criterion_t.ml";;
-
-*)
-
-type t = C of Vdw_criterion_t.t *  (int list) ;;
-
-  
