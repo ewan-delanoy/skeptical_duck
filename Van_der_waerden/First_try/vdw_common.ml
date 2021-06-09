cd9a710afcd1190e10a14ff5943a3687a0e55253
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-06-09 18:06:32 +0200

    test commit

--- Van_der_waerden/First_try/vdw_common.ml
+++ Van_der_waerden/First_try/vdw_common.ml
@@ -259,7 +259,7 @@ let partial_level3 n =
        | 11 -> [1; 2; 4; 5; 10; 11]
       | _ ->List.filter (fun x->List.mem(x mod 8)[1;2;4;5]) (Ennig.ennig 1 n);;     
    
-   let check_for_precomputed_value hashtbl (width,soi) =
+let check_for_precomputed_value hashtbl (width,soi) =
       let (d,relocated_soi) = set_start_to_one soi in 
       match Hashtbl.find_opt hashtbl (width,relocated_soi) with 
       (Some(optimal_size,sol)) -> Some(optimal_size,translate d sol)
@@ -272,4 +272,50 @@ let partial_level3 n =
          if width=1 
          then Some(level1 soi) 
          else None;;
-       
+ 
+(*
+
+The measure below is for width <=3 
+
+*)         
+
+let measure n =
+    if n<1 then 0 else 
+    let q=(n/9) in 
+    match n mod 9 with
+      0 -> 4*q+1 
+    |1 -> 4*q+1
+    |2 -> 4*q+2  
+    |3 -> 4*q+2
+    |4 -> 4*q+3
+    |5 -> 4*q+4
+    |6 -> 4*q+4  
+    |7 -> 4*q+4
+    |8 -> 4*q+4 
+    | _ -> failwith("unforeseen");;   
+        
+let lower_measure n =
+    if n<1 then 0 else 
+    let q=(n/9) in 
+    match n mod 9 with
+      0 -> 4*q
+    |1 -> 4*q
+    |2 -> 4*q 
+    |3 -> 4*q
+    |4 -> 4*q+1
+    |5 -> 4*q+1
+    |6 -> 4*q+2  
+    |7 -> 4*q+2
+    |8 -> 4*q+3 
+    | _ -> failwith("unforeseen");;  
+        
+let oord = Total_ordering.silex_compare Total_ordering.for_integers ;;   
+let oint = Total_ordering.for_integers ;;
+        
+let big_base =   
+          let unordered_base = 
+            naive_restricted_power_set
+           ( Vdw_list_of_constraints_t.Defined_by_max_width 4) 
+             (Set_of_integers.safe_set(Ennig.ennig 1 15))
+          in 
+          Ordered.sort oord unordered_base ;;         
