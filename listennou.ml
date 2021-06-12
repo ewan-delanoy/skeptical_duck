c75ab2a7548ef6bbe7dabed44427b6441945288d
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-06-12 17:19:33 +0200

    add Listennou.replace_if_possible

--- listennou.ml
+++ listennou.ml
@@ -427,3 +427,7 @@ decompose_into_connected_components [3; 4; 5; 6; 7; 10; 11; 12; 13; 14; 15; 31; 
 *)
 
 
+let replace_if_possible l x=
+  match List.assoc_opt x l with 
+  None -> x 
+  |Some y -> y ;;
