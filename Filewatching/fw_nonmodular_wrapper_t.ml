d7e32c56c3d3f1fe973a00d1dd32d138b672cb53
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-06-14 17:48:08 +0200

    change definition of Fw_nonmodular_wrapper_t

--- Filewatching/fw_nonmodular_wrapper_t.ml
+++ Filewatching/fw_nonmodular_wrapper_t.ml
@@ -9,7 +9,6 @@ defined in  the configuration parameter
 
 type t ={
   configuration         : Fw_configuration_t.t ;
-  compilable_files      : (Dfn_rootless_t.t * string ) list;
-  noncompilable_files   : (Dfn_rootless_t.t * string ) list;
+  watched_files         : (Dfn_rootless_t.t * string ) list;
   last_noticed_changes  : Dircopy_diff_t.t ;
 };;
