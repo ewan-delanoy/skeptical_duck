3bd98cbd2861e6472f54aa802651592eabe06b2d
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-06-28 11:14:28 +0200

    optimize Other_coma_state.officialize_changes

--- Compilation_management/other_coma_state.ml
+++ Compilation_management/other_coma_state.ml
@@ -12,7 +12,9 @@ let main_ref=
   let config = Fw_configuration.constructor (root,backup_dir,githubbing,url,[],[]) in 
   ref(Coma_state.empty_one  config);;
 
-let see_yet_unofficial_changes ()=
+let ref_for_unofficial_changes = ref(None : (string list) option) ;;  
+
+let force_compute_unofficial_changes ()=
    let temp1=Coma_state.all_mlx_files (!main_ref) in 
    let this_root = Dfa_root.connectable_to_subpath (Coma_big_constant.This_World.root) 
    and next_root = Dfa_root.connectable_to_subpath (Coma_big_constant.Next_World.root) in 
@@ -27,9 +29,16 @@ let see_yet_unofficial_changes ()=
          and ap2=Absolute_path.of_string(next_root^path) in 
          Io.read_whole_file(ap1)<>Io.read_whole_file(ap2)
    ) temp1 in 
-   Image.image (fun full_path->
+   let answer = Image.image (fun full_path->
       Dfn_rootless.to_line(Dfn_full.to_rootless full_path)  
-   ) temp2;;    
+   ) temp2 in 
+   let _ = (ref_for_unofficial_changes := Some answer) in answer;;    
+
+   let see_yet_unofficial_changes ()=
+      match (!ref_for_unofficial_changes) with 
+      None -> force_compute_unofficial_changes ()
+      |Some answer -> answer ;;
+     
 
    let commands_for_change_officialization l=
    let this_root = Dfa_root.connectable_to_subpath (Coma_big_constant.This_World.root) 
@@ -60,9 +69,13 @@ let duplicate_module old_t1 old_t2=
 let find_endingless modname= 
   Coma_state.endingless_from_mildly_capitalized_module_name (!(Private.main_ref)) modname;;
 
-let forget_one modname=Modify_coma_state.Syntactic_sugar.forget Private.main_ref [modname];;
+let forget_one modname=
+  let _ = (Private.ref_for_unofficial_changes:=None) in 
+  Modify_coma_state.Syntactic_sugar.forget Private.main_ref [modname];;
 
-let forget_several modnames=Modify_coma_state.Syntactic_sugar.forget Private.main_ref modnames;;
+let forget_several modnames=
+  let _ = (Private.ref_for_unofficial_changes:=None) in 
+  Modify_coma_state.Syntactic_sugar.forget Private.main_ref modnames;;
   
 
 let initialize ()=Modify_coma_state.Reference.initialize Private.main_ref ;; 
@@ -84,25 +97,35 @@ let officialize_changes () =
 
 
 
-let recompile opt=Modify_coma_state.Reference.recompile Private.main_ref opt;;
+let recompile opt=
+  let _ = (Private.ref_for_unofficial_changes:=None) in 
+  Modify_coma_state.Reference.recompile Private.main_ref opt;;
    
 
-let refresh ()=Modify_coma_state.Reference.refresh Private.main_ref;;
+let refresh ()=
+  let _ = (Private.ref_for_unofficial_changes:=None) in   
+ Modify_coma_state.Reference.refresh Private.main_ref;;
 
-let register_rootless_line x=Modify_coma_state.Syntactic_sugar.register_one Private.main_ref x;;
+let register_rootless_line x=
+let _ = (Private.ref_for_unofficial_changes:=None) in 
+  Modify_coma_state.Syntactic_sugar.register_one Private.main_ref x;;
 
 let rename_module old_name new_name=
+   let _ = (Private.ref_for_unofficial_changes:=None) in  
    Modify_coma_state.Syntactic_sugar.rename_module Private.main_ref old_name new_name;;
 
 let rename_string_or_value old_name new_name=
+let _ = (Private.ref_for_unofficial_changes:=None) in 
    Modify_coma_state.Reference.rename_string_or_value
    (Private.main_ref) old_name new_name;;
 
 let replace_string old_string new_string=
+let _ = (Private.ref_for_unofficial_changes:=None) in 
    Coma_state.Values_in_modules.replace_string 
    (!(Private.main_ref)) old_string new_string ;;
 
 let repopulate summary=
+  let _ = (Private.ref_for_unofficial_changes:=None) in 
   let (next_dest,next_backup,next_gab) = Coma_big_constant.Next_World.triple in 
   let cs=Create_world_copy.fully_developed_copy
   (!Usual_coma_state.main_ref) summary
