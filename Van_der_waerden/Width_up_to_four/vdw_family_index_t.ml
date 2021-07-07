2ba541a6c3718438ae5d7e8dae5215dd532bbac8
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-07-07 17:46:53 +0200

    register Van_der_Waerden/Width_up_to_four/vdw_nonempty_index.ml

--- Van_der_waerden/Width_up_to_four/vdw_family_index_t.ml
+++
@@ -1,9 +0,0 @@
-(*
-
-#use"Van_der_Waerden/Width_up_to_four/vdw_family_index_t.ml";;
-
-*)
-
-type t = NE of int * int  ;;
-
-  
---
+++ Van_der_waerden/Width_up_to_four/vdw_nonempty_index.ml
@@ -0,0 +1,13 @@
+(*
+
+#use"Van_der_Waerden/Width_up_to_four/vdw_nonempty_index.ml";;
+
+*)
+
+let order = ((fun 
+  (Vdw_nonempty_index_t.NE(x1,y1)) 
+  (Vdw_nonempty_index_t.NE(x2,y2)) ->
+    Total_ordering.standard2 (x1,y1) (x2,y2)
+) :  Vdw_nonempty_index_t.t Total_ordering.t);;
+
+  
