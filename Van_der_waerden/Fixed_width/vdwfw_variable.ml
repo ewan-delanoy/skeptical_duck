abc43df8e3bc0d5a0a4cae53bdc4d3d28ef8a86a
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-07-14 15:28:15 +0200

    rename vdw_variable as vdwfw_variable

---
+++ Van_der_waerden/Fixed_width/vdwfw_variable.ml
@@ -0,0 +1,43 @@
+(*
+
+#use"Van_der_Waerden/Fixed_width/vdwfw_variable.ml";;
+
+*)
+
+
+module Private = struct 
+ 
+
+let main_ref = ref ([]: (Vdwfw_nonempty_index_t.t * (int list list)) list) ;;
+
+end ;;
+
+let get x = List.assoc x (!Private.main_ref) ;;
+
+
+let homogeneous_translation x translation =
+    Vdwfw_current.homogeneous_translation 
+      (get x) translation ;;
+   
+let mem y =
+   match Option.seek (fun (x1,y1)->y1=y) (!Private.main_ref) with 
+   None -> None 
+   |Some(x,_) -> Some x ;;
+
+
+let set x y =
+      match List.assoc_opt x (!Private.main_ref) with 
+      None -> (Private.main_ref := (x,y) :: (!Private.main_ref))
+      |Some old_y ->
+           if old_y <> y 
+           then let msg = "Warning : "^
+                 (Vdwfw_nonempty_index.to_string x)^
+                 " has already been set.\n Resetting ...\n" in 
+               let new_list = Image.image (
+                  fun p -> let (x1,y1) = p in  if x1 = x then (x,y) else p 
+               ) (!Private.main_ref) in 
+               (
+                  print_string msg;
+                  flush stdout;
+                  Private.main_ref := new_list ;
+                );;    
