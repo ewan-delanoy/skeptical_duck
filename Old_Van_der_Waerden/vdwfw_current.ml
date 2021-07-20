aa4ba3821667e2e5be4e5e1e51ac84116d34069d
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-07-20 15:20:06 +0200

    move vdwfw_current to Old_Van_der_Waerden/

---
+++ Old_Van_der_Waerden/vdwfw_current.ml
@@ -0,0 +1,21 @@
+(*
+
+#use"Old_Van_der_Waerden/vdwfw_current.ml";;
+
+*)
+
+let mw = Vdw_max_width_t.MW 4 ;;
+let threshhold = 15 ;; 
+
+let base_for_threshhold = Vdw_common.generic_computer mw threshhold ;;
+let decompose = Vdw_common.decompose mw ;;
+let homogeneous_translation = 
+     Vdw_common.homogeneous_translation 
+     (Vdw_list_of_constraints_t.Defined_by_max_width (
+      (fun (Vdw_max_width_t.MW m)->m) mw) ) ;;
+let lower_measure = Vdw_common.lower_measure mw ;;
+let measure = Vdw_common.measure mw ;;
+let minimal_obstructions_corresponding_to_above = Vdw_common.minimal_obstructions_corresponding_to_above mw ;;
+let rightmost_blowup = Vdw_common.rightmost_blowup mw ;;
+
+
