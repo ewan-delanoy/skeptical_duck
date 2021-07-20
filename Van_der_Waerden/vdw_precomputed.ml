be7ea53173d95e60a1f5cb766bfad595dca3aabe
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-07-20 18:44:49 +0200

    register Van_der_Waerden/vdw_precomputed.ml

---
+++ Van_der_Waerden/vdw_precomputed.ml
@@ -0,0 +1,55 @@
+(*
+
+#use"Van_der_Waerden/vdw_precomputed.ml";;
+
+
+*)
+
+exception Untreated_width of int ;;
+
+module Private = struct 
+
+let measure_in_width_four n =
+    if n<1 then 0 else 
+    let q=(n/9) in 
+    match n mod 9 with
+      0 -> 4*q+1 
+    |1 -> 4*q+1
+    |2 -> 4*q+2  
+    |3 -> 4*q+2
+    |4 -> 4*q+3
+    |5 -> 4*q+4
+    |6 -> 4*q+4  
+    |7 -> 4*q+4
+    |8 -> 4*q+4 
+    | _ -> failwith("unforeseen");;     
+
+let lower_measure_in_width_four n =
+      if n<1 then 0 else 
+      let q=(n/9) in 
+      match n mod 9 with
+        0 -> 4*q
+      |1 -> 4*q
+      |2 -> 4*q 
+      |3 -> 4*q
+      |4 -> 4*q+1
+      |5 -> 4*q+1
+      |6 -> 4*q+2  
+      |7 -> 4*q+2
+      |8 -> 4*q+3 
+      | _ -> failwith("unforeseen");;  
+
+end ;;
+
+let measure (Udw_max_width_t.MW mw) n=
+match mw with 
+4 -> Private.measure_in_width_four n 
+| _ -> raise( Untreated_width mw);;
+
+let lower_measure (Udw_max_width_t.MW mw) n=
+match mw with 
+4 -> Private.lower_measure_in_width_four n 
+| _ -> raise( Untreated_width mw);;  
+
+
+
