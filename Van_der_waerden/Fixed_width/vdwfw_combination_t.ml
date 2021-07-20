f0e04781a9d07c70acdc3b667a4af7ccda5d7aa7
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-07-20 17:43:54 +0200

    register Van_der_Waerden/vdw_max_width_t.ml

--- Van_der_waerden/Fixed_width/vdwfw_combination_t.ml
+++
@@ -1,9 +0,0 @@
-(*
-
-#use"Van_der_Waerden/Fixed_width/vdwfw_combination_t.ml";;
-
-*)
-
-type t = C of (Vdwfw_nonempty_index_t.t * (int list)) list ;;
-
-  
--- Van_der_waerden/Fixed_width/vdwfw_environment_t.ml
+++
@@ -1,7 +0,0 @@
-(*
-
-#use"Van_der_Waerden/Fixed_width/vdwfw_environment_t.ml";;
-
-*)
-
-type t = L of (Vdwfw_nonempty_index_t.t * Vdwfw_combination_t.t ) list ;;
