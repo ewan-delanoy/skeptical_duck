5b4d47adba40add93076659d3e9dd74841f05044
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-07-21 15:47:21 +0200

    add Vdw_chosen.measure

--- Van_der_Waerden/Fixed_width/vdw_chosen.ml
+++ Van_der_Waerden/Fixed_width/vdw_chosen.ml
@@ -6,12 +6,15 @@
 
 let mw = Vdw_max_width_t.MW 4 ;;
 
-let test_for_admissibility = 
-  Vdw_max_width.test_for_admissibility mw;;
-       
-let test_joinability = 
-  Vdw_max_width.test_joinability mw;;
+let lower_measure n = Vdw_precomputed.lower_measure mw ;;
+
+let measure n = Vdw_precomputed.measure mw ;;
 
 let naive_restricted_power_set = 
   Vdw_max_width.naive_restricted_power_set mw;;
 
+let test_for_admissibility = 
+    Vdw_max_width.test_for_admissibility mw;;
+         
+let test_joinability = 
+    Vdw_max_width.test_joinability mw;;
