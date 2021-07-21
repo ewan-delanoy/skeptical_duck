cb1c56b874ead0f3c5b6cb446322e0cff0995571
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-07-21 18:40:31 +0200

    clearer variable naming in Vdw_chosen

--- Van_der_Waerden/Fixed_width/vdw_chosen.ml
+++ Van_der_Waerden/Fixed_width/vdw_chosen.ml
@@ -4,17 +4,17 @@
 
 *)
 
-let mw = Vdw_max_width_t.MW 4 ;;
+let max_width = Vdw_max_width_t.MW 4 ;;
 
-let lower_measure = Vdw_precomputed.lower_measure mw ;;
+let lower_measure = Vdw_precomputed.lower_measure max_width ;;
 
-let measure = Vdw_precomputed.measure mw ;;
+let measure = Vdw_precomputed.measure max_width ;;
 
 let naive_restricted_power_set = 
-  Vdw_max_width.naive_restricted_power_set mw;;
+  Vdw_max_width.naive_restricted_power_set max_width;;
 
 let test_for_admissibility = 
-    Vdw_max_width.test_for_admissibility mw;;
+    Vdw_max_width.test_for_admissibility max_width;;
          
 let test_joinability = 
-    Vdw_max_width.test_joinability mw;;
+    Vdw_max_width.test_joinability max_width;;
