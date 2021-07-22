295ae638a1d91e74996609471adab3008f369e15
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-07-22 19:02:06 +0200

    do Vdw_precomputed.Private.computation1

--- Van_der_Waerden/vdw_precomputed.ml
+++ Van_der_Waerden/vdw_precomputed.ml
@@ -50,8 +50,8 @@ let restricted_power_set =Memoized.recursive (fun old_f (max_width,soi) ->
   Vdw_max_width.extender max_width (old_f (max_width,soi2)) last_elt  
 );;
 
-let act1 () = Ennig.doyle (fun n->
-  restricted_power_set (Vdw_max_width_t.MW 4,Ennig.ennig 1 n)
+let computation1 = Ennig.doyle (fun n->
+  List.length(restricted_power_set (Vdw_max_width_t.MW 4,Ennig.ennig 1 n)) 
 ) 1 24;;
 
 end ;;
