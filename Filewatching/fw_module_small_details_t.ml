e0f853dd213a8bf3196ba424506d2a614a69ad7e
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-08-20 07:30:59 +0200

    register Filewatching/fw_module_small_details_t.ml

---
+++ Filewatching/fw_module_small_details_t.ml
@@ -0,0 +1,17 @@
+(*
+
+#use"Filewatching/fw_module_small_details_t.ml";;
+
+
+*)
+
+type t ={
+  used_modules : Dfa_module_t.t list ;
+  used_libraries : Ocaml_library_t.t list ;
+  has_printer : bool ;
+  subdirectory : Dfa_subdirectory_t.t ;
+  principal_ending : Dfa_ocaml_ending_t.t ;
+  mli_present : bool ;
+  principal_modification_time : string option ;
+  mli_modification_time : string option ;
+};;
