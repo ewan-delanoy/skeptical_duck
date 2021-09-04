a93dc34f0b0d741c8a9f2f4a5b592b1f071ba63c
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-09-04 10:05:36 +0200

    rename Dfn_join.subdirectory_to as subdirectory_to_short

--- Compilation_management/coma_constant.ml
+++ Compilation_management/coma_constant.ml
@@ -36,15 +36,15 @@ let short_path_for_printersfile= Dfn_short.of_line "my_printers.ml";;
 let short_path_for_targetfile= Dfn_short.of_line "targetfile.ocaml_made";;
  
 let rootless_path_for_loadingsfile=
-  Dfn_join.subdirectory_to  utility_files_subdir short_path_for_loadingsfile;;
+  Dfn_join.subdirectory_to_short  utility_files_subdir short_path_for_loadingsfile;;
 let rootless_path_for_painful_debugging_file=
-  Dfn_join.subdirectory_to  fads_subdir short_path_for_painful_debugging_file;;
+  Dfn_join.subdirectory_to_short  fads_subdir short_path_for_painful_debugging_file;;
 let rootless_path_for_parametersfile=
-  Dfn_join.subdirectory_to  parameters_subdir short_path_for_parametersfile;;
+  Dfn_join.subdirectory_to_short  parameters_subdir short_path_for_parametersfile;;
 let rootless_path_for_printersfile=
-  Dfn_join.subdirectory_to  utility_files_subdir short_path_for_printersfile;;
+  Dfn_join.subdirectory_to_short  utility_files_subdir short_path_for_printersfile;;
 let rootless_path_for_targetfile=
-  Dfn_join.subdirectory_to  persistent_data_subdir short_path_for_targetfile;;     
+  Dfn_join.subdirectory_to_short  persistent_data_subdir short_path_for_targetfile;;     
 
 let rootless_path_for_ocamlinit = Dfn_rootless.of_line ".ocamlinit";;
 
--- Decomposed_filename/dfn_join.ml
+++ Decomposed_filename/dfn_join.ml
@@ -8,6 +8,6 @@ let middle_to_ending (Dfn_middle_t.J(s,m)) e= (Dfn_rootless_t.J(s,m,e));;
 let root_to_middle r (Dfn_middle_t.J(s,m))=Dfn_endingless_t.J(r,s,m);; 
 let root_to_rootless r (Dfn_rootless_t.J(s,m,e))=Dfn_full_t.J(r,s,m,e);;
 let root_to_subdirectory (Dfa_root_t.R(r)) (Dfa_subdirectory_t.SD(s))=Dfa_root_t.R(r^"/"^s);;
-let subdirectory_to s (Dfn_short_t.J(m,e))=(Dfn_rootless_t.J(s,m,e));;  
+let subdirectory_to_short s (Dfn_short_t.J(m,e))=(Dfn_rootless_t.J(s,m,e));;  
 let to_ending (Dfn_endingless_t.J(r,s,m)) e = Dfn_full_t.J(r,s,m,e);;
 
