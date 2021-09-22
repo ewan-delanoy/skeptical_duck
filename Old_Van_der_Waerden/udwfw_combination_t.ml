cc1401be3dfd4a9e97ddef1390258ee734f26163
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-09-22 06:31:44 +0200

    add snippet to remove all modules in a subdirectory

--- Old_Van_der_Waerden/udwfw_combination_t.ml
+++
@@ -1,9 +0,0 @@
-(*
-
-#use"Old_Van_der_Waerden/udwfw_combination_t.ml";;
-
-*)
-
-type t = C of (Udwfw_nonempty_index_t.t * (int list)) list ;;
-
-  
--- Old_Van_der_Waerden/udwfw_environment_t.ml
+++
@@ -1,7 +0,0 @@
-(*
-
-#use"Old_Van_der_Waerden/udwfw_environment_t.ml";;
-
-*)
-
-type t = L of (Udwfw_nonempty_index_t.t * Udwfw_combination_t.t ) list ;;
