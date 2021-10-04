2d89d2a62e1ae1ce04a78b56c78ce887ad66c412
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-10-04 14:45:35 +0200

    add Ordered_misc.minimal_elts_in_upwards_filter

--- Ordered_Lists/ordered_misc.ml
+++ Ordered_Lists/ordered_misc.ml
@@ -29,7 +29,51 @@ module Private = struct
       let temp3 = helper_for_minimal_elts_wrt_inclusion ([],temp2) in 
       helper_for_minimal_transversals (temp3,others) ;;        
 
+  let power_set_for_intset x =
+     Ordered.sort (Total_ordering.silex_compare oi) (Listennou.power_set x) ;; 
+
+  let rec helper_for_int_upwards_filter (f,treated,to_be_treated) = 
+     match to_be_treated with 
+     [] -> List.rev treated 
+     | a::others ->
+         if f a 
+         then let remaining_ones = List.filter (
+                fun b->not(Ordered.is_included_in oi a b)
+              ) to_be_treated in 
+              helper_for_int_upwards_filter (f,a::treated,remaining_ones)
+         else helper_for_int_upwards_filter (f,treated,others) ;;    
+
+  let naive_minimal_elts_in_int_upwards_filter f base =
+    let pb = power_set_for_intset base in 
+    helper_for_int_upwards_filter (f,[],pb) ;;     
+
+  let minimal_elts_in_int_upwards_filter f base =
+     let indispensable_ones = List.filter (
+       fun x->(not(f(Ordered.outsert oi x base)))
+     ) base in 
+     let dispensable_ones = Ordered.setminus oi base indispensable_ones in 
+     let pullbacked_f = (fun y->f(Ordered.merge oi indispensable_ones y)) in 
+     let temp1 = naive_minimal_elts_in_int_upwards_filter pullbacked_f dispensable_ones in 
+     Image.image (Ordered.merge oi indispensable_ones) temp1 ;;
+
+  (*   
+  let base0 = Ennig.ennig 1 5 ;;
+  let f0 y= List.exists (fun x->Ordered.is_included_in oi x y) [[1;2;3];[3;4;5]] ;;
+  let res0 = minimal_elts_in_int_upwards_filter f0 base0 ;;
+  *)
+
+  let minimal_elts_in_upwards_filter f base =
+     let get_one = (fun k->List.nth base (k-1)) in 
+     let get_several = Image.image get_one in 
+     let n = List.length base 
+     and normalized_f = (fun indices ->f(get_several indices)) in 
+     let temp1 = minimal_elts_in_int_upwards_filter normalized_f (Ennig.ennig 1 n) in 
+     Image.image get_several temp1 ;;
+
+
 end ;;        
+
+let minimal_elts_in_upwards_filter = Private.minimal_elts_in_upwards_filter ;;
 
 let minimal_elts_wrt_inclusion l= 
   Private.helper_for_minimal_elts_wrt_inclusion ([],l) ;;
