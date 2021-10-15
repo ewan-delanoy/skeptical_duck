c6fc234d27ac59e687b6790c7f16b3ea689574d5
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-10-15 16:08:34 +0200

    remove Fw_indexer.{new_instance,new_state}

--- Filewatching/fw_indexer.ml
+++ Filewatching/fw_indexer.ml
@@ -26,24 +26,6 @@ let create_new_instance () =
 let get_state (Fw_instance_index_t.I ii) =
   try List.nth (!(Private.main_ref)) (ii-1) with 
   _ -> raise (Invalid_instance_index(ii)) ;;
-
-let new_instance () =
-  let raf = Private.main_ref in 
-  let n = List.length (!raf) 
-  and starter = Fw_state_index_t.I 0 in 
-  let _ = (raf := (!raf) @ [starter]) in 
-  (Fw_instance_index_t.I(n+1),starter) ;;
-
-let new_state instance =
-   let raf = Private.main_ref in 
-   let (Fw_state_index_t.I old_state) = get_state instance 
-   and indexed_old_list = Ennig.index_everything (!raf) in 
-   let new_state = (Fw_state_index_t.I (old_state+1)) in 
-   let (Fw_instance_index_t.I ii) = instance in 
-   let new_list = Image.image 
-      (fun (k,st)->if k=ii then new_state else st) indexed_old_list in 
-   let _ = (raf := new_list) in 
-   new_state ;;
   
 let push_state instance =
   let raf = Private.main_ref in 
