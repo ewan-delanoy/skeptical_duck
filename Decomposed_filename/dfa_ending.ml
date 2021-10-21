65e73e082e7fc5921443e68d5d330167b548a706
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-10-21 12:08:18 +0200

    remove Dfa_ending.convert_to_ocaml_ending

--- Decomposed_filename/dfa_ending.ml
+++ Decomposed_filename/dfa_ending.ml
@@ -17,17 +17,7 @@ let of_line e =
 
 let connectable_to_modulename (Dfa_ending_t.E(e)) = "." ^ e ;;
 
-let convert_to_ocaml_ending (Dfa_ending_t.E(e)) =
-   if not(Supstring.begins_with e "ml")
-   then raise(Not_an_ocaml_ending(e))
-   else let n=String.length(e) in 
-        if n=2 then Dfa_ocaml_ending_t.Ml else 
-        if n>3 then raise(Not_an_ocaml_ending(e)) else 
-        match String.get e 2 with 
-         'i'->Dfa_ocaml_ending_t.Mli
-        |'l'->Dfa_ocaml_ending_t.Mll
-        |'y'->Dfa_ocaml_ending_t.Mly
-        | _ -> raise(Not_an_ocaml_ending(e));;
+
 
 let mll =  Dfa_ending_t.E "mll"
 and mly =  Dfa_ending_t.E "mly"
@@ -37,8 +27,6 @@ and mli =  Dfa_ending_t.E "mli" ;;
 let all_ocaml_endings= [mll;mly;ml;mli];;
 
 let all_cee_endings = Image.image (fun s->Dfa_ending_t.E s) ["h";"c"];;
-
-
 
 let compute_on_all_ocaml_endings f=(f ml,f mli,f mll,f mly);;
 
