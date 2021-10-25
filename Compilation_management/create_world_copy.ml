6be56f67da281c8b204b91509306151f6f1e0837
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-10-25 11:37:57 +0200

    remove superfluous line in Create_world_copy.fully_developed_copy

--- Compilation_management/create_world_copy.ml
+++ Compilation_management/create_world_copy.ml
@@ -79,8 +79,7 @@ module Private = struct
   let fully_developed_copy cs ~destination ?(destbackupdir=default_backup_dir) ?(destgab=false) summary=
       let (modules_in_good_order,faraway_fw) = frozen_copy cs ~destination ~destbackupdir ~destgab summary in 
       let faraway_cs1 = Coma_state.passive_constructor faraway_fw in 
-      let faraway_cs = Coma_state.update_just_one_module faraway_cs1  Coma_constant.rootless_path_for_parametersfile in   
-      let faraway_cs2 = Modify_coma_state.Internal.recompile (faraway_cs,[],[],[]) in 
+      let faraway_cs2 = Modify_coma_state.Internal.recompile (faraway_cs1,[],[],[]) in 
       let _=Save_coma_state.save faraway_cs2 in   
       faraway_cs2;;                      
   
