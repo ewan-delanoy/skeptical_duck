8d3cd4efc070c54620ac08fcdc8e9f7c776c9511
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-12-19 16:00:34 +0100

    add measures in width 3 to Sz_precomputed

--- Szemeredi/sz_precomputed.ml
+++ Szemeredi/sz_precomputed.ml
@@ -9,6 +9,20 @@ exception Untreated_width of int ;;
 
 module Private = struct 
 
+let measure_in_width_three n =
+    if n<1 then 0 else 
+    let q=(n/8) in 
+    match n mod 8 with
+     0 -> 4*q
+    |1 -> 4*q+1
+    |2 -> 4*q+2  
+    |3 -> 4*q+2
+    |4 -> 4*q+3
+    |5 -> 4*q+4
+    |6 -> 4*q+4  
+    |7 -> 4*q+4
+    | _ -> failwith("unforeseen");;  
+
 let measure_in_width_four n =
     if n<1 then 0 else 
     let q=(n/9) in 
@@ -44,7 +58,8 @@ end ;;
 
 let measure (Sz_max_width_t.MW mw) n=
 match mw with 
-4 -> Private.measure_in_width_four n 
+  3 -> Private.measure_in_width_three n 
+| 4 -> Private.measure_in_width_four n 
 | _ -> raise( Untreated_width mw);;
 
 let lower_measure (Sz_max_width_t.MW mw) n=
