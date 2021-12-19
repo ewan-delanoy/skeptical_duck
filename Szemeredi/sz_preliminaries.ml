f7299742eed7fe6012caf08723f45de8c081f011
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-12-19 15:53:32 +0100

    add  Sz_preliminaries.force_subset_in_interval

--- Szemeredi/sz_preliminaries.ml
+++ Szemeredi/sz_preliminaries.ml
@@ -7,9 +7,11 @@
 
 module Private = struct
 
+let i_setminus = Ordered.setminus Total_ordering.for_integers  ;;  
 let i_sort = Ordered.safe_set Total_ordering.for_integers  ;;
 let i_is_included_in = Ordered.is_included_in Total_ordering.for_integers ;;
 let il_merge = Ordered.merge Total_ordering.silex_for_intlists ;;
+let il_sort = Ordered.safe_set Total_ordering.silex_for_intlists ;;
 
 let diameter soi =
   if List.length(soi)<2 then 0 else 
@@ -45,6 +47,29 @@ let restricted_power_set =Memoized.recursive (fun old_f (max_width,soi) ->
   extender max_width (old_f (max_width,soi2)) last_elt  
 );;
 
+let arithmetic_progressions_in_interval_with_width_equal_to (a,b) width  =
+  Ennig.doyle (fun x->[x;x+width;x+2*width]) a (b-2*width)  ;; 
+
+let arithmetic_progressions_with_width_up_to_in_interval 
+  interval (Sz_max_width_t.MW width)  = 
+  let (a,b) = interval in 
+  let max_width = (if width<1 then (b-a)/2 else width) in 
+  List.rev(List.flatten(Ennig.doyle 
+  (arithmetic_progressions_in_interval_with_width_equal_to interval) 1 max_width));;  ;;   
+
+
+let force_subset_in_interval (Sz_max_width_t.MW width) subset interval =
+    let old_obses = arithmetic_progressions_with_width_up_to_in_interval interval (Sz_max_width_t.MW width) in 
+    let new_obses = Option.filter_and_unpack (
+      fun old_obstruction ->
+        let new_obstruction = i_setminus old_obstruction subset in
+        if (new_obstruction = old_obstruction) || (new_obstruction = []) 
+        then None 
+        else Some new_obstruction  
+    ) old_obses in 
+    il_sort new_obses ;;
+
 end ;;
 
+let force_subset_in_interval = Private.force_subset_in_interval ;;
 let restricted_power_set = Private.restricted_power_set ;;
