77f33af1af20f2d3cd992b63b9356a4454884657
Ewan Delanoy <ewan.delanoy@zoho.com>
2021-12-22 07:53:30 +0100

    use Transmit_change_to_github in Node_project

--- node_project.ml
+++ node_project.ml
@@ -10,7 +10,7 @@ let compact_replacer =
    Coma_constant.confidential_data_subdir 
   )^"compact_replacer.txt");;
 
-let config = 
+let fw_config = 
   let home = Sys.getenv "HOME" in 
  {Fw_configuration_t.root =
     Dfa_root_t.R (home^"/Teuliou/Sites/Gwerzher_Leoriou");
@@ -34,19 +34,34 @@ let config =
        Dfa_ending_t.E "js")];
    } ;;
 
-let watcher_ref = ref (File_watcher.empty_one config);;
+  let github_config = 
+    let home = Sys.getenv "HOME" in 
+   {Github_configuration_t.root =
+      Dfa_root_t.R (home^"/Teuliou/Sites/Gwerzher_Leoriou");
+     dir_for_backup =
+      Dfa_root_t.R (home^"/Teuliou/Sites/Githubbed_gwl");
+     gitpush_after_backup = true;
+     github_url = "https://github.com/ewan-delanoy/node_app";
+     encoding_protected_files = [
+       compact_replacer,
+     Dfn_rootless_t.J (Dfa_subdirectory_t.SD "models", Dfa_module_t.M "mysql_connection",
+         Dfa_ending_t.E "js")];
+     } ;;
+
+let watcher_ref = ref (File_watcher.empty_one fw_config);;
 
 
 let refresh () =
-    let diff = Check_ocaml_dircopy.check config in 
-    let _ = Reflect_change_in_github.backup config diff None in 
-    watcher_ref:=(File_watcher.of_configuration config) ;;
+    let diff = Check_ocaml_dircopy.check fw_config in 
+    let _ = Transmit_change_to_github.backup github_config diff None in 
+    watcher_ref:=(File_watcher.of_configuration fw_config) ;;
 
 let update opt_msg =
     let old_fw = (!watcher_ref) in 
-    let (fw2,_) = File_watcher.inspect_and_update old_fw true in 
-    let fw3 = File_watcher.reflect_latest_changes_in_github fw2 opt_msg in 
-    let _=(watcher_ref:=fw3) in fw3;;
+    let (fw2,changes) = File_watcher.inspect_and_update old_fw true in 
+    let diff = Dircopy_diff.add_changes Dircopy_diff.empty_one changes in 
+    let _ = Transmit_change_to_github.backup github_config diff opt_msg in 
+    let _=(watcher_ref:=fw2) in fw2;;
    
 
 
