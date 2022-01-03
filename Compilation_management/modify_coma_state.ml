a5a5a87106749e40e429753ecb90f9cb5942c9b3
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-01-03 18:30:34 +0100

    add replace_{string,value} elements in submodules of Modify_coma_state

--- Compilation_management/modify_coma_state.ml
+++ Compilation_management/modify_coma_state.ml
@@ -58,6 +58,14 @@ end ;;
             let _=Coma_state.check_that_no_change_has_occurred cs in 
             Coma_state.rename_string_or_value cs old_sov new_sov ;;      
    
+         let replace_string cs old_s new_s =
+            let _=Coma_state.check_that_no_change_has_occurred cs in 
+            Coma_state.replace_string cs old_s new_s ;;
+
+         let replace_value cs ((preceding_files,path),(old_v,new_v)) =
+            let _=Coma_state.check_that_no_change_has_occurred cs in 
+            Coma_state.replace_value cs ((preceding_files,path),(old_v,new_v)) ;;   
+
    end;;
    
    
@@ -121,6 +129,16 @@ end ;;
             let cs2=After_checking.rename_string_or_value cs old_sov new_sov in 
             let _=Save_coma_state.save cs2 in 
             cs2;;  
+
+         let replace_string cs old_s new_s=
+            let cs2=After_checking.replace_string cs old_s new_s in 
+            let _=Save_coma_state.save cs2 in 
+            cs2;;     
+         
+         let replace_value cs ((preceding_files,path),(old_v,new_v))=
+            let cs2=After_checking.replace_value cs ((preceding_files,path),(old_v,new_v)) in 
+            let _=Save_coma_state.save cs2 in 
+            cs2;;        
    
    end ;;
    
@@ -179,6 +197,13 @@ end ;;
             let new_cs = And_save.rename_string_or_value (!pcs) old_sov new_sov in 
             pcs:=new_cs;;
    
+         let replace_string pcs old_s new_s=
+            let new_cs = And_save.replace_string (!pcs) old_s new_s in 
+            pcs:=new_cs;;   
+         
+         let replace_value pcs ((preceding_files,path),(old_v,new_v))=
+            let new_cs = And_save.replace_value (!pcs) ((preceding_files,path),(old_v,new_v)) in 
+            pcs:=new_cs;;      
    
    end ;;
    
@@ -218,7 +243,28 @@ end ;;
       let new_nonslashed_name = No_slashes.of_string (String.uncapitalize_ascii new_name) in 
       Reference.rename_module cs_ref old_middle_name new_nonslashed_name;; 
    
-   
+   (*   
+   exception Rename_string_or_value_exn of string ;;
+
+   let rename_string_or_value cs_ref old_sov new_sov =
+         if not(String.contains old_sov '.')
+         then Reference.replace_string cs_ref old_sov new_sov 
+         else 
+         let j=Substring.leftmost_index_of_in "." old_sov in
+         if j<0 
+         then raise(Rename_string_or_value_exn(old_sov))
+         else let module_name=Cull_string.beginning (j-1) old_sov in
+         let endingless= Coma_state.decipher_module (!cs_ref)  module_name 
+         and path= Coma_state.decipher_path (!cs_ref)  module_name in 
+         let nm=Dfn_endingless.to_module endingless in
+         let pre_temp2=(Coma_state.ancestors_for_module (!cs_ref) nm)@[nm] in
+         let temp2=Image.image (Coma_state.endingless_at_module (!cs_ref)) pre_temp2 in
+         let preceding_files=Image.image  (fun eless2->
+                           Dfn_full.to_absolute_path(Dfn_join.to_ending eless2 Dfa_ending.ml)
+         ) temp2 in
+         Reference.replace_value cs_ref ((preceding_files,path),(old_sov,new_sov)) ;;       
+   *)  
+
    let rename_subdirectory cs_ref old_subdirname new_subdir_short_name=
        let old_subdir = Coma_state.find_subdir_from_suffix (!cs_ref) old_subdirname  in
        let new_subdir = Tools.compute_long_subdir_name old_subdir new_subdir_short_name  in 
