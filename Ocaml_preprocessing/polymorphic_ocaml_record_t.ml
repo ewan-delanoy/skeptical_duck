aae35ff168d40d130c17fafd2d4e772eadf5dfb2
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-01-24 11:14:22 +0100

    register Ocaml_preprocessing/polymorphic_ocaml_record_t.ml

---
+++ Ocaml_preprocessing/polymorphic_ocaml_record_t.ml
@@ -0,0 +1,22 @@
+(*
+
+#use"Ocaml_preprocessing/polymorphic_ocaml_record_t.ml";;
+
+*)
+
+type field_t = {
+   field_name : string ;
+   field_type : string ;
+   var_name : string ;
+
+} ;;
+
+type instance_t = {
+   fields : string list ;
+} ;;
+
+type t = {
+   fields : field_t list ;
+   instances : instance_t list ;
+} ;;
+
