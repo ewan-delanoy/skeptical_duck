da3867d1204a943709d3c42414d31b72ceda7823
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-03-04 08:47:13 +0100

    register Ocaml_preprocessing/annotated_definition_t.ml

---
+++ Ocaml_preprocessing/annotated_definition_t.ml
@@ -0,0 +1,12 @@
+(*
+
+#use"Ocaml_preprocessing/annotated_definition_t.ml";;
+
+*)
+
+type t = {
+      value_name : string ;
+      is_private : bool ;
+      lines_in_definition : string list ;
+} ;;
+   
