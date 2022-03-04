e798a968c4cbf3d01b4d2f80725af911416d78cd
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-03-04 09:06:17 +0100

    add Annotated_definition.expand_list

--- Githubbed_archive/prepare_fw_poly.ml
+++ Githubbed_archive/prepare_fw_poly.ml
@@ -7,27 +7,6 @@
 open Needed_values ;;
 
 
-   module Annotated_definition = struct 
-
-    let expand anndef =
-       String.concat "\n" anndef.Annotated_definition_t.lines_in_definition ;;
-
-  let order = ((fun anndef1 anndef2 ->
-    let is_private1 = anndef1.Annotated_definition_t.is_private 
-    and is_private2 = anndef1.Annotated_definition_t.is_private in 
-    let trial1 = Total_ordering.standard is_private2 is_private1 in 
-    if trial1 <> Total_ordering_result_t.Equal then trial1 else 
-    let val_name1 = anndef1.Annotated_definition_t.value_name 
-    and val_name2 = anndef2.Annotated_definition_t.value_name in 
-    let trial2 = Total_ordering.lex_for_strings val_name1 val_name2 in 
-    if trial2 <> Total_ordering_result_t.Equal then trial2 else    
-     Total_ordering.standard anndef1 anndef2 
-  ) :>  Annotated_definition_t.t Total_ordering_t.t ) ;;   
-      
-
-
-   end ;; 
-
    
    let pair_for_field (porf:Polymorphic_ocaml_record_t.field_t) =
      (String.make 3 ' ')^
--- Ocaml_preprocessing/annotated_definition.ml
+++ Ocaml_preprocessing/annotated_definition.ml
@@ -4,18 +4,31 @@
 
 *)
 
+module Private = struct 
+
+let order = ((fun anndef1 anndef2 ->
+      let is_private1 = anndef1.Annotated_definition_t.is_private 
+      and is_private2 = anndef1.Annotated_definition_t.is_private in 
+      let trial1 = Total_ordering.standard is_private2 is_private1 in 
+      if trial1 <> Total_ordering_result_t.Equal then trial1 else 
+      let val_name1 = anndef1.Annotated_definition_t.value_name 
+      and val_name2 = anndef2.Annotated_definition_t.value_name in 
+      let trial2 = Total_ordering.lex_for_strings val_name1 val_name2 in 
+      if trial2 <> Total_ordering_result_t.Equal then trial2 else    
+                   Total_ordering.standard anndef1 anndef2 
+      ) :>  Annotated_definition_t.t Total_ordering_t.t ) ;;   
+                  
+      
+end ;;      
+      
+      
 let expand anndef =
       String.concat "\n" anndef.Annotated_definition_t.lines_in_definition ;;
-
- let order = ((fun anndef1 anndef2 ->
-   let is_private1 = anndef1.Annotated_definition_t.is_private 
-   and is_private2 = anndef1.Annotated_definition_t.is_private in 
-   let trial1 = Total_ordering.standard is_private2 is_private1 in 
-   if trial1 <> Total_ordering_result_t.Equal then trial1 else 
-   let val_name1 = anndef1.Annotated_definition_t.value_name 
-   and val_name2 = anndef2.Annotated_definition_t.value_name in 
-   let trial2 = Total_ordering.lex_for_strings val_name1 val_name2 in 
-   if trial2 <> Total_ordering_result_t.Equal then trial2 else    
-    Total_ordering.standard anndef1 anndef2 
- ) :>  Annotated_definition_t.t Total_ordering_t.t ) ;;   
-   
+      
+let expand_list l =
+      let temp1 = Ordered.sort Private.order l in 
+      String.concat "\n" (Image.image expand temp1) ;;
+      
+let order = Private.order ;;
+       
+      
