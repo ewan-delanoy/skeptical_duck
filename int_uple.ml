0117ca049e61847145ca465f4659c865dad2fc1c
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-04-07 13:33:36 +0200

    fix Int_uple.inclusive_list_of_pairs again

--- int_uple.ml
+++ int_uple.ml
@@ -48,7 +48,7 @@ if x5+1<x6 then (1,2,3,4,    x5+1,x6) else
 let inclusive_list_of_pairs=Memoized.make(function n->
         if n<2 then [] else
         let accu=ref([],(1,1))
-        and number_of_iterations=(n*(n-1))/2 
+        and number_of_iterations=(n*(n+1))/2 
         and iterator=(function (l,c)->(c::l,next_inclusive_pair(c)) ) in
         let _=(for k=1 to number_of_iterations do
         accu:=iterator(!accu)
