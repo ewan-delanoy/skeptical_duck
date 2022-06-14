668a55bb4ed6ccf081de149407ee7d81279306cf
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-06-14 16:40:11 +0200

    add Replace_inside.replace_inside_{file,string}_without_printing_count (step two)

--- Filewatching/fw_with_archives.ml
+++ Filewatching/fw_with_archives.ml
@@ -186,7 +186,7 @@ module Private = struct
    let replace_string old_fw (replacee,replacer) = 
       let apply = (fun par files->
          File_watcher.apply_text_transformation_on_some_files par 
-         (Replace_inside.replace_inside_string (replacee,replacer)) files
+         (Replace_inside.replace_inside_string_without_printing_count (replacee,replacer)) files
       ) in 
       let (all_a_files,all_u_files,_) = full_tripartition old_fw  in 
       let old_parent = parent old_fw in    
