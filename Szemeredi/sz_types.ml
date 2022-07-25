c5485f3bc5e63c4f99dc829a66c56fb833288639
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-07-25 17:26:39 +0200

    add Sz_types.increment_in_knowledge.passive_repeat variant

--- Szemeredi/sz_types.ml
+++ Szemeredi/sz_types.ml
@@ -6,5 +6,14 @@
 
 type increment_in_knowledge = 
     Boundary_increment
+   | Passive_repeat  
    | Fork ;;
 
+(*
+
+type 'a standardized_parametrized_set = {
+   exceptions : ((int * 'a) list) ;
+
+}
+
+*)
