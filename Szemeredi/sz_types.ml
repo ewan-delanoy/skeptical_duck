876d310be4fcd610f7381e738a4d7dccd35c108d
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-07-27 09:33:42 +0200

    add Sz_types.{parametrized_subrange,parametrized_ps_list}

--- Szemeredi/sz_types.ml
+++ Szemeredi/sz_types.ml
@@ -9,11 +9,30 @@ type increment_in_knowledge =
    | Passive_repeat  
    | Fork ;;
 
+type uniform_parametrized_subrange = {
+      positive_exceptions : int list ;
+      negative_exceptions : int list ;
+      modulus : int ;
+      usual :  int list ;
+   } ;; 
+
+type  parametrized_subrange = {
+   ps_exceptions : (int * (int list)) list ;
+   ps_usual : uniform_parametrized_subrange ; 
+} ;; 
+
+type parametrized_ps_list = {
+   pl_exceptions : ((int * (int list list)) list ;
+   pl_usual : parametrized_subrange list ;
+} ;; 
+   
+
+
 (*
 
 type 'a standardized_parametrized_set = {
    exceptions : ((int * 'a) list) ;
-
+   usual : 
 }
 
 *)
