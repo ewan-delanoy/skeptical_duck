baed75052219c13bbb6def1344a8d8dfde0337b1
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-09-14 15:44:20 +0200

    rename Sz_types.enhanced_bulk_result as bulk_result

--- Szemeredi/sz_types.ml
+++ Szemeredi/sz_types.ml
@@ -22,14 +22,14 @@ type sycomore_list =
 
 type forced_data = FD of ((int list) list) * ( qualified_point list) ;;
 
-type enhanced_bulk_result = EBR of sycomore_list * ((int list) list) * forced_data ;;  
+type bulk_result = BR of sycomore_list * ((int list) list) * forced_data ;;  
 
-type for_width_one = FW1 of ((int * enhanced_bulk_result) list) ;;
+type for_width_one = FW1 of ((int * bulk_result) list) ;;
 
 type function_of_size = 
 Width_one of for_width_one 
-|Usual_fos of (int -> enhanced_bulk_result) ;; 
+|Usual_fos of (int -> bulk_result) ;; 
 
 type function_of_scrappers_and_size = 
-Usual_foscras of ( (int list) -> int -> enhanced_bulk_result) ;;
+Usual_foscras of ( (int list) -> int -> bulk_result) ;;
 
