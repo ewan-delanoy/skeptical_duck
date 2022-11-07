036b625f4d013b3159d7cd6b4b51379675a7088f
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-11-07 15:09:26 +0100

    move dfa_ocaml_ending_t to lib/Decomposed_filename/

--- Decomposed_filename/dfa_ocaml_ending_t.ml
+++
@@ -1,8 +0,0 @@
-(*
-
-#use"Decomposed_filename/dfa_ocaml_ending_t.ml";;
-
-*)
-
-type t=Ml |Mli |Mll |Mly;;
-
---
+++ lib/Decomposed_filename/dfa_ocaml_ending_t.ml
@@ -0,0 +1,8 @@
+(*
+
+#use"lib/Decomposed_filename/dfa_ocaml_ending_t.ml";;
+
+*)
+
+type t=Ml |Mli |Mll |Mly;;
+
