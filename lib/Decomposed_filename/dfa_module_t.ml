3822b6e96e1bc666b017f6b750477dd9480a7485
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-11-07 15:07:52 +0100

    move dfa_module_t to lib/Decomposed_filename/

--- lib/Decomposed_filename/dfa_module_t.ml
+++ lib/Decomposed_filename/dfa_module_t.ml
@@ -1,6 +1,6 @@
 (*
 
-#use"Decomposed_filename/dfa_module_t.ml";;
+#use"lib/Decomposed_filename/dfa_module_t.ml";;
 
 A module name, or a candidate for one. Uncapitalized. 
 Should contain no slashes.
