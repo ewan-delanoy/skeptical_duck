9017011c5a52d1045513873cbc5628a0d7c5847b
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-11-07 10:57:18 +0100

    remove Coma_constant.utility_files_subdir

--- Compilation_management/coma_constant.ml
+++ Compilation_management/coma_constant.ml
@@ -10,6 +10,9 @@ module Private = struct
 let fads_subdir=
   Dfa_subdirectory.of_line "Fads";;
 
+let directives_subdir=
+  Dfa_subdirectory.of_line "directives";;
+
 let debugging_subdir=
   Dfa_subdirectory.of_line "debugging";;
 
@@ -34,9 +37,6 @@ let githubbed_nonml_files_subdir=
 let nongithubbed_nonml_files_subdir=
   Dfa_subdirectory.extend nonml_files_subdir "nongithubbed_nonml_files";;  
 
-let utility_files_subdir=
-  Dfa_subdirectory.of_line "Utility_files";;
-
 let build_subdir =   Dfa_subdirectory.of_line "_build";;
 let usual_build_subdir= Dfa_subdirectory.extend build_subdir "_usual_build";;
 let debug_build_subdir= Dfa_subdirectory.extend build_subdir "_debug_build";;  
@@ -56,13 +56,13 @@ let short_path_for_targetfile= Dfn_short.of_line "targetfile.ocaml_made";;
 let rootless_path_for_diary_file=
   Dfn_join.subdirectory_to_short  githubbed_archive_subdir short_path_for_diary_file;;
 let rootless_path_for_loadingsfile=
-  Dfn_join.subdirectory_to_short  utility_files_subdir short_path_for_loadingsfile;;
+  Dfn_join.subdirectory_to_short  directives_subdir short_path_for_loadingsfile;;
 let rootless_path_for_painful_debugging_file=
   Dfn_join.subdirectory_to_short  fads_subdir short_path_for_painful_debugging_file;;
 let rootless_path_for_parametersfile=
   Dfn_join.subdirectory_to_short  parameters_subdir short_path_for_parametersfile;;
 let rootless_path_for_printersfile=
-  Dfn_join.subdirectory_to_short  utility_files_subdir short_path_for_printersfile;;
+  Dfn_join.subdirectory_to_short  directives_subdir short_path_for_printersfile;;
 let rootless_path_for_targetfile=
   Dfn_join.subdirectory_to_short  nongithubbed_nonml_files_subdir short_path_for_targetfile;;     
 
@@ -71,7 +71,6 @@ let rootless_path_for_ocamlinit = Dfn_rootless.of_line ".ocamlinit";;
 
 let git_ignored_subdirectories =
   [
-     utility_files_subdir;
      build_subdir;
      fads_subdir;
      watched_not_githubbed_subdir;
@@ -116,7 +115,6 @@ let conventional_files_with_minimal_content =
 let minimal_set_of_needed_dirs = 
   [
     usual_build_subdir ;
-    utility_files_subdir ;
     githubbed_archive_subdir;
     watched_not_githubbed_subdir;
     watched_and_githubbed_subdir;
@@ -148,6 +146,5 @@ end ;;
  let rootless_path_for_printersfile = Private.rootless_path_for_printersfile ;;
  let rootless_path_for_targetfile = Private.rootless_path_for_targetfile ;;
  let usual_build_subdir = Private.usual_build_subdir ;;
- let utility_files_subdir = Private.utility_files_subdir ;;
 
 
