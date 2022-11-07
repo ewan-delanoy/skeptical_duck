7121a85865ee3fc73a24fe0d86b385e18601046f
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-11-07 15:08:37 +0100

    move outside_comments_and_strings to lib/

--- lib/outside_comments_and_strings.ml
+++ lib/outside_comments_and_strings.ml
@@ -1,6 +1,6 @@
 (*
 
-#use"outside_comments_and_strings.ml";;
+#use"lib/outside_comments_and_strings.ml";;
 
 Detect in a text the parts which can possibly contain module
 names, i.e. those parts which are outside comments and outside
