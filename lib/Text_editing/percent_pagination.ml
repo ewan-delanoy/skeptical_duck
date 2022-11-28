0a64431b3016ca0d60adce92bc79bce8a042cd4a
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-11-28 12:16:04 +0100

    add verbose optional argument to Percent_pagination.extract_all_pages

--- lib/Text_editing/percent_pagination.ml
+++ lib/Text_editing/percent_pagination.ml
@@ -84,12 +84,15 @@ let rec helper_for_page_extraction (treated,current_percent_block,to_be_treated)
     helper_for_page_extraction ((current_percent_block,before_block1)::treated,block1,after_block1) ;;
 
 
-let extract_all_pages_in_lined_form text =
+let extract_all_pages_in_lined_form verbose text =
   let indexed_lines = Lines_in_string.indexed_lines text in 
   match seek_next_percent_block ([],indexed_lines) with 
       None -> raise No_pages_to_extract
-     |Some(_before_block1,block1,after_block1) ->
-      helper_for_page_extraction ([],block1,after_block1) ;;
+     |Some(_before_block1,block1,after_block1) -> 
+      let _ = (if verbose then print_string "Starting to extract the pages ... \n\n\n";flush stdout) in 
+      let answer = helper_for_page_extraction ([],block1,after_block1) in 
+      let _ = (if verbose then print_string "Page extraction ended. \n\n\n";flush stdout) in 
+      answer;;
 
 (*    
  
@@ -98,8 +101,8 @@ extract_all_pages_in_lined_form
 
 *)
 
-let extract_all_pages text =
-    let temp1 = extract_all_pages_in_lined_form text in 
+let extract_all_pages verbose text =
+    let temp1 = extract_all_pages_in_lined_form verbose text in 
     Image.image (
       fun (percent_block,lines_in_page) ->
         (extract_page_number_from_percent_block percent_block,
@@ -114,15 +117,11 @@ extract_all_pages
 *)
 
 let modify_string_pagewise f old_text = 
-  let indexed_pages =extract_all_pages old_text in 
-  let indexed_new_pages = Image.image (fun 
-  (page_nbr,old_text) ->
-    (page_nbr,f old_text)  
-  ) indexed_pages in 
+  let indexed_pages =extract_all_pages true old_text in 
   String.concat "\n" (Image.image (
   fun (page_nbr,old_text) ->
      "\n%\n% Page "^(string_of_int page_nbr)^" \n%\n" ^ (f old_text)
-) indexed_new_pages );; 
+) indexed_pages );; 
 
 
 (*    
@@ -134,7 +133,7 @@ modify_string_pagewise (Replace_inside.replace_inside_string ("\n","_"))
 
 end ;; 
 
-let extract_all_pages = Private.extract_all_pages ;;
+let extract_all_pages ?(verbose=true) text = Private.extract_all_pages verbose text ;;
 let extract_first_page_in_string = Private.extract_first_page_and_remerge ;; 
 let extract_first_page_in_file file = Private.extract_first_page_and_remerge (Io.read_whole_file file) ;; 
 
