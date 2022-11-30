87bd54c1e8f8336cc1771bee029799927205b2d9
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-11-30 14:46:51 +0100

    test commit

--- lib/modulekeyword_use_case.ml
+++ lib/modulekeyword_use_case.ml
@@ -14,16 +14,5 @@ let all=[
     Modulekeyword_use_case_t.Duplicate;
     Modulekeyword_use_case_t.Pointed
 ];;
-  
-  
-  (*
-  let f case s=let (i,j)=Option.unpack(Alternative_str.centered_regexp_match case s 1) in 
-  (i,j,Cull_string.interval s i j);;
-  
-  f include_case " include Peggy;; ";;
-  f include_case " include_once;; ";;
-  f moodle_case " module Amy = Lawson ";;
-  f pointed_case " 57+Everybody.talking-78 ";;
-  
-  *)
+
  
