feed2c237945a3cb7b7a07d30519b5e0994a6036
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-11-30 14:14:43 +0100

    register lib/modulekeyword_use_case_t.ml

---
+++ lib/modulekeyword_use_case_t.ml
@@ -0,0 +1,16 @@
+(*
+
+Enumerate the ways in which the "module" keyword can be used in OCaml.
+
+#use"lib/modulekeyword_use_case_t.ml";;
+
+*)
+
+type t =
+    Include 
+  | Open
+  | Duplicate 
+  | Pointed ;; 
+
+
+ 
