612d9ef1c325fc8cc50e446a347e2c42dee25e62
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-12-02 12:24:27 +0100

    disambiguate variants in Compilation_mode

--- lib/Compilation_management/compilation_mode.ml
+++ lib/Compilation_management/compilation_mode.ml
@@ -8,27 +8,26 @@ exception Ending_for_last_module_exn ;;
 exception Ending_for_nonlast_module_exn ;; 
 
 let workspace = function 
-   Compilation_mode_t.Usual->Coma_constant.usual_build_subdir
-                     |Debug->Coma_constant.debug_build_subdir
-                     |Executable->Coma_constant.exec_build_subdir;;
+    Compilation_mode_t.Usual->Coma_constant.usual_build_subdir
+   |Compilation_mode_t.Debug->Coma_constant.debug_build_subdir
+   |Compilation_mode_t.Executable->Coma_constant.exec_build_subdir;;
 
 let ending_for_last_module = function 
-   Compilation_mode_t.Usual-> raise(Ending_for_last_module_exn)
-                     |Debug->".cmo"
-                     |Executable->".ml";;
+    Compilation_mode_t.Usual-> raise(Ending_for_last_module_exn)
+   |Compilation_mode_t.Debug->".cmo"
+   |Compilation_mode_t.Executable->".ml";;
 
 let ending_for_nonlast_module = function 
    Compilation_mode_t.Usual-> raise(Ending_for_nonlast_module_exn)
-                     |Debug->".cmo"
-                     |Executable->".cmx";;                     
-
+   |Compilation_mode_t.Debug->".cmo"
+   |Compilation_mode_t.Executable->".cmx";;
+                     
 let executioner = function 
    Compilation_mode_t.Usual->"ocamlc -bin-annot "
-                     |Debug->"ocamlc -g "
-                     |Executable->"ocamlopt ";;
+   |Compilation_mode_t.Debug->"ocamlc -g "
+   |Compilation_mode_t.Executable->"ocamlopt ";;
 
 let ending_for_final_product = function 
    Compilation_mode_t.Usual->""
-                     |Debug->".ocaml_debuggable "
-                     |Executable->".ocaml_executable ";;   
-           
+   |Compilation_mode_t.Debug->".ocaml_debuggable "
+   |Compilation_mode_t.Executable->".ocaml_executable ";;
