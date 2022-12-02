706664f62ff3af307afc8fd3d5755de815df1b26
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-12-02 12:26:28 +0100

    decorate unused variables in Dfn_full

--- lib/Decomposed_filename/dfn_full.ml
+++ lib/Decomposed_filename/dfn_full.ml
@@ -5,15 +5,15 @@
 *)
 
 
-let to_subdirectory  (Dfn_full_t.J(r,s,m,e))=s;;
-let to_module  (Dfn_full_t.J(r,s,m,e))=m;;
-let to_ending (Dfn_full_t.J(r,s,m,e))=e;;
+let to_subdirectory  (Dfn_full_t.J(_r,s,_m,_e))=s;;
+let to_module  (Dfn_full_t.J(_r,_s,m,_e))=m;;
+let to_ending (Dfn_full_t.J(_r,_s,_m,e))=e;;
 
-let to_rootless (Dfn_full_t.J(r,s,m,e))=(Dfn_rootless_t.J(s,m,e));; 
-let to_endingless (Dfn_full_t.J(r,s,m,e))=(Dfn_endingless_t.J(r,s,m));; 
+let to_rootless (Dfn_full_t.J(_r,s,m,e))=(Dfn_rootless_t.J(s,m,e));; 
+let to_endingless (Dfn_full_t.J(r,s,m,_e))=(Dfn_endingless_t.J(r,s,m));; 
 
    
-let to_rootless_line (Dfn_full_t.J(r,s,m,e))=
+let to_rootless_line (Dfn_full_t.J(_r,s,m,e))=
     (Dfa_subdirectory.connectable_to_subpath s)^
    (Dfa_module.to_line m)^(Dfa_ending.connectable_to_modulename e);;
 
@@ -29,7 +29,7 @@ let from_absolute_path_with_root ap dir=
   Dfn_join.root_to_rootless dir rless;;
 
   
-let relocate (Dfn_full_t.J(r,old_subdir,m,e)) new_subdir=
+let relocate (Dfn_full_t.J(r,_old_subdir,m,e)) new_subdir=
   (Dfn_full_t.J(r,new_subdir,m,e));;  
  
 
