f3621d1cfe25810f101e63ef52e5892e3b294ddf
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-12-07 14:59:50 +0100

    register lib/Ocaml_preprocessing/Polymorphic_ocaml_records/por_space_example.ml

---
+++ lib/Ocaml_preprocessing/Polymorphic_ocaml_records/por_space_example.ml
@@ -0,0 +1,20 @@
+(*
+
+#use"lib/Ocaml_preprocessing/Polymorphic_ocaml_records/por_space_example.ml";;
+
+*)
+
+
+let filewatching = 
+      let file_there = (fun s-> 
+            Absolute_path.create_file_if_absent(home^"/Teuliou/OCaml/Ordinary/Filewatching/"^s^".ml")) in       
+      
+{
+      Por_types.space_name = "Filewatching" ;
+      Por_types.fields = [] ;
+      Por_types.records = [] ;
+      Por_types.type_signature_file = (file_there "gw_poly_t") ;
+      Por_types.implementation_file = (file_there "gw_poly") ;
+      Por_types.has_crobj_conversion = true ;
+} ;;
+   
