445656d935c616b1c1af8a59631344956635a089
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-12-08 15:59:45 +0100

    register lib/Ocaml_preprocessing/Polymorphic_ocaml_records/por_public_definition_t.ml

---
+++ lib/Ocaml_preprocessing/Polymorphic_ocaml_records/por_public_definition_t.ml
@@ -0,0 +1,11 @@
+(*
+
+#use"lib/Ocaml_preprocessing/Polymorphic_ocaml_records/por_public_definition_t.ml";;
+
+*)
+
+type t = {
+      value_name : string ;
+      lines_in_definition : string list ;
+} ;;
+   
