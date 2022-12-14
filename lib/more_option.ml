d50fad5c93d4f8a1bf6e85a633ff6e0e8cdaadaf
Ewan Delanoy <ewan.delanoy@zoho.com>
2022-12-14 13:10:15 +0100

    remove More_option.filter_and_unpack

--- lib/more_option.ml
+++ lib/more_option.ml
@@ -9,15 +9,6 @@ let argument_on_the_right f x opt_y=match opt_y with
   None->x
   |Some(y)->f x y;;
 
-let filter_and_unpack f l=
- let rec filter0=(function
-  (graet,da_ober)->match da_ober with
-   []->List.rev(graet)
-   |x::peurrest->match f(x) with
-		None->filter0(graet,peurrest)
-		|Some(y)->filter0(y::graet,peurrest)
- ) in
- filter0([],l);;
 
 
 
