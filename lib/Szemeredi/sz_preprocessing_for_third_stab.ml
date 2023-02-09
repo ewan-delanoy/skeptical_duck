1489acc608a2ce2337c7921e89b656501c25426d
Ewan Delanoy <ewan.delanoy@zoho.com>
2023-02-09 10:24:39 +0100

    add Sz_preprocessing_for_third_stab.create_root_node

--- lib/Szemeredi/sz_preprocessing_for_third_stab.ml
+++ lib/Szemeredi/sz_preprocessing_for_third_stab.ml
@@ -4,10 +4,23 @@
 
 *)
 
-let ref_for_divisions_successively_made = ref [] ;; 
-let ref_for_nodes_successively_created = ref [] ;; 
+let ref_for_divisions_successively_made = ref ([]: (string * string * string list) list) ;; 
+let ref_for_nodes_successively_created = ref ([]: (string * string * string) list) ;; 
 
-let add_one_more_division node division new_nodes =
-   (ref_for_divisions_successively_made:=
-     (node,division,new_nodes)::(!ref_for_divisions_successively_made)
-   ) ;;
+let add_one_more_division old_node division new_nodes =
+   let _ = (ref_for_divisions_successively_made:=
+     (old_node,division,new_nodes)::(!ref_for_divisions_successively_made)
+   ) in 
+  List.iter (fun new_node ->
+    ref_for_nodes_successively_created:=
+    (new_node,division,old_node)::(!ref_for_nodes_successively_created)
+    ) new_nodes ;;
+
+exception Create_root_node_exn of string ;;
+
+let create_root_node root_node =
+    if ((!ref_for_divisions_successively_made),(!ref_for_nodes_successively_created))<>([],[])
+    then raise(Create_root_node_exn root_node) 
+    else ref_for_nodes_successively_created:=[(root_node,"no_division","")] ;;
+
+create_root_node "whole" ;;     
