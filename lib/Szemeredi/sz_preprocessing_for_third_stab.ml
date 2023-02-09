9bb6967cc171b7cacf1ae58e222b6efa176709dd
Ewan Delanoy <ewan.delanoy@zoho.com>
2023-02-09 09:22:21 +0100

    add Sz_preprocessing_for_third_stab.ref_for_nodes_successively_created

--- lib/Szemeredi/sz_preprocessing_for_third_stab.ml
+++ lib/Szemeredi/sz_preprocessing_for_third_stab.ml
@@ -5,6 +5,7 @@
 *)
 
 let ref_for_divisions_successively_made = ref [] ;; 
+let ref_for_nodes_successively_created = ref [] ;; 
 
 let add_one_more_division node division new_nodes =
    (ref_for_divisions_successively_made:=
