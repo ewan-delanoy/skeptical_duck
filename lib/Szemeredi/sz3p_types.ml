f5a73c806dd8100946abb94b26243035d014bffc
Ewan Delanoy <ewan.delanoy@zoho.com>
2023-02-22 14:57:59 +0100

    register lib/Szemeredi/sz3p_types.ml

---
+++ lib/Szemeredi/sz3p_types.ml
@@ -0,0 +1,17 @@
+(*
+
+#use"lib/Szemeredi/sz3p_types.ml";;
+
+We make an exception to the rule of not having numbers in module names.
+Sz3p is short for "Preprocessing for third stab at Szemeredi problem".
+
+*)
+
+type node = {
+   node_name : string ;
+} ;;
+
+type division = {
+  division_name : string ; 
+} ;;
+
