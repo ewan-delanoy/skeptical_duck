8fcdbda0bbbcd15c5536af202e31ad9636c3e3c7
Ewan Delanoy <ewan.delanoy@zoho.com>
2023-02-23 16:35:22 +0100

    use Sz3p_types.node_system in Sz3p

--- lib/Szemeredi/sz3p.ml
+++ lib/Szemeredi/sz3p.ml
@@ -40,33 +40,47 @@ let empty_node_system = {
   undivided_nodes = [] ; 
 } ;; 
 
-let ref_for_divisions_successively_made = ref ([]: (node * division * node list) list) ;; 
-let ref_for_nodes_successively_created = ref ([]: (node * division * node) list) ;; 
-let ref_for_undivided_nodes = ref ([]: (node * division * node) list) ;; 
+let add_triple_to_divisions_successively_made syst_ref triple =
+  syst_ref:={(!syst_ref) with 
+  divisions_successively_made=
+   triple::((!syst_ref).divisions_successively_made)
+ } ;;
 
-let refs () = (!ref_for_divisions_successively_made,
-               !ref_for_nodes_successively_created,
-               !ref_for_undivided_nodes) ;;
+let add_triple_to_nodes_successively_created syst_ref triple =
+  syst_ref:={(!syst_ref) with 
+  nodes_successively_created=
+   triple::((!syst_ref).nodes_successively_created)
+ } ;; 
+let remove_from_undivides_nodes syst_ref old_node = 
+  syst_ref:={(!syst_ref) with 
+  undivided_nodes =
+  List.filter (fun (node1,_,_)->
+    node1 <> old_node
+  ) ((!syst_ref).undivided_nodes)
+ } ;; 
+ let add_triple_to_undivided_nodes syst_ref triple =
+  syst_ref:={(!syst_ref) with 
+  undivided_nodes=
+   triple::((!syst_ref).undivided_nodes)
+ } ;; 
 
-let add_one_more_division old_node division new_nodes =
-   let _ = (ref_for_divisions_successively_made:=
-     (old_node,division,new_nodes)::(!ref_for_divisions_successively_made)
-   ) in 
-  let _ = List.iter (fun new_node ->
-    ref_for_nodes_successively_created:=
-    (new_node,division,old_node)::(!ref_for_nodes_successively_created)
-    ) new_nodes in 
-  let _ = (
-    ref_for_undivided_nodes := List.filter (fun (node1,_,_)->
-        node1 <> old_node
-      ) (!ref_for_undivided_nodes)
-  ) in 
-  List.iter (fun new_node ->
-    ref_for_undivided_nodes:=
-    (new_node,division,old_node)::(!ref_for_undivided_nodes)
-    ) new_nodes;;
+let add_one_more_division old_syst old_node division new_nodes =
+   let syst_ref = ref old_syst in 
+   let _ = 
+    (add_triple_to_divisions_successively_made syst_ref (old_node,division,new_nodes);
+     List.iter (fun new_node ->
+      add_triple_to_nodes_successively_created syst_ref (new_node,division,old_node)
+    ) new_nodes;
+    remove_from_undivides_nodes syst_ref old_node;
+    List.iter (fun new_node ->
+      add_triple_to_undivided_nodes syst_ref (new_node,division,old_node)
+    ) new_nodes;
+    )
+  in 
+  !syst_ref;;
 
-let add_typical_division old_node appendix = 
+
+let add_typical_division old_syst old_node appendix = 
   let old_name = old_node.node_name in 
   let new_node = {
     node_name = old_name^"_"^ appendix 
@@ -74,46 +88,54 @@ let add_typical_division old_node appendix =
   and new_division = {
     division_name = appendix ^ "_for_"^old_name
   } in 
-  add_one_more_division old_node new_division [new_node]  ;; 
-
-exception Create_root_node_exn of string ;;
+  add_one_more_division old_syst old_node new_division [new_node]  ;; 
 
 let create_root_node root_node =
-    if ((!ref_for_divisions_successively_made),(!ref_for_nodes_successively_created))<>([],[])
-    then raise(Create_root_node_exn root_node.node_name) 
-    else let empty_node = node_of_string "" in  
-        (ref_for_nodes_successively_created:=[(root_node,no_division,empty_node)];
-          ref_for_undivided_nodes:=[(root_node,no_division,empty_node)]; 
-         ) ;;
+    let empty_node = node_of_string "" in  
+         {
+          divisions_successively_made = [];
+          nodes_successively_created=[(root_node,no_division,empty_node)];
+          undivided_nodes=[(root_node,no_division,empty_node)]; 
+         }
+  ;;
 
-let decompose_list_node_according_to_rangeset old_node ranges = 
-  let _ = add_typical_division old_node "length" in 
+let decompose_list_node_according_to_rangeset old_syst old_node ranges = 
+  let syst_ref = ref old_syst in 
+  let _ = 
+    (syst_ref:=add_typical_division (!syst_ref) old_node "length"; 
   List.iter (
     fun (i_min,i_max) ->
        let s_min = string_of_int i_min 
        and s_max = string_of_int i_max in 
        let r = "range_"^s_min^"_"^s_max in 
-       add_typical_division old_node r 
-  ) ranges ;;
+       syst_ref:=add_typical_division (!syst_ref) old_node r 
+  ) ranges) in 
+ !syst_ref;;
   
-let cut_breadth_size_node_in_two old_node = 
-  (add_typical_division old_node "upper_half" ;
-   add_typical_division old_node "lower_half") ;; 
+let cut_breadth_size_node_in_two old_syst old_node = 
+  let syst1 = add_typical_division old_syst old_node "upper_half" in 
+  add_typical_division syst1 old_node "lower_half" ;; 
 
-let cut_all_breadth_size_nodes_in_two () = 
-  let undivided_nodes = Image.image (fun (node,_,_)->node) (!ref_for_undivided_nodes) in 
-  List.iter cut_breadth_size_node_in_two undivided_nodes ;;
+let cut_all_breadth_size_nodes_in_two old_syst () = 
+  let undivided_nodes = 
+    Image.image (fun (node,_,_)->node) (old_syst.undivided_nodes) in 
+  let syst_ref = ref old_syst in   
+  let _ =List.iter (
+    fun node->
+      syst_ref:=cut_breadth_size_node_in_two (!syst_ref) node )
+  undivided_nodes in 
+  !syst_ref ;;
 
 let node1 = node_of_string "whole" ;;
 
-create_root_node node1 ;;     
+let example=create_root_node node1;;     
 
 let node2 = node_of_string "superficial_result" ;;
 let node3 = node_of_string "selected_solutions" ;;
 let node4 = node_of_string "qpoint_list" ;;
 
-add_one_more_division node1 tripartite_division [node2;node3;node4];; 
+let example2 = add_one_more_division example node1 tripartite_division [node2;node3;node4];; 
 
-decompose_list_node_according_to_rangeset node4 [(1,1);(2,2);(3,100)] ;;
+let example3 = decompose_list_node_according_to_rangeset example2 node4 [(1,1);(2,2);(3,100)] ;;
 
-cut_all_breadth_size_nodes_in_two () ;; 
+let example4 = cut_all_breadth_size_nodes_in_two example3 ;; 
