d01a8eef3baab273cbb6d43a9ee6986598067914
Ewan Delanoy <ewan.delanoy@zoho.com>
2023-04-05 17:53:30 +0200

    add new reconstructed function f_1_empty_i2_qpe_extension_upper_half

--- lib/Szemeredi/sz3_preliminaries.ml
+++ lib/Szemeredi/sz3_preliminaries.ml
@@ -635,6 +635,26 @@ Hashtbl.add
    (1,[],IMD(2)) f_1_empty_i2_qpe_constraints_upper_half ;;
 
 (* End of item at  (1,[],IMD(2),Qpe_constraints,Upper_half) *)
+(* Beginning of item at  (1,[],IMD(2),Qpe_extension,Upper_half) *)
+
+let f_1_empty_i2_qpe_extension_upper_half (B b) (S n) = 
+  match List.assoc_opt b
+  [1,1::(Int_range.range 3 (n-b+1));2,[1;2]@(Int_range.range 4 (n-b+2))] with 
+  Some answer -> answer 
+  |None -> Int_range.range (b+2) n ;;
+
+(* 
+
+   Abstract_qpe_extension_mode.global_check
+    (1,[],IMD(2),Upper_half) f_1_empty_i2_qpe_extension_upper_half ;; 
+
+*)
+
+Hashtbl.add
+ Warehouse.hashtbl_for_qpe_extension_upper_half
+   (1,[],IMD(2)) f_1_empty_i2_qpe_extension_upper_half ;;
+
+(* End of item at  (1,[],IMD(2),Qpe_extension,Upper_half) *)
 
 (* End of warehouse fillings. Do not modify this line *)
 end ;;   
@@ -1578,7 +1598,65 @@ end ;;
 *)(* End of prepared page for (Qpe_constraints,Upper_half) *)
 (* Beginning of prepared page for (Qpe_extension,Lower_half) *)(*
 *)(* End of prepared page for (Qpe_extension,Lower_half) *)
-(* Beginning of prepared page for (Qpe_extension,Upper_half) *)(*
+(* Beginning of prepared page for (Qpe_extension,Upper_half) *)(*(*
+
+#use "watched/watched_and_githubbed/Szemeredi_problem/current_stab_at_szemeredi_problem.ml" ;;
+
+
+An attempt at creating an algorithm that (given enough time) can compute sytematically
+any value of the Szemeredi function. 
+
+*)
+
+
+open Skeptical_duck_lib ;; 
+open Needed_values ;; 
+
+
+open Sz3_preliminaries ;;
+open Tools_for_warehouse ;; 
+let see0 = Overall.get_status () ;; 
+
+
+open Qpe_extension_upper_half_mode ;;
+
+
+let vz1 = visualize 1 ;; 
+let rf1 (B b) (S n) = 
+  match List.assoc_opt b
+  [1,1::(Int_range.range 3 3);2,[1;2]@(Int_range.range 4 4)] with 
+  Some answer -> answer 
+  |None -> Int_range.range (b+2) n ;;
+let check_rf1 = partial_check 1 rf1 ;; 
+
+let vz2 = visualize 2 ;; 
+let rf2 (B b) (S n) = 
+  match List.assoc_opt b
+  [1,1::(Int_range.range 3 (n-b+1));2,[1;2]@(Int_range.range 4 (n-b+2))] with 
+  Some answer -> answer 
+  |None -> Int_range.range (b+2) n ;;
+let check_rf2 = partial_check 2 rf2 ;; 
+
+let vz3 = visualize 3 ;; 
+let rf3 (B b) (S n) = 
+  match List.assoc_opt b
+  [1,1::(Int_range.range 3 (n-b+1));2,[1;2]@(Int_range.range 4 (n-b+2))] with 
+  Some answer -> answer 
+  |None -> Int_range.range (b+2) n ;;
+let check_rf3 = partial_check 2 rf3 ;; 
+
+
+
+(* RFI BEGIN *)
+
+let rfi (B b) (S n) = 
+  match List.assoc_opt b
+  [1,1::(Int_range.range 3 (n-b+1));2,[1;2]@(Int_range.range 4 (n-b+2))] with 
+  Some answer -> answer 
+  |None -> Int_range.range (b+2) n ;;  
+
+(* RFI END *)
+let check_rfi = global_check rfi ;; 
 *)(* End of prepared page for (Qpe_extension,Upper_half) *)
 
 
--- watched/watched_and_githubbed/Szemeredi_problem/current_stab_at_szemeredi_problem.ml
+++ watched/watched_and_githubbed/Szemeredi_problem/current_stab_at_szemeredi_problem.ml
@@ -38,18 +38,22 @@ let rf2 (B b) (S n) =
 let check_rf2 = partial_check 2 rf2 ;; 
 
 let vz3 = visualize 3 ;; 
-let rf2 (B b) (S n) = 
+let rf3 (B b) (S n) = 
   match List.assoc_opt b
   [1,1::(Int_range.range 3 (n-b+1));2,[1;2]@(Int_range.range 4 (n-b+2))] with 
   Some answer -> answer 
   |None -> Int_range.range (b+2) n ;;
-let check_rf2 = partial_check 2 rf2 ;; 
+let check_rf3 = partial_check 2 rf3 ;; 
 
 
 
 (* RFI BEGIN *)
 
-let rfi (B b) (S _n) = [];;   
+let rfi (B b) (S n) = 
+  match List.assoc_opt b
+  [1,1::(Int_range.range 3 (n-b+1));2,[1;2]@(Int_range.range 4 (n-b+2))] with 
+  Some answer -> answer 
+  |None -> Int_range.range (b+2) n ;;  
 
 (* RFI END *)
 let check_rfi = global_check rfi ;; 
