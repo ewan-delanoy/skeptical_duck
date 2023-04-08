9c7e2012ea445def32d9d3d423aa1ea819a06bf5
Ewan Delanoy <ewan.delanoy@zoho.com>
2023-04-08 14:24:25 +0200

    add new reconstructed function f_2_empty_i1_qpe_core_upper_half

--- lib/Szemeredi/sz3_preliminaries.ml
+++ lib/Szemeredi/sz3_preliminaries.ml
@@ -1267,6 +1267,27 @@ Hashtbl.add
 Warehouse.record_one_more_insertion (2,[],IMD(1),Qpe_core,Lower_half) ;;
 
 (* End of item at  (2,[],IMD(1),Qpe_core,Lower_half) *)
+(* Beginning of item at  (2,[],IMD(1),Qpe_core,Upper_half) *)
+
+let f_2_empty_i1_qpe_core_upper_half (B _b) (S n) = 
+   if n<=5 then Empty_point else
+    P(1,[],B(n-5),S(n-3))
+  ;;
+
+(* 
+
+   Abstract_qpe_core_mode.global_check
+    (2,[],IMD(1),Upper_half) f_2_empty_i1_qpe_core_upper_half ;; 
+
+*)
+
+Hashtbl.add
+ Warehouse.wet_hashtbl_for_qpe_core_upper_half
+   (2,[],IMD(1)) f_2_empty_i1_qpe_core_upper_half ;;
+
+Warehouse.record_one_more_insertion (2,[],IMD(1),Qpe_core,Upper_half) ;;
+
+(* End of item at  (2,[],IMD(1),Qpe_core,Upper_half) *)
 (* Beginning of item at  (2,[],IMD(1),Qpe_constraints,Lower_half) *)
 
 let f_2_empty_i1_qpe_constraints_lower_half (B _b) (S n) = 
@@ -2777,40 +2798,31 @@ open Needed_values ;;
 open Sz3_preliminaries ;;
 open Tools_for_warehouse ;; 
 let see0 = Overall.get_status () ;; 
+open Unimode ;;
 
 
-open Qpe_core_upper_half_mode ;;
+Int_range.scale visualize 1 3;; 
+let rf1 (B _b) (S n) = 
+   Empty_point 
+;; 
+let check_rf1 = partial_check 1 (Qpe_core_ARG rf1) ;; 
 
-
-let vz1 = visualize 1 ;; 
-let rf1 (B b) (S _n) = 
-  if b=1 then Empty_point else
-    P(1,[],B(b-1),S(b+1));;
-let check_rf1 = partial_check 1 rf1 ;; 
-
-let vz2 = visualize 2 ;; 
-let rf2 (B b) (S n) = 
-  if b=1 then Empty_point else
-    P(1,[],B(b-1),S(b+1));;
-let check_rf2 = partial_check 2 rf2 ;; 
-
-let vz3 = visualize 3 ;; 
-let rf3 (B b) (S n) = 
-  if b=1 then Empty_point else
-    P(1,[],B(b-1),S(b+1));;
-let check_rf3 = partial_check 2 rf3 ;; 
+let rf2 (B _b) (S n) = 
+   Empty_point 
+;; 
+let check_rf2 = partial_check 2 (Qpe_core_ARG rf2) ;; 
 
 
 
 (* RFI BEGIN *)
 
-let rfi (B b) (S _n) = 
-  if b=1 then Empty_point else
-    P(1,[],B(b-1),S(b+1));; 
+let rfi (B _b) (S n) = 
+   if n<=5 then Empty_point else
+    P(1,[],B(n-5),S(n-3))
+  ;; 
 
 (* RFI END *)
-let check_rfi = global_check rfi ;; 
-*)(* End of prepared page for (Qpe_core,Upper_half) *)
+let check_rfi = Chronometer.it global_check (Qpe_core_ARG rfi) ;; *)(* End of prepared page for (Qpe_core,Upper_half) *)
 (* Beginning of prepared page for (Qpe_constraints,Lower_half) *)(*(*
 
 #use "watched/watched_and_githubbed/Szemeredi_problem/current_stab_at_szemeredi_problem.ml" ;;
--- watched/watched_and_githubbed/Szemeredi_problem/current_stab_at_szemeredi_problem.ml
+++ watched/watched_and_githubbed/Szemeredi_problem/current_stab_at_szemeredi_problem.ml
@@ -21,19 +21,23 @@ open Unimode ;;
 
 Int_range.scale visualize 1 3;; 
 let rf1 (B _b) (S n) = 
-  if n<=2 then 0 else  3-(n mod 3) ;; 
-let check_rf1 = partial_check 1 (Qpl_length_ARG rf1) ;; 
+   Empty_point 
+;; 
+let check_rf1 = partial_check 1 (Qpe_core_ARG rf1) ;; 
 
 let rf2 (B _b) (S n) = 
-  if n<=2 then 0 else  3-(n mod 3) ;; 
-let check_rf2 = partial_check 2 (Qpl_length_ARG rf2) ;; 
+   Empty_point 
+;; 
+let check_rf2 = partial_check 2 (Qpe_core_ARG rf2) ;; 
 
 
 
 (* RFI BEGIN *)
 
 let rfi (B _b) (S n) = 
-   3-(n mod 3) ;; 
+   if n<=5 then Empty_point else
+    P(1,[],B(n-5),S(n-3))
+  ;; 
 
 (* RFI END *)
-let check_rfi = Chronometer.it global_check (Qpl_length_ARG rfi) ;; 
+let check_rfi = Chronometer.it global_check (Qpe_core_ARG rfi) ;; 
