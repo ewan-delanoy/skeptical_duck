21e8fb6663b879a65080409401ae48215e6582df
Ewan Delanoy <ewan.delanoy@zoho.com>
2023-06-07 07:44:12 +0200

    add snippet in diary

--- watched/watched_and_githubbed/diary_archive.ml
+++ watched/watched_and_githubbed/diary_archive.ml
@@ -6,133 +6,27 @@ open Needed_values ;;
 
 
 (************************************************************************************************************************
-Snippet 122 : Some debugging related to the Sz3_preliminaries module
+Snippet 122 : 
 ************************************************************************************************************************)
 
 module Snip122=struct
 
-(*
-open Sz3_preliminaries ;; 
+let building_site = home^"/Teuliou/html_files/Translations/Building_site/";;
 
-let key3 = Kay.constructor (7, [5], 3, 0) ;;
-(*
-let long1 = Medium_level.compute_recursively_and_remember key3 ;;
-*)
+let emptiable_ap = Absolute_path.of_string (building_site^"emptiable_cmist.txt") ;;
+let polished_ap = Absolute_path.of_string (building_site^"polished_cmist.txt") ;;
+let walker_ap = Absolute_path.of_string (building_site^"walker_cmist.txt") ;;  
 
-let hashtbl = Hashtbl_here.greedy ;;
-let hf_hook_finder = Medium_level.default_hook_finder ;; 
-(*
-let long2 = Compute.recursively (hashtbl,hf_hook_finder) key3 ;; 
-*)
-
-(*
-let long3 = Compute.needed_subcomputations (hashtbl,hf_hook_finder) [key3] ;; 
-*)
-
-let v0 = ([],[key3]) ;;     
-let extender = Memoized.small 
-    (Compute.pusher_for_needed_subcomputations (hashtbl,hf_hook_finder)) 
-        v0 ;;  
-
-let gg n=
-  let (a,b) = extender n in
-        (Image.image (fun (key,_)->Kay.deconstructor key) a,"******",
-        Image.image Kay.deconstructor b) ;; 
-      
-let v1 = extender 6 ;; 
-let bad1 = Compute.pusher_for_needed_subcomputations (hashtbl,hf_hook_finder) v1 ;; 
-
-let (HF hook_finder) = hf_hook_finder ;;   
-let (helper,to_be_treated) = v1 ;; 
-let (key,others) = List_again.head_with_tail to_be_treated ;; 
-let (peek_res1,hook_opt) = Peek_and_seek.peek_for_easy_case hashtbl helper key ;;
-let hook_opt2 = hook_finder key ;;
-let hook2 = Option.get hook_opt2 ;; 
-let bad2 = Peek_and_seek.peek_for_hook hashtbl helper key hook2 ;; 
-let (i,j,k) = (function St_fork(i1,j1,k1)->(i1,j1,k1) | _ -> failwith("aaa") ) hook2;;
-let bad3 = Peek_and_seek.peek_for_fork_case hashtbl helper key (i,j,k) ;; 
-
-let cstr = [i;j;k] ;; 
-let candidates = Image.image (Kay.remove_one_element key) cstr ;; 
-let bad4 = 
-      Peek_and_seek.partition_leaves_in_fork_case hashtbl helper candidates ;; 
-let candy = List.hd candidates ;; 
-let bad5 = Peek_and_seek.seek_obvious_access hashtbl helper candy;;
-
-*)    
-
-
-(*
-
-let key2 = Kay.constructor (7, [], 3, 0) ;;
-let hf_hook_finder = Medium_level.improved_hook_finder ;; 
-let (HF hook_finder) = hf_hook_finder ;; 
-
-(*
-let long1 = hook_finder key2 ;; 
-*)
-
-(*
-let long2 = Medium_level.rigorous_quest_for_cumulative_case key2 ;; 
-*)
-
-let (Key(fis,_)) = key2 ;; 
-let domain = List.rev(Finite_int_set.to_usual_int_list fis) ;; 
-
-(*
-let long3 = Medium_level.rigorous_quest_for_individual_cumulative_case key2 5 ;; 
-*)
-
-let old_key = key2 ;; 
-let pivot = 5 ;;
-let simpler_key = Kay.remove_one_element old_key pivot ;; 
-
-(*
-let long4 = Medium_level.compute_recursively_and_remember simpler_key ;;
-*)
-
-*)
-
-
-(*
-
-let key1 = Kay.constructor (8, [], 3, 0) ;;
-
-(*
-let long1 = Medium_level.needed_subcomputations key1 ;; 
-*)
-
-let hashtbl = Hashtbl_here.cautious ;; 
-
-let hf_hook_finder = Medium_level.improved_hook_finder ;; 
-
-(*
-let long2 = Compute.recursively (hashtbl,hook_finder) key1 ;; 
-*)
-
-let tamp1= Peek_and_seek.seek_obvious_access hashtbl [] key1;;
-let long3 = Compute.needed_subcomputations (hashtbl,hf_hook_finder) [key1] ;; 
-
-let v0 = ([],[key1]) ;;     
-let extender = Memoized.small 
-    (Compute.pusher_for_needed_subcomputations (hashtbl,hf_hook_finder)) 
-        v0 ;;  
-let v1 = extender 7 ;; 
-(*
-let long4 = Compute.pusher_for_needed_subcomputations (hashtbl,hf_hook_finder) v1 ;; 
-*)
-
-let (HF hook_finder) = hf_hook_finder ;;   
-let (helper,to_be_treated) = v1 ;; 
-let (key,others) = List_again.head_with_tail to_be_treated ;; 
-let (peek_res1,hook_opt) = Peek_and_seek.peek_for_easy_case hashtbl helper key ;;
-let long5 = hook_finder key ;;
-
-let gg n=
-  let (a,b) = extender n in
-  (Image.image (fun (key,_)->Kay.deconstructor key) a,Image.image Kay.deconstructor b) ;; 
-
-*)  
+  let u1 = Io.read_whole_file walker_ap ;; 
+  let u2 = Lines_in_string.lines u1 ;; 
+  let u3 = List.filter (fun line -> line<>"") u2 ;; 
+  let u4 = List.filter (fun line -> 
+      Supstring.contains line "Conocimiento obscuro"
+    ) u3 ;;
+  let u5 = List.hd u4 ;; 
+  let u6 = Cull_string.cobeginning 126 u5 ;; 
+  let u7 = Cull_string.beginning 5 u6 ;; 
+  let u8 = Strung.explode u7 ;; 
 
 
 end ;;
@@ -987,7 +881,7 @@ let cs1 = Usual_coma_state.Private.main_ref ;;
 
 
 (************************************************************************************************************************
-Snippet 110 : Construct a get_variant_name function from a long type definition 
+Snippet 109 : Construct a get_variant_name function from a long type definition 
 ************************************************************************************************************************)
 let ap1 = Absolute_path.of_string "lib/Padioleau/yp_php_lexer.mll" ;; 
 let old_text = Io.read_whole_file ap1;;
@@ -1014,7 +908,7 @@ let new_text = before_u1 ^ u7 ^ after_u1 ;;
 Io.overwrite_with ap1 new_text ;; 
 
 (************************************************************************************************************************
-Snippet 110 : PARI-GP code to compute an explicit primitive element for a Galois extension with group S5
+Snippet 109 : PARI-GP code to compute an explicit primitive element for a Galois extension with group S5
 ************************************************************************************************************************)
 open Needed_values ;;
 
