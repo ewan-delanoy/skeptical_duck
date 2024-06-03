9e658bbef22014695c8013a5ab9d23761efabf72
Ewan Delanoy <ewan.delanoy@zoho.com>
2024-06-03 12:51:21 +0200

    register lib/Java_project/jpr_types.ml

---
+++ lib/Java_project/jpr_types.ml
@@ -0,0 +1,16 @@
+(*
+
+#use"lib/Java_project/jpr_types.ml";;
+
+*) 
+
+type project_root = Pr of string ;;
+
+type source = Src of string ;;
+
+type java_file = Jf of string ;; 
+
+type java_classname = Jcn of string ;; 
+
+type java_package = Jpkg of string ;; 
+
