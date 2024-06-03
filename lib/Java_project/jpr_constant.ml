007c6e571920627f126020ea7e8bfdb58af6b537
Ewan Delanoy <ewan.delanoy@zoho.com>
2024-06-03 14:13:22 +0200

    register lib/Java_project/jpr_constant.ml

---
+++ lib/Java_project/jpr_constant.ml
@@ -0,0 +1,17 @@
+(*
+
+#use"lib/Java_project/jpr_constant.ml";;
+
+*) 
+
+module Private = struct 
+
+let path_for_spring_5_3_with_boot_2_7 = 
+   (Sys.getenv "HOME")^"/Downloads/my_springs/spring_0/" ;;
+
+end ;;
+
+let spring_5_3_with_boot_2_7 = 
+   Jpr_types.Pr (Private.path_for_spring_5_3_with_boot_2_7) ;;
+
+
