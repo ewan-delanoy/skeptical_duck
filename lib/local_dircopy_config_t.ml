9b01b4ee7bec8659c072b060fc7602beae95eb51
ewan.delanoy <ewan.delanoy@zoho.com>
2024-07-05 16:00:08 +0200

    register lib/local_dircopy_config_t.ml

---
+++ lib/local_dircopy_config_t.ml
@@ -0,0 +1,22 @@
+(*
+
+#use"lib/local_dircopy_t.ml";;
+
+*)
+
+type t = {
+   allowed_number_of_digits: int;
+   remote_dir: Directory_name_t.t ;
+   frontier_dir: Directory_name_t.t; 
+} ;;
+
+
+
+
+
+
+
+
+
+
+
