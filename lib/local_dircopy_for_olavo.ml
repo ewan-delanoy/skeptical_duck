4db123b1778979e1286605822d46be8a911e8ca1
ewan.delanoy <ewan.delanoy@zoho.com>
2024-07-06 13:28:50 +0200

    register lib/local_dircopy_for_olavo.ml

---
+++ lib/local_dircopy_for_olavo.ml
@@ -0,0 +1,38 @@
+(*
+
+#use"lib/local_dircopy_for_olavo.ml";;
+
+If you wish to reinitialize the object from scratch, you can do 
+
+
+
+*)
+
+
+module Private = struct 
+
+let config = {
+   Local_dircopy_config_t.allowed_number_of_digits = 3;
+   remote_dir = "/media/" ^ (Sys.getenv "USER") ^ "/HEAVY/Other/OC/" ;
+   frontier_dir = (Sys.getenv "HOME") ^ "/Downloads/OC/Lennet"; 
+   file_for_persistence = "videos_with_olavo.txt";
+} ;; 
+
+let main_ref = ref (Local_dircopy.reload config) ;;
+
+
+
+end ;;
+
+
+let show_files () = Local_dircopy.show_files (!Private.main_ref) ;;
+let update () = (Private.main_ref:= Local_dircopy.update (!Private.main_ref)) ;;
+
+
+
+
+
+
+
+
+
