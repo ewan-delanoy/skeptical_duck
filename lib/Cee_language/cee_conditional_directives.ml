bcb1b48167aad43bffb3cccd22e0bb0ba49346f8
ewan.delanoy <ewan.delanoy@zoho.com>
2024-08-02 17:05:26 +0200

    add types into Cee_conditional_directives

--- lib/Cee_language/cee_conditional_directives.ml
+++ lib/Cee_language/cee_conditional_directives.ml
@@ -5,11 +5,35 @@
 *)
 
 exception Else_with_elif of int * int * int ;;
+exception Endif_without_matching_non_elif of int ;;
 exception Lonely_elif of int * string ;;
 exception Lonely_else of int * string ;;
 exception Lonely_endif of int * string ;;
 exception Two_elses of int * int * int ;;
-exception Unfinished of ( int * (int option) ) ;; 
+exception Unfinished of ( int * bool * (int option) ) ;; 
+
+type initial_command_t = {
+   short_path : string ;
+   ending : string ;
+   core_of_command : string ;
+} ;;
+
+type config_t = {
+   source : Directory_name_t.t ;
+   destination : Directory_name_t.t ;
+   commands : initial_command_t list;
+} ;;
+
+let rec first_nonelif_finder 
+  (line_idx,treated,partially_treated) = match partially_treated with 
+    [] -> raise(Endif_without_matching_non_elif(line_idx))
+    |(start_idx,is_an_elif,middle_idx_opt) :: other_partially_treated ->
+      if is_an_elif 
+      then first_nonelif_finder 
+      (line_idx,(start_idx,middle_idx_opt,line_idx) :: treated,other_partially_treated)
+      else ((start_idx,middle_idx_opt,line_idx) :: treated,other_partially_treated);;    
+
+
 
 let rec helper_for_conditional_directive_linedexing (treated,partially_treated,to_be_treated) = 
   match to_be_treated with 
@@ -20,20 +44,20 @@ let rec helper_for_conditional_directive_linedexing (treated,partially_treated,t
         )
   |(line_idx,line) :: other_lines ->
      if String.starts_with line ~prefix:"#if"
-     then helper_for_conditional_directive_linedexing (treated,(line_idx,None)::partially_treated,other_lines)   
+     then helper_for_conditional_directive_linedexing (treated,(line_idx,false,None)::partially_treated,other_lines)   
      else  
      if String.starts_with line ~prefix:"#else"
      then 
         (
           match partially_treated with 
           [] -> raise(Lonely_else(line_idx,line))
-          |(start_idx,middle_idx_opt) :: other_partially_treated ->
+          |(start_idx,is_an_elif,middle_idx_opt) :: other_partially_treated ->
             (
               match middle_idx_opt with 
               (Some (idx1)) -> raise(Two_elses(start_idx,idx1,line_idx))
               |None ->
                 helper_for_conditional_directive_linedexing 
-                (treated,(start_idx,Some line_idx)::other_partially_treated,other_lines)   
+                (treated,(start_idx,is_an_elif,Some line_idx)::other_partially_treated,other_lines)   
             )
          ) 
      else   
@@ -42,13 +66,13 @@ let rec helper_for_conditional_directive_linedexing (treated,partially_treated,t
            (
              match partially_treated with 
              [] -> raise(Lonely_elif(line_idx,line))
-             |(start_idx,middle_idx_opt) :: other_partially_treated ->
+             |(start_idx,is_an_elif,middle_idx_opt) :: other_partially_treated ->
                (
                  match middle_idx_opt with 
                  (Some (idx1)) -> raise(Else_with_elif(start_idx,idx1,line_idx))
                  |None ->
                    helper_for_conditional_directive_linedexing 
-                   (treated,(line_idx,None)::(start_idx,Some line_idx)::other_partially_treated,other_lines)   
+                   (treated,(line_idx,true,None)::(start_idx,is_an_elif,Some line_idx)::other_partially_treated,other_lines)   
                )
             ) 
      else    
@@ -57,11 +81,12 @@ let rec helper_for_conditional_directive_linedexing (treated,partially_treated,t
          (
            match partially_treated with 
            [] -> raise(Lonely_endif(line_idx,line))
-           |(start_idx,middle_idx_opt) :: other_partially_treated ->
-             (
+           | _ ->
+              let (treated2,partially_treated2) = 
+              first_nonelif_finder 
+              (line_idx,treated,partially_treated) in 
                helper_for_conditional_directive_linedexing 
-                 ((start_idx,middle_idx_opt,line_idx)::treated,other_partially_treated,other_lines)   
-             )
+                 (treated2,partially_treated2,other_lines)   
           ) 
      else      
     helper_for_conditional_directive_linedexing (treated,partially_treated,other_lines) ;;
@@ -160,46 +185,76 @@ let watermark_text text =
    ) lines in 
    (String.concat "\n" (List.flatten temp1)) ;;
 
-let main_pp_command (root_dir,raw_core_of_command,short_path) = 
-    let core_of_command = adapt_command root_dir raw_core_of_command in 
-    let s_ap = root_dir ^ short_path in 
-    let short_s_ap = Cull_string.coending 2 s_ap 
-    and ending = Cull_string.ending 2 s_ap in
-    let second_filename = 
-       (short_s_ap^"_"^random_marker^"_second"^ending) in
-    let third_filename = 
-      (short_s_ap^"_"^random_marker^"_third"^ending) in 
-    core_of_command^" -E "^second_filename^" -o "^third_filename  ;;
+let make_initial_commands source raw_command = 
+  let i1 = Option.get(Substring.leftmost_index_of_in_from_opt " -c " raw_command 1) in 
+  let i2 = Option.get(Substring.leftmost_index_of_in_from_opt " " raw_command (i1+4)) in
+  let short_filename = Cull_string.interval raw_command (i1+4) (i2-1) in 
+  let modulename = Cull_string.coending 2 short_filename in 
+  let present_endings = (
+    if Sys.file_exists ((Directory_name.connectable_to_subpath source) ^ modulename ^ ".h") 
+      then [".h";".c"]
+      else [".c"]
+  )  in 
+  Image.image (fun edg ->
+  {
+    short_path = modulename ;
+    ending = edg ;
+    core_of_command =Cull_string.beginning (i1-1) raw_command ;
+ } ) present_endings ;;
+ 
 
-let watermark_file (root_dir,raw_core_of_command,short_path) = 
-    let s_ap = root_dir ^ short_path in 
-    let short_s_ap = Cull_string.coending 2 s_ap 
-    and ending = Cull_string.ending 2 short_path in
-    let ap = Absolute_path.of_string s_ap in  
-    let old_text = Io.read_whole_file ap in 
-    let second_text = watermark_text old_text 
-    and second_filename = 
+let initialize_configuration 
+  ~source:src ~destination:dest raw_commands = 
+  {
+    source = src ;
+    destination = dest ;
+    commands = List.flatten(Image.image (make_initial_commands src) raw_commands);
+ } ;;
+
+
+let main_preprocessing_command config init_cmd = 
+  let src_dir = Directory_name.connectable_to_subpath config.source in  
+  let core_of_command = adapt_command src_dir init_cmd.core_of_command in 
+  let s_ap = src_dir ^ init_cmd.short_path ^ init_cmd.ending in 
+  let short_s_ap = Cull_string.coending 2 s_ap 
+  and ending = Cull_string.ending 2 s_ap in
+  let second_filename = 
        (short_s_ap^"_"^random_marker^"_second"^ending) in
-    let second_file = Absolute_path.create_file_if_absent second_filename in
-    let _ = Io.overwrite_with second_file second_text in 
-    let current_dir = Sys.getcwd () in 
+  let third_filename = 
+      (short_s_ap^"_"^random_marker^"_third"^ending) in 
+  core_of_command^" -E "^second_filename^" -o "^third_filename  ;;
+
+let watermark_file config init_cmd  = 
+  let src_dir = Directory_name.connectable_to_subpath config.source 
+  and dest_dir = Directory_name.connectable_to_subpath config.destination in  
+  let s_ap = src_dir ^ init_cmd.short_path ^ init_cmd.ending in 
+  let short_s_ap = Cull_string.coending 2 s_ap 
+  and ending = init_cmd.ending in
+  let ap = Absolute_path.of_string s_ap in  
+  let old_text = Io.read_whole_file ap in 
+  let second_text = watermark_text old_text 
+  and second_filename = 
+       (short_s_ap^"_"^random_marker^"_second"^ending) in
+  let second_file = Absolute_path.create_file_if_absent second_filename in
+  let _ = Io.overwrite_with second_file second_text in 
+  let current_dir = Sys.getcwd () in 
     let third_filename = 
       (short_s_ap^"_"^random_marker^"_third"^ending) in 
-    let cmd1 = main_pp_command (root_dir,raw_core_of_command,short_path) in 
-    let _ = (print_string("Executing "^cmd1^" ...");flush stdout) in 
-    let cmds = [
-      " cd "^root_dir;
+  let cmd1 = main_preprocessing_command config init_cmd in 
+  let _ = (print_string("Executing "^cmd1^" ...");flush stdout) in 
+  let cmds = [
+      " cd "^src_dir;
       cmd1;
       " cd "^current_dir;      
-    ] in 
-    let _ = Unix_command.conditional_multiple_uc cmds in 
-    let third_file = Absolute_path.of_string third_filename in 
-    let third_text =Io.read_whole_file third_file in 
-    let new_text = rewrite_using_watermarks old_text third_text in 
-    let fourth_filename = 
-      (short_s_ap^"_"^random_marker^"_fourth"^ending) in 
-    let fourth_file = Absolute_path.create_file_if_absent fourth_filename in  
-    Io.overwrite_with fourth_file new_text ;;
+  ] in 
+  let _ = Unix_command.conditional_multiple_uc cmds in 
+  let third_file = Absolute_path.of_string third_filename in 
+  let third_text =Io.read_whole_file third_file in 
+  let new_text = rewrite_using_watermarks old_text third_text in 
+  let fourth_filename = 
+      (dest_dir ^ init_cmd.short_path ^ending) in 
+  let fourth_file = Absolute_path.create_file_if_absent fourth_filename in  
+  Io.overwrite_with fourth_file new_text ;;
 
 
 
