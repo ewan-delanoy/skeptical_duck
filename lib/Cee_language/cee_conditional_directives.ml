37d2434e12e9165b586d1c0162e7b5534d7b3863
ewan.delanoy <ewan.delanoy@zoho.com>
2024-08-05 17:02:29 +0200

    refactor Cee_conditional_directives

--- lib/Cee_language/cee_conditional_directives.ml
+++ lib/Cee_language/cee_conditional_directives.ml
@@ -187,262 +187,56 @@ module Walker = struct
 
 end ;;  
 
-let compute_small_spaces text =
+let compute_small_spaces_in_text text =
    Walker.iterate(Walker.make text) ;; 
 
-type multiline_t = {
-   is_finished : bool ;
-   last_line_idx : int ;
-} ;;
-
-module Multiline = struct 
-  
-let opt line_idx line = 
-  if String.ends_with line ~suffix:"\\"
-  then Some (
-      {
-       is_finished = false ;
-       last_line_idx = line_idx ;
-      }) 
-  else None ;;
-
-let finish_here ml = {
-   ml with 
-   is_finished = true;
-} ;;
-
-end ;;   
-
-type incomplete_conditional_directive_t = {
-   start_idx : int ;
-   multiline : multiline_t option ;
-   is_an_elif : bool ;
-   middle_idx : int option ;
-} ;;
-
-module Incomplete_conditional_directive = struct 
-
-let make line_idx line ~is_elif= {
-  start_idx = line_idx ;
-  multiline = Multiline.opt line_idx line ;
-  is_an_elif  = is_elif ;
-  middle_idx = None ;
-} ;;
-
-let add_elsie line_idx icd = 
-  match icd.middle_idx with 
-  (Some middle_idx) -> raise (Two_elses(icd.start_idx,middle_idx,line_idx))
-  | None ->
-  {
-  icd with
-  multiline = Option.map Multiline.finish_here icd.multiline  ;
-  middle_idx = Some line_idx ;
-} ;;
-
-let add_usual (line_idx,line) icd = 
-  match icd.multiline with 
-   None -> icd
-  |(Some multiline) -> 
-    if multiline.is_finished 
-    then icd 
-    else  
-    let new_multiline = {
-      last_line_idx = line_idx;
-      is_finished = (not(String.ends_with line ~suffix:"\\"))
-      } in 
-    {
-      icd with 
-      multiline = Some new_multiline ;
-    } ;;
-
-let range_for_master_ivy icd = 
-  let end_idx = (
-    match icd.multiline with 
-    None -> icd.start_idx
-    |(Some multiline) -> multiline.last_line_idx
-  ) in 
-  (icd.start_idx,end_idx) ;;  
-
-
-end ;;  
-
-exception Unfinished of incomplete_conditional_directive_t ;; 
-
-type complete_conditional_directive_t = {
-  before_the_end : incomplete_conditional_directive_t ;
-  end_idx : int;
-} ;;
-
-module Complete_conditional_directive = struct 
-  
-let make line_idx icd = {
-  before_the_end = icd ;
-  end_idx = line_idx;
-} ;;
-
-let range_for_master_ivy ccd = 
-   Incomplete_conditional_directive.range_for_master_ivy ccd.before_the_end ;; 
-
-end ;;
-
-type one_ivy_several_elives_t = {
-    the_ivy : incomplete_conditional_directive_t;
-    elives : incomplete_conditional_directive_t list
-} ;;
-
-module One_ivy_several_elives = struct 
-
-let head oise = 
-  match oise.elives with 
-  [] -> oise.the_ivy 
-  |elif :: _ -> elif ;;
-
-let make ivy = {
-  the_ivy = ivy;
-  elives = [] 
-} ;;
-
-let add_elif new_elif oise = {
-  oise with 
-  elives = new_elif :: oise.elives ;
-} ;;
-
-let add_elsie line_idx oise = 
-  match oise.elives with 
-  [] -> {
-    oise with 
-    the_ivy = Incomplete_conditional_directive.add_elsie line_idx oise.the_ivy
-  }
-  | last_elif :: other_elives ->
-
-  {
-  oise with 
-  elives = (Incomplete_conditional_directive.add_elsie line_idx last_elif) :: other_elives ;
-} ;; 
-
-let add_usual (line_idx,line) oise = 
-  match oise.elives with 
-  [] -> {
-    oise with 
-    the_ivy = Incomplete_conditional_directive.add_usual (line_idx,line) oise.the_ivy
-  }
-  | last_elif :: other_elives ->
-
-  {
-  oise with 
-  elives = (Incomplete_conditional_directive.add_usual (line_idx,line) last_elif) :: other_elives ;
-} ;; 
-
-end ;;  
-
-type partially_treated_t = PT of one_ivy_several_elives_t list ;;
-
-module Partially_treated = struct 
-
-let empty_one = PT [] ;;
-
-let head (PT l) = One_ivy_several_elives.head(List.hd l) ;;  
-
-
-let add_ivy (line_idx,line) (PT l) = 
-  let new_ivy = Incomplete_conditional_directive.make line_idx line ~is_elif:false in 
-  PT((One_ivy_several_elives.make new_ivy)::l) ;;
-
-let add_elif (line_idx,line) (PT l) =  
-  match l with 
-  [] -> raise(Lonely_elif(line_idx,line)) 
-  |oise :: other_blocks ->
-  let new_elif = Incomplete_conditional_directive.make line_idx line ~is_elif:true in 
-  PT((One_ivy_several_elives.add_elif new_elif oise)::other_blocks) ;;
-  
-
-let add_endif line_idx treated (PT l) =  
-  match l with 
-  [] -> raise(Lonely_endif(line_idx))
-  |oise :: other_blocks ->
-    let new_treated_items =
-      List.rev_map (Complete_conditional_directive.make line_idx)
-        (oise.elives@[oise.the_ivy]) in
-     (List.rev_append new_treated_items treated,PT other_blocks) ;;
- 
-let add_elsie line_idx (PT l) =  
-    match l with 
-    [] -> raise(Lonely_else(line_idx))
-  |oise :: other_blocks -> 
-   PT((One_ivy_several_elives.add_elsie line_idx oise) :: other_blocks) ;;
-
-let add_usual (line_idx,line) (PT l) =  
-    match l with 
-    [] -> PT l 
-    |oise :: other_blocks -> 
-    PT((One_ivy_several_elives.add_usual (line_idx,line) oise)::other_blocks) ;;
-    
-
-
-end ;;  
-
-
-let test_for_directive_at_beginning_of_line directive_name line =
-  if not(String.starts_with line ~prefix:"#")
-  then false  
-  else  
-  match Strung.char_finder_from_inclusive_opt (fun c->
-          not(List.mem c [' ';'\t';'\r'])
-        ) line 2 with 
-  None -> false 
-  |Some idx1 -> Substring.is_a_substring_located_at directive_name line idx1 ;;   
-
-let rec helper_for_conditional_directive_linedexing (treated,partially_treated,to_be_treated) = 
-  match to_be_treated with 
-  [] -> (
-          if partially_treated = Partially_treated.empty_one 
-          then List.rev treated 
-          else raise(Unfinished(Partially_treated.head partially_treated)) 
-        )
-  |(line_idx,line) :: other_lines ->
-     if test_for_directive_at_beginning_of_line "if" line
-     then helper_for_conditional_directive_linedexing 
-         (treated,Partially_treated.add_ivy (line_idx,line) partially_treated,other_lines)   
-     else  
-     if test_for_directive_at_beginning_of_line "else" line
-     then helper_for_conditional_directive_linedexing
-          (treated,Partially_treated.add_elsie line_idx partially_treated,other_lines)
-     else   
-     if test_for_directive_at_beginning_of_line "elif" line
-     then helper_for_conditional_directive_linedexing
-          (treated,Partially_treated.add_elif (line_idx,line) partially_treated,other_lines)     
-     else    
-     if test_for_directive_at_beginning_of_line "endif" line
-     then let (treated2,partially_treated2) = Partially_treated.add_endif line_idx treated partially_treated in 
-          helper_for_conditional_directive_linedexing 
-                 (treated2,partially_treated2,other_lines)   
-     else helper_for_conditional_directive_linedexing 
-          (treated,Partially_treated.add_usual (line_idx,line)  partially_treated,other_lines) ;;
-
-let compute_line_indices_for_cds_in_text text = 
-  let lines = Lines_in_string.indexed_lines text in 
-  helper_for_conditional_directive_linedexing ([],Partially_treated.empty_one,lines) ;;
 (*
 
 let text1 = String.concat "\n" 
 [
-   "#if 1\\";
-   "2\\";
-   "3";
-   "#elif 4\\";
-   "5";
-   "#if 6";
-   "7";
+   "#if 1";
+   "2";
+   "#elif 3";
+   "4";
+   "#if 5";
+   "6";
+   "#endif 7";
    "#endif 8";
-   "#endif 9";
+   "9";
+   "#if 10\\";
+   "11\\";
+   "12\\";
+   "13";
+   "14";
+   "#endif 15"
 ] ;;
 
-let see1 = Cee_conditional_directives.compute_line_indices_for_cds_in_text text1 ;;
+let check1 = Cee_conditional_directives.compute_small_spaces_in_text text1 ;;
 
-*)
+let text2 = String.concat "\n" 
+[
+   "1";
+   "2";
+   "#if 3\\";
+   "4\\";
+   "5";
+   "#elif 6\\";
+   "7";
+   "#if 8";
+   "9";
+   "#endif 10";
+   "11";
+   "12";
+   "#endif 13";
+   "14";
+] ;;
 
-let compute_line_indices_for_cds_in_file ap = 
-  compute_line_indices_for_cds_in_text (Io.read_whole_file ap)  ;;  
+let check2 = Cee_conditional_directives.compute_small_spaces_in_text text2 ;;
+
+*)   
+
+let compute_small_spaces_in_file ap = 
+  compute_small_spaces_in_text (Io.read_whole_file ap)  ;;  
 
 let adapt_ii_element root_dir i_elt = 
   if i_elt="-I." then "-I"^root_dir else 
@@ -477,24 +271,6 @@ let parametrized_line cd_idx =
   "char* unused_string"^(string_of_int cd_idx)^"=\""^
   random_marker^(parametrized_marker cd_idx)^random_marker^"\";" ;;
 
-let compute_intervals_to_be_removed_in_cut_range middle_idx ccd block_is_accepted = 
-  let end_idx = ccd.end_idx in 
-  if block_is_accepted
-  then [Complete_conditional_directive.range_for_master_ivy ccd;(middle_idx,end_idx)]
-  else [(ccd.before_the_end.start_idx,middle_idx);(end_idx,end_idx)] ;;  
-  
-
-let compute_intervals_to_be_removed_in_uncut_range ccd block_is_accepted = 
-  let end_idx = ccd.end_idx in 
-  if block_is_accepted
-  then [Complete_conditional_directive.range_for_master_ivy ccd;(end_idx,end_idx)]
-  else [(ccd.before_the_end.start_idx,end_idx)] ;;    
-
-let compute_intervals_to_be_removed ccd block_is_accepted = 
-  match  ccd.before_the_end.middle_idx  with 
-  (Some middle_idx) -> compute_intervals_to_be_removed_in_cut_range middle_idx ccd block_is_accepted
-  | None -> compute_intervals_to_be_removed_in_uncut_range ccd block_is_accepted ;;
-
 let is_in_interval x (a,b) = (a<=x) && (x<=b) ;; 
 
 let is_in_interval_union x intervals =
@@ -502,61 +278,44 @@ let is_in_interval_union x intervals =
 
 let rewrite_using_watermarks old_text watermarked_text =   
   let lines = Lines_in_string.indexed_lines old_text 
-  and ccds = compute_line_indices_for_cds_in_text old_text  in 
-  let indexed_ccds = Int_range.index_everything ccds in 
-  let intervals_to_be_removed = List.flatten(Image.image (
-     fun (ccd_idx,ccd) ->
-      let block_is_accepted = 
-        Substring.is_a_substring_of (parametrized_marker ccd_idx) watermarked_text in 
-      compute_intervals_to_be_removed  ccd block_is_accepted 
-  ) indexed_ccds) in 
+  and ssps = compute_small_spaces_in_text old_text  in 
+  let indexed_ssps = Int_range.index_everything ssps in 
+  let accepted_ssps = List.filter(
+     fun (ssp_idx,ssp) ->
+      if ssp.namespace = 0 then true else 
+      Substring.is_a_substring_of (parametrized_marker ssp_idx) watermarked_text 
+  ) indexed_ssps in 
+  let accepted_intervals = Image.image (
+    fun (_,ssp) -> (ssp.start_idx,ssp.end_idx)
+  ) accepted_ssps in 
   let accepted_lines = List.filter_map (
      fun (line_idx,line) ->
-       if is_in_interval_union line_idx intervals_to_be_removed 
-       then None 
-      else Some line 
+       if is_in_interval_union line_idx accepted_intervals 
+       then Some line 
+      else None
   ) lines in 
   String.concat "\n" accepted_lines ;;
 
-let rec helper_for_text_watermarking indexed_ccds
-   (treated,ccd_idx_in_progress_opt,to_be_treated) =
-   match to_be_treated with 
-   [] -> List.rev treated 
-   |(line_idx,line) :: others ->
-      if (test_for_directive_at_beginning_of_line "if" line) ||
-        (test_for_directive_at_beginning_of_line "elif" line) 
-      then let ccd_idx =  
-             fst(List.find (fun (_idx,ccd)->ccd.before_the_end.start_idx=line_idx) indexed_ccds) in 
-           let treated2 = (
-             match ccd_idx_in_progress_opt with 
-             None -> treated 
-             |Some ccd_idx_in_progress -> (parametrized_line ccd_idx_in_progress)::treated
-           )  in 
-           (
-            if String.ends_with line ~suffix:"\\"
-            then  helper_for_text_watermarking indexed_ccds 
-                 (line::treated2,Some ccd_idx,others)
-            else  helper_for_text_watermarking indexed_ccds 
-                 ((parametrized_line ccd_idx)::line::treated2,None,others)  
-           )
-      else  
-      match ccd_idx_in_progress_opt with 
-      None -> helper_for_text_watermarking indexed_ccds 
-             (line::treated,None,others)
-      |Some ccd_idx_in_progress ->
-        if String.ends_with line ~suffix:"\\"
-          then  helper_for_text_watermarking indexed_ccds 
-               (line::treated,ccd_idx_in_progress_opt,others)
-          else  helper_for_text_watermarking indexed_ccds 
-               ((parametrized_line ccd_idx_in_progress)::line::treated,None,others)        
+let pairs_of_indices_for_watermarking indexed_ssps = 
+  List.filter_map (
+    fun (ssp_idx,ssp) ->
+      if ssp.namespace = 0 then None else 
+      Some(ssp.start_idx,ssp_idx) 
+  ) indexed_ssps ;;
 
 let watermark_text text = 
    let lines = Lines_in_string.indexed_lines text 
-   and ccds = compute_line_indices_for_cds_in_text text in 
-   let indexed_ccds = Int_range.index_everything ccds in 
-   let temp1 = helper_for_text_watermarking indexed_ccds 
-   ([],None,lines) in 
-   (String.concat "\n" temp1) ;;
+   and ssps = compute_small_spaces_in_text text in 
+   let indexed_ssps = Int_range.index_everything ssps in
+   let pairs = pairs_of_indices_for_watermarking indexed_ssps in 
+   let temp1 = Image.image (
+      fun (line_idx,line) ->
+        match List.assoc_opt line_idx pairs with 
+        None -> [line]
+        | (Some ssp_idx) ->
+           [parametrized_line ssp_idx;line]
+   ) lines in  
+   (String.concat "\n" (List.flatten temp1)) ;;
 
 (*
 
