b188306ab3998720e1e03c4c6a7c3ee4de73f9a1
ewan.delanoy <ewan.delanoy@zoho.com>
2024-08-06 11:00:55 +0200

    make Cee_conditional_directives use Cee_initial_command

--- lib/Cee_language/cee_conditional_directives.ml
+++ lib/Cee_language/cee_conditional_directives.ml
@@ -338,24 +338,12 @@ print_string(Cee_conditional_directives.rewrite_using_watermarks text1 text3);;
 
 *)
 
-
-let make_initial_command raw_command = 
-  let i1 = Option.get(Substring.leftmost_index_of_in_from_opt " -c " raw_command 1) in 
-  let i2 = Option.get(Substring.leftmost_index_of_in_from_opt " " raw_command (i1+4)) in
-  let short_filename = Cull_string.interval raw_command (i1+4) (i2-1) in 
-  {
-    Cee_initial_command_t.short_path = Cull_string.coending 2 short_filename ;
-    ending = Cull_string.ending 2 short_filename ;
-    core_of_command =Cull_string.beginning (i1-1) raw_command ;
-  }  ;;
- 
-
 let initialize_configuration 
   ~source:src ~destination:dest raw_commands = 
   {
     source = src ;
     destination = dest ;
-    commands = Image.image make_initial_command  raw_commands;
+    commands = Image.image Cee_initial_command.make raw_commands;
  } ;;
 
 
--- lib/Cee_language/cee_initial_command.ml
+++ lib/Cee_language/cee_initial_command.ml
@@ -5,9 +5,13 @@
 *)
 
 
-type t = {
-   short_path : string ;
-   ending : string ;
-   core_of_command : string ;
-} ;;
+let make raw_command = 
+   let i1 = Option.get(Substring.leftmost_index_of_in_from_opt " -c " raw_command 1) in 
+   let i2 = Option.get(Substring.leftmost_index_of_in_from_opt " " raw_command (i1+4)) in
+   let short_filename = Cull_string.interval raw_command (i1+4) (i2-1) in 
+   {
+     Cee_initial_command_t.short_path = Cull_string.coending 2 short_filename ;
+     ending = Cull_string.ending 2 short_filename ;
+     core_of_command =Cull_string.beginning (i1-1) raw_command ;
+   }  ;;
 
