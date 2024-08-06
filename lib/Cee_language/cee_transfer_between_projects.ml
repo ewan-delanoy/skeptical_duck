3a5f798f79db324037fc990585c291109aa06399
ewan.delanoy <ewan.delanoy@zoho.com>
2024-08-06 11:02:10 +0200

    register lib/Cee_language/cee_transfer_between_projects.ml

---
+++ lib/Cee_language/cee_transfer_between_projects.ml
@@ -0,0 +1,15 @@
+(*
+
+#use"lib/Cee_language/cee_transfer_between_projects.ml";;
+
+*)
+
+
+let initialize 
+  ~source:src ~destination:dest raw_commands = 
+  {
+    Cee_transfer_between_projects_t.source = src ;
+    destination = dest ;
+    commands = Image.image Cee_initial_command.make raw_commands;
+ } ;;
+
