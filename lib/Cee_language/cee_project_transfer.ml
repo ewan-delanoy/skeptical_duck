5d0af60fb38ebfb854bfcaf149adcb05ac7c2472
ewan.delanoy <ewan.delanoy@zoho.com>
2024-08-19 11:20:28 +0200

    put Capsule submodule after Private submodule in Cee_project_transfer

--- lib/Cee_language/cee_project_transfer.ml
+++ lib/Cee_language/cee_project_transfer.ml
@@ -34,12 +34,9 @@ module type CAPSULE_TYPE = sig
   end ;;
 
 
+module Private = struct 
 
- 
-module Capsule = (struct 
-
-
- type immutable_t = {
+  type immutable_capsule_t = {
     source : Directory_name_t.t ;
     destination : Directory_name_t.t ;
     commands : Cee_compilation_command_t.t list;
@@ -47,21 +44,23 @@ module Capsule = (struct
     separate_commands_opt : (Cee_compilation_command_t.separate_t list) option;
     directly_compiled_files_opt : (string list) option ;
     filecontents : (string, string) Hashtbl.t ;
- } ;;
-
- type t = immutable_t ref ;;
- let str_order = Total_ordering.lex_for_strings ;;
- let str_sort = Ordered.sort str_order ;;  
- let source cpsl = (!cpsl).source ;;
-
- let destination cpsl = (!cpsl).destination ;; 
-
- let commands cpsl = (!cpsl).commands ;;
-
-
- let make 
- ~source:src ~destination:dest raw_commands = 
- ref({
+  } ;;
+  
+  type capsule_t = immutable_capsule_t ref ;;
+  let str_order = Total_ordering.lex_for_strings ;;
+  let str_sort = Ordered.sort str_order ;;  
+  
+  let str_mem = Ordered.mem str_order ;; 
+  let capsule_source cpsl = (!cpsl).source ;;
+  
+  let capsule_destination cpsl = (!cpsl).destination ;; 
+  
+  let capsule_commands cpsl = (!cpsl).commands ;;
+  
+  
+  let make_capsule 
+  ~source:src ~destination:dest raw_commands = 
+  ref({
    source = src ;
    destination = dest ;
    commands = Image.image Cee_compilation_command.parse raw_commands;
@@ -69,65 +68,65 @@ module Capsule = (struct
    separate_commands_opt = None ;
    directly_compiled_files_opt = None ;
    filecontents = Hashtbl.create 3000
-}) ;;
-
-let compute_all_h_or_c_files cpt = 
+  }) ;;
+  
+  let compute_all_h_or_c_files_in_capsule cpt = 
   let src = Directory_name.connectable_to_subpath cpt.source in 
   let temp1 = Unix_again.quick_beheaded_complete_ls src in
   str_sort(List.filter (
     fun fn -> List.exists (fun edg ->
        String.ends_with fn ~suffix:edg) [".h";".c";".macros"]
- ) temp1 );;  
-
- let all_h_or_c_files cpsl_ref = 
+  ) temp1 );;  
+  
+  let all_h_or_c_files_in_capsule cpsl_ref = 
    let old_cpsl = (!cpsl_ref) in
    match old_cpsl.all_h_or_c_files_opt with 
   (Some old_answer) -> old_answer 
   |None ->
-    let answer = compute_all_h_or_c_files old_cpsl in 
+    let answer = compute_all_h_or_c_files_in_capsule old_cpsl in 
     let new_cpsl = {old_cpsl with 
       all_h_or_c_files_opt = Some answer 
     } in 
     let _ = (cpsl_ref:=new_cpsl) in 
     answer ;;
-
-  let compute_separate_commands cpsl = 
+  
+  let compute_separate_commands_in_capsule cpsl = 
     List.filter_map (function 
       (Cee_compilation_command_t.Batch(_)) -> None 
       |Cee_compilation_command_t.Separate(s) -> Some s
     ) cpsl.commands ;;
       
-  let separate_commands cpsl_ref = 
+  let separate_commands_in_capsule cpsl_ref = 
     let old_cpsl = (!cpsl_ref) in
        match old_cpsl.separate_commands_opt with 
       (Some old_answer) -> old_answer 
       |None ->
-        let answer = compute_separate_commands old_cpsl in 
+        let answer = compute_separate_commands_in_capsule old_cpsl in 
         let new_cpsl = {old_cpsl with 
         separate_commands_opt = Some answer 
         } in 
         let _ = (cpsl_ref:=new_cpsl) in 
         answer ;;
-
-  let compute_directly_compiled_files cpsl_ref = 
+  
+  let compute_directly_compiled_files_in_capsule cpsl_ref = 
     str_sort(Image.image (
       fun cmd ->
        (cmd.Cee_compilation_command_t.short_path) ^ 
        (cmd.Cee_compilation_command_t.ending)
-    ) (separate_commands cpsl_ref)) ;;    
+    ) (separate_commands_in_capsule cpsl_ref)) ;;    
     
-  let directly_compiled_files cpsl_ref = 
+  let directly_compiled_files_in_capsule cpsl_ref = 
        match (!cpsl_ref).directly_compiled_files_opt with 
       (Some old_answer) -> old_answer 
       |None ->
-        let answer = compute_directly_compiled_files cpsl_ref in 
+        let answer = compute_directly_compiled_files_in_capsule cpsl_ref in 
         let new_cpsl = {(!cpsl_ref) with 
           directly_compiled_files_opt = Some answer 
         } in 
         let _ = (cpsl_ref:=new_cpsl) in 
         answer ;;
    
-  let read_file cpsl_ref fn =
+  let read_file_in_capsule cpsl_ref fn =
     let cpsl = (!cpsl_ref) in 
     match Hashtbl.find_opt cpsl.filecontents fn with 
     (Some old_answer) -> old_answer 
@@ -138,13 +137,13 @@ let compute_all_h_or_c_files cpt =
       let _ = Hashtbl.add cpsl.filecontents fn text in 
       text ;;
       
-  let modify_file cpsl_ref fn new_content=
+  let modify_file_in_capsule cpsl_ref fn new_content=
     let cpsl = (!cpsl_ref) in 
     let dest_dir = Directory_name.connectable_to_subpath cpsl.destination in 
     let ap = Absolute_path.of_string (dest_dir ^ fn) in
     Io.overwrite_with ap new_content;;
-
-  let text_for_makefile cpsl =
+  
+  let text_for_makefile_in_capsule cpsl =
     let temp1 = Int_range.index_everything cpsl.commands in 
     let temp2 = Image.image (fun (cmd_idx,cmd)->
       let s_idx = string_of_int cmd_idx in 
@@ -155,23 +154,16 @@ let compute_all_h_or_c_files cpt =
     let temp3 = ("make all:")::(List.flatten temp2) in 
     String.concat "\n" temp3 ;; 
     
-  let write_makefile cpsl_ref =
+  let write_capsule_makefile cpsl_ref =
     let cpsl = (!cpsl_ref) in 
     let dest_dir = Directory_name.connectable_to_subpath cpsl.destination in  
     let path_for_makefile = Absolute_path.of_string (dest_dir ^ "Makefile" ) in 
-    Io.overwrite_with path_for_makefile (text_for_makefile cpsl) ;;
-
-
-end :CAPSULE_TYPE);; 
-
-module Private = struct 
-
-
-    let str_order = Total_ordering.lex_for_strings ;;
-    let str_mem = Ordered.mem str_order ;;  
+    Io.overwrite_with path_for_makefile (text_for_makefile_in_capsule cpsl) ;;
+  ;; 
+   
 
 let names_for_temporary_files_during_preprocessing cpsl separate_cmd  = 
-  let dest_dir = Directory_name.connectable_to_subpath (Capsule.destination cpsl)  in  
+  let dest_dir = Directory_name.connectable_to_subpath (capsule_destination cpsl)  in  
   let endingless = dest_dir ^ separate_cmd.Cee_compilation_command_t.short_path  in 
   let (short_preprocessable,short_preprocessed) = 
      Cee_compilation_command.short_names_for_temporary_files_during_preprocessing separate_cmd in 
@@ -182,7 +174,7 @@ let names_for_temporary_files_during_preprocessing cpsl separate_cmd  =
   (file_to_be_preprocessed,preprocessed_file) ;;
 
 let main_preprocessing_command cpsl separate_cmd = 
-  let dest_dir = Directory_name.connectable_to_subpath (Capsule.destination cpsl) in  
+  let dest_dir = Directory_name.connectable_to_subpath (capsule_destination cpsl) in  
   let core_of_command = Cee_compilation_command.adapt_command 
     ~root_dir:dest_dir separate_cmd.Cee_compilation_command_t.core_of_command in 
   let (name_for_preprocessable_file,name_for_preprocessed_file) = 
@@ -196,7 +188,7 @@ let announce cmd =
 let keep_temporary_files_mode = ref false ;;  
 
   let compute_preprocessing_output cpsl separate_cmd text_to_be_preprocessed = 
-    let dest_dir = Directory_name.connectable_to_subpath (Capsule.destination cpsl) in  
+    let dest_dir = Directory_name.connectable_to_subpath (capsule_destination cpsl) in  
     let dest_last = (Cull_string.after_rightmost (Cull_string.coending 1 dest_dir) '/' ) ^ "/" in
     let (short_preprocessable,short_preprocessed) = 
      Cee_compilation_command.short_names_for_temporary_files_during_preprocessing separate_cmd in 
@@ -224,9 +216,9 @@ let keep_temporary_files_mode = ref false ;;
    answer;;
 
 let remove_cds_in_file cpsl ~name_for_watermarkable_file separate_cmd  = 
-  let dest_dir = Directory_name.connectable_to_subpath (Capsule.destination cpsl) in  
+  let dest_dir = Directory_name.connectable_to_subpath (capsule_destination cpsl) in  
   let dest_last = (Cull_string.after_rightmost (Cull_string.coending 1 dest_dir) '/' ) ^ "/" in 
-  let old_text = Capsule.read_file cpsl (separate_cmd.Cee_compilation_command_t.short_path ^ separate_cmd.Cee_compilation_command_t.ending) in 
+  let old_text = read_file_in_capsule cpsl (separate_cmd.Cee_compilation_command_t.short_path ^ separate_cmd.Cee_compilation_command_t.ending) in 
   let text_to_be_preprocessed = Cee_text.watermark_text ~name_for_watermarkable_file old_text in
   let preprocessed_text = compute_preprocessing_output cpsl separate_cmd text_to_be_preprocessed in 
   let new_text = Cee_text.rewrite_using_watermarks old_text ~name_for_watermarkable_file ~watermarked_text:preprocessed_text in 
@@ -255,10 +247,10 @@ let remove_cds_in_directly_compiled_files cpsl separate_cmds =
     ) temp1 ;;
 
 let remove_cds_in_all_directly_compiled_files cpsl = 
-  remove_cds_in_directly_compiled_files cpsl (Capsule.separate_commands cpsl) ;; 
+  remove_cds_in_directly_compiled_files cpsl (separate_commands_in_capsule cpsl) ;; 
 
 let cleanup_temporary_data_for_file cpsl separate_cmd  = 
-  let dest_dir = Directory_name.connectable_to_subpath (Capsule.destination cpsl)  in  
+  let dest_dir = Directory_name.connectable_to_subpath (capsule_destination cpsl)  in  
   let s_ap = dest_dir ^ separate_cmd.Cee_compilation_command_t.short_path ^ separate_cmd.Cee_compilation_command_t.ending in 
   let short_s_ap = Cull_string.coending 2 s_ap 
   and ending = separate_cmd.Cee_compilation_command_t.ending in
@@ -272,13 +264,13 @@ let cleanup_temporary_data_for_file cpsl separate_cmd  =
   ]  ;;
   
 let cleanup_temporary_files_after_cds_removal cpsl =   
-  Image.image (cleanup_temporary_data_for_file cpsl) (Capsule.separate_commands cpsl) ;;
+  Image.image (cleanup_temporary_data_for_file cpsl) (separate_commands_in_capsule cpsl) ;;
 
    
   exception Check_presence_in_project_exn of string ;;
 
   let check_presence_in_project cpsl fn =
-     if str_mem fn (Capsule.all_h_or_c_files cpsl) 
+     if str_mem fn (all_h_or_c_files_in_capsule cpsl) 
      then fn 
      else raise(Check_presence_in_project_exn(fn));;  
    
@@ -288,7 +280,7 @@ exception Normalize_included_filename_exn of string * string ;;
 let normalize_nonpointed_included_filename cpsl includer_fn included_fn =
    match List.find_opt (fun nfn->
      String.ends_with nfn ~suffix:included_fn   
-   ) (Capsule.all_h_or_c_files cpsl) with 
+   ) (all_h_or_c_files_in_capsule cpsl) with 
   None -> raise (Normalize_included_filename_exn(includer_fn,included_fn))
   |(Some nfn) -> nfn;;   
 
@@ -309,7 +301,7 @@ let normalize_nonpointed_included_filename cpsl includer_fn included_fn =
     exception Included_files_exn of string * string * string ;;
 
     let included_files cpsl includer_fn=
-      let temp1 = Cee_text.included_local_files_in_text(Capsule.read_file cpsl includer_fn) 
+      let temp1 = Cee_text.included_local_files_in_text(read_file_in_capsule cpsl includer_fn) 
       and includer_dir = Cull_string.before_rightmost includer_fn '/' in 
       Image.image (fun (included_fn,indices)->
             try (normalize_included_filename cpsl includer_dir included_fn,indices) with
@@ -317,7 +309,7 @@ let normalize_nonpointed_included_filename cpsl includer_fn included_fn =
             ) temp1;;
       
     let nonstandard_inclusion_formats_in_individual_includer cpsl includer_fn = 
-      let text = Capsule.read_file cpsl includer_fn in 
+      let text = read_file_in_capsule cpsl includer_fn in 
       let temp1 = Cee_text.included_local_files_in_text text
       and includer_dir = Cull_string.before_rightmost includer_fn '/' in 
       let lines = Lines_in_string.indexed_lines text in 
@@ -342,27 +334,51 @@ let normalize_nonpointed_included_filename cpsl includer_fn included_fn =
     let _ =(
       if not(dry_run)
       then List.iter (fun (fn,ab,ba)->
-              let old_text = Capsule.read_file cpsl fn in 
+              let old_text = read_file_in_capsule cpsl fn in 
               let new_text = Replace_inside.replace_inside_string (ab,ba) old_text in 
-              Capsule.modify_file cpsl fn new_text) replacements_to_be_made
+              modify_file_in_capsule cpsl fn new_text) replacements_to_be_made
     ) in 
     replacements_to_be_made;; 
 
     let standardize_guards_in_files cpsl files = 
       List.filter_map (fun 
          fn ->
-          let old_text = Capsule.read_file cpsl fn in 
+          let old_text = read_file_in_capsule cpsl fn in 
           match Cee_text.standardize_guard_in_text old_text with 
           None -> None 
           |Some new_text ->
-            let _ = Capsule.modify_file cpsl fn new_text in 
+            let _ = modify_file_in_capsule cpsl fn new_text in 
             Some fn 
       ) files ;;
 
 
 end ;;
 
-let make_capsule = Capsule.make ;;
+module Capsule = (struct
+   
+  type t = Private.capsule_t 
+  let source = Private.capsule_source ;;
+
+  let destination = Private.capsule_destination ;;
+
+  let commands = Private.capsule_commands ;;
+  
+  let make = Private.make_capsule ;;
+  
+  let all_h_or_c_files = Private.all_h_or_c_files_in_capsule ;;  
+
+  let separate_commands = Private.separate_commands_in_capsule ;;
+
+  let directly_compiled_files = Private.directly_compiled_files_in_capsule ;;
+
+  let read_file = Private.read_file_in_capsule ;; 
+
+  let modify_file = Private.modify_file_in_capsule ;;
+  
+  let write_makefile = Private.write_capsule_makefile ;;
+
+ end : CAPSULE_TYPE);;
+
 
 let remove_conditional_directives_in_directly_compiled_files = Private.remove_cds_in_all_directly_compiled_files ;; 
 
