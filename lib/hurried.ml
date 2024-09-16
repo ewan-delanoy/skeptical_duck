cd1d7900ddcdc46700572d4430effafd9270e80c
ewan.delanoy <ewan.delanoy@zoho.com>
2024-09-16 15:40:16 +0200

    add Hurried.reaggregate

--- lib/hurried.ml
+++ lib/hurried.ml
@@ -18,6 +18,34 @@ let partition_in_two_parts f l=
   tempf([],l);; 
 
 (* partition_in_two_parts (fun x->(x mod 3)<>0) (Ennig.ennig 1 21) *)
+let rec helper_for_reaggregation 
+      decomposer (treated,current_level,incomplete_item,to_be_treated) = 
+    match to_be_treated with 
+    [] -> List.rev((current_level,List.rev incomplete_item)::treated)  
+    |pair :: other_pairs ->
+        let (new_level,other_data) = decomposer pair in 
+        if new_level = current_level 
+        then helper_for_reaggregation 
+        decomposer (treated,current_level,
+                     other_data::incomplete_item,other_pairs)
+        else helper_for_reaggregation 
+        decomposer ((current_level,List.rev incomplete_item)::treated,
+                      new_level,[other_data],other_pairs) ;;            
+
+let reaggregate decomposer l = 
+  match l with 
+  [] -> []
+  |pair :: other_pairs ->
+    let (new_level,other_data) = decomposer pair in 
+    helper_for_reaggregation 
+    decomposer ([],new_level,[other_data],other_pairs) ;;
+
+(*
+
+reaggregate (fun x->(x/10,x mod 10)) (Int_range.range 1 50) ;; 
+
+*)    
+
 
 end ;; 
 
@@ -38,3 +66,5 @@ let connected_components f l =
 (* connected_components (fun x->(x mod 3)<>0) (Ennig.ennig 1 21) *)  
 
 let partition_in_two_parts = Private.partition_in_two_parts ;;    
+
+let reaggregate = Private.reaggregate ;;
