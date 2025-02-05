b94aad50a7e249d18a8661504afda70157e1e60f
ewan.delanoy <ewan.delanoy@zoho.com>
2025-02-05 10:28:54 +0100

    register lib/Cee_language/cee_prawn_t.ml

---
+++ lib/Cee_language/cee_prawn_t.ml
@@ -0,0 +1,16 @@
+(*
+
+#use"lib/Cee_language/cee_prawn_t.ml";;
+
+
+A prawn is an atom in a shadow algebra for a file.
+
+Its index is relative to the list of all possible prawns for 
+this file.
+
+*)
+
+
+type  t = P of int list ;; 
+
+type index_t = I of int ;;
