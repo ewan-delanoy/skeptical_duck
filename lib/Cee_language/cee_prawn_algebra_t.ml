d0c884845d55d5381ff1f6caa60b76d54437d868
ewan.delanoy <ewan.delanoy@zoho.com>
2025-02-11 09:53:09 +0100

    include indices in Cee_prawn_algebra_t.t

--- lib/Cee_language/cee_prawn_algebra_t.ml
+++ lib/Cee_language/cee_prawn_algebra_t.ml
@@ -2,7 +2,10 @@
 
 #use"lib/Cee_language/cee_prawn_algebra_t.ml";;
 
+Indices are included in the list, as the 
+first coordinate of the pair.
+
 *)
 
 
-type  t = A of Cee_prawn_t.t list ;; 
+type  t = A of (int * Cee_prawn_t.t) list ;; 
