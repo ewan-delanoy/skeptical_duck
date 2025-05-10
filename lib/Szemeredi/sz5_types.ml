ca94ff6952e7e4ed2f7e01d6c60b7687868ae7f9
ewan.delanoy <ewan.delanoy@zoho.com>
2025-05-10 09:36:51 +0200

    register lib/Szemeredi/sz5_types.ml

---
+++ lib/Szemeredi/sz5_types.ml
@@ -0,0 +1,12 @@
+(*
+
+#use"lib/Szemeredi/sz5_types.ml";;
+
+We make an exception here to the rule of not having numbers in module names.
+Sz5 is short for "fifth stab at Szemeredi problem".
+
+
+*)
+
+type width = W of int ;; 
+
