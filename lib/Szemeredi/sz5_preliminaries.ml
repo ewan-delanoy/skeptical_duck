43fae66cf0e9ab844a9e0ef95720c13b14954601
ewan.delanoy <ewan.delanoy@zoho.com>
2025-05-11 10:23:40 +0200

    add Sz5_preliminaries.Constraint.{force,reduce,shift}

--- lib/Szemeredi/sz5_preliminaries.ml
+++ lib/Szemeredi/sz5_preliminaries.ml
@@ -13,40 +13,32 @@ type constraint_t = Sz5_types.constraint_t = C of int list list ;;
 
 let i_order = Total_ordering.for_integers ;;
 
+let i_is_included_in = Ordered.is_included_in i_order ;;
 let i_max = Ordered.max i_order ;;
+
+let i_setminus = Ordered.setminus i_order ;;
 
 let il_order = Total_ordering.silex_for_intlists ;;
 
+let il_fold_merge = Ordered.fold_merge il_order ;;
+let il_merge = Ordered.merge il_order ;;
 let il_sort = Ordered.sort il_order ;;
 
 module Constraint = struct 
+
+exception Shift_exn of int list list ;; 
 
 module Private = struct
 let constructor ll = 
    let temp1 = il_sort ll in 
    C(Ordered_misc.minimal_elts_wrt_inclusion temp1) ;;
 
-let son (C ll) =
-   let translated_forms = List.filter_map (
-     fun l ->
-      if List.mem 1 l then None else 
-      Some(Image.image (fun t->t-1) l)  
-   ) ll in 
-   constructor translated_forms ;;
 
-let daughter (W w) (C ll) =
-   let translated_forms = Image.image (
-     fun l ->
-      let (a,b) =List_again.head_with_tail l in 
-      let reduced_l = (if a=1 then b else l) in
-      Image.image (fun t->t-1) reduced_l 
-   ) ll 
-   and extra_obstructions = 
-    Int_range.scale (fun j->[1+j;1+2*j]) 1 w
-  in 
-  constructor (extra_obstructions @ translated_forms) ;;
 
-let max (C ll) = Max.list (Image.image i_max ll) ;;
+let max (C ll) = 
+  if ll = [] then (-1) else
+  if ll = [[]] then 0 else  
+  Max.list (Image.image i_max ll) ;;
 
 let order = (( fun cstr1 cstr2 ->
   let trial1 = Total_ordering.for_integers (max cstr1) (max cstr2) in 
@@ -55,8 +47,55 @@ let order = (( fun cstr1 cstr2 ->
   Total_ordering.silex_compare il_order l1 l2
 ) : constraint_t Total_ordering_t.t) ;;
 
+let left_complements (W w) a= 
+  Int_range.scale (fun t->[a-2*t;a-t]) 1 (min w (a/2)) ;;
+
+let middle_complements (W w) a= 
+  Int_range.scale (fun t->[a-t;a+t]) 1 (min w (a-1)) ;;  
+
+let right_complements (W w) a= 
+  Int_range.scale (fun t->[a+t;a+2*t]) 1 w ;;    
+
+let complements_for_individual w a =
+   il_sort (
+    (left_complements w a) 
+    @ (middle_complements w a) 
+    @ (right_complements w a)
+   ) ;; 
+
+let complements w l = 
+  Ordered.fold_merge 
+  il_order
+  (Image.image (complements_for_individual w) l) ;;
+
+let force w forced_subset (C ll) =
+   let base = il_merge (complements w forced_subset) ll in 
+   let cleaned_base = List.filter_map (
+     fun z ->
+      let reduced_z = i_setminus z forced_subset in 
+      if reduced_z = [] then None else Some reduced_z
+   ) base in 
+   constructor cleaned_base ;;
+
+let remove removed_subset (C ll) =
+   C(List.filter (
+    fun z -> not(i_is_included_in removed_subset z)
+   ) ll) ;;
+
+let shift (C ll) =
+   if List.exists (fun z->List.hd(z)=1) ll   
+   then raise(Shift_exn(ll))
+   else  C(Image.image (Image.image(fun t->t-1)) ll);;
+   
+let son cstr = shift(remove [1] cstr);;
+
+let daughter_opt w cstr = 
+  try Some(shift(force w [1] cstr)) with Shift_exn _ -> None ;;
+
+
 let immediate_descendants_for_individual w cstr =
-  Ordered.sort order [son cstr;daughter w cstr] ;;
+  let l = List.filter_map (fun o->o) [Some(son cstr);daughter_opt w cstr] in 
+  Ordered.sort order l ;;
 
 let immediate_descendants w l_cstr =
    Ordered.fold_merge order (Image.image 
@@ -75,17 +114,20 @@ let rec helper_for_generating_tribe (w,whole_so_far,to_be_treated) =
 let generated_tribe w l_cstr = 
    helper_for_generating_tribe (w,l_cstr,l_cstr) ;;
 
+
+
 end ;;     
 
 let constructor = Private.constructor ;;
 
-let daughter = Private.daughter ;;
+let daughter_opt = Private.daughter_opt ;;
 
+let force = Private.force ;;
 let generated_tribe = Private.generated_tribe ;;
 let order = Private.order ;;
+
+let shift = Private.shift ;;
 let son = Private.son ;;
 
 
 end ;;  
-
-Ordered_misc.minimal_elts_wrt_inclusion ;;
