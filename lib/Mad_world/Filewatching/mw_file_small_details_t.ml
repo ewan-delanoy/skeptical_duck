7cc02b4332cc7bafe349f6da2137a5f0bbded537
ewan.delanoy <ewan.delanoy@zoho.com>
2025-05-17 13:33:08 +0200

    register lib/Mad_world/Filewatching/mw_file_small_details_t.ml

---
+++ lib/Mad_world/Filewatching/mw_file_small_details_t.ml
@@ -0,0 +1,13 @@
+(*
+
+#use"lib/Mad_world/Filewatching/mw_file_small_details_t.ml";;
+
+
+*)
+
+type t ={
+  used_modules : Dfa_module_t.t list ;
+  used_libraries : Ocaml_library_t.t list ;
+  has_printer : bool ;
+  modification_time : string ;
+};;
