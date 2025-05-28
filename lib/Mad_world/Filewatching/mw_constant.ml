e8bab50a137cdef134f31c2dfbafe5244b507d2f
ewan.delanoy <ewan.delanoy@zoho.com>
2025-05-28 14:55:35 +0200

    delete mw_file_small_details_t , mw_file_small_details , mw_instance_index_t , mw_state_index_t , mw_indexer , mw_poly_t , mw_poly , mw_configuration , mw_file_watcher , mw_with_archives , mw_module_small_details_t , mw_with_small_details , mw_module_small_details , mw_determine_order , mw_with_dependencies , mw_transmit_change_to_github , mw_commands_for_batch_compilation , mw_with_batch_compilation , mw_with_githubbing , mw_with_persisting , mw_modify_coma_state , mw_needed_data_summary_t , mw_needed_data_summary , mw_create_world_copy , mw_usual_coma_state , mw_other_coma_state , mw_constant , mw_check_ocaml_dircopy , mw_needed_values , mw_self_contained_module_copy

--- lib/Mad_world/Filewatching/mw_constant.ml
+++
@@ -1,7 +0,0 @@
-(*
-
-#use"lib/Mad_world/Filewatching/mw_constant.ml";;
-
-*)
-
-let clone_download_location = (Sys.getenv "HOME")^"/Downloads/Clone";;
--- lib/Mad_world/Filewatching/mw_instance_index_t.ml
+++
@@ -1,7 +0,0 @@
-(*
-
-#use"lib/Mad_world/Filewatching/mw_instance_index_t.ml";;
-
-*)
-
-type t = I of int ;;
