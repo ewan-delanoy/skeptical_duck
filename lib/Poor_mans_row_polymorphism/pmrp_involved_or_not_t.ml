0607f7f10be4e68196ce0fe63128827837638e9b
ewan.delanoy <ewan.delanoy@zoho.com>
2025-06-14 13:23:30 +0200

    register lib/Poor_mans_row_polymorphism/pmrp_involved_or_not_t.ml

---
+++ lib/Poor_mans_row_polymorphism/pmrp_involved_or_not_t.ml
@@ -0,0 +1,14 @@
+(*
+
+#use"lib/Poor_mans_row_polymorphism/pmrp_involved_or_not_t.ml";;
+
+*)
+
+
+type involved_or_not_t   =  
+ Involved of  Pmrp_field_set_t.t
+ | Not_involved of string ;;
+
+
+
+
