111553372860ced3e58699c17b65fd6e5dedcba8
ewan.delanoy <ewan.delanoy@zoho.com>
2025-06-26 15:20:55 +0200

    simplify fwc_github_configuration

--- lib/Filewatching/Fw_classes/fwc_github_configuration.ml
+++ lib/Filewatching/Fw_classes/fwc_github_configuration.ml
@@ -51,24 +51,13 @@ let of_concrete_object ccrt_obj =
    ~v_encoding_protected_files:(Crobj_converter_combinator.to_pair_list Dfn_rootless.of_concrete_object Dfn_rootless.of_concrete_object (g label_for_encoding_protected_files)) ;;
   ;;
 
-let of_concrete_object ccrt_obj = 
- let g=Concrete_object.get_record ccrt_obj in 
- {
-   Fw_poly.origin with  
-  Fw_poly_t.type_name = Some "Fwc_github_configuration_t" ;
-   dir_for_backup = Some(Dfa_root.of_concrete_object (g label_for_dir_for_backup))  ;
-   encoding_protected_files = Some(Crobj_converter_combinator.to_pair_list Dfn_rootless.of_concrete_object Dfn_rootless.of_concrete_object (g label_for_encoding_protected_files))  ;
-   github_url = Some(Crobj_converter.string_of_concrete_object (g label_for_github_url))  ;
-   gitpush_after_backup = Some(Crobj_converter.bool_of_concrete_object (g label_for_gitpush_after_backup))  ;
- } ;;
-
 let to_concrete_object fw = 
  let items =  
  [
-   label_for_dir_for_backup, Dfa_root.to_concrete_object ( Fw_poly.dir_for_backup fw ) ;
-   label_for_encoding_protected_files, Crobj_converter_combinator.of_pair_list Dfn_rootless.to_concrete_object Dfn_rootless.to_concrete_object ( Fw_poly.encoding_protected_files fw ) ;
-   label_for_github_url, Crobj_converter.string_to_concrete_object ( Fw_poly.github_url fw ) ;
-   label_for_gitpush_after_backup, Crobj_converter.bool_to_concrete_object ( Fw_poly.gitpush_after_backup fw ) ;
+   label_for_dir_for_backup, Dfa_root.to_concrete_object ( dir_for_backup fw ) ;
+   label_for_encoding_protected_files, Crobj_converter_combinator.of_pair_list Dfn_rootless.to_concrete_object Dfn_rootless.to_concrete_object ( encoding_protected_files fw ) ;
+   label_for_github_url, Crobj_converter.string_to_concrete_object ( github_url fw ) ;
+   label_for_gitpush_after_backup, Crobj_converter.bool_to_concrete_object ( gitpush_after_backup fw ) ;
  ] in 
  Concrete_object_t.Record items ;;
 
