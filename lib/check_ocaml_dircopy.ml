6f0150f074a483346b93eddb3f621ac9b9887834
ewan.delanoy <ewan.delanoy@zoho.com>
2025-06-28 13:39:14 +0200

    remove use of Fw_constant in Check_ocaml_dircopy

--- lib/check_ocaml_dircopy.ml
+++ lib/check_ocaml_dircopy.ml
@@ -10,6 +10,8 @@ Usable on a github clone of the remote master version.
 exception Failure_in_clone_directory_creation;;
 exception Failure_during_github_cloning;;
 
+let clone_download_location = (Sys.getenv "HOME")^"/Downloads/Clone";;
+
 let filter_diff_according_to_admissibility data diff=
    let filter_list = List.filter (Fw_configuration.test_for_admissibility data) in 
    {
@@ -23,13 +25,13 @@ let commands_for_confidentiality encoding_protected_files =
    Image.image (
      fun (replacer,replacee) ->
        let s_replacer = Dfn_rootless.to_line  replacer in 
-       let s_full_path = Fw_constant.clone_download_location^"/"^(Dfn_rootless.to_line replacee) in 
+       let s_full_path = clone_download_location^"/"^(Dfn_rootless.to_line replacee) in 
        Unix_command.prefix_for_reverse_replacing_patterns^s_replacer^" "^s_full_path
    ) encoding_protected_files ;;
 
 
 let check fw_data github_data =
-  let name_of_clone_directory = Fw_constant.clone_download_location in 
+  let name_of_clone_directory = clone_download_location in 
   let i=(
     if Sys.file_exists(name_of_clone_directory)
     then Unix_command.uc("rm -rf "^name_of_clone_directory) 
