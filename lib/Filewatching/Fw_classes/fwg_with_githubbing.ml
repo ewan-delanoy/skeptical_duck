6198ea2c28d880dbae13f8530788e92454a1ee87
ewan.delanoy <ewan.delanoy@zoho.com>
2025-06-28 11:08:56 +0200

    register lib/Filewatching/Fw_classes/fwg_with_githubbing.ml

---
+++ lib/Filewatching/Fw_classes/fwg_with_githubbing.ml
@@ -0,0 +1,27 @@
+(* 
+
+#use"lib/Filewatching/Fw_classes/fwg_with_githubbing.ml";;
+
+*)
+
+
+
+type t = Fw_flattened_poly_t.t ;;
+
+let parent fw = {
+  fw with 
+  Fw_flattened_poly_t.type_name = Some "Fw_with_batch_compilation" 
+ } ;; 
+let github_configuration fw = {
+  fw with 
+  Fw_flattened_poly_t.type_name = Some "Fw_github_configuration" 
+ } ;; 
+
+let make fw_batch github_config = {
+  fw_batch with 
+  Fw_flattened_poly_t.type_name = Some "Fw_with_githubbing" ;
+  dir_for_backup = Some (Fwg_github_configuration.dir_for_backup github_config) ;
+  gitpush_after_backup = Some (Fwg_github_configuration.gitpush_after_backup github_config) ;
+  github_url = Some (Fwg_github_configuration.github_url github_config)  ;
+  encoding_protected_files = Some (Fwg_github_configuration.encoding_protected_files github_config) ;
+} ;;
