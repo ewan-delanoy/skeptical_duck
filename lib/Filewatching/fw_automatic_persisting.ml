5b3c35a7db4963e6261591454ce5c768d1ae1449
ewan.delanoy <ewan.delanoy@zoho.com>
2025-07-18 12:28:23 +0200

    register lib/Filewatching/fw_automatic_persisting.ml

---
+++ lib/Filewatching/fw_automatic_persisting.ml
@@ -0,0 +1,79 @@
+(* 
+
+#use"lib/Filewatching/fw_automatic_persisting.ml";;
+
+*)
+
+let forget_modules fw mod_names=
+   let _=Fwc_with_githubbing.Inherited.check_that_no_change_has_occurred fw in 
+   let fw2 = Fwc_with_githubbing.forget_modules fw mod_names in 
+   let _=Fw_persisting.persist fw2 in 
+   fw2;;
+   
+let forget_nonmodular_rootlesses fw rootless_paths=
+   let _=Fwc_with_githubbing.Inherited.check_that_no_change_has_occurred fw in 
+   let fw2 = Fwc_with_githubbing.forget_nonmodular_rootlesses fw rootless_paths in 
+   let _=Fw_persisting.persist fw2 in 
+   fw2;;
+   
+         
+let save_latest_changes fw opt_comment=
+   let fw2= Fwc_with_githubbing.inspect_and_update fw opt_comment in 
+   let _=Fw_persisting.persist fw2 in 
+   fw2;;
+let set_internet_access fw bowl=   
+   let fw2=Fwc_with_githubbing.Inherited.set_gitpush_after_backup fw bowl in 
+   let _=Fw_persisting.persist fw2 in 
+   fw2;;
+let refresh fw =
+   let fw_config = Fwc_with_githubbing.Inherited.to_fw_configuration fw
+   and github_config = Fwc_with_githubbing.github_configuration fw in 
+   let root = Fwc_configuration.root fw_config in 
+   let proj_name = Cull_string.after_rightmost (Dfa_root.without_trailing_slash root) '/' in
+   let _=(Unix_again.create_subdirs_and_fill_files_if_necessary root
+       Fw_constant.minimal_set_of_needed_dirs 
+           (Fw_constant.conventional_files_with_minimal_content proj_name)) in 
+   let fw_with_deps = Fwc_with_dependencies.of_configuration fw_config in 
+   let fw2= Fwc_with_githubbing.Inherited.make fw_with_deps github_config  in 
+   let _=Fw_persisting.persist fw2 in 
+   fw2;;       
+
+let register_rootless_paths fw rootless_path=
+   let _=Fwc_with_githubbing.Inherited.check_that_no_change_has_occurred fw in 
+   let fw2 = Fwc_with_githubbing.register_rootless_paths fw rootless_path in 
+   let _=Fw_persisting.persist fw2 in 
+   fw2;;  
+   
+let relocate_module_to fw old_module new_subdir=
+   let _=Fwc_with_githubbing.Inherited.check_that_no_change_has_occurred fw in 
+   let fw2 = Fwc_with_githubbing.relocate_module_to fw old_module new_subdir in 
+   let _=Fw_persisting.persist fw2 in 
+   fw2;;   
+   
+let rename_module fw old_middle_name new_nonslashed_name=
+   let _=Fwc_with_githubbing.Inherited.check_that_no_change_has_occurred fw in 
+   let fw2=Fwc_with_githubbing.rename_module fw old_middle_name new_nonslashed_name in 
+   let _=Fw_persisting.persist fw2 in 
+   fw2;;  
+
+let rename_subdirectory fw old_subdir new_subdir=
+   let _=Fwc_with_githubbing.Inherited.check_that_no_change_has_occurred fw in 
+   let fw2=Fwc_with_githubbing.rename_subdirectory_as fw (old_subdir,new_subdir) in 
+   let _=Fw_persisting.persist fw2 in 
+   fw2;;  
+
+let replace_string fw old_s new_s=
+   let _=Fwc_with_githubbing.Inherited.check_that_no_change_has_occurred fw in 
+   let fw2=Fwc_with_githubbing.replace_string fw old_s new_s in 
+   let _=Fw_persisting.persist fw2 in 
+   fw2;;     
+    
+         
+let replace_value fw ((preceding_files,path),(old_v,new_v))=
+   let _=Fwc_with_githubbing.Inherited.check_that_no_change_has_occurred fw in 
+   let fw2= Fwc_with_githubbing.replace_value fw ((preceding_files,path),(old_v,new_v)) in 
+   let _=Fw_persisting.persist fw2 in 
+   fw2;;        
+   
+   
+ 
