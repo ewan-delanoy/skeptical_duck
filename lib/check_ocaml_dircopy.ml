35fd310a4b1509ff71e1e4a64e5f5994333c7dc2
ewan.delanoy <ewan.delanoy@zoho.com>
2025-07-19 14:54:20 +0200

    add Check_ocaml_dircopy.modify_forgotten_files_artificially

--- lib/check_ocaml_dircopy.ml
+++ lib/check_ocaml_dircopy.ml
@@ -10,6 +10,7 @@ Usable on a github clone of the remote master version.
 exception Failure_in_clone_directory_creation;;
 exception Failure_during_github_cloning;;
 
+module Private = struct
 let clone_download_location = (Sys.getenv "HOME")^"/Downloads/Clone";;
 
 let filter_diff_according_to_admissibility data diff=
@@ -29,6 +30,20 @@ let commands_for_confidentiality encoding_protected_files =
        Unix_command.prefix_for_reverse_replacing_patterns^s_replacer^" "^s_full_path
    ) encoding_protected_files ;;
 
+let ref_for_forgotten_changed_files = ref ([]: Absolute_path.t list) ;;
+
+let store_changed_files root diff =
+  let temp1 = diff.Dircopy_diff_t.recently_changed in 
+  let temp2 = Image.image (
+   fun rl -> Dfn_full.to_absolute_path
+   (Dfn_join.root_to_rootless root rl)
+  ) temp1 in 
+  ref_for_forgotten_changed_files:=temp2 ;;
+
+let modify_forgotten_files_artificially () = 
+  List.iter (
+     fun ap ->Io.append_string_to_file " " ap
+  )(!ref_for_forgotten_changed_files) ;;
 
 let check fw_data github_data =
   let name_of_clone_directory = clone_download_location in 
@@ -56,5 +71,12 @@ let check fw_data github_data =
   let diff1=Prepare_dircopy_update.compute_restricted_diff
      root_dir remotedir (Fwc_configuration.ignored_subdirectories fw_data,
         (Image.image Dfn_rootless.to_line (Fwc_configuration.ignored_files fw_data) )) in
-  filter_diff_according_to_admissibility fw_data diff1;;
+  let diff2 =filter_diff_according_to_admissibility fw_data diff1 in 
+  let _ = store_changed_files root_dir diff2 in 
+  diff2 ;;
           
+end ;;
+
+let check = Private.check ;;
+
+let modify_forgotten_files_artificially = Private.modify_forgotten_files_artificially ;; 
