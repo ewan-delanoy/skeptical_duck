76859265d2fdf61d0e51552133fe910bb5dec8a6
ewan.delanoy <ewan.delanoy@zoho.com>
2026-01-20 14:21:18 +0100

    register lib/situation_of_linebreak_inside_text_t.ml

---
+++ lib/situation_of_linebreak_inside_text_t.ml
@@ -0,0 +1,12 @@
+(*
+
+#use"lib/situation_of_linebreak_inside_text_t.ml";;
+
+*)
+
+type situation_of_linebreak_inside_text_t = 
+    Lbit_inside_a_long_double_quoted_string  
+   |Lbit_inside_a_starry_comment
+   |Lbit_outside_comments_or_strings ;;
+
+ 
