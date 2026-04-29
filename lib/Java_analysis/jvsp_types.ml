d2f5200c1a67e4789514d966b5a8c31674907004
ewan.delanoy <ewan.delanoy@zoho.com>
2026-04-29 10:50:05 +0200

    register lib/Java_analysis/jvsp_types.ml

---
+++ lib/Java_analysis/jvsp_types.ml
@@ -0,0 +1,62 @@
+(*
+
+#use"lib/Java_analysis/jvsp_types.ml";;
+
+*)
+
+
+type ident = { id : string; pos : int } ;;
+
+type op = string ;; 
+
+type token =
+  IDENTIFIER of ident
+| LITERAL of op
+| PRIMITIVE_TYPE of op
+(* Separators *)
+|LP		(* ( *)
+|RP		(* ) *)
+|LC		(* { *)
+|RC		(* } *)
+|LB		(* [ *)
+|RB		(* ] *)
+|SM		(* ; *)
+|CM		(* , *)
+|DOT	(* . *)
+
+(* Operators *)
+|EQ		  (* = *)
+|GT		  (* > *)
+|LT		  (* < *)
+|NOT		(* ! *)
+|COMPL	(* ~ *)
+|COND		(* ? *)
+|COLON	(* : *)
+|EQ_EQ	(* == *)
+|LE		  (* <= *)
+|GE		  (* >= *)
+|NOT_EQ		(* != *)
+|AND_AND	(* && *)
+|OR_OR	(* || *)
+|INCR		(* ++ *)
+|DECR		(* -- *)
+|PLUS		(* + *)
+|MINUS	(* - *)
+|TIMES	(* * *)
+|DIV		(* / *)
+|AND		(* & *)
+|OR		  (* | *)
+|XOR		(* ^ *)
+|MOD		(* % *)
+|LS		  (* << *)
+|SRS		(* >> *)
+|URS		(* >>> *)
+|OPERATOR_EQ of op	(* += -= *= /= &= |= ^= %= <<= >>= >>>= *)
+(* Keywords*)
+|ABSTRACT |BOOLEAN |BREAK |BYTE |CASE |CATCH |CHAR |CLASS |CONST |CONTINUE
+|DEFAULT |DO |DOUBLE |ELSE |EXTENDS |FINAL |FINALLY |FLOAT |FOR |GOTO
+|IF |IMPLEMENTS |IMPORT |INSTANCEOF |INT |INTERFACE |LONG
+|NATIVE |NEW |PACKAGE |PRIVATE |PROTECTED |PUBLIC |RETURN
+|SHORT |STATIC |STRICTFP |SUPER |SWITCH |SYNCHRONIZED
+|THIS |THROW |THROWS |TRANSIENT |TRY |VOID |VOLATILE |WHILE
+|EOF ;;
