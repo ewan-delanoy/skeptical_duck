31ccdfbfe4f9673b810f386a556253818f59730c
ewan.delanoy <ewan.delanoy@zoho.com>
2026-05-02 14:48:00 +0200

    add Jvsp_types.token_type

--- lib/Java_analysis/jvsp_types.ml
+++ lib/Java_analysis/jvsp_types.ml
@@ -66,3 +66,59 @@ type token =
 |WHITESPACE of string ;;
 
 type positioned_token = { tok:token; start:int; endd:int;} ;;
+
+type token_type =
+  IDENTIFIER_T 
+| LITERAL_T  
+| PRIMITIVE_TYPE_T 
+(* Separators *)
+|LP_T		(* ( *)
+|RP_T		(* ) *)
+|LC_T		(* { *)
+|RC_T		(* } *)
+|LB_T		(* [ *)
+|RB_T		(* ] *)
+|SM_T		(* ; *)
+|CM_T		(* , *)
+|DOT_T	(* . *)
+
+(* Operators *)
+|EQ_T		  (* = *)
+|GT_T		  (* > *)
+|LT_T		  (* < *)
+|NOT_T		(* ! *)
+|COMPL_T	(* ~ *)
+|COND_T		(* ? *)
+|COLON_T	(* : *)
+|EQ_EQ_T	(* == *)
+|LE_T		  (* <= *)
+|GE_T		  (* >= *)
+|NOT_EQ_T		(* != *)
+|AND_AND_T	(* && *)
+|OR_OR_T	(* || *)
+|INCR_T		(* ++ *)
+|DECR_T		(* -- *)
+|PLUS_T		(* + *)
+|MINUS_T	(* - *)
+|TIMES_T	(* * *)
+|DIV_T		(* / *)
+|AND_T		(* & *)
+|OR_T		    (* | *)
+|XOR_T		(* ^ *)
+|MOD_T		(* % *)
+|LS_T		  (* << *)
+|SRS_T		(* >> *)
+|URS_T		(* >>> *)
+|OPERATOR_EQ_T 	(* += -= *= /= &= |= ^= %= <<= >>= >>>= *)
+|ANNOTATOR_T 
+(* Keywords*)
+|ABSTRACT_T |BOOLEAN_T |BREAK_T |BYTE_T |CASE_T |CATCH_T |CHAR_T |CLASS_T |CONST_T |CONTINUE_T
+|DEFAULT_T |DO_T |DOUBLE_T |ELSE_T |EXTENDS_T |FINAL_T |FINALLY_T |FLOAT_T |FOR_T |GOTO_T
+|IF_T |IMPLEMENTS_T |IMPORT_T |INSTANCEOF_T |INT_T |INTERFACE_T |LONG_T
+|NATIVE_T |NEW_T |PACKAGE_T |PRIVATE_T |PROTECTED_T |PUBLIC_T |RETURN_T
+|SHORT_T |STATIC_T |STRICTFP_T |SUPER_T |SWITCH_T |SYNCHRONIZED_T
+|THIS_T |THROW_T |THROWS_T |TRANSIENT_T |TRY_T |VOID_T |VOLATILE_T |WHILE_T
+|EOF_T  ;;
+
+
+
