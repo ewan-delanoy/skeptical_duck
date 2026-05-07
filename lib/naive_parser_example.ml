1f25affa6d703184a3364d1eb1b99c3926c44584
ewan.delanoy <ewan.delanoy@zoho.com>
2026-05-07 10:22:31 +0200

    add Naive_parser_example.digit

--- lib/naive_parser_example.ml
+++ lib/naive_parser_example.ml
@@ -4,6 +4,19 @@
 
 *)
 
+
+let digit =
+   Naive_parser_t.NP(
+    fun text idx ->
+      if idx > (String.length text)
+      then None 
+      else let c = String.get text (idx-1) in 
+           let i = int_of_string c in 
+           if (48<=i) &&(i<=57)
+           then Some(c,idx+1)
+           else None
+   ) ;;
+
 let fixed_string substr =
    Naive_parser_t.NP(
     fun text idx ->
