96e663177a026fb60b252835946b5705d14b2b80
ewan.delanoy <ewan.delanoy@zoho.com>
2026-05-07 09:17:56 +0200

    register lib/Job_Hunting/jh_types.ml

---
+++ lib/Job_Hunting/jh_types.ml
@@ -0,0 +1,17 @@
+(*
+
+#use"lib/image.ml";;
+The most used function in all those modules !
+
+
+*)
+
+
+let image f l=
+  let rec tempf=(fun
+   (graet,da_ober)->match da_ober with
+   []->List.rev graet
+   |a::peurrest->tempf(f(a)::graet,peurrest)
+  ) in
+  tempf([],l);;
+ 
