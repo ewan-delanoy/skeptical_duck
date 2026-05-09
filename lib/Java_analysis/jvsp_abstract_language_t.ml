40a5ffaf8d07c7db9747ba9b215a2962bac62824
ewan.delanoy <ewan.delanoy@zoho.com>
2026-05-09 09:50:22 +0200

    register lib/Java_analysis/jvsp_abstract_language_t.ml

---
+++ lib/Java_analysis/jvsp_abstract_language_t.ml
@@ -0,0 +1,15 @@
+(*
+
+#use"lib/Java_analysis/jvsp_abstract_language_t.ml";;
+
+*)
+
+
+type element_in_concat = 
+   Ref of string |Atomic of Jvsp_types.token_type | Star of string ;;
+
+type element_in_Disjunction = Concat of element_in_concat list ;;
+     
+type form = Disjunction of element_in_Disjunction list ;;
+
+type t = AL of (string * form) list ;; 
