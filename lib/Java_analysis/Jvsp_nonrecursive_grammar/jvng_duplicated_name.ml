c7f0653f51ba0b4e615bd70fc13d2e7442ece7b4
ewan.delanoy <ewan.delanoy@zoho.com>
2026-05-29 12:52:46 +0200

    register lib/Java_analysis/Jvsp_nonrecursive_grammar/jvng_duplicated_name.ml

---
+++ lib/Java_analysis/Jvsp_nonrecursive_grammar/jvng_duplicated_name.ml
@@ -0,0 +1,66 @@
+(*
+
+#use"lib/Java_analysis/Jvsp_nonrecursive_grammar/jvng_duplicated_name.ml";;
+
+*)
+
+
+type t = PDN of string ;;
+
+module Private = struct 
+
+let veil str = ((PDN str): t);;
+
+let unveil (PDN str: t) =str ;;
+
+let is_a_digit c= let i = int_of_char c in (48<=i)&&(i<=57) ;;
+
+let is_a_digit_or_dot c = (is_a_digit c) || (c='.') ;; 
+
+let is_not_a_digit_or_dot c = not(is_a_digit_or_dot c) ;;
+
+let core str =
+   match String_find_char.backwards_from_inclusive_opt is_not_a_digit_or_dot str (String.length str) with 
+   None -> ""
+   |Some k -> Cull_string.beginning k str ;;
+
+let constructor str k = 
+   let suffix = (if k=1 then "" else string_of_int k) in 
+   veil ((core str)^suffix);;
+
+
+let name prn =
+   let str = unveil prn in 
+   match String_find_char.backwards_from_inclusive_opt is_not_a_digit_or_dot str (String.length str) with 
+   None -> ""
+   |Some k -> Cull_string.beginning k str ;;
+
+(*
+
+name (veil "Gabriel234") ;;
+name (veil "Gabriel") ;;
+
+*)
+
+let index prn =
+   let str = unveil prn in 
+   match String_find_char.backwards_from_inclusive_opt is_not_a_digit_or_dot str (String.length str) with 
+   None -> int_of_string str
+   |Some k -> 
+      if k=String.length str 
+      then 1   
+      else int_of_string(Cull_string.cobeginning k str) ;; 
+
+(*
+
+index (veil "Gabriel234") ;;
+index (veil "Gabriel") ;;
+
+*)      
+
+end ;;
+
+let constructor = Private.constructor ;;
+let index = Private.index ;;
+let name = Private.name ;;
+
