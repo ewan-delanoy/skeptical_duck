729623cc32b9ba37c019601ecbf6a7826df3e5f1
ewan.delanoy <ewan.delanoy@zoho.com>
2026-05-30 10:51:58 +0200

    register lib/Java_analysis/Jvsp_nonrecursive_grammar/jvng_battery_of_analizers.ml

---
+++ lib/Java_analysis/Jvsp_nonrecursive_grammar/jvng_battery_of_analizers.ml
@@ -0,0 +1,37 @@
+(*
+
+#use"lib/Java_analysis/Jvsp_nonrecursive_grammar/jvng_battery_of_analizers.ml";;
+
+*)
+
+open Jvng_types ;;
+
+exception Untreated of Jvng_duplicated_name.t ;;
+exception Incomplete of Jvng_duplicated_name.t ;;
+
+module Private = struct 
+let get_local_analizer battery dname =  
+  if List.exists (fun pref ->Jvng_duplicated_name.starts_with dname ~prefix:pref) ["Atomic";"Molecular"]
+  then let name = Jvng_duplicated_name.name dname in
+       Jvng_local_analizer.one_level_above_molecular 
+    (Jvsp_util.token_type_sequence_from_codes_in_production_names name)
+  else match List.assoc_opt dname battery.deciders_for_optionals_or_stars with 
+        None -> raise(Untreated(dname))
+     |(Some analizer) -> analizer;;
+
+end ;;     
+
+let decide battery dname strm=
+  let analizer = Private.get_local_analizer battery dname in 
+  match (Jvng_local_analizer.use analizer strm.remaining_list) with 
+     None -> raise(Incomplete(dname))
+    |Some answer -> bool_of_string answer ;;    
+
+let choose battery dis_name strm= 
+  match List.assoc_opt dis_name battery.choosers_for_disjunctions with 
+  None -> raise(Untreated(dis_name))
+  |Some(analizer) ->match (Jvng_local_analizer.use analizer strm.remaining_list) with 
+     None -> raise(Incomplete(dis_name))
+    |Some answer -> answer ;;    
+
+
