30faff76b5bda37756765a793ce5d5b0b8dc7ccd
ewan.delanoy <ewan.delanoy@zoho.com>
2026-05-30 11:26:11 +0200

    add Jvng_jvag_form.uniform_decomposition_opt

--- lib/Java_analysis/Jvsp_nonrecursive_grammar/jvng_jvag_form.ml
+++ lib/Java_analysis/Jvsp_nonrecursive_grammar/jvng_jvag_form.ml
@@ -16,3 +16,11 @@ let uniform_composition link l = match link with
    |Jvag_types.Disjunction_L -> Disjunction(l)
    |Jvag_types.Star_L -> Star(List.hd l)
    |Jvag_types.Synonym_L -> Synonym(List.hd l);;     
+
+let uniform_decomposition_opt form = match form with
+   |Molecular _ -> None    
+   |Concat l ->  Some(Jvag_types.Concat_L,l)
+   |Disjunction l -> Some(Jvag_types.Disjunction_L,l)
+   |Star nm -> Some(Jvag_types.Star_L,[nm])
+   |Optional nm -> Some(Jvag_types.Optional_L,[nm])
+   |Synonym nm -> Some(Jvag_types.Synonym_L,[nm]) ;;   
