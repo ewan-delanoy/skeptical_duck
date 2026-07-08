020e7807a7819125bd434a8cfa3873878d9388d9
ewan.delanoy <ewan.delanoy@zoho.com>
2026-07-08 19:14:08 +0200

    register lib/Lr_parsing/lrp_item.ml

---
+++ lib/Lr_parsing/lrp_item.ml
@@ -0,0 +1,34 @@
+(*
+
+Lrp is short for "LR Parsing"
+
+#use"lib/Lr_parsing/lrp_item.ml";;
+
+*)
+
+open Lrp_types ;;
+
+let items_from_production (Prod(p,l)) =
+  let temp = Three_parts.generic l in 
+  let temp2 = List.rev_map (fun (left,center,right)->(List.rev left,center::right)) temp in 
+  (Image.image (fun (left,right)->Item(p,left@("."::right))) temp2)@[Item(p,l@["."])] ;; 
+
+(* items_from_production (Prod("x",["1";"2";"3";"4"])) ;; *) 
+
+let production_from_item (Item(p,l)) = Prod(p,List.filter(fun x->x<>".") l) ;; 
+
+let index_of_dot (Item(_p,l))= List_again.index_of_in "." l;;
+
+let index_of_production_in_grammar item (G productions) = 
+  List_again.index_of_in (production_from_item item) productions ;;
+
+let order gram = ((fun item1 item2 ->
+  let trial1 = Total_ordering.for_integers (index_of_dot item1) (index_of_dot item2) in 
+  if trial1<> Total_ordering_result_t.Equal then trial1 else 
+  Total_ordering.for_integers 
+    (index_of_production_in_grammar item1 gram) 
+      (index_of_production_in_grammar item2 gram)  
+): item Total_ordering_t.t);;
+
+let sort gram = Ordered.sort (order gram);;
+
