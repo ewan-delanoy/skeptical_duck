312fbde8e444d78127a2f1203056f021122c11d9
ewan.delanoy <ewan.delanoy@zoho.com>
2026-07-10 10:04:06 +0200

    inherit some values in Lrp_grammar

--- lib/Lr_parsing/lrp_grammar.ml
+++ lib/Lr_parsing/lrp_grammar.ml
@@ -12,3 +12,10 @@ let make l= {
    core = BG l ;
    registry = Lrp_registry.default ;
 } ;;
+
+let nonterminals gram = Lrp_bare_grammar.nonterminals gram.core;;
+let start_symbol gram = Lrp_bare_grammar.start_symbol gram.core ;; 
+
+let starter_lr0_state gram = Lrp_bare_grammar.starter_lr0_state gram.core ;; 
+let symbols gram = Lrp_bare_grammar.start_symbol gram.core ;; 
+let terminals gram = Lrp_bare_grammar.terminals gram.core;;
