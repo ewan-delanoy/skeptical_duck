6a03826e08ea01fe724943a32958ab20a3e435c0
ewan.delanoy <ewan.delanoy@zoho.com>
2026-07-22 19:05:43 +0200

    add Lrp_types.lr_molecule (second try)

--- lib/Lr_parsing/lrp_types.ml
+++ lib/Lr_parsing/lrp_types.ml
@@ -36,6 +36,8 @@ type lr0_atom = Atom of item ;;
 
 type lr0_molecule = St of lr0_atom list ;; 
 
+type lr_molecule = Lrm of ((item * string) list) ;;
+
 type registered_lr0_molecule = RSt of int * lr0_molecule ;;
 
 type registry_for_lr0_molecules = Rg of ( lr0_molecule * ((string list) list) ) list ;;
